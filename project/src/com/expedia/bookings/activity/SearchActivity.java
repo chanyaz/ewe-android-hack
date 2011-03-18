c1c1199952a0d6f73e71a6d260ae2e6b0ccff89f
Michael Pardo <pardom@mobiata.com>
2011-03-18 10:18:35 -0400

    Manage activities.

--- project/src/com/expedia/bookings/activity/SearchActivity.java
+++ project/src/com/expedia/bookings/activity/SearchActivity.java
@@ -4,46 +4,88 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.ActivityGroup;
+import android.app.LocalActivityManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.FrameLayout;
 
+import com.expedia.bookings.R;
 import com.mobiata.hotellib.app.SearchListener;
 
 public class SearchActivity extends ActivityGroup {
 	//////////////////////////////////////////////////////////////////////////////////
 	// Constants
-	
+
 	//////////////////////////////////////////////////////////////////////////////////
 	// Private members
-	
+
+	private FrameLayout mContent;
+
+	private LocalActivityManager mLocalActivityManager;
+	private String mTag;
+	private Intent mIntent;
+	private View mLaunchedView;
+
 	private List<SearchListener> mSearchListeners;
-	
+
 	//////////////////////////////////////////////////////////////////////////////////
 	// Overrides
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		
-	}
-	
-	@Override
-    public void onContentChanged() {
-        super.onContentChanged();
 
-    }
-	
+		mLocalActivityManager = getLocalActivityManager();
+
+		initializeViews();
+	}
+
+	@Override
+	public void onContentChanged() {
+		super.onContentChanged();
+
+	}
+
 	//////////////////////////////////////////////////////////////////////////////////
 	// Public methods
-	
+
 	public void addSearchListner(SearchListener searchListener) {
-		if(mSearchListeners == null) {
+		if (mSearchListeners == null) {
 			mSearchListeners = new ArrayList<SearchListener>();
 		}
-		
+
 		mSearchListeners.add(searchListener);
 	}
-	
+
 	//////////////////////////////////////////////////////////////////////////////////
 	// Private methods
+
+	private void initializeViews() {
+		mContent = (FrameLayout) findViewById(R.id.content_layout);
+	}
+
+	private void showActivity(Class activity) {
+		mIntent = new Intent(this, activity);
+		mTag = activity.getCanonicalName();
+
+		final Window w = mLocalActivityManager.startActivity(mTag, mIntent);
+		final View wd = w != null ? w.getDecorView() : null;
+		if (mLaunchedView != wd && mLaunchedView != null) {
+			if (mLaunchedView.getParent() != null) {
+				mContent.removeView(mLaunchedView);
+			}
+		}
+		mLaunchedView = wd;
+
+		if (mLaunchedView != null) {
+			mLaunchedView.setVisibility(View.VISIBLE);
+			mLaunchedView.setFocusableInTouchMode(true);
+			((ViewGroup) mLaunchedView).setDescendantFocusability(ViewGroup.FOCUS_AFTER_DESCENDANTS);
+		}
+		
+		mContent.addView(mLaunchedView);
+	}
 }
