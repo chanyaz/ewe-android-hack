c84a0db66fa504d05b2d3dbc7f1f3c41e474e169
Daniel Lew <dlew@mobiata.com>
2011-03-21 13:03:03 -0500

    Added initial search list results implementation

--- project/src/com/expedia/bookings/activity/SearchListActivity.java
+++ project/src/com/expedia/bookings/activity/SearchListActivity.java
@@ -1,81 +1,90 @@
 package com.expedia.bookings.activity;
 
-import com.expedia.bookings.R;
-import com.mobiata.hotellib.app.SearchListener;
-import com.mobiata.hotellib.data.SearchResponse;
-
 import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
-import android.widget.AbsListView;
-import android.widget.AbsListView.OnScrollListener;
 
-public class SearchListActivity extends ListActivity implements SearchListener, OnScrollListener {
+import com.expedia.bookings.R;
+import com.expedia.bookings.widget.HotelAdapter;
+import com.mobiata.android.BackgroundDownloader;
+import com.mobiata.android.BackgroundDownloader.Download;
+import com.mobiata.android.BackgroundDownloader.OnDownloadComplete;
+import com.mobiata.hotellib.app.SearchListener;
+import com.mobiata.hotellib.data.Codes;
+import com.mobiata.hotellib.data.SearchParams;
+import com.mobiata.hotellib.data.SearchResponse;
+import com.mobiata.hotellib.server.ExpediaServices;
+
+public class SearchListActivity extends ListActivity implements SearchListener {
 	//////////////////////////////////////////////////////////////////////////////////
 	// Constants
-	
+
 	//////////////////////////////////////////////////////////////////////////////////
 	// Private members
 
 	private SearchActivity mParent;
-	
+	private HotelAdapter mAdapter;
+
 	//////////////////////////////////////////////////////////////////////////////////
 	// Overrides
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search_list);
 
 		mParent = (SearchActivity) getParent();
-		mParent.addSearchListner(this);		
-	}
-	
-	//////////////////////////////////////////////////////////////////////////////////
-	// OnScrollListener implementation
-	
-	@Override
-    public void onContentChanged() {
-        super.onContentChanged();
+		if (mParent == null) {
+			// Testing code - this allows the SearchListActivity to run standalone as a test.
+			final Context context = this;
+			BackgroundDownloader downloader = BackgroundDownloader.getInstance();
+			Download download = new Download() {
+				@Override
+				public Object doDownload() {
+					SearchParams params = new SearchParams();
+					params.setFreeformLocation("Minneapolis");
 
-    }
+					return ExpediaServices.search(context, params, Codes.F_SORT_BY_DISTANCE);
+				}
+			};
 
-	@Override
-	public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-		// TODO Auto-generated method stub
-		
+			OnDownloadComplete callback = new OnDownloadComplete() {
+				@Override
+				public void onDownload(Object results) {
+					onSearchCompleted((SearchResponse) results);
+				}
+			};
+
+			downloader.startDownload("mykey", download, callback);
+		}
+		else {
+			mParent.addSearchListner(this);
+		}
 	}
 
-	@Override
-	public void onScrollStateChanged(AbsListView view, int scrollState) {
-		// TODO Auto-generated method stub
-		
-	}
-	
 	//////////////////////////////////////////////////////////////////////////////////
 	// SearchListener implementation
-	
+
 	@Override
 	public void onSearchStarted() {
-		// TODO Auto-generated method stub
-		
+		// TODO: Implement hanging doorknob
+
 	}
 
 	@Override
 	public void onSearchProgress(int strId) {
-		// TODO Auto-generated method stub
-		
+		// TODO: Implement hanging doorknob (will it have status updates?)
 	}
 
 	@Override
 	public void onSearchFailed(String message) {
-		// TODO Auto-generated method stub
-		
+		// TODO: Implement an error screen
 	}
 
 	@Override
 	public void onSearchCompleted(SearchResponse response) {
-		// TODO Auto-generated method stub
-		
+		mAdapter = new HotelAdapter(this, response);
+		setListAdapter(mAdapter);
 	}
 
 	@Override
@@ -87,9 +96,9 @@ public class SearchListActivity extends ListActivity implements SearchListener, 
 	@Override
 	public void clearResults() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	//////////////////////////////////////////////////////////////////////////////////
 	// Private methods
 
---
+++ project/src/com/expedia/bookings/widget/HotelAdapter.java
@@ -0,0 +1,110 @@
+package com.expedia.bookings.widget;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.expedia.bookings.R;
+import com.mobiata.android.ImageCache;
+import com.mobiata.hotellib.data.Property;
+import com.mobiata.hotellib.data.SearchResponse;
+
+public class HotelAdapter extends BaseAdapter {
+
+	private Context mContext;
+	private LayoutInflater mInflater;
+
+	private SearchResponse mSearchResponse;
+
+	private Property[] mCachedProperties;
+
+	public HotelAdapter(Context context, SearchResponse searchResponse) {
+		mContext = context;
+		mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		mSearchResponse = searchResponse;
+
+		mCachedProperties = mSearchResponse.getFilteredAndSortedProperties();
+	}
+
+	/**
+	 * Checks that we have the latest set of sorts/filters on the data.  If not, notify
+	 * that the dataset has changed and update the data.  Should be called before
+	 * any method that uses mCachedProperties.
+	 */
+	public void checkCachedProperties() {
+		if (mSearchResponse.filterChanged()) {
+			mCachedProperties = mSearchResponse.getFilteredAndSortedProperties();
+			notifyDataSetChanged();
+		}
+	}
+
+	@Override
+	public int getCount() {
+		checkCachedProperties();
+		return mCachedProperties.length;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		checkCachedProperties();
+		return mCachedProperties[position];
+	}
+
+	@Override
+	public boolean hasStableIds() {
+		return true;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		checkCachedProperties();
+		return Integer.valueOf(mCachedProperties[position].getPropertyId());
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		HotelViewHolder holder;
+		if (convertView == null) {
+			convertView = mInflater.inflate(R.layout.row_hotel, parent, false);
+
+			holder = new HotelViewHolder();
+			holder.thumbnail = (ImageView) convertView.findViewById(R.id.Thumbnail);
+			holder.name = (TextView) convertView.findViewById(R.id.Name);
+			holder.price = (TextView) convertView.findViewById(R.id.Price);
+			holder.hotelRating = (RatingBar) convertView.findViewById(R.id.HotelRating);
+			holder.distance = (TextView) convertView.findViewById(R.id.Distance);
+			convertView.setTag(holder);
+		}
+		else {
+			holder = (HotelViewHolder) convertView.getTag();
+		}
+
+		Property property = (Property) getItem(position);
+		holder.thumbnail.setImageResource(R.drawable.ic_image_placeholder);
+		holder.name.setText(property.getName());
+		holder.price.setText(property.getLowRate().getFormattedMoney());
+		holder.hotelRating.setRating(property.getHotelRating() / 2f);
+		holder.distance.setText(property.getDistanceFromUser().formatDistance(mContext));
+
+		// See if there's a first image; if there is, use that as the thumbnail
+		if (property.getMediaCount() > 0) {
+			ImageCache.getInstance().loadImage(property.getMedia(0).getUrl(), holder.thumbnail);
+		}
+
+		return convertView;
+	}
+
+	private static class HotelViewHolder {
+		public ImageView thumbnail;
+		public TextView name;
+		public TextView price;
+		public RatingBar hotelRating;
+		public TextView distance;
+	}
+}
