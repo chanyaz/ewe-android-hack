9f3277994793025c548a38ad80b2c9bde384a051
Daniel Lew <dlew@mobiata.com>
2011-04-11 18:08:33 -0500

    Added basic sale info to search list results

--- project/src/com/expedia/bookings/widget/HotelAdapter.java
+++ project/src/com/expedia/bookings/widget/HotelAdapter.java
@@ -1,6 +1,7 @@
 package com.expedia.bookings.widget;
 
 import android.content.Context;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +12,9 @@ import android.widget.TextView;
 
 import com.expedia.bookings.R;
 import com.mobiata.android.ImageCache;
+import com.mobiata.android.text.StrikethroughTagHandler;
 import com.mobiata.hotellib.data.Property;
+import com.mobiata.hotellib.data.Rate;
 import com.mobiata.hotellib.data.SearchResponse;
 
 public class HotelAdapter extends BaseAdapter {
@@ -103,6 +106,7 @@ public class HotelAdapter extends BaseAdapter {
 			holder = new HotelViewHolder();
 			holder.thumbnail = (ImageView) convertView.findViewById(R.id.thumbnail_image_view);
 			holder.name = (TextView) convertView.findViewById(R.id.name_text_view);
+			holder.from = (TextView) convertView.findViewById(R.id.from_text_view);
 			holder.price = (TextView) convertView.findViewById(R.id.price_text_view);
 			holder.hotelRating = (RatingBar) convertView.findViewById(R.id.hotel_rating_bar);
 			holder.distance = (TextView) convertView.findViewById(R.id.distance_text_view);
@@ -115,7 +119,20 @@ public class HotelAdapter extends BaseAdapter {
 		Property property = (Property) getItem(position);
 		holder.thumbnail.setImageResource(R.drawable.ic_image_placeholder);
 		holder.name.setText(property.getName());
-		holder.price.setText(property.getLowRate().getFormattedMoney());
+
+		// We assume we have a lowest rate here; this may not be a safe assumption
+		Rate lowestRate = property.getLowestRate();
+		// Detect if the property is on sale, if it is do special things
+		if (lowestRate.getSavingsPercent() > 0) {
+			holder.from.setText(Html.fromHtml(mContext.getString(R.string.from_template, lowestRate
+					.getAverageBaseRate().getFormattedMoney()), null, new StrikethroughTagHandler()));
+		}
+		else {
+			holder.from.setText(R.string.from);
+		}
+
+		holder.price.setText(lowestRate.getAverageRate().getFormattedMoney());
+
 		holder.hotelRating.setRating((float) property.getHotelRating());
 		holder.distance.setText(property.getDistanceFromUser().formatDistance(mContext));
 
@@ -130,6 +147,7 @@ public class HotelAdapter extends BaseAdapter {
 	private static class HotelViewHolder {
 		public ImageView thumbnail;
 		public TextView name;
+		public TextView from;
 		public TextView price;
 		public RatingBar hotelRating;
 		public TextView distance;
