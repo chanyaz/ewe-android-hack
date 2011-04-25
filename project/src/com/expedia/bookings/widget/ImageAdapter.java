70527c4c079c5fc458c57dfe24b9ddf772ae6d2d
Daniel Lew <dlew@mobiata.com>
2011-04-25 15:31:32 -0500

    6758: Wide gallery images now fill screen
    They're cropped vertically.  Vertical images are the same
    as before.

--- project/src/com/expedia/bookings/widget/ImageAdapter.java
+++ project/src/com/expedia/bookings/widget/ImageAdapter.java
@@ -3,14 +3,17 @@ package com.expedia.bookings.widget;
 import java.util.List;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 
 import com.expedia.bookings.R;
 import com.mobiata.android.ImageCache;
+import com.mobiata.android.ImageCache.OnImageLoaded;
 import com.mobiata.hotellib.data.Media;
 
 public class ImageAdapter extends BaseAdapter {
@@ -48,14 +51,30 @@ public class ImageAdapter extends BaseAdapter {
 		Media media = (Media) getItem(position);
 		String url = media.getUrl();
 
+		// Don't depend on the callback to set the ImageView - just refresh the adapter with new data
+		OnImageLoaded callback = new OnImageLoaded() {
+			public void onImageLoaded(String url, Bitmap bitmap) {
+				notifyDataSetChanged();
+			}
+		};
+
 		ImageCache imageCache = ImageCache.getInstance();
 		if (imageCache.containsImage(url)) {
-			imageView.setImageBitmap(imageCache.getImage(url));
+			Bitmap bitmap = imageCache.getImage(url);
+			if (bitmap.getWidth() > bitmap.getHeight()) {
+				imageView.setScaleType(ScaleType.CENTER_CROP);
+			}
+			else {
+				imageView.setScaleType(ScaleType.FIT_CENTER);
+			}
+
+			imageView.setImageBitmap(bitmap);
 		}
 		else {
 			// Set a placeholder image while we load the image
 			imageView.setImageResource(R.drawable.ic_image_placeholder);
-			imageCache.loadImage(media.getUrl(), imageView);
+			imageView.setScaleType(ScaleType.CENTER_CROP);
+			imageCache.loadImage(media.getUrl(), callback);
 		}
 
 		return convertView;
