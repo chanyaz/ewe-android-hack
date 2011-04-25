555174dff70d713bdae7eb18cf4269ecca4331ca
Daniel Lew <dlew@mobiata.com>
2011-04-25 09:50:33 -0500

    Customized layout of AboutActivity
    Much of it remains non-fuctional, to be added soon

--- project/src/com/expedia/bookings/activity/AboutActivity.java
+++ project/src/com/expedia/bookings/activity/AboutActivity.java
@@ -1,6 +1,67 @@
 package com.expedia.bookings.activity;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+
+import com.expedia.bookings.R;
+import com.mobiata.android.SocialUtils;
+
 public class AboutActivity extends com.mobiata.android.app.AboutActivity {
-	// This class is currently empty, but it has been created because
-	// I guarantee you that we'll want to customize it a bit for EB. 
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		final Context context = this;
+
+		// Section about Expedia
+		ViewGroup expediaSection = addSection();
+		addSimpleRow(expediaSection, getString(R.string.contact_expedia), new OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+			}
+		});
+		addSimpleRow(expediaSection, getString(R.string.expedia_website), new OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+			}
+		});
+
+		// Section about this app
+		ViewGroup appSection = addSection();
+		addSimpleRow(appSection, getString(R.string.app_feedback), new OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+			}
+		});
+		addSimpleRow(appSection, getString(R.string.app_support), new OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+			}
+		});
+		addSimpleRow(appSection, getString(R.string.TellAFriend), new OnClickListener() {
+			public void onClick(View v) {
+				SocialUtils.email(context, getString(R.string.tell_a_friend_subject),
+						getString(R.string.tell_a_friend_body));
+			}
+		});
+
+		// Section about some of our other apps
+		ViewGroup otherAppsSection = addSection(getString(R.string.travel_smart));
+		addAppAbout(otherAppsSection, APP_FLIGHTTRACK, 0);
+		addAppAbout(otherAppsSection, APP_FLIGHTBOARD, 0);
+	}
+
+	@Override
+	public String getAboutHtml() {
+		return getString(R.string.copyright);
+	}
+
+	@Override
+	public boolean useDefaultBehavior() {
+		return false;
+	}
 }
