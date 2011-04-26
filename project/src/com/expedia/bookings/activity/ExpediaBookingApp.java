74c3d0fe28281386a242178f603131867cd39027
Daniel Lew <dlew@mobiata.com>
2011-04-26 11:02:19 -0500

    Some more flags determined automatically

--- project/src/com/expedia/bookings/activity/ExpediaBookingApp.java
+++ project/src/com/expedia/bookings/activity/ExpediaBookingApp.java
@@ -1,12 +1,16 @@
 package com.expedia.bookings.activity;
 
 import com.mobiata.android.Log;
+import com.mobiata.android.util.AndroidUtils;
+import com.mobiata.hotellib.Params;
 
 public class ExpediaBookingApp extends com.activeandroid.Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
 
-		Log.configureLogging("ExpediaBookings", true);
+		Params params = Params.getInstance();
+		params.mIsRelease = AndroidUtils.isRelease(this);
+		Log.configureLogging("ExpediaBookings", !params.mIsRelease);
 	}
 }
