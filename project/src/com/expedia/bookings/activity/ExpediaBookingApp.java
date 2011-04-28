6df16ed149eb614c95ea99006fc811b4bff3c30e
Daniel Lew <dlew@mobiata.com>
2011-04-28 14:49:18 -0500

    Added crash tracking to app

--- project/src/com/expedia/bookings/activity/ExpediaBookingApp.java
+++ project/src/com/expedia/bookings/activity/ExpediaBookingApp.java
@@ -1,10 +1,20 @@
 package com.expedia.bookings.activity;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.lang.Thread.UncaughtExceptionHandler;
+
+import com.expedia.bookings.tracking.TrackingUtils;
 import com.mobiata.android.Log;
 import com.mobiata.android.util.AndroidUtils;
 import com.mobiata.hotellib.Params;
+import com.omniture.AppMeasurement;
 
-public class ExpediaBookingApp extends com.activeandroid.Application {
+public class ExpediaBookingApp extends com.activeandroid.Application implements UncaughtExceptionHandler {
+
+	private UncaughtExceptionHandler mOriginalUncaughtExceptionHandler;
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
@@ -12,5 +22,30 @@ public class ExpediaBookingApp extends com.activeandroid.Application {
 		Params params = Params.getInstance();
 		params.mIsRelease = AndroidUtils.isRelease(this);
 		Log.configureLogging("ExpediaBookings", !params.mIsRelease);
+
+		// Setup logging for crashes
+		mOriginalUncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+		Thread.setDefaultUncaughtExceptionHandler(this);
+	}
+
+	@Override
+	public void uncaughtException(Thread thread, Throwable ex) {
+		// Log the crash
+		Log.d("Tracking \"crash\" onClick");
+		AppMeasurement s = new AppMeasurement(this);
+		TrackingUtils.addStandardFields(this, s);
+		s.events = "event39";
+		s.eVar28 = s.prop16 = "App.Crash";
+
+		final Writer writer = new StringWriter();
+		final PrintWriter printWriter = new PrintWriter(writer);
+		ex.printStackTrace(printWriter);
+		s.prop36 = printWriter.toString();
+
+		// TODO: Register this as an onClick instead of page event
+		s.track();
+
+		// Call the original exception handler
+		mOriginalUncaughtExceptionHandler.uncaughtException(thread, ex);
 	}
 }
