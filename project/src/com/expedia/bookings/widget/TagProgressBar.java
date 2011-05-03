3169ab69304c4f303042dd06173f64d6de5392be
Michael Pardo <pardom@mobiata.com>
2011-05-03 09:31:23 -0400

    Remove unused imports.

--- project/src/com/expedia/bookings/widget/TagProgressBar.java
+++ project/src/com/expedia/bookings/widget/TagProgressBar.java
@@ -7,7 +7,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Paint.Align;
 import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.hardware.Sensor;
