23f2872d92a03a36121ac9867134ad5dc2cdd978
Michael Pardo <pardom@mobiata.com>
2011-05-04 13:42:08 -0400

    7001. Don't recycle images on search completed.

--- project/src/com/expedia/bookings/activity/SearchListActivity.java
+++ project/src/com/expedia/bookings/activity/SearchListActivity.java
@@ -9,7 +9,6 @@ import android.widget.ListView;
 import com.expedia.bookings.R;
 import com.expedia.bookings.widget.HotelAdapter;
 import com.expedia.bookings.widget.ListViewScrollBar;
-import com.mobiata.android.ImageCache;
 import com.mobiata.hotellib.app.SearchListener;
 import com.mobiata.hotellib.data.Codes;
 import com.mobiata.hotellib.data.Property;
@@ -106,9 +105,7 @@ public class SearchListActivity extends ListActivity implements SearchListener {
 		}
 
 		mSearchResponse = response;
-
 		mAdapter = new HotelAdapter(this, mSearchResponse);
-		ImageCache.getInstance().recycleCache(true);
 
 		setListAdapter(mAdapter);
 		mScrollBar.setSearchResponse(mSearchResponse);
