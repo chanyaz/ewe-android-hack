b16c875f9d0f0a8310d0f2abda368342432de25f
Daniel Lew <dlew@mobiata.com>
2011-05-06 00:33:55 -0500

    6862: Fixed keyboard opening on landscape layout

--- project/src/com/expedia/bookings/activity/BookingInfoActivity.java
+++ project/src/com/expedia/bookings/activity/BookingInfoActivity.java
@@ -782,11 +782,11 @@ public class BookingInfoActivity extends Activity implements Download, OnDownloa
 		mExpirationMonthEditText.setNextFocusUpId(nextId);
 		mExpirationYearEditText.setNextFocusUpId(nextId);
 	}
-	
+
 	private void focusAndOpenKeyboard(View view) {
 		view.requestFocus();
 		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-		imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
+		imm.showSoftInput(view, 0);
 	}
 
 	// BillingInfo syncing and saving/loading
