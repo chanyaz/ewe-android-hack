437b69fb8050eddcd3f148b0460b603c4e49bb3e
Michael Pardo <pardom@mobiata.com>
2011-05-06 09:52:17 -0400

    6998. Submodule update. Use getPresentationMessage.

--- project/src/com/expedia/bookings/activity/SearchActivity.java
+++ project/src/com/expedia/bookings/activity/SearchActivity.java
@@ -285,7 +285,7 @@ public class SearchActivity extends ActivityGroup implements LocationListener {
 					}
 					else {
 						mSearchProgressBar.setShowProgress(false);
-						mSearchProgressBar.setText(errorOne.getMessage());
+						mSearchProgressBar.setText(errorOne.getPresentableMessage(SearchActivity.this));
 					}
 					handledError = true;
 				}
