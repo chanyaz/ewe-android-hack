855b3983724ede6e7cf2d6c36464d28d58742cb9
Michael Pardo <pardom@mobiata.com>
2011-05-25 11:12:16 -0400

    Properly layout children.

--- project/src/com/expedia/bookings/widget/TagProgressBar.java
+++ project/src/com/expedia/bookings/widget/TagProgressBar.java
@@ -8,6 +8,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.expedia.bookings.widget.gl.GLTagProgressBar;
+import com.mobiata.android.Log;
 
 public class TagProgressBar extends ViewGroup {
 	private Context mContext;
@@ -23,36 +24,43 @@ public class TagProgressBar extends ViewGroup {
 
 	@Override
 	protected void onLayout(boolean changed, int l, int t, int r, int b) {
-		mLayout.layout(0, 0, getWidth(), getHeight());
-		mGLTagProgressBar.layout(0, 0, getWidth(), getHeight());
-		mTextView.layout(l, b - mTextView.getMeasuredHeight(), r, b);
+		final int width = getWidth();
+		final int height = getHeight();
+
+		mLayout.layout(0, 0, width, height);
+		mGLTagProgressBar.layout(0, 0, width, height);
+		
+		mTextView.layout(0, height - mTextView.getMeasuredHeight(), width, height);
+		
+		Log.t("h: %d", mTextView.getMeasuredHeight());
 	}
 
 	private void init(Context context) {
 		mContext = context;
-		
+
 		// Create views
 		mLayout = new RelativeLayout(context);
 		mGLTagProgressBar = new GLTagProgressBar(context);
 		mTextView = new TextView(context);
-		
+
 		// Create layout params
 		RelativeLayout.LayoutParams layoutParams;
-		
+
 		// Set params for GLTagProgressBar
 		layoutParams = new RelativeLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
 		mGLTagProgressBar.setLayoutParams(layoutParams);
-		
+
 		// Set params for TextView
 		layoutParams = new RelativeLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
 		layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
 		mTextView.setLayoutParams(layoutParams);
-		mTextView.setGravity(Gravity.CENTER);		
-		
+		mTextView.setTextColor(0xFF555555);
+		mTextView.setGravity(Gravity.CENTER);
+
 		// Add views to layout
 		mLayout.addView(mGLTagProgressBar);
 		mLayout.addView(mTextView);
-		
+
 		// Add layout
 		addView(mLayout, new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
 	}
