e67b19d8b640af440e9790ee927a1a7b40c52f85
Kunal Jham <kunal@mobiata.com>
2011-08-02 16:33:37 -0700

    resolving conflict

--- project/src/com/expedia/bookings/activity/SearchActivity.java
+++ project/src/com/expedia/bookings/activity/SearchActivity.java
@@ -1136,8 +1136,7 @@ public class SearchActivity extends ActivityGroup implements LocationListener {
 							TrackingUtils.trackErrorPage(SearchActivity.this, "LocationNotFound");
 							mSearchProgressBar.setShowProgress(false);
 							mSearchProgressBar.setText(R.string.geolocation_failed);
-							simulateErrorResponse(R.string.geolocation_failed);
-						}
+							simulateErrorResponse(R.string.geolocation_failed);						}
 					}
 				});
 			}
