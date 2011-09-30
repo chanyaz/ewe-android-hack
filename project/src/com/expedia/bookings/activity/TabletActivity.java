913a520a4d1a18359161237a55b18461b4a87c04
Daniel Lew <dan@mobiata.com>
2011-09-30 09:13:13 -0500

    Added basic methods to override
    Doing it this way so there's some organization when we start
    implementing things in these methods (as I am sure we will someday)

--- project/src/com/expedia/bookings/activity/TabletActivity.java
+++ project/src/com/expedia/bookings/activity/TabletActivity.java
@@ -1,11 +1,59 @@
 package com.expedia.bookings.activity;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import com.google.android.maps.MapActivity;
 
 public class TabletActivity extends MapActivity {
+
+	//////////////////////////////////////////////////////////////////////////
+	// Lifecycle events
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+	// ActionBar
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		return super.onOptionsItemSelected(item);
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+	// MapActivity
 
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
 	}
+
 }
