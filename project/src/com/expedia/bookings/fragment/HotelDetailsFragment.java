b65474ab0581e80d2817bcbc9807b6d625c1446a
Kunal Jham <kunal@mobiata.com>
2011-09-30 10:19:43 -0700

    adding skeleton for details fragment

---
+++ project/src/com/expedia/bookings/fragment/HotelDetailsFragment.java
@@ -0,0 +1,49 @@
+package com.expedia.bookings.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HotelDetailsFragment extends Fragment {
+
+	//////////////////////////////////////////////////////////////////////////
+	// Lifecycle events
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	public void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		// TODO Auto-generated method stub
+		super.onSaveInstanceState(outState);
+	}
+}
---
+++ project/src/com/expedia/bookings/utils/FragmentEventListener.java
@@ -0,0 +1,6 @@
+package com.expedia.bookings.utils;
+
+public interface FragmentEventListener {
+	
+	public abstract void onFragmentEvent(int eventId, Object data);
+}
