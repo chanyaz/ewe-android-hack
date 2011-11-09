8e89ae850fec60f7cca9acbebc250627029d5efa
Kunal Jham <kunal@mobiata.com>
2011-11-08 20:39:17 -0800

    separating out room rates layout into utils
    the purpose of this is so that it can be reused in the
    mini details fragment

--- project/src/com/expedia/bookings/fragment/HotelDetailsFragment.java
+++ project/src/com/expedia/bookings/fragment/HotelDetailsFragment.java
@@ -4,14 +4,7 @@ import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.text.Html;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.ForegroundColorSpan;
-import android.text.style.StyleSpan;
-import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -19,7 +12,6 @@ import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -31,17 +23,14 @@ import com.expedia.bookings.data.Codes;
 import com.expedia.bookings.data.HotelDescription;
 import com.expedia.bookings.data.HotelDescription.DescriptionSection;
 import com.expedia.bookings.data.Property;
-import com.expedia.bookings.data.Rate;
-import com.expedia.bookings.data.Rate.BedType;
-import com.expedia.bookings.data.Rate.BedTypeId;
 import com.expedia.bookings.data.Review;
 import com.expedia.bookings.data.ReviewsResponse;
 import com.expedia.bookings.fragment.EventManager.EventHandler;
+import com.expedia.bookings.utils.AvailabilitySummaryLayoutUtils;
 import com.expedia.bookings.utils.LayoutUtils;
 import com.expedia.bookings.utils.StrUtils;
 import com.expedia.bookings.widget.HotelCollage;
 import com.expedia.bookings.widget.HotelCollage.OnCollageImageClickedListener;
-import com.expedia.bookings.widget.SummarizedRoomRates;
 
 public class HotelDetailsFragment extends Fragment implements EventHandler {
 
@@ -57,7 +46,6 @@ public class HotelDetailsFragment extends Fragment implements EventHandler {
 	// CONSTANTS
 	//----------------------------------
 
-	private static final int MAX_SUMMARIZED_RATE_RESULTS = 3;
 	private static final int MAX_REVIEWS_PER_ROW = 2;
 	private static final int MAX_DESCRIPTION_SECTIONS_PER_ROW = 2;
 	private static final int ANIMATION_SPEED = 350;
@@ -66,9 +54,6 @@ public class HotelDetailsFragment extends Fragment implements EventHandler {
 	// VIEWS
 	//----------------------------------
 
-	private ViewGroup mAvailabilitySummaryContainer;
-	private ViewGroup mAvailabilityRatesContainer;
-	private TextView mEmptyAvailabilitySummaryTextView;
 	private TextView mHotelLocationTextView;
 	private TextView mHotelNameTextView;
 	private TextView mReviewsTitle;
@@ -80,7 +65,6 @@ public class HotelDetailsFragment extends Fragment implements EventHandler {
 	private RatingBar mUserRating;
 	private ViewGroup mHotelDescriptionContainer;
 	private View mSeeAllReviewsButton;
-	private ProgressBar mRatesProgressBar;
 
 	//----------------------------------
 	// OTHERS
@@ -98,9 +82,6 @@ public class HotelDetailsFragment extends Fragment implements EventHandler {
 
 		mHotelNameTextView = (TextView) view.findViewById(R.id.hotel_name_text_view);
 		mHotelLocationTextView = (TextView) view.findViewById(R.id.hotel_address_text_view);
-		mAvailabilitySummaryContainer = (ViewGroup) view.findViewById(R.id.availability_summary_container);
-		mAvailabilityRatesContainer = (ViewGroup) view.findViewById(R.id.rates_container);
-		mEmptyAvailabilitySummaryTextView = (TextView) view.findViewById(R.id.empty_summart_container);
 		mCollageHandler = new HotelCollage(view, mPictureClickedListener);
 		mReviewsTitle = (TextView) view.findViewById(R.id.reviews_title);
 		mUserRating = (RatingBar) view.findViewById(R.id.user_rating_bar);
@@ -110,7 +91,6 @@ public class HotelDetailsFragment extends Fragment implements EventHandler {
 		mAmenitiesContainer = (ViewGroup) view.findViewById(R.id.amenities_table_row);
 		mHotelDescriptionContainer = (ViewGroup) view.findViewById(R.id.hotel_description_section);
 		mSeeAllReviewsButton = view.findViewById(R.id.see_all_reviews_button);
-		mRatesProgressBar = (ProgressBar) view.findViewById(R.id.rates_progress_bar);
 		mReviewsLoadingContainer = view.findViewById(R.id.reviews_loading_container);
 
 		// Disable the scrollbar on the amenities HorizontalScrollView
@@ -176,10 +156,10 @@ public class HotelDetailsFragment extends Fragment implements EventHandler {
 			mReviewsSection.setVisibility(View.GONE);
 		}
 
-		setupAvailabilitySummary();
+		AvailabilitySummaryLayoutUtils.setupAvailabilitySummary(((TabletActivity) getActivity()), getView());
 		// update the summarized rates if they are available
 		AvailabilityResponse availabilityResponse = ((TabletActivity) getActivity()).getRoomsAndRatesAvailability();
-		updateSummarizedRates(availabilityResponse);
+		AvailabilitySummaryLayoutUtils.updateSummarizedRates(((TabletActivity) getActivity()), availabilityResponse, getView());
 
 		int dimenResId = (property.getTotalReviews() > 3) ? R.dimen.min_height_two_rows_reviews
 				: R.dimen.min_height_one_row_review;
@@ -212,19 +192,14 @@ public class HotelDetailsFragment extends Fragment implements EventHandler {
 	public void handleEvent(int eventCode, Object data) {
 		switch (eventCode) {
 		case TabletActivity.EVENT_AVAILABILITY_SEARCH_STARTED:
-			showLoadingForRates();
+			AvailabilitySummaryLayoutUtils.showLoadingForRates(((TabletActivity) getActivity()), getView());
 			break;
 		case TabletActivity.EVENT_AVAILABILITY_SEARCH_ERROR:
-			mEmptyAvailabilitySummaryTextView.setVisibility(View.VISIBLE);
-			mRatesProgressBar.setVisibility(View.INVISIBLE);
-			mEmptyAvailabilitySummaryTextView.setText((String) data);
-			mAvailabilityRatesContainer.setVisibility(View.GONE);
+			AvailabilitySummaryLayoutUtils.showErrorForRates(getView(), (String) data);
 			break;
 		case TabletActivity.EVENT_AVAILABILITY_SEARCH_COMPLETE:
-			mEmptyAvailabilitySummaryTextView.setVisibility(View.GONE);
-			mRatesProgressBar.setVisibility(View.GONE);
-			mAvailabilityRatesContainer.setVisibility(View.VISIBLE);
-			updateSummarizedRates((AvailabilityResponse) data);
+			AvailabilitySummaryLayoutUtils.showRatesContainer(getView());
+			AvailabilitySummaryLayoutUtils.updateSummarizedRates(((TabletActivity) getActivity()), (AvailabilityResponse) data, getView());
 			break;
 		case TabletActivity.EVENT_PROPERTY_SELECTED:
 			updateViews((Property) data);
@@ -243,161 +218,6 @@ public class HotelDetailsFragment extends Fragment implements EventHandler {
 
 	//////////////////////////////////////////////////////////////////////////
 	// PRIVATE METHODS
-
-	private void showLoadingForRates() {
-		mEmptyAvailabilitySummaryTextView.setVisibility(View.VISIBLE);
-		mRatesProgressBar.setVisibility(View.VISIBLE);
-		mEmptyAvailabilitySummaryTextView.setText(getString(R.string.room_rates_loading));
-		mAvailabilityRatesContainer.setVisibility(View.GONE);
-	}
-
-	private void setupAvailabilitySummary() {
-		final Property property = ((TabletActivity) getActivity()).getPropertyToDisplay();
-		boolean isPropertyOnSale = property.getLowestRate().getSavingsPercent() > 0;
-		if (isPropertyOnSale) {
-			mAvailabilitySummaryContainer.setBackgroundResource(R.drawable.bg_summarized_room_rates_sale);
-		}
-		else {
-			mAvailabilitySummaryContainer.setBackgroundResource(R.drawable.bg_summarized_room_rates);
-		}
-
-		View minPriceRow = getView().findViewById(R.id.min_price_row_container);
-		TextView minPrice = (TextView) minPriceRow.findViewById(R.id.min_price_text_view);
-
-		String displayRateString = StrUtils.formatHotelPrice(property.getLowestRate().getDisplayRate());
-		String minPriceString = getString(R.string.min_room_price_template, displayRateString);
-		int startingIndexOfDisplayRate = minPriceString.indexOf(displayRateString);
-
-		// style the minimum available price text
-		StyleSpan textStyleSpan = new StyleSpan(Typeface.BOLD);
-		ForegroundColorSpan textColorSpan = new ForegroundColorSpan(getResources().getColor(
-				R.color.hotel_price_text_color));
-		ForegroundColorSpan textWhiteColorSpan = new ForegroundColorSpan(getResources().getColor(android.R.color.white));
-		ForegroundColorSpan textBlackColorSpan = new ForegroundColorSpan(getResources().getColor(android.R.color.black));
-
-		Spannable str = new SpannableString(minPriceString);
-
-		str.setSpan(textStyleSpan, 0, minPriceString.length(), 0);
-
-		if (isPropertyOnSale) {
-			str.setSpan(textWhiteColorSpan, 0, minPriceString.length(), 0);
-		}
-		else {
-			str.setSpan(textColorSpan, startingIndexOfDisplayRate,
-					startingIndexOfDisplayRate + displayRateString.length(), 0);
-			str.setSpan(textBlackColorSpan, 0, startingIndexOfDisplayRate - 1, 0);
-		}
-
-		minPrice.setText(str);
-
-		TextView perNighTextView = (TextView) minPriceRow.findViewById(R.id.per_night_text_view);
-		perNighTextView.setTextColor(isPropertyOnSale ? getResources().getColor(android.R.color.white) : getResources()
-				.getColor(android.R.color.black));
-
-		if (Rate.showInclusivePrices()) {
-			perNighTextView.setVisibility(View.GONE);
-		}
-		else {
-			perNighTextView.setVisibility(View.VISIBLE);
-		}
-	}
-
-	private void layoutAvailabilitySummary() {
-		final Property property = ((TabletActivity) getActivity()).getPropertyToDisplay();
-		final SummarizedRoomRates summarizedRoomRates = ((TabletActivity) getActivity()).getSummarizedRoomRates();
-		boolean isPropertyOnSale = property.getLowestRate().getSavingsPercent() > 0;
-		mAvailabilityRatesContainer.removeAllViews();
-
-		// first adding all rows since the rows will exist regardless of whether
-		// there are enough rooms available or not 
-		for (int i = 0; i < MAX_SUMMARIZED_RATE_RESULTS; i++) {
-			View summaryRow = mInflater.inflate(R.layout.snippet_availability_summary_row, null);
-			setHeightOfWeightOneForRow(summaryRow);
-
-			if (i == (MAX_SUMMARIZED_RATE_RESULTS - 1)) {
-				summaryRow.findViewById(R.id.divider).setVisibility(View.GONE);
-			}
-			mAvailabilityRatesContainer.addView(summaryRow);
-		}
-
-		for (int i = 0; i < MAX_SUMMARIZED_RATE_RESULTS; i++) {
-			View summaryRow = mAvailabilityRatesContainer.getChildAt(i);
-			ObjectAnimator animator = ObjectAnimator.ofFloat(summaryRow, "alpha", 0, 1);
-			animator.setDuration(ANIMATION_SPEED);
-			animator.start();
-
-			if (i > (summarizedRoomRates.numSummarizedRates() - 1)) {
-				continue;
-			}
-
-			final Rate rate = summarizedRoomRates.getRate(i);
-			summaryRow.setOnClickListener(new OnClickListener() {
-
-				@Override
-				public void onClick(View v) {
-					((TabletActivity) getActivity()).bookRoom(rate);
-				}
-			});
-
-			View chevron = summaryRow.findViewById(R.id.availability_chevron_image_view);
-			chevron.setVisibility(View.VISIBLE);
-			TextView summaryDescription = (TextView) summaryRow.findViewById(R.id.availability_description_text_view);
-			TextView priceTextView = (TextView) summaryRow.findViewById(R.id.availability_summary_price_text_view);
-
-			Pair<BedTypeId, Rate> pair = summarizedRoomRates.getBedTypeToRatePair(i);
-			for (BedType bedType : pair.second.getBedTypes()) {
-				if (bedType.bedTypeId == pair.first) {
-					summaryDescription.setText(Html.fromHtml(getString(R.string.bed_type_start_value_template,
-							bedType.bedTypeDescription)));
-					break;
-				}
-			}
-
-			priceTextView.setText(StrUtils.formatHotelPrice(pair.second.getDisplayRate()));
-			if (isPropertyOnSale) {
-				priceTextView.setTextColor(getResources().getColor(R.color.hotel_price_sale_text_color));
-			}
-			else {
-				priceTextView.setTextColor(getResources().getColor(R.color.hotel_price_text_color));
-			}
-		}
-
-		View selectRoomContainer = mInflater.inflate(R.layout.snippet_select_room_button, null);
-		setHeightOfWeightOneForRow(selectRoomContainer);
-
-		View selectRoomButton = selectRoomContainer.findViewById(R.id.book_now_button);
-		selectRoomButton.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// if the user just presses the book now button,
-				// default to giving the user the minimum rate available
-				((TabletActivity) getActivity()).bookRoom(summarizedRoomRates.getMinimumRateAvaialable());
-			}
-		});
-		mAvailabilityRatesContainer.addView(selectRoomContainer);
-
-	}
-
-	private void setHeightOfWeightOneForRow(View view) {
-		LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, 0);
-		lp.weight = 1;
-		view.setLayoutParams(lp);
-	}
-
-	private void updateSummarizedRates(AvailabilityResponse availabilityResponse) {
-
-		if (availabilityResponse != null) {
-			layoutAvailabilitySummary();
-			mEmptyAvailabilitySummaryTextView.setVisibility(View.GONE);
-			mRatesProgressBar.setVisibility(View.GONE);
-		}
-		else {
-			// since the data is not yet available,
-			// make sure to clean out any old data and show the loading screen
-			showLoadingForRates();
-		}
-	}
 
 	private void addReviews(ReviewsResponse reviewsResponse) {
 		mSomeReviewsContainer.removeAllViews();
---
+++ project/src/com/expedia/bookings/utils/AvailabilitySummaryLayoutUtils.java
@@ -0,0 +1,225 @@
+package com.expedia.bookings.utils;
+
+import android.animation.ObjectAnimator;
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.expedia.bookings.R;
+import com.expedia.bookings.activity.TabletActivity;
+import com.expedia.bookings.data.AvailabilityResponse;
+import com.expedia.bookings.data.Property;
+import com.expedia.bookings.data.Rate;
+import com.expedia.bookings.data.Rate.BedType;
+import com.expedia.bookings.data.Rate.BedTypeId;
+import com.expedia.bookings.widget.SummarizedRoomRates;
+
+public class AvailabilitySummaryLayoutUtils {
+
+	public static void updateSummarizedRates(TabletActivity activity, AvailabilityResponse availabilityResponse, View view) {
+
+		View emptyAvailabilitySummaryTextView = view.findViewById(R.id.empty_summart_container);
+		ProgressBar ratesProgressBar = (ProgressBar) view.findViewById(R.id.rates_progress_bar);
+
+		if (availabilityResponse != null) {
+			layoutAvailabilitySummary(activity, view);
+			emptyAvailabilitySummaryTextView.setVisibility(View.GONE);
+			ratesProgressBar.setVisibility(View.GONE);
+		}
+		else {
+			// since the data is not yet available,
+			// make sure to clean out any old data and show the loading screen
+			showLoadingForRates(activity, view);
+		}
+	}
+
+	private static final int MAX_SUMMARIZED_RATE_RESULTS = 3;
+	private static final int ANIMATION_SPEED = 350;
+
+	public static void setupAvailabilitySummary(TabletActivity activity, View view) {
+		final Property property = activity.getPropertyToDisplay();
+		View availabilitySummaryContainer = view.findViewById(R.id.availability_summary_container);
+
+		boolean isPropertyOnSale = property.getLowestRate().getSavingsPercent() > 0;
+		if (isPropertyOnSale) {
+			availabilitySummaryContainer.setBackgroundResource(R.drawable.bg_summarized_room_rates_sale);
+		}
+		else {
+			availabilitySummaryContainer.setBackgroundResource(R.drawable.bg_summarized_room_rates);
+		}
+
+		View minPriceRow = view.findViewById(R.id.min_price_row_container);
+		TextView minPrice = (TextView) minPriceRow.findViewById(R.id.min_price_text_view);
+
+		String displayRateString = StrUtils.formatHotelPrice(property.getLowestRate().getDisplayRate());
+		String minPriceString = activity.getString(R.string.min_room_price_template, displayRateString);
+		int startingIndexOfDisplayRate = minPriceString.indexOf(displayRateString);
+
+		// style the minimum available price text
+		StyleSpan textStyleSpan = new StyleSpan(Typeface.BOLD);
+		ForegroundColorSpan textColorSpan = new ForegroundColorSpan(activity.getResources().getColor(
+				R.color.hotel_price_text_color));
+		ForegroundColorSpan textWhiteColorSpan = new ForegroundColorSpan(activity.getResources().getColor(
+				android.R.color.white));
+		ForegroundColorSpan textBlackColorSpan = new ForegroundColorSpan(activity.getResources().getColor(
+				android.R.color.black));
+
+		Spannable str = new SpannableString(minPriceString);
+
+		str.setSpan(textStyleSpan, 0, minPriceString.length(), 0);
+
+		if (isPropertyOnSale) {
+			str.setSpan(textWhiteColorSpan, 0, minPriceString.length(), 0);
+		}
+		else {
+			str.setSpan(textColorSpan, startingIndexOfDisplayRate,
+					startingIndexOfDisplayRate + displayRateString.length(), 0);
+			str.setSpan(textBlackColorSpan, 0, startingIndexOfDisplayRate - 1, 0);
+		}
+
+		minPrice.setText(str);
+
+		TextView perNighTextView = (TextView) minPriceRow.findViewById(R.id.per_night_text_view);
+		perNighTextView.setTextColor(isPropertyOnSale ? activity.getResources().getColor(android.R.color.white)
+				: activity.getResources().getColor(android.R.color.black));
+
+		if (Rate.showInclusivePrices()) {
+			perNighTextView.setVisibility(View.GONE);
+		}
+		else {
+			perNighTextView.setVisibility(View.VISIBLE);
+		}
+	}
+
+	public static void layoutAvailabilitySummary(final TabletActivity activity, View view) {
+		final Property property = activity.getPropertyToDisplay();
+		final SummarizedRoomRates summarizedRoomRates = activity.getSummarizedRoomRates();
+		ViewGroup availabilityRatesContainer = (ViewGroup) view.findViewById(R.id.rates_container);
+		LayoutInflater inflater = activity.getLayoutInflater();
+
+		boolean isPropertyOnSale = property.getLowestRate().getSavingsPercent() > 0;
+		availabilityRatesContainer.removeAllViews();
+
+		// first adding all rows since the rows will exist regardless of whether
+		// there are enough rooms available or not 
+		for (int i = 0; i < MAX_SUMMARIZED_RATE_RESULTS; i++) {
+			View summaryRow = inflater.inflate(R.layout.snippet_availability_summary_row, null);
+			setHeightOfWeightOneForRow(summaryRow);
+
+			if (i == (MAX_SUMMARIZED_RATE_RESULTS - 1)) {
+				summaryRow.findViewById(R.id.divider).setVisibility(View.GONE);
+			}
+			availabilityRatesContainer.addView(summaryRow);
+		}
+
+		for (int i = 0; i < MAX_SUMMARIZED_RATE_RESULTS; i++) {
+			View summaryRow = availabilityRatesContainer.getChildAt(i);
+			ObjectAnimator animator = ObjectAnimator.ofFloat(summaryRow, "alpha", 0, 1);
+			animator.setDuration(ANIMATION_SPEED);
+			animator.start();
+
+			if (i > (summarizedRoomRates.numSummarizedRates() - 1)) {
+				continue;
+			}
+
+			final Rate rate = summarizedRoomRates.getRate(i);
+			summaryRow.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					 activity.bookRoom(rate);
+				}
+			});
+
+			View chevron = summaryRow.findViewById(R.id.availability_chevron_image_view);
+			chevron.setVisibility(View.VISIBLE);
+			TextView summaryDescription = (TextView) summaryRow.findViewById(R.id.availability_description_text_view);
+			TextView priceTextView = (TextView) summaryRow.findViewById(R.id.availability_summary_price_text_view);
+
+			Pair<BedTypeId, Rate> pair = summarizedRoomRates.getBedTypeToRatePair(i);
+			for (BedType bedType : pair.second.getBedTypes()) {
+				if (bedType.bedTypeId == pair.first) {
+					summaryDescription.setText(Html.fromHtml(activity.getString(R.string.bed_type_start_value_template,
+							bedType.bedTypeDescription)));
+					break;
+				}
+			}
+
+			priceTextView.setText(StrUtils.formatHotelPrice(pair.second.getDisplayRate()));
+			if (isPropertyOnSale) {
+				priceTextView.setTextColor(activity.getResources().getColor(R.color.hotel_price_sale_text_color));
+			}
+			else {
+				priceTextView.setTextColor(activity.getResources().getColor(R.color.hotel_price_text_color));
+			}
+		}
+
+		View selectRoomContainer = inflater.inflate(R.layout.snippet_select_room_button, null);
+		setHeightOfWeightOneForRow(selectRoomContainer);
+
+		View selectRoomButton = selectRoomContainer.findViewById(R.id.book_now_button);
+		selectRoomButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// if the user just presses the book now button,
+				// default to giving the user the minimum rate available
+				activity.bookRoom(summarizedRoomRates.getMinimumRateAvaialable());
+			}
+		});
+		availabilityRatesContainer.addView(selectRoomContainer);
+
+	}
+
+	public static void showLoadingForRates(TabletActivity activity, View view) {
+		TextView emptyAvailabilitySummaryTextView = (TextView) view.findViewById(R.id.empty_summart_container);
+		ProgressBar ratesProgressBar = (ProgressBar) view.findViewById(R.id.rates_progress_bar);
+		View availabilityRatesContainer = view.findViewById(R.id.rates_container);
+
+		emptyAvailabilitySummaryTextView.setVisibility(View.VISIBLE);
+		ratesProgressBar.setVisibility(View.VISIBLE);
+		emptyAvailabilitySummaryTextView.setText(activity.getString(R.string.room_rates_loading));
+		availabilityRatesContainer.setVisibility(View.GONE);
+	}
+
+	public static void showRatesContainer(View view) {
+		TextView emptyAvailabilitySummaryTextView = (TextView) view.findViewById(R.id.empty_summart_container);
+		ProgressBar ratesProgressBar = (ProgressBar) view.findViewById(R.id.rates_progress_bar);
+		View availabilityRatesContainer = view.findViewById(R.id.rates_container);
+
+		emptyAvailabilitySummaryTextView.setVisibility(View.GONE);
+		ratesProgressBar.setVisibility(View.GONE);
+		availabilityRatesContainer.setVisibility(View.VISIBLE);
+	}
+
+	public static void showErrorForRates(View view, String string) {
+		TextView emptyAvailabilitySummaryTextView = (TextView) view.findViewById(R.id.empty_summart_container);
+		ProgressBar ratesProgressBar = (ProgressBar) view.findViewById(R.id.rates_progress_bar);
+		View availabilityRatesContainer = view.findViewById(R.id.rates_container);
+
+		emptyAvailabilitySummaryTextView.setVisibility(View.VISIBLE);
+		ratesProgressBar.setVisibility(View.INVISIBLE);
+		emptyAvailabilitySummaryTextView.setText(string);
+		availabilityRatesContainer.setVisibility(View.GONE);
+	}
+
+	private static void setHeightOfWeightOneForRow(View view) {
+		LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, 0);
+		lp.weight = 1;
+		view.setLayoutParams(lp);
+	}
+
+}
