d967347a3f60b6fed13968688f2e0de134f2c8ae
Kunal Jham <kunal@mobiata.com>
2011-11-29 14:17:22 -0500

    10786: updating receipt photo frame

--- project/src/com/expedia/bookings/fragment/BookingConfirmationFragment.java
+++ project/src/com/expedia/bookings/fragment/BookingConfirmationFragment.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Fragment;
-import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
