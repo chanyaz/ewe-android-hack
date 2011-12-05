875cbf1fd238cda60390ddf9f8cc3902f96e22f2
Kunal Jham <kunal@mobiata.com>
2011-12-05 10:11:55 -0800

    Revert "11354: adding cushion for reviews container"
    This reverts commit 233f154cf36f71fcccf67f19542959a315dd5e99.
    
    Removing this change since its not the right way to make it.

--- project/src/com/expedia/bookings/activity/TabletUserReviewsListActivity.java
+++ project/src/com/expedia/bookings/activity/TabletUserReviewsListActivity.java
@@ -10,7 +10,7 @@ import com.expedia.bookings.R;
 public class TabletUserReviewsListActivity extends UserReviewsListActivity {
 
 	private View mRootView;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,17 +20,8 @@ public class TabletUserReviewsListActivity extends UserReviewsListActivity {
 	@Override
 	public boolean dispatchTouchEvent(MotionEvent ev) {
 		if (!isFinishing() && mRootView != null) {
-			int cushion_for_container = getResources().getInteger(R.integer.cushion_for_reviews_container);
 			Rect bounds = new Rect();
 			mRootView.getHitRect(bounds);
-			
-			// 11354: add cushion so that reviews are not mistakenly
-			// or easily dismissed
-			bounds.bottom += cushion_for_container;
-			bounds.top -= cushion_for_container;
-			bounds.left -= cushion_for_container;
-			bounds.right += cushion_for_container;
-			
 			if (!bounds.contains((int) ev.getX(), (int) ev.getY())) {
 				finish();
 			}
