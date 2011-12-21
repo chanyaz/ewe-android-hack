4b06da90a242b45ce7ee7a0fa808044f0467f53a
Doug Melton <doug@mobiata.com>
2011-12-20 19:08:30 -0800

    Changed the default POS to search locale by country only, ignoring language

--- project/src/com/expedia/bookings/utils/LocaleUtils.java
+++ project/src/com/expedia/bookings/utils/LocaleUtils.java
@@ -1,6 +1,8 @@
 package com.expedia.bookings.utils;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import android.content.Context;
 
@@ -8,105 +10,43 @@ import com.expedia.bookings.R;
 
 public class LocaleUtils {
 
+	@SuppressWarnings("serial")
+	private static final Map<String, Integer> POINT_OF_SALE_RES_ID = new HashMap<String, Integer>() {
+		{
+			put("US", R.string.point_of_sale_us);
+			put("UK", R.string.point_of_sale_uk);
+			put("AU", R.string.point_of_sale_au);
+			put("FR", R.string.point_of_sale_fr);
+			put("DE", R.string.point_of_sale_de);
+			put("IT", R.string.point_of_sale_it);
+			put("NL", R.string.point_of_sale_nl);
+			put("ES", R.string.point_of_sale_es);
+			put("NO", R.string.point_of_sale_no);
+			put("DK", R.string.point_of_sale_dk);
+			put("SE", R.string.point_of_sale_se);
+			put("IE", R.string.point_of_sale_ie);
+			put("BE", R.string.point_of_sale_be);
+			put("CA", R.string.point_of_sale_ca);
+			put("NZ", R.string.point_of_sale_nz);
+			put("JP", R.string.point_of_sale_jp);
+			put("MX", R.string.point_of_sale_mx);
+			put("SG", R.string.point_of_sale_sg);
+			put("MY", R.string.point_of_sale_my);
+			put("KR", R.string.point_of_sale_kr);
+			put("TH", R.string.point_of_sale_th);
+			put("PH", R.string.point_of_sale_ph);
+			put("ID", R.string.point_of_sale_id);
+			put("BR", R.string.point_of_sale_br);
+			put("HK", R.string.point_of_sale_hk);
+			put("TW", R.string.point_of_sale_tw);
+		}
+	};
+
 	public static String getDefaultPointOfSale(Context context) {
 		Locale locale = Locale.getDefault();
-		int resId = getPointOfSaleResId(locale);
-		return context.getString(resId > 0 ? resId : R.string.point_of_sale_us);
-	}
-
-	private static int getPointOfSaleResId(Locale locale) {
-		String localeString = locale.toString();
-		if (localeString.equals("en_US")) {
-			return R.string.point_of_sale_us;
-		}
-		else if (localeString.equals("en_GB")) {
-			return R.string.point_of_sale_uk;
-		}
-		else if (localeString.equals("en_AU")) {
-			return R.string.point_of_sale_au;
-		}
-		else if (localeString.equals("fr_FR")) {
-			return R.string.point_of_sale_fr;
-		}
-		else if (localeString.equals("de_DE")) {
-			return R.string.point_of_sale_de;
-		}
-		else if (localeString.equals("it_IT")) {
-			return R.string.point_of_sale_it;
-		}
-		else if (localeString.equals("nl_NL")) {
-			return R.string.point_of_sale_nl;
-		}
-		else if (localeString.equals("es_ES")) {
-			return R.string.point_of_sale_es;
-		}
-		else if (localeString.equals("nb_NO")) {
-			return R.string.point_of_sale_no;
-		}
-		else if (localeString.equals("da_DK")) {
-			return R.string.point_of_sale_dk;
-		}
-		else if (localeString.equals("sv_SE")) {
-			return R.string.point_of_sale_se;
-		}
-		else if (localeString.equals("en_IE")) {
-			return R.string.point_of_sale_ie;
-		}
-		else if (localeString.equals("fr_BE")) {
-			return R.string.point_of_sale_be;
-		}
-		else if (localeString.equals("nl_BE")) {
-			return R.string.point_of_sale_be;
-		}
-		else if (localeString.equals("en_CA")) {
-			return R.string.point_of_sale_ca;
-		}
-		else if (localeString.equals("fr_CA")) {
-			return R.string.point_of_sale_ca;
-		}
-		else if (localeString.equals("en_NZ")) {
-			return R.string.point_of_sale_nz;
-		}
-		else if (localeString.equals("ja_JP")) {
-			return R.string.point_of_sale_jp;
-		}
-		else if (localeString.equals("es_MX")) {
-			return R.string.point_of_sale_mx;
-		}
-		else if (localeString.equals("en_SG")) {
-			return R.string.point_of_sale_sg;
-		}
-		else if (localeString.equals("ms_MY")) {
-			return R.string.point_of_sale_my;
-		}
-		else if (localeString.equals("ko_KR")) {
-			return R.string.point_of_sale_kr;
-		}
-		else if (localeString.equals("th_TH")) {
-			return R.string.point_of_sale_th;
-		}
-		else if (localeString.equals("fil_PH")) {
-			return R.string.point_of_sale_ph;
-		}
-		else if (localeString.equals("en_PH")) {
-			return R.string.point_of_sale_ph;
-		}
-		else if (localeString.equals("id_ID")) {
-			return R.string.point_of_sale_id;
-		}
-		else if (localeString.equals("pt_BR")) {
-			return R.string.point_of_sale_br;
-		}
-		else if (localeString.equals("zh-HK")) {
-			return R.string.point_of_sale_hk;
-		}
-		else if (localeString.equals("zh-TW")) {
-			return R.string.point_of_sale_tw;
-		}
-		else if (localeString.equals("id")) {
-			return R.string.point_of_sale_id;
-		}
-
-		return 0;
+		String country = locale.getCountry();
+		int resId = POINT_OF_SALE_RES_ID.containsKey(country) ? POINT_OF_SALE_RES_ID.get(country)
+				: R.string.point_of_sale_us;
+		return context.getString(resId);
 	}
 }
