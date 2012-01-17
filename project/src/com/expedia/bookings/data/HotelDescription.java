124f4e39740913720a6a48876fa2bc47047c6e42
Brennan Taylor <brennan@mobiata.com>
2012-01-16 21:01:12 -0800

    Faster parseDescription
    Realized most of the work done to recognize the tag was useless and we
    ought to throw the tag out in all the dont-care cases.

--- project/src/com/expedia/bookings/data/HotelDescription.java
+++ project/src/com/expedia/bookings/data/HotelDescription.java
@@ -32,15 +32,10 @@ public class HotelDescription {
 
 	/* Preliminary version of this parser. To get it to be a little more
 	 * general and easy to use (read slower) add a stack of StringBuilders for
-	 * when you did deeper into nested tags. Allows pruning of empty leaves
+	 * when you dig deeper into nested tags. Allows pruning of empty leaves
 	 * easily as well.
 	 *
-	 * FIXME: The if-else construct is very slow and was to get this working.
-	 *        They need to be replaced with a better technique. Either a jump
-	 *        table or if a quality trie can be found do that.
-	 *
-	 * FIXME: Probably ripe with out-of-bounds issues with the indexOf operations.
-	 *        Replace with our own.
+	 * FIXME: May have out-of-bounds issues with the indexOf operations.
 	 */
         public void parseDescription(String html) {
                 // See MOHotelDescription.m
@@ -69,73 +64,69 @@ public class HotelDescription {
                         i = end + 1;
                         tag = html.substring(start + 1, end);
 
-                        if (tag.equalsIgnoreCase("p")) {
-                                continue;
-                        } else if (tag.equalsIgnoreCase("/p")) {
-				continue;
-                        } else if (tag.equalsIgnoreCase("b")) {
-                                continue;
-                        } else if (tag.equalsIgnoreCase("/b")) {
-				continue;
-                        } else if (tag.equalsIgnoreCase("br")) {
-				continue;
-                        } else if (tag.equalsIgnoreCase("br/")) {
-				continue;
-                        } else if (tag.equalsIgnoreCase("li")) {
-				if (! html.substring(i).startsWith("<ul>")){
-					if (str.length() > 0) {
-						str.append(bullet);
-					} else {
-						str.append(justBullet);
+			switch(tag.charAt(0)){
+			case 'l': // li
+				if ('i' == tag.charAt(1) && tag.length() == 2) {
+					if (! html.substring(i).startsWith("<ul>")){
+						if (str.length() > 0) {
+							str.append(bullet);
+						} else {
+							str.append(justBullet);
+						}
 					}
 				}
-                        } else if (tag.equalsIgnoreCase("/li")) {
-				continue;
-                        } else if (tag.equalsIgnoreCase("/ul")) {
-                                str.append("<br/>");
-                        } else if (tag.equalsIgnoreCase("ul")) {
-				if (html.substring(i).startsWith("</ul>")) {
-					// Skip this noise
-					i += 5;
-				} else if (str.length() > 0) {
-					str.append("<br/>");
+				break;
+			case 'u': // ul
+				if ('l' == tag.charAt(1) && tag.length() == 2) {
+					if (html.substring(i).startsWith("</ul>")) {
+						// Skip this noise
+						i += 5;
+					} else if (str.length() > 0) {
+						str.append("<br/>");
+					}
 				}
-                        } else if (tag.equalsIgnoreCase("strong")) {
-				if (sectionString != null && str.length() > 0) {
-					mSections.add(new DescriptionSection(sectionString, str.toString().trim()));
-					str = new StringBuilder();
-					sectionString = null;
+				break;
+			case 's': // strong
+				if (tag.equals("strong")) {
+					if (sectionString != null && str.length() > 0) {
+						mSections.add(new DescriptionSection(sectionString, str.toString().trim()));
+						str = new StringBuilder();
+						sectionString = null;
+					}
+					if (html.substring(i).startsWith("<strong>")) {
+						// Parse section
+						i += 8;
+						start = html.indexOf('<', i);
+						sectionString = html.substring(i, start);
+						end = html.indexOf('<', start + 1);
+						end = html.indexOf('>', end + 1);
+						i = end + 1;
+					} else if (html.substring(i).startsWith("<B>")) {
+						// Parse section
+						i += 3;
+						start = html.indexOf('<', i);
+						sectionString = html.substring(i, start);
+						end = html.indexOf('<', start + 1);
+						end = html.indexOf('>', end + 1);
+						i = end + 1;
+					} else {
+						start = html.indexOf('<', i);
+						end = html.indexOf('>', start);
+						sectionString = html.substring(i, start);
+						i = end + 1;
+					}
 				}
-				if (html.substring(i).startsWith("<strong>")) {
-					// Parse section
-					i += 8;
-					start = html.indexOf('<', i);
-					sectionString = html.substring(i, start);
-					end = html.indexOf('<', start + 1);
-					end = html.indexOf('>', end + 1);
-					i = end + 1;
-				} else if (html.substring(i).startsWith("<B>")) {
-					// Parse section
-					i += 3;
-					start = html.indexOf('<', i);
-					sectionString = html.substring(i, start);
-					end = html.indexOf('<', start + 1);
-					end = html.indexOf('>', end + 1);
-					i = end + 1;
-				} else {
-					start = html.indexOf('<', i);
-					end = html.indexOf('>', start);
-					sectionString = html.substring(i, start);
-					i = end + 1;
+				break;
+			case '/':
+				if(tag.equals("/ul")){
+					if ('l' == tag.charAt(2) && tag.length() == 3) {
+						str.append("<br/>");
+					}
 				}
-                        } else if (tag.equalsIgnoreCase("/strong")) {
-				continue;
-                        } else if (tag.equalsIgnoreCase("i")) {
-				continue;
-                        } else if (tag.equalsIgnoreCase("/i")) {
-				continue;
-                        } else {
-				str.append("<" + tag + ">");
+				break;
+			default:
+				// drop the tag
+				break;
 			}
                 }
 
