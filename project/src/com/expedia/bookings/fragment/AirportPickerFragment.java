35ff36ce91ed8a6d707def3d20b220a0e3085dc1
Daniel Lew <dan@mobiata.com>
2012-05-31 17:22:03 -0500

    Added recently searched airports

--- project/src/com/expedia/bookings/fragment/AirportPickerFragment.java
+++ project/src/com/expedia/bookings/fragment/AirportPickerFragment.java
@@ -7,16 +7,21 @@ import android.support.v4.app.ListFragment;
 import android.view.View;
 import android.widget.ListView;
 
+import com.mobiata.flightlib.data.RecentSearchList;
 import com.mobiata.flightlib.widget.AirportAdapter;
 
 public class AirportPickerFragment extends ListFragment {
 	public static final String TAG = AirportPickerFragment.class.getCanonicalName();
+
+	private static final String RECENT_SEARCH_LIST_FILE = "recent-airports.dat";
 
 	private static final String STATE_FILTER = "STATE_FILTER";
 
 	private AirportAdapter mAdapter;
 
 	private AirportPickerFragmentListener mListener;
+
+	private RecentSearchList mRecentSearchList;
 
 	// Stores what should be the initially filtered text when
 	// the view is created.  Held due to timing issues that
@@ -52,6 +57,10 @@ public class AirportPickerFragment extends ListFragment {
 
 		mAdapter = new AirportAdapter(getActivity());
 		mAdapter.openDb();
+
+		mRecentSearchList = new RecentSearchList(getActivity(), RECENT_SEARCH_LIST_FILE);
+		mAdapter.mRecentSearchList = mRecentSearchList;
+
 		mAdapter.filter(mInitialFilter != null ? mInitialFilter : "");
 
 		setListAdapter(mAdapter);
@@ -78,7 +87,24 @@ public class AirportPickerFragment extends ListFragment {
 	public void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
 
-		mListener.onAirportClick(mAdapter.getAirportCode(position));
+		if (id == AirportAdapter.ID_CLEAR_RECENT) {
+			// Clear recents
+			mRecentSearchList.clear();
+			mRecentSearchList.saveList(getActivity(), RECENT_SEARCH_LIST_FILE);
+
+			// Update the adapter
+			mAdapter.filter("");
+			mAdapter.notifyDataSetChanged();
+		}
+		else {
+			String airportCode = mAdapter.getAirportCode(position);
+
+			// Save airport to recent search list
+			mRecentSearchList.addItem(airportCode);
+			mRecentSearchList.saveList(getActivity(), RECENT_SEARCH_LIST_FILE);
+
+			mListener.onAirportClick(airportCode);
+		}
 	}
 
 	public void filter(CharSequence constraint) {
