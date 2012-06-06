ccd35cf6796b4fcfeaa8723bdd1d031e446f09ce
Joel Drotos <jdrotos@mobiata.com>
2012-06-06 10:58:37 -0500

    Flight details is shaping up

--- project/src/com/expedia/bookings/activity/FlightDetailsActivity.java
+++ project/src/com/expedia/bookings/activity/FlightDetailsActivity.java
@@ -1,6 +1,9 @@
 package com.expedia.bookings.activity;
 
+import java.sql.Time;
 import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -21,6 +24,7 @@ import com.expedia.bookings.data.FlightTrip;
 import com.expedia.bookings.utils.Ui;
 import com.mobiata.flightlib.data.Airport;
 import com.mobiata.flightlib.data.Flight;
+import com.mobiata.flightlib.data.FlightNote;
 import com.mobiata.flightlib.data.Waypoint;
 import com.mobiata.flightlib.utils.DateTimeUtils;
 import com.mobiata.flightlib.utils.FormatUtils;
@@ -86,7 +90,7 @@ public class FlightDetailsActivity extends FragmentActivity {
 			fragment.setArguments(args);
 			return fragment;
 		}
-
+		
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 			View view = inflater.inflate(R.layout.fragment_trip, container, false);
@@ -99,100 +103,231 @@ public class FlightDetailsActivity extends FragmentActivity {
 			// Construct the trip.  This is a bit complex.
 			ViewGroup tripContainer = Ui.findView(view, R.id.trip_container);
 
-			int segmentCount = leg.getSegmentCount();
-			for (int segmentIndex = 0; segmentIndex < segmentCount; segmentIndex++) {
-				Flight segment = leg.getSegment(segmentIndex);
-
-				if (segmentIndex == 0) {
-					// Add the initial departure waypoint
-					addWaypoint(inflater, tripContainer, segment, true);
-					addDivider(inflater, tripContainer);
-				}
-				else {
-					// Add a layover waypoint
-					addLayover(inflater, tripContainer, leg.getSegment(segmentIndex - 1), segment);
-					addDivider(inflater, tripContainer);
-				}
-
-				// Add departing flight details
-				addFlightMovement(inflater, tripContainer, segment, true);
-				addDivider(inflater, tripContainer);
-
-				// Add the flight info row
-				addFlightInfo(inflater, tripContainer, segment);
-				addDivider(inflater, tripContainer);
-
-				// Add arrival flight details
-				addFlightMovement(inflater, tripContainer, segment, false);
-				addDivider(inflater, tripContainer);
-
-				if (segmentIndex == segmentCount - 1) {
-					// Add the final destination waypoint
-					addWaypoint(inflater, tripContainer, segment, false);
-				}
+			
+			for(int i = 0; i < leg.getSegmentCount(); i++){
+				Flight seg = leg.getSegment(i);
+				
+				View segmentView = buildSegmentView(inflater, seg,tripContainer);
+				
+				tripContainer.addView(segmentView);
 			}
+			
+			//int segmentCount = leg.getSegmentCount();
+			
+//			for (int segmentIndex = 0; segmentIndex < segmentCount; segmentIndex++) {
+//				Flight segment = leg.getSegment(segmentIndex);
+//
+//				View wayPoint;
+//				if (segmentIndex == 0) {
+//					// If this is the last waypoint then we don't need to add any info...
+//					wayPoint = buildWaypoint(inflater,  segment, tripContainer, SegmentPosition.DEPARTURE);	
+//				}else if (segmentIndex < segmentCount - 1){
+//					//We will add a waypoint and a bunch of info for the segment
+//					wayPoint = buildWaypoint(inflater,  segment, tripContainer, SegmentPosition.INTERMEDIATE);
+//				}else{
+//					wayPoint = buildWaypoint(inflater,  segment, tripContainer, SegmentPosition.ARRIVAL);
+//				}
+//				
+//				tripContainer.addView(wayPoint);
+//			}
 
 			return view;
 		}
+		
+		private View buildSegmentView(LayoutInflater inflater, Flight segment, ViewGroup tripContainer){
+			View wpView = inflater.inflate(R.layout.snippet_flight_detail_segment_info, tripContainer, false);
 
-		private void addDivider(LayoutInflater inflater, ViewGroup tripContainer) {
-			View view = inflater.inflate(R.layout.snippet_flight_detail_divider, tripContainer, false);
-			tripContainer.addView(view);
-		}
-
-		private void addWaypoint(LayoutInflater inflater, ViewGroup tripContainer, Flight segment,
-				boolean isDeparture) {
-			View view = inflater.inflate(R.layout.snippet_waypoint, tripContainer, false);
-
-			Waypoint waypoint = (isDeparture) ? segment.mOrigin : segment.mDestination;
-			Airport airport = waypoint.getAirport();
-
-			String location = airport.mCity;
-			if (!TextUtils.isEmpty(airport.mStateCode)) {
-				location += ", " + airport.mStateCode;
+			Waypoint orig = segment.mOrigin;
+			Waypoint dest = segment.mDestination;
+			
+			String origCity = orig.getAirport().mCity;
+			String destCity = dest.getAirport().mCity;
+			String cityToCity = origCity + " to " + destCity;
+			
+			
+ 			String origTz = orig.getAirport().mTimeZone.getDisplayName();
+ 			String destTz = orig.getAirport().mTimeZone.getDisplayName();
+			
+			String carrier = "" + segment.getOperatingFlightCode().getAirline().mAirlineName;
+			String flightNumber = "" + segment.getOperatingFlightCode().mNumber;
+//			Time takeOffTime = new Time(segment.mLastNotifiedTakeoffTime);
+//			String depTime = "" + takeOffTime.getHours() + ":" + takeOffTime.getMinutes();
+		
+			
+			boolean twentyFourHourClock = false;//TODO:get from someplace
+			
+			
+			Ui.setText(wpView, R.id.flight_details_dep_arr_tv, cityToCity);
+			Ui.setText(wpView, R.id.flight_details_carrier_tv, carrier);
+			Ui.setText(wpView, R.id.flight_details_flight_number_tv, flightNumber);
+			Ui.setText(wpView, R.id.flight_details_departure_time_tv, genBaseTime(orig.getMostRelevantDateTime(),twentyFourHourClock));
+			Ui.setText(wpView, R.id.flight_details_departure_tz_tv, orig.getAirport().mTimeZone.getDisplayName(false,TimeZone.SHORT));
+			Ui.setText(wpView, R.id.flight_details_departure_ampm_tv, getAmPm(orig.getMostRelevantDateTime(),twentyFourHourClock));
+			Ui.setText(wpView, R.id.flight_details_arrival_time_tv, genBaseTime(dest.getMostRelevantDateTime(),twentyFourHourClock));
+			Ui.setText(wpView, R.id.flight_details_arrival_tz_tv, dest.getAirport().mTimeZone.getDisplayName(false,TimeZone.SHORT));
+			Ui.setText(wpView, R.id.flight_details_arrival_ampm_tv, getAmPm(dest.getMostRelevantDateTime(),twentyFourHourClock));
+			Ui.setText(wpView, R.id.flight_details_duration_tv, getFlightDuration(orig.getMostRelevantDateTime(),dest.getMostRelevantDateTime()));
+			
+			ViewGroup amenities = (ViewGroup)wpView.findViewById(R.id.flight_details_data_amenities_ll);
+			for(int i = 0; i < 4; i++){
+				View amen = inflater.inflate(R.layout.snippet_flight_detail_amenity, amenities,false);
+				Ui.setText(amen, R.id.flight_amenity_label_tv,"Meal");
+				amenities.addView(amen);
 			}
-
-			Ui.setText(view, R.id.location_text_view, location);
-			Ui.setText(view, R.id.airport_name_text_view, airport.mName);
-
-			tripContainer.addView(view);
+			
+			return wpView;
 		}
-
-		private void addLayover(LayoutInflater inflater, ViewGroup tripContainer, Flight lastSegment,
-				Flight nextSegment) {
-			int duration = DateTimeUtils.compareDateTimes(lastSegment.mDestination.getMostRelevantDateTime(),
-					nextSegment.mOrigin.getMostRelevantDateTime());
-
-			String durationStr = DateTimeUtils.formatDuration(getResources(), duration);
-
-			View view = inflater.inflate(R.layout.snippet_layover, tripContainer, false);
-			Ui.setText(view, R.id.layover_text_view, Html.fromHtml(getString(R.string.layover_template, durationStr)));
-			Ui.setText(view, R.id.airport_name_text_view, nextSegment.mOrigin.mAirportCode);
-			tripContainer.addView(view);
+		
+		private String genBaseTime(Calendar cal, boolean twentyFourHour){
+			String retVal = "";
+			
+			if(!twentyFourHour){
+				retVal = String.format("%d:%02d",cal.get(Calendar.HOUR),cal.get(Calendar.MINUTE));
+			}else{
+				retVal = String.format("%d:%02d",cal.get(Calendar.HOUR_OF_DAY),cal.get(Calendar.MINUTE));
+			}
+			
+			return retVal;
 		}
-
-		private void addFlightMovement(LayoutInflater inflater, ViewGroup tripContainer, Flight segment,
-				boolean isDeparture) {
-			View view = inflater.inflate(R.layout.snippet_flight_movement, tripContainer, false);
-
-			DateFormat df = android.text.format.DateFormat.getTimeFormat(getActivity());
-
-			Waypoint waypoint = (isDeparture) ? segment.mOrigin : segment.mDestination;
-			Ui.setText(view, R.id.time_text_view, df.format(waypoint.getMostRelevantDateTime().getTime()));
-			Ui.setText(view, R.id.airport_code_text_view, waypoint.mAirportCode);
-
-			tripContainer.addView(view);
+		
+		//TODO:We need to use locals...
+		private String getAmPm(Calendar cal, boolean twentyFourHour){
+			if(twentyFourHour){
+				return "";
+			}else if(cal.get(Calendar.HOUR_OF_DAY)%12 > 0){
+				return "PM";
+			}else{
+				return "AM";
+			}
 		}
-
-		private void addFlightInfo(LayoutInflater inflater, ViewGroup tripContainer, Flight segment) {
-			int duration = DateTimeUtils.compareDateTimes(segment.mOrigin.getMostRelevantDateTime(),
-					segment.mDestination.getMostRelevantDateTime());
-			String durationStr = DateTimeUtils.formatDuration(getResources(), duration);
-
-			View view = inflater.inflate(R.layout.snippet_flight_info, tripContainer, false);
-			Ui.setText(view, R.id.duration_text_view, durationStr);
-			Ui.setText(view, R.id.flight_number_text_view, FormatUtils.formatFlightNumberShort(segment, getActivity()));
-			tripContainer.addView(view);
+		
+		
+		//TODO:This whole thing needs to be rewritten inorder to use locals etc..
+		private String getFlightDuration(Calendar orig, Calendar dest ){
+			String retStr = "";
+			int minutes = 1000 * 60;
+			int hours = minutes * 60;
+			int days = hours * 24;
+			
+			//TODO:Don't assume orig before dest
+			long dif = dest.getTimeInMillis() - orig.getTimeInMillis();
+			if(dif/days > 1){
+				retStr += "" + (int)Math.floor(dif/days) + "d";
+			}
+			
+			
+			
+			retStr += String.format("%dh%dm", (int)Math.floor(dif/hours),(int)Math.round((dif%hours)/minutes));
+			return retStr;
 		}
+		
+
+//		@Override
+//		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//			View view = inflater.inflate(R.layout.fragment_trip, container, false);
+//
+//			Bundle args = getArguments();
+//
+//			FlightTrip trip = Db.getFlightSearchResponse().getTrip(args.getInt(ARG_POSITION));
+//			FlightLeg leg = trip.getLeg(args.getInt(EXTRA_LEG_POSITION, 0));
+//
+//			// Construct the trip.  This is a bit complex.
+//			ViewGroup tripContainer = Ui.findView(view, R.id.trip_container);
+//
+//			int segmentCount = leg.getSegmentCount();
+//			for (int segmentIndex = 0; segmentIndex < segmentCount; segmentIndex++) {
+//				Flight segment = leg.getSegment(segmentIndex);
+//
+//				if (segmentIndex == 0) {
+//					// Add the initial departure waypoint
+//					addWaypoint(inflater, tripContainer, segment, true);
+//					addDivider(inflater, tripContainer);
+//				}
+//				else {
+//					// Add a layover waypoint
+//					addLayover(inflater, tripContainer, leg.getSegment(segmentIndex - 1), segment);
+//					addDivider(inflater, tripContainer);
+//				}
+//
+//				// Add departing flight details
+//				addFlightMovement(inflater, tripContainer, segment, true);
+//				addDivider(inflater, tripContainer);
+//
+//				// Add the flight info row
+//				addFlightInfo(inflater, tripContainer, segment);
+//				addDivider(inflater, tripContainer);
+//
+//				// Add arrival flight details
+//				addFlightMovement(inflater, tripContainer, segment, false);
+//				addDivider(inflater, tripContainer);
+//
+//				if (segmentIndex == segmentCount - 1) {
+//					// Add the final destination waypoint
+//					addWaypoint(inflater, tripContainer, segment, false);
+//				}
+//			}
+//
+//			return view;
+//		}
+
+//		private void addDivider(LayoutInflater inflater, ViewGroup tripContainer) {
+//			View view = inflater.inflate(R.layout.snippet_flight_detail_divider, tripContainer, false);
+//			tripContainer.addView(view);
+//		}
+//
+//		private void addWaypoint(LayoutInflater inflater, ViewGroup tripContainer, Flight segment,
+//				boolean isDeparture) {
+//			View view = inflater.inflate(R.layout.snippet_waypoint, tripContainer, false);
+//
+//			Waypoint waypoint = (isDeparture) ? segment.mOrigin : segment.mDestination;
+//			Airport airport = waypoint.getAirport();
+//
+//			String location = airport.mCity;
+//			if (!TextUtils.isEmpty(airport.mStateCode)) {
+//				location += ", " + airport.mStateCode;
+//			}
+//
+//			Ui.setText(view, R.id.location_text_view, location);
+//			Ui.setText(view, R.id.airport_name_text_view, airport.mName);
+//
+//			tripContainer.addView(view);
+//		}
+//
+//		private void addLayover(LayoutInflater inflater, ViewGroup tripContainer, Flight lastSegment,
+//				Flight nextSegment) {
+//			int duration = DateTimeUtils.compareDateTimes(lastSegment.mDestination.getMostRelevantDateTime(),
+//					nextSegment.mOrigin.getMostRelevantDateTime());
+//
+//			String durationStr = DateTimeUtils.formatDuration(getResources(), duration);
+//
+//			View view = inflater.inflate(R.layout.snippet_layover, tripContainer, false);
+//			Ui.setText(view, R.id.layover_text_view, Html.fromHtml(getString(R.string.layover_template, durationStr)));
+//			Ui.setText(view, R.id.airport_name_text_view, nextSegment.mOrigin.mAirportCode);
+//			tripContainer.addView(view);
+//		}
+//
+//		private void addFlightMovement(LayoutInflater inflater, ViewGroup tripContainer, Flight segment,
+//				boolean isDeparture) {
+//			View view = inflater.inflate(R.layout.snippet_flight_movement, tripContainer, false);
+//
+//			DateFormat df = android.text.format.DateFormat.getTimeFormat(getActivity());
+//
+//			Waypoint waypoint = (isDeparture) ? segment.mOrigin : segment.mDestination;
+//			Ui.setText(view, R.id.time_text_view, df.format(waypoint.getMostRelevantDateTime().getTime()));
+//			Ui.setText(view, R.id.airport_code_text_view, waypoint.mAirportCode);
+//
+//			tripContainer.addView(view);
+//		}
+//
+//		private void addFlightInfo(LayoutInflater inflater, ViewGroup tripContainer, Flight segment) {
+//			int duration = DateTimeUtils.compareDateTimes(segment.mOrigin.getMostRelevantDateTime(),
+//					segment.mDestination.getMostRelevantDateTime());
+//			String durationStr = DateTimeUtils.formatDuration(getResources(), duration);
+//
+//			View view = inflater.inflate(R.layout.snippet_flight_info, tripContainer, false);
+//			Ui.setText(view, R.id.duration_text_view, durationStr);
+//			Ui.setText(view, R.id.flight_number_text_view, FormatUtils.formatFlightNumberShort(segment, getActivity()));
+//			tripContainer.addView(view);
+//		}
 	}
 }
