9ced7fae6f084fc4eb8e997dfc5054e3f4c8c8fa
Daniel Lew <dan@mobiata.com>
2012-06-08 13:57:11 -0500

    Added more methods to Ui

--- project/src/com/expedia/bookings/utils/Ui.java
+++ project/src/com/expedia/bookings/utils/Ui.java
@@ -19,17 +19,27 @@ public class Ui extends com.mobiata.android.util.Ui {
 	}
 
 	/**
+	 * Even more convenient method for adding a single Fragment.
+	 * 
+	 * Should only be used if there is a single Fragment that is in android.R.id.content.
+	 */
+	public static <T extends Fragment> T findOrAddSupportFragment(CompatFragmentActivity activity,
+			Class<T> fragmentClass, String tag) {
+		return findOrAddSupportFragment(activity, android.R.id.content, fragmentClass, tag);
+	}
+
+	/**
 	 * Convenience method that either:
 	 * 
 	 * 1. Finds and returns the Fragment if already exists in FragmentManager
-	 * 2. Creates and adds the Fragment to android.R.id.content if doesn't exit
+	 * 2. Creates and adds the Fragment to containerViewId if doesn't exit
 	 * 
 	 * Either way, it returns the Fragment, ready for use.
 	 * 
 	 * Should only be used if there is a single Fragment that is in android.R.id.content.
 	 */
 	public static <T extends Fragment> T findOrAddSupportFragment(CompatFragmentActivity activity,
-			Class<T> fragmentClass, String tag) {
+			int containerViewId, Class<T> fragmentClass, String tag) {
 		T fragment = findSupportFragment(activity, tag);
 		if (fragment == null) {
 			try {
@@ -38,7 +48,7 @@ public class Ui extends com.mobiata.android.util.Ui {
 			catch (Exception e) {
 				throw new RuntimeException(e);
 			}
-			activity.getSupportFragmentManager().beginTransaction().add(android.R.id.content, fragment, tag).commit();
+			activity.getSupportFragmentManager().beginTransaction().add(containerViewId, fragment, tag).commit();
 		}
 		return fragment;
 	}
