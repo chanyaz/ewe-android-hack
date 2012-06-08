7f69e05b0a87b1c05ec7c545e8ee8c6ad086b187
Joel Drotos <jdrotos@mobiata.com>
2012-06-08 10:15:15 -0500

    Show correct details, cleaned up code

--- project/src/com/expedia/bookings/activity/FlightDetailsActivity.java
+++ project/src/com/expedia/bookings/activity/FlightDetailsActivity.java
@@ -1,7 +1,12 @@
 package com.expedia.bookings.activity;
 
 import com.expedia.bookings.R;
+import com.expedia.bookings.data.Db;
+import com.expedia.bookings.data.FlightTrip;
+import com.expedia.bookings.data.Money;
 import com.expedia.bookings.fragment.TripFragment;
+import com.expedia.bookings.utils.Ui;
+import com.mobiata.android.Log;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -10,21 +15,31 @@ public class FlightDetailsActivity extends FragmentActivity {
 
 	public static final String EXTRA_STARTING_POSITION = "EXTRA_STARTING_POSITION";
 	public static final String EXTRA_LEG_POSITION = "EXTRA_LEG_POSITION";
-
-	//private FlightAdapter mAdapter;
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_flight_details);
 		
-		if (savedInstanceState == null) {
-            // During initial setup, plug in the details fragment.
-            TripFragment details = TripFragment.newInstance(getIntent().getIntExtra(EXTRA_STARTING_POSITION, 0), getIntent().getBooleanExtra(EXTRA_LEG_POSITION, true));
-            getSupportFragmentManager().beginTransaction().add(R.id.flight_details_card_holder_ll, details).commit();
-        }
+		int pos = getIntent().getIntExtra(EXTRA_STARTING_POSITION, 0);
+		int legPos = getIntent().getIntExtra(EXTRA_LEG_POSITION, 0);
 		
+		Log.i("onCreate - EXTRA_STARTING_POSITION:" + pos);
+		Log.i("onCreate - EXTRA_LEG_POSITION:" + legPos);
+		
+		FlightTrip trip = Db.getFlightSearch().getTrips(legPos).get(pos);
+	
+		Ui.setText(this, R.id.flight_details_num_seats_tv, "" + trip.getSeatsRemaining());//TODO: Seats remaining cutoff like results activity!?!?!
+		Ui.setText(this, R.id.flight_details_seat_price_tv, "" + trip.getTotalFare().getFormattedMoney(Money.F_NO_DECIMAL));
+	
+        // During initial setup, plug in the details fragment.
+        TripFragment details = TripFragment.newInstance(pos,legPos);
+        getSupportFragmentManager().beginTransaction().add(R.id.flight_details_card_holder_ll, details).commit();
 	}
 
+	@Override
+	protected void onResume(){
+		super.onResume();
+	}
 }
--- project/src/com/expedia/bookings/fragment/TripFragment.java
+++ project/src/com/expedia/bookings/fragment/TripFragment.java
@@ -14,36 +14,40 @@ import com.mobiata.flightlib.data.Waypoint;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class TripFragment extends Fragment {
 	private static final String ARG_POSITION = "ARG_POSITION";
-	private static final String ARG_IS_INBOUND = "ARG_IS_INBOUND";
+	private static final String ARG_LEG_POS = "ARG_LEG_POS";
 
-	public static TripFragment newInstance(int position, boolean isInbound) {
+	/**
+	 * Used to describe the position of the Waypoint within the leg.
+	 */
+	private enum HeaderPosition{
+		FIRST,INTERMEDIATE,LAST
+	}
+	
+	public static TripFragment newInstance(int position, int legPosition) {
 		TripFragment fragment = new TripFragment();
 		Bundle args = new Bundle();
 		args.putInt(ARG_POSITION, position);
-		args.putBoolean(ARG_IS_INBOUND, isInbound);
+		args.putInt(ARG_LEG_POS, legPosition);
 		fragment.setArguments(args);
 		return fragment;
 	}
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_trip, container, false);
-
-		Bundle args = getArguments();
-
-		FlightTrip trip = Db.getFlightSearch().getSearchResponse().getTrip(args.getInt(ARG_POSITION));
-		FlightLeg leg = trip.getLeg(args.getInt(FlightDetailsActivity.EXTRA_LEG_POSITION, 0));
-
-		// Construct the trip.  This is a bit complex.
-		ViewGroup tripContainer = Ui.findView(view, R.id.trip_container);
-
+		Bundle args = getArguments();	
 		
+		FlightTrip trip = Db.getFlightSearch().getTrips(args.getInt(ARG_LEG_POS, 0)).get(args.getInt(ARG_POSITION));
+		FlightLeg leg = trip.getLeg(args.getInt(ARG_LEG_POS, 0));
+		
+		View view = inflater.inflate(R.layout.fragment_trip, container, false);
+		ViewGroup tripContainer = Ui.findView(view, R.id.trip_container);
 		for(int i = 0; i < leg.getSegmentCount(); i++){
 			Flight seg = leg.getSegment(i);
 			
@@ -69,6 +73,13 @@ public class TripFragment extends Fragment {
 		return view;
 	}
 	
+	/**
+	 * Creates an returns one of the detail cards
+	 * @param inflater
+	 * @param segment
+	 * @param tripContainer
+	 * @return
+	 */
 	private View buildSegmentView(LayoutInflater inflater, Flight segment, ViewGroup tripContainer){
 		View wpView = inflater.inflate(R.layout.snippet_flight_detail_segment_info, tripContainer, false);
 
@@ -77,20 +88,11 @@ public class TripFragment extends Fragment {
 		
 		String origCity = orig.getAirport().mCity;
 		String destCity = dest.getAirport().mCity;
-		String cityToCity = origCity + " to " + destCity;
-		
-		
-			String origTz = orig.getAirport().mTimeZone.getDisplayName();
-			String destTz = orig.getAirport().mTimeZone.getDisplayName();
-		
+		String cityToCity = origCity + " to " + destCity;	
 		String carrier = "" + segment.getOperatingFlightCode().getAirline().mAirlineName;
 		String flightNumber = "" + segment.getOperatingFlightCode().mNumber;
-//		Time takeOffTime = new Time(segment.mLastNotifiedTakeoffTime);
-//		String depTime = "" + takeOffTime.getHours() + ":" + takeOffTime.getMinutes();
 	
-		
-		boolean twentyFourHourClock = false;//TODO:get from someplace
-		
+		boolean twentyFourHourClock = DateFormat.is24HourFormat(this.getActivity());
 		
 		Ui.setText(wpView, R.id.flight_details_dep_arr_tv, cityToCity);
 		Ui.setText(wpView, R.id.flight_details_carrier_tv, carrier);
@@ -106,6 +108,7 @@ public class TripFragment extends Fragment {
 		ViewGroup amenities = (ViewGroup)wpView.findViewById(R.id.flight_details_data_amenities_ll);
 		
 		//Add placeholders
+		//TODO:Get real amenities...
 		for(int i = 0; i < 4; i++){
 			View amen = inflater.inflate(R.layout.snippet_flight_detail_amenity, amenities,false);
 			Ui.setText(amen, R.id.flight_amenity_label_tv,"Meal");
@@ -116,11 +119,16 @@ public class TripFragment extends Fragment {
 		return wpView;
 	}
 	
-	enum HeaderPosition{
-		FIRST,INTERMEDIATE,LAST
-	}
 	
-	public View buildSegmentHeader(LayoutInflater inflater, Flight segment, ViewGroup tripContainer, HeaderPosition pos){
+	/**
+	 * Creates and returns one of the waypoint headers with the neighboring Circle (and a layout place to add details)
+	 * @param inflater
+	 * @param segment
+	 * @param tripContainer
+	 * @param pos
+	 * @return
+	 */
+	private View buildSegmentHeader(LayoutInflater inflater, Flight segment, ViewGroup tripContainer, HeaderPosition pos){
 		View segHead = inflater.inflate(R.layout.snippet_flight_detail_segment, tripContainer, false);
 		
 		Waypoint wp;
@@ -132,7 +140,7 @@ public class TripFragment extends Fragment {
 
 		Calendar cal =  wp.getMostRelevantDateTime();
 		
-		Ui.setText(segHead, R.id.flight_details_departure_time_tv, String.format("%d:%02d",cal.get(Calendar.HOUR),cal.get(Calendar.MINUTE)));
+		Ui.setText(segHead, R.id.flight_details_departure_time_tv, DateFormat.format("h:mmaa", cal.getTimeInMillis()));
 		Ui.setText(segHead,R.id.flight_details_departure_airport_tv , String.format("%s (%s)", wp.getAirport().mName, wp.mAirportCode));
 		
 		
@@ -145,46 +153,52 @@ public class TripFragment extends Fragment {
 		return segHead;
 	}
 	
-	private String genBaseTime(Calendar cal, boolean twentyFourHour){
-		String retVal = "";
+	
+	/*
+	 * TIME HELPERS
+	 */
+	private CharSequence genBaseTime(Calendar cal, boolean twentyFourHour){
+		CharSequence retVal = "";
 		
 		if(!twentyFourHour){
-			retVal = String.format("%d:%02d",cal.get(Calendar.HOUR),cal.get(Calendar.MINUTE));
+			retVal = DateFormat.format("h:mm", cal.getTimeInMillis());//String.format("%d:%02d",cal.get(Calendar.HOUR),cal.get(Calendar.MINUTE));
 		}else{
-			retVal = String.format("%d:%02d",cal.get(Calendar.HOUR_OF_DAY),cal.get(Calendar.MINUTE));
+			retVal = DateFormat.format("h:mm", cal.getTimeInMillis());//String.format("%d:%02d",cal.get(Calendar.HOUR_OF_DAY),cal.get(Calendar.MINUTE));
 		}
 		
 		return retVal;
 	}
 	
-	//TODO:We need to use locals...
-	private String getAmPm(Calendar cal, boolean twentyFourHour){
+	private CharSequence getAmPm(Calendar cal, boolean twentyFourHour){
 		if(twentyFourHour){
 			return "";
-		}else if(cal.get(Calendar.HOUR_OF_DAY)%12 > 0){
-			return "PM";
 		}else{
-			return "AM";
+			return DateFormat.format("aa", cal.getTimeInMillis()).toString().toUpperCase();
 		}
 	}
 	
-	
 	//TODO:This whole thing needs to be rewritten inorder to use locals etc..
-	private String getFlightDuration(Calendar orig, Calendar dest ){
-		String retStr = "";
+	private CharSequence getFlightDuration(Calendar orig, Calendar dest ){
+		//TODO:Move to someplace static...
 		int minutes = 1000 * 60;
 		int hours = minutes * 60;
 		int days = hours * 24;
+	
+		String retStr = "";
 		
-		//TODO:Don't assume orig before dest
 		long dif = dest.getTimeInMillis() - orig.getTimeInMillis();
+		if(dif < 0)
+			dif = orig.getTimeInMillis() - dest.getTimeInMillis();
+		
 		if(dif/days > 1){
 			retStr += "" + (int)Math.floor(dif/days) + "d";
 		}
 		
-		
-		
 		retStr += String.format("%dh%dm", (int)Math.floor(dif/hours),(int)Math.round((dif%hours)/minutes));
 		return retStr;
 	}
+	
+	/*
+	 * END TIME HELPERS
+	 */
 }
