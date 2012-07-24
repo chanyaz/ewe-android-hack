5b85678051b256db83a1d7e86d8be71bb8340161
Brad McManus <brad@mobiata.com>
2012-07-24 15:54:08 -0400

    Add unit test for FlightFilter
    Note: this test is broken. I am seeing what happens on Jenkins test job when
    a test fails. I figure this is better than breaking then fixing the code

---
+++ tests/src/com/expedia/bookings/test/tests/unit/FlightFilterTest.java
@@ -0,0 +1,20 @@
+package com.expedia.bookings.test.tests.unit;
+
+import com.expedia.bookings.data.FlightFilter;
+import junit.framework.TestCase;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: brad
+ * Date: 7/24/12
+ * Time: 3:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FlightFilterTest extends TestCase {
+
+	public void testNewFlightFilterDefaultsSortToPrice() {
+		FlightFilter flightFilter = new FlightFilter();
+		assertEquals(FlightFilter.Sort.ARRIVAL, flightFilter.getSort());
+	}
+
+}
