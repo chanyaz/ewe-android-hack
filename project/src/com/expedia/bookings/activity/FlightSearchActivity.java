b32774e2f12c05cc2cf8f4518a48802a62e3fbdd
Daniel Lew <dan@mobiata.com>
2012-07-30 14:10:21 -0500

    FlightSearchActivity now uses FlightSearchParamsFragment
    It also has a new custom background

--- project/src/com/expedia/bookings/activity/FlightSearchActivity.java
+++ project/src/com/expedia/bookings/activity/FlightSearchActivity.java
@@ -1,75 +1,27 @@
 package com.expedia.bookings.activity;
 
-import java.util.Calendar;
-
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.text.format.DateFormat;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnFocusChangeListener;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.TextView.OnEditorActionListener;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.expedia.bookings.R;
 import com.expedia.bookings.data.Codes;
-import com.expedia.bookings.data.Date;
 import com.expedia.bookings.data.Db;
-import com.expedia.bookings.data.FlightSearchParams;
-import com.expedia.bookings.fragment.AirportPickerFragment;
-import com.expedia.bookings.fragment.AirportPickerFragment.AirportPickerFragmentListener;
-import com.expedia.bookings.fragment.CalendarDialogFragment;
-import com.expedia.bookings.fragment.CalendarDialogFragment.CalendarDialogFragmentListener;
-import com.expedia.bookings.fragment.PassengerPickerFragment;
+import com.expedia.bookings.fragment.FlightSearchParamsFragment;
 import com.expedia.bookings.fragment.SimpleSupportDialogFragment;
 import com.expedia.bookings.utils.DebugMenu;
 import com.expedia.bookings.utils.Ui;
 import com.mobiata.android.hockey.HockeyPuck;
 import com.mobiata.android.util.AndroidUtils;
-import com.nineoldandroids.animation.ValueAnimator;
-import com.nineoldandroids.animation.ValueAnimator.AnimatorUpdateListener;
 
-public class FlightSearchActivity extends SherlockFragmentActivity implements AirportPickerFragmentListener,
-		CalendarDialogFragmentListener {
+public class FlightSearchActivity extends SherlockFragmentActivity {
 
 	public static final String EXTRA_DATA_EXPIRED = "EXTRA_DATA_EXPIRED";
 
-	private static final String TAG_AIRPORT_PICKER = "TAG_AIRPORT_PICKER";
-	private static final String TAG_DATE_PICKER = "TAG_DATE_PICKER";
-	private static final String TAG_PASSENGER_PICKER = "TAG_PASSENGER_PICKER";
-
-	private static final String INSTANCE_FINISHED_SEARCH = "INSTANCE_FINISHED_SEARCH";
-
-	// Controls the ratio of how large a selected EditText should take up
-	// 1 == takes up the full size, 0 == takes up 50%.
-	private static final float EDITTEXT_EXPANSION_RATIO = .25f;
-
-	private View mFocusStealer;
-	private LinearLayout mAirportsContainer;
-	private EditText mDepartureAirportEditText;
-	private EditText mArrivalAirportEditText;
-	private Button mDatesButton;
-	private View mPassengersButton;
-
-	// Indicates whether we've finished a request to the server.
-	// We need this because otherwise we cannot tell the difference
-	// between a request that timed out and failed vs. a request
-	// that had an actual error.
-	private boolean mFinishedSearch = false;
+	private FlightSearchParamsFragment mSearchParamsFragment;
 
 	private HockeyPuck mHockeyPuck;
 
@@ -80,112 +32,26 @@ public class FlightSearchActivity extends SherlockFragmentActivity implements Ai
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		View root = findViewById(android.R.id.content);
+		root.setBackgroundResource(R.drawable.bg_search_nyc);
+
+		if (savedInstanceState == null) {
+			mSearchParamsFragment = FlightSearchParamsFragment.newInstance(Db.getFlightSearch().getSearchParams(),
+					false);
+			getSupportFragmentManager().beginTransaction().add(android.R.id.content, mSearchParamsFragment,
+					FlightSearchParamsFragment.TAG).commit();
+		}
+		else {
+			mSearchParamsFragment = Ui.findSupportFragment(this, FlightSearchParamsFragment.TAG);
+		}
+
 		if (savedInstanceState == null && getIntent().getBooleanExtra(EXTRA_DATA_EXPIRED, false)) {
 			SimpleSupportDialogFragment df = SimpleSupportDialogFragment.newInstance(null,
 					getString(R.string.error_data_expired));
 			df.show(getSupportFragmentManager(), "dataExpiredDf");
 		}
 
-		if (savedInstanceState != null) {
-			mFinishedSearch = savedInstanceState.getBoolean(INSTANCE_FINISHED_SEARCH, false);
-		}
-
 		setTitle(R.string.search_flights);
-
-		// Inflate the views
-		setContentView(R.layout.activity_flight_search);
-
-		mFocusStealer = Ui.findView(this, R.id.focus_stealer);
-
-		// Configure airport pickers
-		mAirportsContainer = Ui.findView(this, R.id.airports_container);
-		mDepartureAirportEditText = Ui.findView(this, R.id.departure_airport_edit_text);
-		mArrivalAirportEditText = Ui.findView(this, R.id.arrival_airport_edit_text);
-
-		OnFocusChangeListener airportFocusChangeListener = new OnFocusChangeListener() {
-			@Override
-			public void onFocusChange(View v, boolean hasFocus) {
-				if (hasFocus) {
-					setFragment(TAG_AIRPORT_PICKER);
-
-					if (v == mDepartureAirportEditText) {
-						setAirportPickerFilter(mDepartureAirportEditText.getText());
-					}
-					else {
-						setAirportPickerFilter(mArrivalAirportEditText.getText());
-					}
-
-					expandAirportEditText(v);
-				}
-				else {
-					// If we're losing focus, set the airport code to what was in the field
-					//
-					// Note: this will not work if the EditTexts start displaying text different from
-					// the airportCode.  But I expect the design of this page will change at some point anyhow.
-					FlightSearchParams params = Db.getFlightSearch().getSearchParams();
-					if (v == mDepartureAirportEditText) {
-						params.setDepartureAirportCode(mDepartureAirportEditText.getText().toString().toUpperCase());
-					}
-					else {
-						params.setArrivalAirportCode(mArrivalAirportEditText.getText().toString().toUpperCase());
-					}
-				}
-			}
-		};
-
-		mDepartureAirportEditText.setOnFocusChangeListener(airportFocusChangeListener);
-		mArrivalAirportEditText.setOnFocusChangeListener(airportFocusChangeListener);
-
-		mDepartureAirportEditText.setOnEditorActionListener(new OnEditorActionListener() {
-			@Override
-			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-				if (actionId == EditorInfo.IME_ACTION_NEXT) {
-					// Treat it like a click
-					onAirportClick(mDepartureAirportEditText.getText().toString());
-					return true;
-				}
-
-				return false;
-			}
-		});
-
-		mArrivalAirportEditText.setOnEditorActionListener(new OnEditorActionListener() {
-			@Override
-			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-				if (actionId == EditorInfo.IME_ACTION_NEXT) {
-					// Treat it like a click
-					onAirportClick(mArrivalAirportEditText.getText().toString());
-					return true;
-				}
-
-				return false;
-			}
-		});
-
-		// Configure date button
-		mDatesButton = Ui.findView(this, R.id.dates_button);
-		mDatesButton.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				clearEditTextFocus();
-
-				setFragment(TAG_DATE_PICKER);
-			}
-		});
-
-		// Configure passenger button
-		mPassengersButton = Ui.findView(this, R.id.passengers_button);
-		mPassengersButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				clearEditTextFocus();
-
-				setFragment(TAG_PASSENGER_PICKER);
-			}
-		});
-
-		updateDateButton();
 
 		// HockeyApp init
 		mHockeyPuck = new HockeyPuck(this, Codes.HOCKEY_APP_ID, !AndroidUtils.isRelease(this));
@@ -196,221 +62,15 @@ public class FlightSearchActivity extends SherlockFragmentActivity implements Ai
 	protected void onResume() {
 		super.onResume();
 
-		mDepartureAirportEditText.addTextChangedListener(mAirportTextWatcher);
-		mArrivalAirportEditText.addTextChangedListener(mAirportTextWatcher);
-
 		//HockeyApp crash
 		mHockeyPuck.onResume();
-	}
-
-	@Override
-	protected void onPause() {
-		super.onPause();
-
-		mDepartureAirportEditText.removeTextChangedListener(mAirportTextWatcher);
-		mArrivalAirportEditText.removeTextChangedListener(mAirportTextWatcher);
 	}
 
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
 
-		outState.putBoolean(INSTANCE_FINISHED_SEARCH, mFinishedSearch);
-
 		mHockeyPuck.onSaveInstanceState(outState);
-	}
-
-	//////////////////////////////////////////////////////////////////////////
-	// View control
-
-	// It is surprisingly difficult to get rid of an EditText's focus.  This
-	// method should be called when you want to ensure that the EditTexts on
-	// the screen no longer have focus.
-	private void clearEditTextFocus() {
-		EditText textWithFocus = null;
-
-		if (mDepartureAirportEditText.hasFocus()) {
-			textWithFocus = mDepartureAirportEditText;
-		}
-		else if (mArrivalAirportEditText.hasFocus()) {
-			textWithFocus = mArrivalAirportEditText;
-		}
-
-		if (textWithFocus != null) {
-			mFocusStealer.requestFocus();
-
-			InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-			imm.hideSoftInputFromWindow(textWithFocus.getWindowToken(), 0);
-		}
-
-		resetAirportEditTexts();
-	}
-
-	private static final String DATE_FORMAT = "MMM d";
-
-	private void updateDateButton() {
-		FlightSearchParams params = Db.getFlightSearch().getSearchParams();
-
-		Calendar dateStart = params.getDepartureDate() == null ? null : params.getDepartureDate().getCalendar();
-		Calendar dateEnd = params.getReturnDate() == null ? null : params.getReturnDate().getCalendar();
-
-		if (dateStart == null && dateEnd == null) {
-			mDatesButton.setText(getString(R.string.dates_prompt));
-		}
-		else if (dateStart != null && dateEnd == null) {
-			CharSequence start = DateFormat.format(DATE_FORMAT, dateStart);
-			mDatesButton.setText(getString(R.string.one_way_TEMPLATE, start));
-		}
-		else if (dateStart != null && dateEnd != null) {
-			CharSequence start = DateFormat.format(DATE_FORMAT, dateStart);
-			CharSequence end = DateFormat.format(DATE_FORMAT, dateEnd);
-			mDatesButton.setText(getString(R.string.round_trip_TEMPLATE, start, end));
-		}
-	}
-
-	private void expandAirportEditText(View focusView) {
-		final View expandingView;
-		final View shrinkingView;
-
-		if (focusView == mDepartureAirportEditText) {
-			expandingView = mDepartureAirportEditText;
-			shrinkingView = mArrivalAirportEditText;
-		}
-		else {
-			expandingView = mArrivalAirportEditText;
-			shrinkingView = mDepartureAirportEditText;
-		}
-
-		// Show an animation to expand/collapse one or the other.
-		final LinearLayout.LayoutParams expandingLayoutParams = (LinearLayout.LayoutParams) expandingView
-				.getLayoutParams();
-		final LinearLayout.LayoutParams shrinkingLayoutParams = (LinearLayout.LayoutParams) shrinkingView
-				.getLayoutParams();
-		final float halfWeight = mAirportsContainer.getWeightSum() / 2;
-		final float startVal = expandingLayoutParams.weight - halfWeight;
-		final float endVal = halfWeight * EDITTEXT_EXPANSION_RATIO;
-
-		ValueAnimator anim = ValueAnimator.ofFloat(startVal, endVal);
-		anim.addUpdateListener(new AnimatorUpdateListener() {
-			@Override
-			public void onAnimationUpdate(ValueAnimator animator) {
-				float val = (Float) animator.getAnimatedValue();
-				expandingLayoutParams.weight = halfWeight + val;
-				shrinkingLayoutParams.weight = halfWeight - val;
-				mAirportsContainer.requestLayout();
-			}
-		});
-		anim.setDuration(getResources().getInteger(android.R.integer.config_mediumAnimTime));
-		anim.start();
-	}
-
-	private void resetAirportEditTexts() {
-		// Animate both views back to 50/50
-		final float halfWeight = mAirportsContainer.getWeightSum() / 2;
-
-		final LinearLayout.LayoutParams expandingLayoutParams;
-		final LinearLayout.LayoutParams shrinkingLayoutParams;
-
-		if (((LinearLayout.LayoutParams) mDepartureAirportEditText.getLayoutParams()).weight > halfWeight) {
-			expandingLayoutParams = (LinearLayout.LayoutParams) mArrivalAirportEditText.getLayoutParams();
-			shrinkingLayoutParams = (LinearLayout.LayoutParams) mDepartureAirportEditText.getLayoutParams();
-		}
-		else {
-			expandingLayoutParams = (LinearLayout.LayoutParams) mDepartureAirportEditText.getLayoutParams();
-			shrinkingLayoutParams = (LinearLayout.LayoutParams) mArrivalAirportEditText.getLayoutParams();
-		}
-
-		float startVal = expandingLayoutParams.weight - halfWeight;
-
-		// If they weren't already focused, don't bother with this animation
-		if (Math.abs(startVal) < .001) {
-			return;
-		}
-
-		ValueAnimator anim = ValueAnimator.ofFloat(expandingLayoutParams.weight - halfWeight, 0);
-		anim.addUpdateListener(new AnimatorUpdateListener() {
-			@Override
-			public void onAnimationUpdate(ValueAnimator animator) {
-				float val = (Float) animator.getAnimatedValue();
-				expandingLayoutParams.weight = halfWeight + val;
-				shrinkingLayoutParams.weight = halfWeight - val;
-				mAirportsContainer.requestLayout();
-			}
-		});
-		anim.setDuration(getResources().getInteger(android.R.integer.config_mediumAnimTime));
-		anim.start();
-	}
-
-	//////////////////////////////////////////////////////////////////////////
-	// Fragment control
-
-	/**
-	 * Sets the currently displayed Fragment in the content pane.
-	 * 
-	 * The tag determines which Fragment is shown.  If it's null,
-	 * then all Fragments will be detached from the content pane.
-	 * 
-	 * If the tag Fragment is already being displayed, nothing will
-	 * happen.  
-	 * 
-	 * @param tag the fragment to display, or null to display none
-	 * @return the Fragment to be loaded into the content pane
-	 */
-	private Fragment setFragment(String tag) {
-		FragmentManager fm = getSupportFragmentManager();
-
-		Fragment newFragment = fm.findFragmentByTag(tag);
-		Fragment currentFragment = fm.findFragmentById(R.id.content_frame);
-
-		// It's already on the correct fragment, don't do anything
-		if (newFragment != null && currentFragment != null && newFragment == currentFragment) {
-			return currentFragment;
-		}
-
-		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-
-		ft.setCustomAnimations(R.anim.slide_up, R.anim.slide_down);
-
-		if (currentFragment != null) {
-			// if fragment to be detached is the date picker, enable a convenience interaction mode for the future
-			if (currentFragment.getTag().equals(TAG_DATE_PICKER)) {
-				((CalendarDialogFragment) currentFragment).setOneWayResearchMode(true);
-			}
-			ft.detach(currentFragment);
-		}
-
-		if (tag != null) {
-			if (newFragment == null) {
-				if (tag.equals(TAG_AIRPORT_PICKER)) {
-					newFragment = new AirportPickerFragment();
-				}
-				else if (tag.equals(TAG_DATE_PICKER)) {
-					FlightSearchParams params = Db.getFlightSearch().getSearchParams();
-					CalendarDialogFragment fragment;
-
-					Calendar departureDate = params.getDepartureDate() == null ? null : params.getDepartureDate()
-							.getCalendar();
-					Calendar returnDate = params.getReturnDate() == null ? null : params.getReturnDate().getCalendar();
-
-					fragment = CalendarDialogFragment.newInstance(departureDate, returnDate);
-
-					fragment.setShowsDialog(false);
-					newFragment = fragment;
-				}
-				else if (tag.equals(TAG_PASSENGER_PICKER)) {
-					newFragment = new PassengerPickerFragment();
-				}
-
-				ft.add(R.id.content_frame, newFragment, tag);
-			}
-			else {
-				ft.attach(newFragment);
-			}
-		}
-
-		ft.commit();
-
-		return newFragment;
 	}
 
 	//////////////////////////////////////////////////////////////////////////
@@ -440,8 +100,8 @@ public class FlightSearchActivity extends SherlockFragmentActivity implements Ai
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case R.id.search:
+			Db.getFlightSearch().setSearchParams(mSearchParamsFragment.getSearchParams());
 			startActivity(new Intent(FlightSearchActivity.this, FlightSearchResultsActivity.class));
-			setFragment(null);
 			return true;
 		case R.id.settings:
 			Intent intent = new Intent(this, ExpediaBookingPreferenceActivity.class);
@@ -454,75 +114,5 @@ public class FlightSearchActivity extends SherlockFragmentActivity implements Ai
 		}
 
 		return super.onOptionsItemSelected(item);
-	}
-
-	//////////////////////////////////////////////////////////////////////////
-	// Airport textview config
-
-	private TextWatcher mAirportTextWatcher = new TextWatcher() {
-
-		@Override
-		public void onTextChanged(CharSequence s, int start, int before, int count) {
-			setAirportPickerFilter(s);
-		}
-
-		@Override
-		public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-			// Do nothing
-		}
-
-		@Override
-		public void afterTextChanged(Editable s) {
-			// Do nothing
-		}
-	};
-
-	private void setAirportPickerFilter(CharSequence s) {
-		AirportPickerFragment fragment = Ui.findSupportFragment(this, TAG_AIRPORT_PICKER);
-		if (fragment != null) {
-			fragment.filter(s);
-		}
-	}
-
-	//////////////////////////////////////////////////////////////////////////
-	// AirportPickerFragmentListener
-
-	@Override
-	public void onAirportClick(String airportCode) {
-		FlightSearchParams params = Db.getFlightSearch().getSearchParams();
-		if (mDepartureAirportEditText.hasFocus()) {
-			params.setDepartureAirportCode(airportCode);
-			mDepartureAirportEditText.setText(airportCode);
-			mArrivalAirportEditText.requestFocus();
-		}
-		else {
-			params.setArrivalAirportCode(airportCode);
-			mArrivalAirportEditText.setText(airportCode);
-
-			// Act like they just clicked the dates button
-			mDatesButton.performClick();
-		}
-	}
-
-	//////////////////////////////////////////////////////////////////////////
-	// CalendarDialogFragmentListener
-
-	@Override
-	public void onChangeDates(Calendar start, Calendar end) {
-		FlightSearchParams params = Db.getFlightSearch().getSearchParams();
-		if (start != null) {
-			params.setDepartureDate(new Date(start));
-		}
-		else {
-			params.setDepartureDate(null);
-		}
-
-		if (end != null) {
-			params.setReturnDate(new Date(end));
-		}
-		else {
-			params.setReturnDate(null);
-		}
-		updateDateButton();
 	}
 }
