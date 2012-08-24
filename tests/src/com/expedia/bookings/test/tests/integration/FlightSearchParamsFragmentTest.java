84f98cf28280dc637f011e1fec251647ec0b0ec9
Brad McManus <brad@mobiata.com>
2012-08-23 18:27:31 -0700

    591/598: Beef up FlightSearchParamsTest

--- tests/src/com/expedia/bookings/test/tests/integration/FlightSearchParamsFragmentTest.java
+++ tests/src/com/expedia/bookings/test/tests/integration/FlightSearchParamsFragmentTest.java
@@ -1,5 +1,6 @@
 package com.expedia.bookings.test.tests.integration;
 
+import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
@@ -32,7 +33,7 @@ public class FlightSearchParamsFragmentTest extends InstrumentationTestCase {
 	private Instrumentation mInstr;
 	private Instrumentation.ActivityMonitor mMonitor;
 
-	private FlightSearchActivity mActivity;
+	private Activity mActivity;
 
 	@Override
 	public void setUp() throws Exception {
@@ -40,7 +41,7 @@ public class FlightSearchParamsFragmentTest extends InstrumentationTestCase {
 		mInstr = getInstrumentation();
 
 		Intent intent = new Intent(Intent.ACTION_MAIN);
-		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 		intent.setClassName(mInstr.getTargetContext(), FlightSearchActivity.class.getName());
 
 		// register monitor for FlightSearchActivity
@@ -50,7 +51,7 @@ public class FlightSearchParamsFragmentTest extends InstrumentationTestCase {
 		mInstr.startActivitySync(intent);
 
 		// assert the activity gets launched
-		mActivity = (FlightSearchActivity) mInstr.waitForMonitorWithTimeout(mMonitor, 1000);
+		mActivity = mInstr.waitForMonitorWithTimeout(mMonitor, 1000);
 		assertNotNull(mActivity);
 		assertEquals(FlightSearchActivity.class, mActivity.getClass());
 
@@ -66,50 +67,48 @@ public class FlightSearchParamsFragmentTest extends InstrumentationTestCase {
 
 	@MediumTest
 	public void testFlightSearchBindsUiValuesToDb() {
-		// select 'DTW' for departure airport
-		String expectedDepartureAirportCode = "DTW";
-		InputUtils.selectAirport(this, mActivity, mInstr, mSolo, expectedDepartureAirportCode,
-				R.id.departure_airport_edit_text);
+		performFlightSearch("DTW", "ATL", 5, R.id.search, true);
+	}
 
-		// selected 'ATL' for arrival airport
-		String expectedArrivalAirportCode = "ATL";
-		InputUtils.selectAirport(this, mActivity, mInstr, mSolo, expectedArrivalAirportCode,
-				R.id.arrival_airport_edit_text);
+	@MediumTest
+	public void testFlightSearchBindsUiValuesToDbAndChangingValuesLaunchesNewCorrectSearch() {
+		performFlightSearch("DTW", "ATL", 4, R.id.search, true);
 
-		// click dates button so that the calendar appears
-		TouchUtils.clickView(this, Ui.findView(mActivity, R.id.dates_button));
+		mSolo.goBack();
 
-		// select a day 4 days in the future
-		Time expectedDay = CalendarTouchUtils.selectDay(mActivity, mSolo, 4, R.id.calendar_date_picker);
+		performFlightSearch("SFO", "SEA", 3, R.id.search, true);
+	}
 
-		// search so that the FlightSearchParamsFragment closes and saves params to Db
-		TouchUtils.clickView(this, Ui.findView(mActivity, R.id.search));
+	@MediumTest
+	public void testFlightSearchModificationsInlineReflectedInFlightSearchActivity() {
+		performFlightSearch("DTW", "JFK", 4, R.id.search, true);
 
-		// grab and assert values from FlightSearchParams (from Db)
-		FlightSearchParams params = Db.getFlightSearch().getSearchParams();
+		TouchUtils.clickView(this, Ui.findView(mSolo.getCurrentActivity(), R.id.menu_search));
 
-		assertEquals(expectedDepartureAirportCode, params.getDepartureAirportCode());
-		assertEquals(expectedArrivalAirportCode, params.getArrivalAirportCode());
-		assertEquals(expectedDay, new Time(params.getDepartureDate().getCalendar()));
+		String expectedAirport1 = "ATL";
+		String expectedAirport2 = "MSP";
+		performFlightSearch(expectedAirport1, expectedAirport2, 0, R.id.menu_search, false);
+
+		mSolo.goBack();
+
+		assertSearchParamsInUi(expectedAirport1, expectedAirport2);
 	}
 
 	@MediumTest
 	public void testFlightSearchParamsRetainsValuesOnRotation() {
 		// select 'DTW' for departure airport
 		String expectedDepartureAirportCode = "DTW";
-		InputUtils.selectAirport(this, mActivity, mInstr, mSolo, expectedDepartureAirportCode,
-				R.id.departure_airport_edit_text);
+		InputUtils.selectAirport(this, mInstr, mSolo, expectedDepartureAirportCode, R.id.departure_airport_edit_text);
 
 		// select 'ATL' for arrival airport
 		String expectedArrivalAirportCode = "ATL";
-		InputUtils.selectAirport(this, mActivity, mInstr, mSolo, expectedArrivalAirportCode,
-				R.id.arrival_airport_edit_text);
+		InputUtils.selectAirport(this, mInstr, mSolo, expectedArrivalAirportCode, R.id.arrival_airport_edit_text);
 
 		// click on dates button to engage calendar edit mode
 		TouchUtils.clickView(this, Ui.findView(mActivity, R.id.dates_button));
 
 		// click on day 3 days in future
-		Time expectedStartDay = CalendarTouchUtils.selectDay(mActivity, mSolo, 3, R.id.calendar_date_picker);
+		Time expectedStartDay = CalendarTouchUtils.selectDay(mSolo, 3, R.id.calendar_date_picker);
 
 		mInstr.waitForIdleSync();
 
@@ -125,6 +124,57 @@ public class FlightSearchParamsFragmentTest extends InstrumentationTestCase {
 
 		mSolo.sleep(1000);
 
+		assertSearchParamsInUi(expectedDepartureAirportCode, expectedArrivalAirportCode);
+
+		// assert the calendar text view retains the day on rotation
+		EditText datesButton = (EditText) mSolo.getView(R.id.dates_button);
+		String expectedMonthDay = Integer.toString(expectedStartDay.monthDay);
+
+		assertNotNull(datesButton);
+		assertTrue(datesButton.getText().toString().contains(expectedMonthDay));
+
+		// TODO: Fix this assertion. Unable to reproduce when interacting with app manually, must fix setUp/tearDown
+		// click calendar button to display cal
+		//		TouchUtils.clickView(this, mSolo.getView(R.id.dates_button));
+		// assert the calendar displays the correct day
+		//		CalendarDatePicker calendarDatePicker = (CalendarDatePicker) mSolo.getView(R.id.calendar_date_picker);
+		//		assertEquals(expectedStartDay, calendarDatePicker.getStartTime());
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// HELPER METHODS
+
+	/**
+	 * Helper method that will perform a flight search for the given airports and days
+	 * @param air1
+	 * @param air2
+	 * @param daysOffset
+	 */
+	private void performFlightSearch(String air1, String air2, int daysOffset, int searchId, boolean assertDate) {
+		InputUtils.selectAirport(this, mInstr, mSolo, air1, R.id.departure_airport_edit_text);
+
+		InputUtils.selectAirport(this, mInstr, mSolo, air2, R.id.arrival_airport_edit_text);
+
+		// click dates button so that the calendar appears
+		mSolo.clickOnView(mSolo.getView(R.id.dates_button));
+
+		// select a day 'daysOffset' in the future
+		Time expectedDay = CalendarTouchUtils.selectDay(mSolo, daysOffset, R.id.calendar_date_picker);
+
+		// search so that the FlightSearchParamsFragment closes and saves params to Db
+		TouchUtils.clickView(this, Ui.findView(mSolo.getCurrentActivity(), searchId));
+
+		// grab and assert values from FlightSearchParams (from Db)
+		FlightSearchParams params = Db.getFlightSearch().getSearchParams();
+
+		assertEquals(air1, params.getDepartureAirportCode());
+		assertEquals(air2, params.getArrivalAirportCode());
+		if (assertDate) {
+			assertEquals(expectedDay, new Time(params.getDepartureDate().getCalendar()));
+		}
+	}
+
+	private void assertSearchParamsInUi(String expectedDepartureAirportCode, String expectedArrivalAirportCode) {
 		// assert the departure view retains text after rotation
 		AlwaysFilterAutoCompleteTextView departureView = (AlwaysFilterAutoCompleteTextView) mSolo
 				.getView(R.id.departure_airport_edit_text);
@@ -136,21 +186,6 @@ public class FlightSearchParamsFragmentTest extends InstrumentationTestCase {
 				.getView(R.id.arrival_airport_edit_text);
 		assertNotNull(arrivalView);
 		assertTrue(arrivalView.getText().toString().contains(expectedArrivalAirportCode));
-
-		// assert the calendar text view retains the day on rotation
-		EditText datesButton = (EditText) mSolo.getView(R.id.dates_button);
-		String expectedMonthDay = Integer.toString(expectedStartDay.monthDay);
-
-		assertNotNull(datesButton);
-		assertTrue(datesButton.getText().toString().contains(expectedMonthDay));
-
-		// click calendar button to display cal
-		TouchUtils.clickView(this, mSolo.getView(R.id.dates_button));
-
-		// TODO: Fix this assertion. Unable to reproduce when interacting with app manually, must fix setUp/tearDown
-		// assert the calendar displays the correct day
-		//		CalendarDatePicker calendarDatePicker = (CalendarDatePicker) mSolo.getView(R.id.calendar_date_picker);
-		//		assertEquals(expectedStartDay, calendarDatePicker.getStartTime());
 	}
 
 }
