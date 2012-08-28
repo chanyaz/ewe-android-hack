2704b467c12e055614b6829b9b7e55f988a1261a
Joel Drotos <jdrotos@mobiata.com>
2012-08-28 16:38:42 -0500

    Removed the email field if a user is logged in

--- project/src/com/expedia/bookings/activity/FlightPaymentCreditCardActivity.java
+++ project/src/com/expedia/bookings/activity/FlightPaymentCreditCardActivity.java
@@ -6,6 +6,7 @@ import com.actionbarsherlock.view.MenuInflater;
 import com.expedia.bookings.R;
 import com.expedia.bookings.data.BillingInfo;
 import com.expedia.bookings.data.Db;
+import com.expedia.bookings.data.User;
 import com.expedia.bookings.model.YoYo;
 import com.expedia.bookings.section.ISectionEditable.SectionChangeListener;
 import com.expedia.bookings.section.SectionBillingInfo;
@@ -21,19 +22,24 @@ public class FlightPaymentCreditCardActivity extends SherlockActivity {
 	BillingInfo mBillingInfo;
 
 	SectionBillingInfo mSectionCreditCard;
-	
+
 	boolean mAttemptToLeaveMade = false;
 	YoYo mYoYo;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_flight_payment_creditcard);
+		if(User.isLoggedIn(this)){
+			setContentView(R.layout.activity_flight_payment_creditcard_logged_in);
+			Db.getBillingInfo().setEmail(Db.getUser().getEmail());//Set the billingInfo email address
+		}else{
+			setContentView(R.layout.activity_flight_payment_creditcard);
+		}
 
 		mBillingInfo = Db.getBillingInfo();
 
 		mSectionCreditCard = Ui.findView(this, R.id.creditcard_section);
-			
+
 		mYoYo = getIntent().getParcelableExtra(YoYo.TAG_YOYO);
 
 		mSectionCreditCard.addChangeListener(new SectionChangeListener() {
@@ -46,7 +52,6 @@ public class FlightPaymentCreditCardActivity extends SherlockActivity {
 			}
 		});
 
-		
 	}
 
 	@Override
@@ -58,16 +63,17 @@ public class FlightPaymentCreditCardActivity extends SherlockActivity {
 	public void bindAll() {
 		mSectionCreditCard.bind(mBillingInfo);
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-	    MenuInflater inflater = this.getSupportMenuInflater();
-	    if(mYoYo != null && mYoYo.isLast(this.getClass())){
-	    	inflater.inflate(R.menu.menu_done, menu);
-	    }else{
-	    	inflater.inflate(R.menu.menu_next, menu);
-	    }
-	    menu.findItem(R.id.menu_yoyo).getActionView().setOnClickListener(new OnClickListener(){
+		MenuInflater inflater = this.getSupportMenuInflater();
+		if (mYoYo != null && mYoYo.isLast(this.getClass())) {
+			inflater.inflate(R.menu.menu_done, menu);
+		}
+		else {
+			inflater.inflate(R.menu.menu_next, menu);
+		}
+		menu.findItem(R.id.menu_yoyo).getActionView().setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				mAttemptToLeaveMade = true;
@@ -75,9 +81,9 @@ public class FlightPaymentCreditCardActivity extends SherlockActivity {
 					Intent intent = mYoYo.generateIntent(FlightPaymentCreditCardActivity.this, getIntent());
 					startActivity(intent);
 				}
-			}		
+			}
 		});
-	    return true;
+		return true;
 	}
 
 }
