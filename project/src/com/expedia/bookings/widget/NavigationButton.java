8f9083be0158218e0b0f09eab11874c0c52b3395
Daniel Lew <dan@mobiata.com>
2012-08-28 16:54:45 -0500

    Improved NavigationButton internals
    Two main improvements:
    
    1. The custom View may now take up the full width of the action bar.
    
    2. Removed an unnecessary layer of LinearLayouts with a merge.

--- project/src/com/expedia/bookings/widget/NavigationButton.java
+++ project/src/com/expedia/bookings/widget/NavigationButton.java
@@ -7,6 +7,7 @@ import com.expedia.bookings.utils.Ui;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,37 +22,38 @@ import android.widget.TextView;
  *
  */
 public class NavigationButton extends LinearLayout {
-	
+
 	View mDropDownContent;
 	ViewGroup mSideViews;
 	ImageDropdown mImageDropdown;
 	Context mContext;
 	TextView mTitle;
-	
-	public static NavigationButton createNewInstance(Context context, int iconResId){
-		return createNewInstance(context,context.getResources().getDrawable(iconResId));
+
+	public static NavigationButton createNewInstance(Context context, int iconResId) {
+		return createNewInstance(context, context.getResources().getDrawable(iconResId));
 	}
-	
-	public static NavigationButton createNewInstance(Context context, Drawable icon){
+
+	public static NavigationButton createNewInstance(Context context, Drawable icon) {
 		NavigationButton nb = new NavigationButton(context);
 		nb.getImageDropdown().setImageDrawable(icon);
 		return nb;
 	}
-	
-	public static NavigationButton createNewInstanceAndAttach(Context context, int iconResId, ActionBar actionBar){
-		return createNewInstanceAndAttach(context,context.getResources().getDrawable(iconResId) ,actionBar);
+
+	public static NavigationButton createNewInstanceAndAttach(Context context, int iconResId, ActionBar actionBar) {
+		return createNewInstanceAndAttach(context, context.getResources().getDrawable(iconResId), actionBar);
 	}
-	public static NavigationButton createNewInstanceAndAttach(Context context, Drawable icon, ActionBar actionBar){
+
+	public static NavigationButton createNewInstanceAndAttach(Context context, Drawable icon, ActionBar actionBar) {
 		actionBar.setHomeButtonEnabled(false);
 		actionBar.setDisplayShowHomeEnabled(false);
 		actionBar.setDisplayShowTitleEnabled(false);
 		actionBar.setDisplayShowCustomEnabled(true);
-		
+
 		NavigationButton nb = createNewInstance(context, icon);
 		actionBar.setCustomView(nb);
 		return nb;
 	}
-	
+
 	private NavigationButton(Context context) {
 		super(context);
 		init(context);
@@ -68,69 +70,79 @@ public class NavigationButton extends LinearLayout {
 	}
 
 	private void init(Context context) {
+		setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+				LinearLayout.LayoutParams.MATCH_PARENT));
+		setOrientation(LinearLayout.HORIZONTAL);
+		setGravity(Gravity.CENTER_VERTICAL);
+
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        inflater.inflate(R.layout.widget_image_dropdown, this);
-		
+		inflater.inflate(R.layout.widget_image_dropdown, this);
+
 		mContext = context;
-		mTitle = Ui.findView(this,R.id.image_dropdown_title);
+		mTitle = Ui.findView(this, R.id.image_dropdown_title);
 		mImageDropdown = Ui.findView(this, R.id.image_dropdown);
-		
+
 		mDropDownContent = inflater.inflate(R.layout.snippet_nav_dropdown, null);
-		mSideViews = Ui.findView(this, R.id.image_dropdown_side_container);	
+		mSideViews = Ui.findView(this, R.id.image_dropdown_side_container);
 		mImageDropdown.setDropdownView(mDropDownContent);
 	}
-	
-	public void setDropdownAdapter(ListAdapter adapter){
+
+	public void setDropdownAdapter(ListAdapter adapter) {
 		ListView dropDownList = Ui.findView(mDropDownContent, R.id.nav_dropdown_list);
 		dropDownList.setAdapter(adapter);
 	}
-	
-	public void setDrawable(Drawable drawable){
+
+	public void setDrawable(Drawable drawable) {
 		mImageDropdown.setImageDrawable(drawable);
 	}
-	
-	public void addSideView(View view){
+
+	public void addSideView(View view) {
 		mSideViews.addView(view);
 	}
 
-	public void clearTitleAndCustomViews(){
+	public void clearTitleAndCustomViews() {
 		mSideViews.removeAllViews();
 		mTitle.setText("");
 		mTitle.setVisibility(View.GONE);
 	}
 
-	public String getTitle(){
+	public String getTitle() {
 		return (mTitle == null || mTitle.getText() == null) ? "" : mTitle.getText().toString();
 	}
-	
-	public ImageDropdown getImageDropdown(){
+
+	public ImageDropdown getImageDropdown() {
 		return mImageDropdown;
 	}
-	
+
+	public void setDisplayShowHomeEnabled(boolean enabled) {
+		mImageDropdown.setVisibility(enabled ? View.VISIBLE : View.GONE);
+	}
+
 	////////////////////////////////////////
 	// These methods replicate the methods found in the action bar
-	
-	public void setTitle(CharSequence title){
+
+	public void setTitle(CharSequence title) {
 		mTitle.setText(title);
 		mTitle.setVisibility(View.VISIBLE);
 	}
-	public void setTitle(int resid){
+
+	public void setTitle(int resid) {
 		mTitle.setText(resid);
 		mTitle.setVisibility(View.VISIBLE);
 	}
-	
-	public void setCustomView(int resId){
+
+	public void setCustomView(int resId) {
 		mSideViews.removeAllViews();
 		LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		inflater.inflate(resId, mSideViews);
 	}
-	
-	public void setCustomView(View view){
+
+	public void setCustomView(View view) {
 		mSideViews.removeAllViews();
 		mSideViews.addView(view);
 	}
-	
-	public void setCustomView(View view, LayoutParams layoutParams){
+
+	public void setCustomView(View view, LayoutParams layoutParams) {
 		mSideViews.removeAllViews();
 		mSideViews.addView(view, layoutParams);
 	}
