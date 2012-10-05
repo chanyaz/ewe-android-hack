0bcab5b5a01e3ff146c651da9a5da31a248ba7b4
Daniel Lew <dan@mobiata.com>
2012-10-05 09:43:34 -0500

    F765: Added new flight search to conf page

--- project/src/com/expedia/bookings/activity/FlightConfirmationActivity.java
+++ project/src/com/expedia/bookings/activity/FlightConfirmationActivity.java
@@ -6,8 +6,11 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.MotionEvent;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 import com.expedia.bookings.R;
 import com.expedia.bookings.data.Db;
+import com.expedia.bookings.data.FlightSearchParams;
 import com.expedia.bookings.fragment.BlurredBackgroundFragment;
 import com.expedia.bookings.fragment.FlightConfirmationFragment;
 import com.expedia.bookings.tracking.OmnitureTracking;
@@ -16,10 +19,14 @@ import com.expedia.bookings.utils.Ui;
 import com.expedia.bookings.widget.NavigationButton;
 import com.expedia.bookings.widget.NavigationDropdownAdapter;
 import com.expedia.bookings.widget.NavigationDropdownAdapter.NoOpButton;
+import com.mobiata.android.Log;
+import com.mobiata.android.json.JSONUtils;
 
 public class FlightConfirmationActivity extends SherlockFragmentActivity {
 
-	private static final boolean QUICKLAUNCH = false;
+	private static final boolean QUICKLAUNCH = true;
+
+	private static final int REQUEST_CODE_SEARCH_PARAMS = 1;
 
 	private BlurredBackgroundFragment mBgFragment;
 
@@ -91,5 +98,46 @@ public class FlightConfirmationActivity extends SherlockFragmentActivity {
 		if (!ActionBarNavUtils.removePopupDropdownIfNecessaryOnBackPressed(mNavButton)) {
 			super.onBackPressed();
 		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (requestCode == REQUEST_CODE_SEARCH_PARAMS && resultCode == RESULT_OK) {
+			Log.i("New search requested");
+
+			FlightSearchParams params = JSONUtils.getJSONable(data, FlightSearchOverlayActivity.EXTRA_SEARCH_PARAMS,
+					FlightSearchParams.class);
+			Db.getFlightSearch().setSearchParams(params);
+
+			Intent intent = new Intent(this, FlightSearchResultsActivity.class);
+			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent);
+
+			// Make sure to finish this class, in case 
+			// the user launched directly to the conf page
+			finish();
+		}
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+	// Action bar
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getSupportMenuInflater().inflate(R.menu.menu_flight_confirmation, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_search:
+			Intent intent = new Intent(this, FlightSearchOverlayActivity.class);
+			startActivityForResult(intent, REQUEST_CODE_SEARCH_PARAMS);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
