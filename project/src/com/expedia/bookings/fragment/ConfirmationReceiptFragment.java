56dcd6329c9f4ce5806d8456517c42de9fc20b02
Doug Melton <doug@mobiata.com>
2012-10-08 14:03:06 -0700

    H582: Fixed a tablet-only NPE situation

--- project/src/com/expedia/bookings/fragment/ConfirmationReceiptFragment.java
+++ project/src/com/expedia/bookings/fragment/ConfirmationReceiptFragment.java
@@ -47,8 +47,10 @@ public class ConfirmationReceiptFragment extends Fragment {
 
 		// Font face can't be set in xml
 		TextView enjoyYourStay = Ui.findView(receipt, R.id.text_enjoy_your_stay);
-		final Typeface type = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto-Light.ttf");
-		enjoyYourStay.setTypeface(type);
+		if (enjoyYourStay != null) {
+			final Typeface type = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto-Light.ttf");
+			enjoyYourStay.setTypeface(type);
+		}
 
 		TextView contactView = Ui.findView(receipt, R.id.contact_text_view);
 		String contactText = ConfirmationUtils.determineContactText(getActivity());
