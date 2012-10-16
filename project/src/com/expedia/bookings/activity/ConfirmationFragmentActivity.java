7b5e76aba121bbb3f9640cb699faec52e1f31fad
Daniel Lew <dan@mobiata.com>
2012-10-16 10:56:17 -0500

    Re-organized ConfirmationUtils
    - Moved methods out that were only being used in one place
    
    - Moved all saving methods into one location

--- project/src/com/expedia/bookings/activity/ConfirmationFragmentActivity.java
+++ project/src/com/expedia/bookings/activity/ConfirmationFragmentActivity.java
@@ -1,12 +1,18 @@
 package com.expedia.bookings.activity;
 
-import org.json.JSONObject;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.text.Html;
+import android.text.TextUtils;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentMapActivity;
@@ -17,18 +23,23 @@ import com.expedia.bookings.data.BillingInfo;
 import com.expedia.bookings.data.BookingResponse;
 import com.expedia.bookings.data.Codes;
 import com.expedia.bookings.data.Db;
+import com.expedia.bookings.data.Money;
+import com.expedia.bookings.data.Policy;
 import com.expedia.bookings.data.Property;
 import com.expedia.bookings.data.Rate;
+import com.expedia.bookings.data.RateBreakdown;
 import com.expedia.bookings.data.SearchParams;
 import com.expedia.bookings.fragment.BookingConfirmationFragment.BookingConfirmationFragmentListener;
 import com.expedia.bookings.fragment.SimpleSupportDialogFragment;
 import com.expedia.bookings.tracking.Tracker;
+import com.expedia.bookings.tracking.TrackingUtils;
+import com.expedia.bookings.utils.CalendarUtils;
 import com.expedia.bookings.utils.ConfirmationUtils;
 import com.expedia.bookings.utils.DebugMenu;
+import com.expedia.bookings.utils.StrUtils;
 import com.mobiata.android.Log;
-import com.mobiata.android.json.JSONUtils;
+import com.mobiata.android.SocialUtils;
 import com.mobiata.android.util.AndroidUtils;
-import com.mobiata.android.util.IoUtils;
 
 public class ConfirmationFragmentActivity extends SherlockFragmentMapActivity implements
 		BookingConfirmationFragmentListener {
@@ -51,7 +62,7 @@ public class ConfirmationFragmentActivity extends SherlockFragmentMapActivity im
 		if (savedInstanceState == null) {
 			if (ConfirmationUtils.hasSavedConfirmationData(this)) {
 				// Load saved data from disk
-				if (!loadSavedConfirmationData()) {
+				if (!ConfirmationUtils.loadSavedConfirmationData(this)) {
 					// If we failed to load the saved confirmation data, we should
 					// delete the file and go back (since we are only here if we were called
 					// directly from a startup).
@@ -165,27 +176,6 @@ public class ConfirmationFragmentActivity extends SherlockFragmentMapActivity im
 		return super.onOptionsItemSelected(item);
 	}
 
-	//////////////////////////////////////////////////////////////////////////////////////////
-	// Breadcrumb (reloading activity)
-
-	public boolean loadSavedConfirmationData() {
-		Log.i("Loading saved confirmation data...");
-		try {
-			JSONObject data = new JSONObject(IoUtils.readStringFromFile(ConfirmationUtils.CONFIRMATION_DATA_FILE, this));
-			Db.setSearchParams((SearchParams) JSONUtils.getJSONable(data, Codes.SEARCH_PARAMS, SearchParams.class));
-			Db.setSelectedProperty((Property) JSONUtils.getJSONable(data, Codes.PROPERTY, Property.class));
-			Db.setSelectedRate((Rate) JSONUtils.getJSONable(data, Codes.RATE, Rate.class));
-			Db.setBillingInfo((BillingInfo) JSONUtils.getJSONable(data, Codes.BILLING_INFO, BillingInfo.class));
-			Db.setBookingResponse((BookingResponse) JSONUtils.getJSONable(data, Codes.BOOKING_RESPONSE,
-					BookingResponse.class));
-			return true;
-		}
-		catch (Exception e) {
-			Log.e("Could not load ConfirmationFragmentActivity state.", e);
-			return false;
-		}
-	}
-
 	//////////////////////////////////////////////////////////////////////////
 	// Actions
 
@@ -226,15 +216,162 @@ public class ConfirmationFragmentActivity extends SherlockFragmentMapActivity im
 
 	@Override
 	public void onShareBooking() {
-		String contactText = ConfirmationUtils.determineContactText(this);
-		ConfirmationUtils.share(this, Db.getSearchParams(), Db.getSelectedProperty(), Db.getBookingResponse(),
-				Db.getBillingInfo(), Db.getSelectedRate(), Db.getCouponDiscountRate(), contactText);
+		share(Db.getSearchParams(), Db.getSelectedProperty(), Db.getBookingResponse(),
+				Db.getBillingInfo(), Db.getSelectedRate(), Db.getCouponDiscountRate());
 	}
 
 	@Override
 	public void onShowOnMap() {
 		Tracker.trackViewOnMap(this);
-		startActivity(ConfirmationUtils.generateIntentToShowPropertyOnMap(Db.getSelectedProperty()));
+
+		Intent newIntent = new Intent(Intent.ACTION_VIEW);
+		String queryAddress = StrUtils.formatAddress(Db.getSelectedProperty().getLocation()).replace("\n", " ");
+		newIntent.setData(Uri.parse("geo:0,0?q=" + queryAddress));
+		startActivity(newIntent);
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+	// Sharing
+
+	private void share(SearchParams searchParams, Property property, BookingResponse bookingResponse,
+			BillingInfo billingInfo, Rate rate, Rate discountRate) {
+		Context context = this;
+		Resources res = getResources();
+
+		DateFormat dateFormatter = new SimpleDateFormat("MM/dd");
+		dateFormatter.setTimeZone(CalendarUtils.getFormatTimeZone());
+		DateFormat fullDateFormatter = android.text.format.DateFormat.getMediumDateFormat(context);
+		fullDateFormatter.setTimeZone(CalendarUtils.getFormatTimeZone());
+		DateFormat dayFormatter = new SimpleDateFormat("EEE");
+		dayFormatter.setTimeZone(CalendarUtils.getFormatTimeZone());
+
+		Date checkIn = searchParams.getCheckInDate().getTime();
+		Date checkOut = searchParams.getCheckOutDate().getTime();
+
+		// Create the subject
+		String dateStart = dateFormatter.format(checkIn);
+		String dateEnd = dateFormatter.format(checkOut);
+		String subject = context.getString(R.string.share_subject_template, property.getName(), dateStart, dateEnd);
+
+		// Create the body 
+		StringBuilder body = new StringBuilder();
+		body.append(context.getString(R.string.share_body_start));
+		body.append("\n\n");
+
+		body.append(property.getName());
+		body.append("\n");
+		body.append(StrUtils.formatAddress(property.getLocation()));
+		body.append("\n\n");
+
+		if (!TextUtils.isEmpty(bookingResponse.getHotelConfNumber())) {
+			appendLabelValue(context, body, R.string.confirmation_number, bookingResponse.getHotelConfNumber());
+			body.append("\n");
+		}
+		appendLabelValue(context, body, R.string.itinerary_number, bookingResponse.getItineraryId());
+		body.append("\n\n");
+
+		appendLabelValue(context, body, R.string.name,
+				context.getString(R.string.name_template, billingInfo.getFirstName(), billingInfo.getLastName()));
+		body.append("\n");
+		appendLabelValue(context, body, R.string.CheckIn,
+				dayFormatter.format(checkIn) + ", " + fullDateFormatter.format(checkIn));
+		body.append("\n");
+		appendLabelValue(context, body, R.string.CheckOut,
+				dayFormatter.format(checkOut) + ", " + fullDateFormatter.format(checkOut));
+		body.append("\n");
+		int numDays = searchParams.getStayDuration();
+		appendLabelValue(context, body, R.string.stay_duration,
+				res.getQuantityString(R.plurals.length_of_stay, numDays, numDays));
+		body.append("\n\n");
+
+		appendLabelValue(context, body, R.string.room_type, Html.fromHtml(rate.getRoomDescription()).toString());
+		body.append("\n");
+		appendLabelValue(context, body, R.string.bed_type, rate.getRatePlanName());
+		body.append("\n");
+		appendLabelValue(context, body, R.string.adults, searchParams.getNumAdults() + "");
+		body.append("\n");
+		appendLabelValue(context, body, R.string.children, searchParams.getNumChildren() + "");
+		body.append("\n\n");
+
+		if (rate.getRateBreakdownList() != null) {
+			for (RateBreakdown breakdown : rate.getRateBreakdownList()) {
+				Date date = breakdown.getDate().getCalendar().getTime();
+				String dateStr = dayFormatter.format(date) + ", " + fullDateFormatter.format(date);
+				Money amount = breakdown.getAmount();
+				if (amount.isZero()) {
+					appendLabelValue(body, context.getString(R.string.room_rate_template, dateStr),
+							context.getString(R.string.free));
+				}
+				else {
+					appendLabelValue(body, context.getString(R.string.room_rate_template, dateStr),
+							amount.getFormattedMoney());
+				}
+				body.append("\n");
+			}
+			body.append("\n\n");
+		}
+
+		if (rate.getTotalAmountBeforeTax() != null) {
+			appendLabelValue(context, body, R.string.subtotal, rate.getTotalAmountBeforeTax().getFormattedMoney());
+			body.append("\n");
+		}
+
+		Money totalSurcharge = new Money(rate.getTotalSurcharge());
+		Money extraGuestFee = rate.getExtraGuestFee();
+		if (extraGuestFee != null) {
+			appendLabelValue(context, body, R.string.extra_guest_charge, extraGuestFee.getFormattedMoney());
+			body.append("\n");
+			if (totalSurcharge != null) {
+				totalSurcharge = totalSurcharge.copy();
+				totalSurcharge.subtract(extraGuestFee);
+			}
+		}
+		if (totalSurcharge != null) {
+			appendLabelValue(context, body, R.string.TaxesAndFees, totalSurcharge.getFormattedMoney());
+			body.append("\n");
+		}
+
+		if (discountRate != null) {
+			Money discount = new Money(discountRate.getTotalAmountAfterTax());
+			discount.subtract(rate.getTotalAmountAfterTax());
+			appendLabelValue(context, body, R.string.discount, discount.getFormattedMoney());
+			body.append("\n");
+			appendLabelValue(context, body, R.string.Total, discountRate.getTotalAmountAfterTax().getFormattedMoney());
+			body.append("\n");
+		}
+		else {
+			if (rate.getTotalAmountAfterTax() != null) {
+				body.append("\n");
+				appendLabelValue(context, body, R.string.Total, rate.getTotalAmountAfterTax().getFormattedMoney());
+			}
+		}
+
+		Policy cancellationPolicy = rate.getRateRules().getPolicy(Policy.TYPE_CANCEL);
+		if (cancellationPolicy != null) {
+			body.append("\n\n");
+			body.append(context.getString(R.string.cancellation_policy));
+			body.append("\n");
+			body.append(Html.fromHtml(cancellationPolicy.getDescription()));
+		}
+
+		body.append("\n\n");
+		body.append(ConfirmationUtils.determineContactText(this));
+
+		SocialUtils.email(context, subject, body.toString());
+
+		// Track the share
+		Log.d("Tracking \"CKO.CP.ShareBooking\" onClick");
+		TrackingUtils.trackSimpleEvent(context, null, null, "Shopper", "CKO.CP.ShareBooking");
+	}
+
+	private void appendLabelValue(Context context, StringBuilder sb, int labelStrId, String value) {
+		appendLabelValue(sb, context.getString(labelStrId), value);
+	}
+
+	private void appendLabelValue(StringBuilder sb, String label, String value) {
+		sb.append(label);
+		sb.append(": ");
+		sb.append(value);
 	}
 
 	//////////////////////////////////////////////////////////////////////////
--- project/src/com/expedia/bookings/utils/ConfirmationUtils.java
+++ project/src/com/expedia/bookings/utils/ConfirmationUtils.java
@@ -1,18 +1,11 @@
 package com.expedia.bookings.utils;
 
 import java.io.File;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import org.json.JSONObject;
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.res.Resources;
-import android.net.Uri;
 import android.text.Html;
-import android.text.TextUtils;
 import android.text.util.Linkify;
 import android.view.View;
 import android.widget.TextView;
@@ -21,151 +14,20 @@ import com.expedia.bookings.R;
 import com.expedia.bookings.data.BillingInfo;
 import com.expedia.bookings.data.BookingResponse;
 import com.expedia.bookings.data.Codes;
-import com.expedia.bookings.data.Money;
+import com.expedia.bookings.data.Db;
 import com.expedia.bookings.data.Policy;
 import com.expedia.bookings.data.Property;
 import com.expedia.bookings.data.Rate;
-import com.expedia.bookings.data.RateBreakdown;
 import com.expedia.bookings.data.SearchParams;
-import com.expedia.bookings.tracking.TrackingUtils;
 import com.mobiata.android.Log;
-import com.mobiata.android.SocialUtils;
+import com.mobiata.android.json.JSONUtils;
 import com.mobiata.android.util.AndroidUtils;
 import com.mobiata.android.util.IoUtils;
 
 public class ConfirmationUtils {
-	public static final String CONFIRMATION_DATA_FILE = "confirmation.dat";
-	public static final String CONFIRMATION_DATA_VERSION_FILE = "confirmation-version.dat";
 
-	public static void share(Context context, SearchParams searchParams, Property property,
-			BookingResponse bookingResponse, BillingInfo billingInfo, Rate rate, Rate discountRate, String contactText) {
-		Resources res = context.getResources();
-
-		DateFormat dateFormatter = new SimpleDateFormat("MM/dd");
-		dateFormatter.setTimeZone(CalendarUtils.getFormatTimeZone());
-		DateFormat fullDateFormatter = android.text.format.DateFormat.getMediumDateFormat(context);
-		fullDateFormatter.setTimeZone(CalendarUtils.getFormatTimeZone());
-		DateFormat dayFormatter = new SimpleDateFormat("EEE");
-		dayFormatter.setTimeZone(CalendarUtils.getFormatTimeZone());
-
-		Date checkIn = searchParams.getCheckInDate().getTime();
-		Date checkOut = searchParams.getCheckOutDate().getTime();
-
-		// Create the subject
-		String dateStart = dateFormatter.format(checkIn);
-		String dateEnd = dateFormatter.format(checkOut);
-		String subject = context.getString(R.string.share_subject_template, property.getName(), dateStart, dateEnd);
-
-		// Create the body 
-		StringBuilder body = new StringBuilder();
-		body.append(context.getString(R.string.share_body_start));
-		body.append("\n\n");
-
-		body.append(property.getName());
-		body.append("\n");
-		body.append(StrUtils.formatAddress(property.getLocation()));
-		body.append("\n\n");
-
-		if (!TextUtils.isEmpty(bookingResponse.getHotelConfNumber())) {
-			appendLabelValue(context, body, R.string.confirmation_number, bookingResponse.getHotelConfNumber());
-			body.append("\n");
-		}
-		appendLabelValue(context, body, R.string.itinerary_number, bookingResponse.getItineraryId());
-		body.append("\n\n");
-
-		appendLabelValue(context, body, R.string.name,
-				context.getString(R.string.name_template, billingInfo.getFirstName(), billingInfo.getLastName()));
-		body.append("\n");
-		appendLabelValue(context, body, R.string.CheckIn,
-				dayFormatter.format(checkIn) + ", " + fullDateFormatter.format(checkIn));
-		body.append("\n");
-		appendLabelValue(context, body, R.string.CheckOut,
-				dayFormatter.format(checkOut) + ", " + fullDateFormatter.format(checkOut));
-		body.append("\n");
-		int numDays = searchParams.getStayDuration();
-		appendLabelValue(context, body, R.string.stay_duration,
-				res.getQuantityString(R.plurals.length_of_stay, numDays, numDays));
-		body.append("\n\n");
-
-		appendLabelValue(context, body, R.string.room_type, Html.fromHtml(rate.getRoomDescription()).toString());
-		body.append("\n");
-		appendLabelValue(context, body, R.string.bed_type, rate.getRatePlanName());
-		body.append("\n");
-		appendLabelValue(context, body, R.string.adults, searchParams.getNumAdults() + "");
-		body.append("\n");
-		appendLabelValue(context, body, R.string.children, searchParams.getNumChildren() + "");
-		body.append("\n\n");
-
-		if (rate.getRateBreakdownList() != null) {
-			for (RateBreakdown breakdown : rate.getRateBreakdownList()) {
-				Date date = breakdown.getDate().getCalendar().getTime();
-				String dateStr = dayFormatter.format(date) + ", " + fullDateFormatter.format(date);
-				Money amount = breakdown.getAmount();
-				if (amount.isZero()) {
-					appendLabelValue(body, context.getString(R.string.room_rate_template, dateStr),
-							context.getString(R.string.free));
-				}
-				else {
-					appendLabelValue(body, context.getString(R.string.room_rate_template, dateStr),
-							amount.getFormattedMoney());
-				}
-				body.append("\n");
-			}
-			body.append("\n\n");
-		}
-
-		if (rate.getTotalAmountBeforeTax() != null) {
-			appendLabelValue(context, body, R.string.subtotal, rate.getTotalAmountBeforeTax().getFormattedMoney());
-			body.append("\n");
-		}
-
-		Money totalSurcharge = new Money(rate.getTotalSurcharge());
-		Money extraGuestFee = rate.getExtraGuestFee();
-		if (extraGuestFee != null) {
-			appendLabelValue(context, body, R.string.extra_guest_charge, extraGuestFee.getFormattedMoney());
-			body.append("\n");
-			if (totalSurcharge != null) {
-				totalSurcharge = totalSurcharge.copy();
-				totalSurcharge.subtract(extraGuestFee);
-			}
-		}
-		if (totalSurcharge != null) {
-			appendLabelValue(context, body, R.string.TaxesAndFees, totalSurcharge.getFormattedMoney());
-			body.append("\n");
-		}
-
-		if (discountRate != null) {
-			Money discount = new Money(discountRate.getTotalAmountAfterTax());
-			discount.subtract(rate.getTotalAmountAfterTax());
-			appendLabelValue(context, body, R.string.discount, discount.getFormattedMoney());
-			body.append("\n");
-			appendLabelValue(context, body, R.string.Total, discountRate.getTotalAmountAfterTax().getFormattedMoney());
-			body.append("\n");
-		}
-		else {
-			if (rate.getTotalAmountAfterTax() != null) {
-				body.append("\n");
-				appendLabelValue(context, body, R.string.Total, rate.getTotalAmountAfterTax().getFormattedMoney());
-			}
-		}
-
-		Policy cancellationPolicy = rate.getRateRules().getPolicy(Policy.TYPE_CANCEL);
-		if (cancellationPolicy != null) {
-			body.append("\n\n");
-			body.append(context.getString(R.string.cancellation_policy));
-			body.append("\n");
-			body.append(Html.fromHtml(cancellationPolicy.getDescription()));
-		}
-
-		body.append("\n\n");
-		body.append(contactText);
-
-		SocialUtils.email(context, subject, body.toString());
-
-		// Track the share
-		Log.d("Tracking \"CKO.CP.ShareBooking\" onClick");
-		TrackingUtils.trackSimpleEvent(context, null, null, "Shopper", "CKO.CP.ShareBooking");
-	}
+	private static final String CONFIRMATION_DATA_FILE = "confirmation.dat";
+	private static final String CONFIRMATION_DATA_VERSION_FILE = "confirmation-version.dat";
 
 	//////////////////////////////////////////////////////////////////////////////////////////
 	// Breadcrumb (reloading activity)
@@ -191,7 +53,26 @@ public class ConfirmationUtils {
 			return true;
 		}
 		catch (Exception e) {
-			Log.e("Could not save ConfirmationActivity state.", e);
+			Log.e("Could not save hotel confirmation data state.", e);
+			return false;
+		}
+	}
+
+	public static boolean loadSavedConfirmationData(Context context) {
+		Log.i("Loading saved confirmation data...");
+		try {
+			JSONObject data = new JSONObject(IoUtils.readStringFromFile(ConfirmationUtils.CONFIRMATION_DATA_FILE,
+					context));
+			Db.setSearchParams((SearchParams) JSONUtils.getJSONable(data, Codes.SEARCH_PARAMS, SearchParams.class));
+			Db.setSelectedProperty((Property) JSONUtils.getJSONable(data, Codes.PROPERTY, Property.class));
+			Db.setSelectedRate((Rate) JSONUtils.getJSONable(data, Codes.RATE, Rate.class));
+			Db.setBillingInfo((BillingInfo) JSONUtils.getJSONable(data, Codes.BILLING_INFO, BillingInfo.class));
+			Db.setBookingResponse((BookingResponse) JSONUtils.getJSONable(data, Codes.BOOKING_RESPONSE,
+					BookingResponse.class));
+			return true;
+		}
+		catch (Exception e) {
+			Log.e("Could not load hotel confirmation data state.", e);
 			return false;
 		}
 	}
@@ -207,12 +88,8 @@ public class ConfirmationUtils {
 		return savedConfResults.delete();
 	}
 
-	public static Intent generateIntentToShowPropertyOnMap(Property property) {
-		Intent newIntent = new Intent(Intent.ACTION_VIEW);
-		String queryAddress = StrUtils.formatAddress(property.getLocation()).replace("\n", " ");
-		newIntent.setData(Uri.parse("geo:0,0?q=" + queryAddress));
-		return newIntent;
-	}
+	//////////////////////////////////////////////////////////////////////////////////////////
+	// Miscellaneous
 
 	public static void determineCancellationPolicy(Rate rate, View view) {
 		Policy cancellationPolicy = rate.getRateRules().getPolicy(Policy.TYPE_CANCEL);
@@ -247,15 +124,4 @@ public class ConfirmationUtils {
 
 		contactView.setText(contactText);
 	}
-
-	private static void appendLabelValue(Context context, StringBuilder sb, int labelStrId, String value) {
-		appendLabelValue(sb, context.getString(labelStrId), value);
-	}
-
-	private static void appendLabelValue(StringBuilder sb, String label, String value) {
-		sb.append(label);
-		sb.append(": ");
-		sb.append(value);
-	}
-
 }
