b277c15329cc9486b692eb608edc6dc23b7d9eca
Doug Melton <doug@mobiata.com>
2012-10-15 17:48:28 -0700

    Abstracted MapImageView into its own class

--- project/src/com/expedia/bookings/fragment/HotelDetailsMiniMapFragment.java
+++ project/src/com/expedia/bookings/fragment/HotelDetailsMiniMapFragment.java
@@ -1,40 +1,27 @@
 package com.expedia.bookings.fragment;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Paint.Style;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.expedia.bookings.R;
 import com.expedia.bookings.data.Db;
 import com.expedia.bookings.data.Property;
 import com.expedia.bookings.data.SearchParams;
+import com.expedia.bookings.widget.MapImageView;
 import com.mobiata.android.ImageCache;
 import com.mobiata.android.Log;
 import com.mobiata.android.services.GoogleServices;
 import com.mobiata.android.services.GoogleServices.MapType;
+import com.mobiata.android.util.Ui;
 
 public class HotelDetailsMiniMapFragment extends Fragment {
 
-	private static int ZOOM = 13; // We want this to be different depending on dpi too
-	private static int PIXEL_COEFFICIENT = 8192 * 256; // 2^ZOOM * map pixel width
-	private static int DENSITY_SCALE_FACTOR = 1; // This has to be calculated at runtime
-
-	private ImageView mStaticMapImageView;
-	private String mStaticMapUri;
-
-	private StaticMapPoint mCenterPoint;
-	private StaticMapPoint mPoiPoint;
+	private MapImageView mStaticMapImageView;
 
 	private HotelMiniMapFragmentListener mListener;
 
@@ -56,7 +43,8 @@ public class HotelDetailsMiniMapFragment extends Fragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		mStaticMapImageView = new MapImageView(getActivity());
+		View view = inflater.inflate(R.layout.fragment_hotel_details_mini_map, container, false);
+		mStaticMapImageView = Ui.findView(view, R.id.mini_map);
 		mStaticMapImageView.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -64,143 +52,22 @@ public class HotelDetailsMiniMapFragment extends Fragment {
 			}
 		});
 
-		// High DPI screens should utilize scale=2 for this API
-		// https://developers.google.com/maps/documentation/staticmaps/
-		if (getResources().getDisplayMetrics().density > 1.5) {
-			DENSITY_SCALE_FACTOR = 2;
-			ZOOM = 12;
-			PIXEL_COEFFICIENT = 4096 * 256;
-		}
-
-		return mStaticMapImageView;
-	}
-
-	public void populateViews() {
 		SearchParams searchParams = Db.getSearchParams();
 		Property searchProperty = Db.getSelectedProperty();
-		if (searchParams == null || searchProperty == null) {
-			return;
-		}
-		double latitude = searchProperty.getLocation().getLatitude();
-		double longitude = searchProperty.getLocation().getLongitude();
-		mCenterPoint = new StaticMapPoint(latitude, longitude);
+		if (searchParams != null && searchProperty != null) {
+			mStaticMapImageView.setCenterPoint(searchProperty.getLocation());
 
-		// Fill in the POI / current location point appropriately
-		switch (searchParams.getSearchType()) {
-		case POI:
-		case ADDRESS:
-		case MY_LOCATION:
-			//TODO: for EH 1.6
-			//mPoiPoint = new StaticMapPoint(searchParams.getSearchLatitude(), searchParams.getSearchLongitude());
-			break;
-		}
-
-		int width = mStaticMapImageView.getWidth();
-		int height = mStaticMapImageView.getHeight();
-
-		mStaticMapUri = GoogleServices.getStaticMapUrl(width / DENSITY_SCALE_FACTOR, height / DENSITY_SCALE_FACTOR,
-				ZOOM, MapType.ROADMAP, latitude, longitude) + "&scale=" + DENSITY_SCALE_FACTOR;
-
-		ImageCache.loadImage(mStaticMapUri, mStaticMapImageView);
-	}
-
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-
-		if (getActivity().isFinishing() && mStaticMapUri != null) {
-			Log.d("Clearing out map image.");
-
-			ImageCache.removeImage(mStaticMapUri, true);
-		}
-	}
-
-	private class StaticMapPoint {
-		double latitudeDeg, longitudeDeg;
-		double latitudeRad, longitudeRad;
-
-		StaticMapPoint(double latitude, double longitude) {
-			latitudeDeg = latitude;
-			longitudeDeg = longitude;
-			latitudeRad = latitude * Math.PI / 180;
-			longitudeRad = longitude * Math.PI / 180;
-		}
-	}
-
-	private class MapImageView extends ImageView {
-		private int mCircleRadius;
-		private Paint mPaint;
-
-		public MapImageView(Context context) {
-			super(context);
-
-			Resources res = getResources();
-
-			int width = ViewGroup.LayoutParams.MATCH_PARENT;
-			int height = res.getDimensionPixelSize(R.dimen.hotel_details_map_visible_size) * 2;
-			mCircleRadius = res.getDimensionPixelSize(R.dimen.mini_map_circle_radius);
-			mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-
-			setScaleType(ScaleType.CENTER_CROP);
-			setLayoutParams(new ViewGroup.LayoutParams(width, height));
-			setImageResource(R.drawable.bg_gallery);
-		}
-
-		@Override
-		protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-			super.onLayout(changed, left, top, right, bottom);
-
-			if (changed && bottom - top > 0 && right - left > 0) {
-				populateViews();
+			// Fill in the POI / current location point appropriately
+			switch (searchParams.getSearchType()) {
+			case POI:
+			case ADDRESS:
+			case MY_LOCATION:
+				mStaticMapImageView.setPoiPoint(searchParams.getSearchLatitude(), searchParams.getSearchLongitude());
+				break;
 			}
 		}
 
-		@Override
-		protected void onDraw(Canvas canvas) {
-			super.onDraw(canvas);
-
-			// Draw the hotel marker
-			drawMarker(canvas, mCenterPoint);
-
-			// Poi or my location
-			if (mPoiPoint != null) {
-				drawMarker(canvas, mPoiPoint);
-			}
-		}
-
-		// Draws a marker on the appropriate place on the map, given its longitude and latitude
-		private void drawMarker(Canvas canvas, StaticMapPoint point) {
-			int x = getWidth() / 2;
-			int y = getHeight() / 2;
-
-			// The point of this math is to calculate the number of pixels between the center
-			// of the map and the requested latitude/longitude. It's not totally straightforward,
-			// due to the stretching of the map to fit it on a square (Mercator's projection).
-			// http://blog.whatclinic.com/2008/10/how-to-make-google-static-maps-interactive.html
-			if (point != mCenterPoint) {
-				double deltaXdeg = point.longitudeDeg - mCenterPoint.longitudeDeg;
-				double deltaXpx = (deltaXdeg / 360.0) * PIXEL_COEFFICIENT * DENSITY_SCALE_FACTOR;
-
-				double centerYrad = mCenterPoint.latitudeRad;
-				double centerYprojected = Math.log((1 + Math.sin(centerYrad)) / (1 - Math.sin(centerYrad))) / 2;
-				double pointYrad = point.latitudeRad;
-				double pointYprojected = Math.log((1 + Math.sin(pointYrad)) / (1 - Math.sin(pointYrad))) / 2;
-				double deltaYprojected = centerYprojected - pointYprojected;
-				double deltaYpx = (deltaYprojected / (2 * Math.PI)) * PIXEL_COEFFICIENT * DENSITY_SCALE_FACTOR;
-
-				x += deltaXpx;
-				y += deltaYpx;
-			}
-
-			mPaint.setStyle(Style.FILL);
-			mPaint.setARGB(0xC0, 0x47, 0x71, 0x99);
-			canvas.drawCircle(x, y, mCircleRadius, mPaint);
-
-			mPaint.setStyle(Style.STROKE);
-			mPaint.setStrokeWidth(2);
-			mPaint.setColor(Color.BLACK);
-			canvas.drawCircle(x, y, mCircleRadius, mPaint);
-		}
+		return view;
 	}
 
 	//////////////////////////////////////////////////////////////////////////
---
+++ project/src/com/expedia/bookings/widget/MapImageView.java
@@ -0,0 +1,181 @@
+package com.expedia.bookings.widget;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.expedia.bookings.R;
+import com.expedia.bookings.data.Location;
+import com.mobiata.android.ImageCache;
+import com.mobiata.android.Log;
+import com.mobiata.android.services.GoogleServices;
+import com.mobiata.android.services.GoogleServices.MapType;
+
+public class MapImageView extends ImageView {
+
+	private static int ZOOM = 13; // We want this to be different depending on dpi too
+	private static int PIXEL_COEFFICIENT = 8192 * 256; // 2^ZOOM * map pixel width
+	private static int DENSITY_SCALE_FACTOR = 1; // This has to be calculated at runtime
+
+	private StaticMapPoint mCenterPoint;
+	private StaticMapPoint mPoiPoint;
+
+	private String mStaticMapUri;
+
+	private int mCircleRadius;
+	private Paint mPaint;
+
+	public MapImageView(Context context) {
+		super(context);
+		initMapView();
+	}
+
+	public MapImageView(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
+
+	public MapImageView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+        
+		initMapView();
+	}
+
+	private void initMapView() {
+		Resources res = getResources();
+
+		// High DPI screens should utilize scale=2 for this API
+		// https://developers.google.com/maps/documentation/staticmaps/
+		if (res.getDisplayMetrics().density > 1.5) {
+			DENSITY_SCALE_FACTOR = 2;
+			ZOOM = 12;
+			PIXEL_COEFFICIENT = 4096 * 256;
+		}
+        
+		mCircleRadius = res.getDimensionPixelSize(R.dimen.mini_map_circle_radius);
+		mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+	}
+
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		super.onSizeChanged(w, h, oldw, oldh);
+		if (w > 0 && h > 0) {
+			regenerateUri();
+		}
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+
+		// Draw the hotel marker
+		drawMarker(canvas, mCenterPoint);
+
+		// Poi or my location
+		if (mPoiPoint != null) {
+			drawMarker(canvas, mPoiPoint);
+		}
+	}
+
+	@Override
+	protected void onDetachedFromWindow() {
+		Log.d("Clearing out map image.");
+
+		if (mStaticMapUri != null) {
+			ImageCache.removeImage(mStaticMapUri, true);
+		}
+		super.onDetachedFromWindow();
+	}
+
+	public void setCenterPoint(Location location) {
+		double latitude = location.getLatitude();
+		double longitude = location.getLongitude();
+		mCenterPoint = new StaticMapPoint(latitude, longitude);
+		regenerateUri();
+	}
+
+	private void regenerateUri() {
+		if (mCenterPoint == null) {
+			return;
+		}
+
+		double latitude = mCenterPoint.latitudeDeg;
+		double longitude = mCenterPoint.longitudeDeg;
+
+		int width = getWidth();
+		int height = getResources().getDimensionPixelSize(R.dimen.hotel_details_map_visible_size) * 2;
+
+		String oldUri = mStaticMapUri;
+
+		mStaticMapUri = GoogleServices.getStaticMapUrl(width / DENSITY_SCALE_FACTOR, height / DENSITY_SCALE_FACTOR,
+				ZOOM, MapType.ROADMAP, latitude, longitude) + "&scale=" + DENSITY_SCALE_FACTOR;
+
+		if (!mStaticMapUri.equals(oldUri)) {
+			ImageCache.loadImage(mStaticMapUri, this);
+
+			if (oldUri != null) {
+				Log.d("Clearing out map image.");
+				ImageCache.removeImage(oldUri, true);
+			}
+		}
+	}
+
+	public void setPoiPoint(double latitude, double longitude) {
+		mPoiPoint = new StaticMapPoint(latitude, longitude);
+	}
+
+	public String getUri() {
+		return mStaticMapUri;
+	}
+
+	// Draws a marker on the appropriate place on the map, given its longitude and latitude
+	private void drawMarker(Canvas canvas, StaticMapPoint point) {
+		int x = getWidth() / 2;
+		int y = getHeight() / 2;
+
+		// The point of this math is to calculate the number of pixels between the center
+		// of the map and the requested latitude/longitude. It's not totally straightforward,
+		// due to the stretching of the map to fit it on a square (Mercator's projection).
+		// http://blog.whatclinic.com/2008/10/how-to-make-google-static-maps-interactive.html
+		if (point != mCenterPoint) {
+			double deltaXdeg = point.longitudeDeg - mCenterPoint.longitudeDeg;
+			double deltaXpx = (deltaXdeg / 360.0) * PIXEL_COEFFICIENT * DENSITY_SCALE_FACTOR;
+
+			double centerYrad = mCenterPoint.latitudeRad;
+			double centerYprojected = Math.log((1 + Math.sin(centerYrad)) / (1 - Math.sin(centerYrad))) / 2;
+			double pointYrad = point.latitudeRad;
+			double pointYprojected = Math.log((1 + Math.sin(pointYrad)) / (1 - Math.sin(pointYrad))) / 2;
+			double deltaYprojected = centerYprojected - pointYprojected;
+			double deltaYpx = (deltaYprojected / (2 * Math.PI)) * PIXEL_COEFFICIENT * DENSITY_SCALE_FACTOR;
+
+			x += deltaXpx;
+			y += deltaYpx;
+		}
+
+		mPaint.setStyle(Style.FILL);
+		mPaint.setARGB(0xC0, 0x47, 0x71, 0x99);
+		canvas.drawCircle(x, y, mCircleRadius, mPaint);
+
+		mPaint.setStyle(Style.STROKE);
+		mPaint.setStrokeWidth(2);
+		mPaint.setColor(Color.BLACK);
+		canvas.drawCircle(x, y, mCircleRadius, mPaint);
+	}
+
+	private class StaticMapPoint {
+		double latitudeDeg, longitudeDeg;
+		double latitudeRad, longitudeRad;
+
+		StaticMapPoint(double latitude, double longitude) {
+			latitudeDeg = latitude;
+			longitudeDeg = longitude;
+			latitudeRad = latitude * Math.PI / 180;
+			longitudeRad = longitude * Math.PI / 180;
+		}
+	}
+
+}
