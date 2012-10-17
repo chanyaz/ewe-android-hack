9cc2cd4e6cbb33a7d8c4908f38e135e16ff56400
Daniel Lew <dan@mobiata.com>
2012-10-17 09:42:36 -0500

    F1013: Fixed infinite loop bug
    Was caused by not upgrading yet to NavUtils' navigation.

--- project/src/com/expedia/bookings/activity/FlightSearchResultsActivity.java
+++ project/src/com/expedia/bookings/activity/FlightSearchResultsActivity.java
@@ -355,10 +355,7 @@ public class FlightSearchResultsActivity extends SherlockFragmentActivity implem
 		switch (item.getItemId()) {
 		case android.R.id.home: {
 			// Push user back to search page if they hit the home button
-			Intent intent = new Intent(this, FlightSearchActivity.class);
-			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP + Intent.FLAG_ACTIVITY_CLEAR_TASK
-					+ Intent.FLAG_ACTIVITY_SINGLE_TOP);
-			startActivity(intent);
+			NavUtils.goToFlights(this);
 			return true;
 		}
 		case R.id.menu_select_sort_price:
