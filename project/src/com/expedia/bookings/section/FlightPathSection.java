fc62fedddb1f13143fcd273951afdf474c0dd5b4
Daniel Lew <dan@mobiata.com>
2012-10-18 13:46:54 -0500

    F1048: Pushed down icons 1dp

--- project/src/com/expedia/bookings/section/FlightPathSection.java
+++ project/src/com/expedia/bookings/section/FlightPathSection.java
@@ -2,18 +2,20 @@ package com.expedia.bookings.section;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.expedia.bookings.R;
 import com.expedia.bookings.utils.FontCache;
-import com.expedia.bookings.utils.StrUtils;
 import com.expedia.bookings.utils.FontCache.Font;
+import com.expedia.bookings.utils.StrUtils;
 import com.mobiata.android.util.Ui;
 import com.mobiata.flightlib.data.Flight;
 
 public class FlightPathSection extends LinearLayout {
 
+	private ImageView mIconImageView;
 	private TextView mInfoTextView;
 
 	public FlightPathSection(Context context, AttributeSet attrs) {
@@ -24,6 +26,8 @@ public class FlightPathSection extends LinearLayout {
 	public void onFinishInflate() {
 		super.onFinishInflate();
 
+		mIconImageView = Ui.findView(this, R.id.icon_image_view);
+
 		mInfoTextView = Ui.findView(this, R.id.info_text_view);
 		mInfoTextView.setTypeface(FontCache.getTypeface(Font.ROBOTO_LIGHT));
 	}
@@ -33,13 +37,13 @@ public class FlightPathSection extends LinearLayout {
 			mInfoTextView.setText(getResources().getString(R.string.depart_from_TEMPLATE,
 					StrUtils.formatWaypoint(flight.mOrigin)));
 
-			mInfoTextView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_departure_arrow_small, 0, 0, 0);
+			mIconImageView.setImageResource(R.drawable.ic_departure_arrow_small);
 		}
 		else {
 			mInfoTextView.setText(getResources().getString(R.string.arrive_at_TEMPLATE,
 					StrUtils.formatWaypoint(flight.mDestination)));
 
-			mInfoTextView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_return_arrow_small, 0, 0, 0);
+			mIconImageView.setImageResource(R.drawable.ic_return_arrow_small);
 		}
 	}
 }
