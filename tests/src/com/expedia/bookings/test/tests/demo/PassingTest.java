72f3e1bfc5a60df1317f032a4260d09e3074d0cc
Kevin Carpenter <kevc@mobiata.com>
2012-12-14 11:05:24 -0500

    Add automatically failing and passing tests for automation testing.

---
+++ tests/src/com/expedia/bookings/test/tests/demo/PassingTest.java
@@ -0,0 +1,49 @@
+package com.expedia.bookings.test.tests.demo;
+
+import android.content.res.Resources;
+import android.test.ActivityInstrumentationTestCase2;
+import android.util.DisplayMetrics;
+
+import com.expedia.bookings.activity.SearchActivity;
+import com.expedia.bookings.test.utils.HotelsRobotHelper;
+import com.jayway.android.robotium.solo.Solo;
+
+public class PassingTest extends ActivityInstrumentationTestCase2<SearchActivity> {
+
+	public PassingTest() { //Default constructor
+		super("com.expedia.bookings", SearchActivity.class);
+	}
+
+	private static final String TAG = "SearchTest";
+
+	private Solo mSolo;
+
+	private Resources mRes;
+	DisplayMetrics mMetric;
+	private HotelsRobotHelper mDriver;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		mSolo = new Solo(getInstrumentation(), getActivity());
+		//Log.configureLogging("ExpediaBookings", true);
+
+		mRes = getActivity().getBaseContext().getResources();
+		mMetric = mRes.getDisplayMetrics();
+		mDriver = new HotelsRobotHelper(mSolo, mRes);
+	}
+
+	public void testMethod() throws Exception {
+		mDriver.enterLog(TAG, "We're failing on purpose because failing is fun!");
+		mDriver.launchHotels();
+		mDriver.delay();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		//Robotium will finish all the activities that have been opened
+		mDriver.enterLog(TAG, "tearing down...");
+
+		mSolo.finishOpenedActivities();
+	}
+
+}
