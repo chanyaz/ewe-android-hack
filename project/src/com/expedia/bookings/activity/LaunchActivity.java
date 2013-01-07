0be44136ea4ee97de97d0df69e8da440ffb3f42a
Joel Drotos <jdrotos@mobiata.com>
2013-01-07 10:40:29 -0600

    Added viewpager for switching between waterfall and itin page

--- project/src/com/expedia/bookings/activity/LaunchActivity.java
+++ project/src/com/expedia/bookings/activity/LaunchActivity.java
@@ -1,7 +1,17 @@
 package com.expedia.bookings.activity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.ViewGroup;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
@@ -9,6 +19,7 @@ import com.actionbarsherlock.view.MenuItem;
 import com.expedia.bookings.R;
 import com.expedia.bookings.data.Codes;
 import com.expedia.bookings.data.Db;
+import com.expedia.bookings.fragment.ItinItemListFragment;
 import com.expedia.bookings.fragment.LaunchFragment;
 import com.expedia.bookings.tracking.OmnitureTracking;
 import com.expedia.bookings.utils.DebugMenu;
@@ -21,6 +32,10 @@ public class LaunchActivity extends SherlockFragmentActivity {
 	private static final int REQUEST_SETTINGS = 1;
 
 	private LaunchFragment mLaunchFragment;
+	private ItinItemListFragment mItinListFragment;
+
+	private PagerAdapter mPagerAdapter;
+	private ViewPager mViewPager;
 
 	private HockeyPuck mHockeyPuck;
 
@@ -33,14 +48,43 @@ public class LaunchActivity extends SherlockFragmentActivity {
 		setContentView(R.layout.activity_launch);
 		getWindow().setBackgroundDrawable(null);
 
-		if (savedInstanceState == null) {
+		mLaunchFragment = Ui.findSupportFragment(this, LaunchFragment.TAG);
+		if (mLaunchFragment == null) {
 			mLaunchFragment = LaunchFragment.newInstance();
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.content_container, mLaunchFragment, LaunchFragment.TAG).commit();
 		}
-		else {
-			mLaunchFragment = Ui.findSupportFragment(this, LaunchFragment.TAG);
+		mItinListFragment = Ui.findSupportFragment(this, ItinItemListFragment.TAG);
+		if (mItinListFragment == null) {
+			mItinListFragment = ItinItemListFragment.newInstance();
 		}
+
+		List<Fragment> frags = new ArrayList<Fragment>();
+		frags.add(mLaunchFragment);
+		frags.add(mItinListFragment);
+		mPagerAdapter = new PagerAdapter(getSupportFragmentManager(), frags);
+
+		mViewPager = Ui.findView(this, R.id.viewpager);
+		mViewPager.setAdapter(mPagerAdapter);
+		mViewPager.setOnPageChangeListener(new OnPageChangeListener() {
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+			}
+
+			@Override
+			public void onPageSelected(int arg0) {
+				if (arg0 == 0) {
+					gotoWaterfall();
+				}
+				else if (arg0 == 1) {
+					gotoItineraries();
+				}
+			}
+
+		});
 
 		// HockeyApp init
 		mHockeyPuck = new HockeyPuck(this, Codes.HOCKEY_APP_ID, !AndroidUtils.isRelease(this));
@@ -92,6 +136,22 @@ public class LaunchActivity extends SherlockFragmentActivity {
 
 	@Override
 	public boolean onPrepareOptionsMenu(Menu menu) {
+		if (menu != null) {
+			boolean itinButtonEnabled = true;
+			if (mViewPager != null && mViewPager.getCurrentItem() == 1) {
+				itinButtonEnabled = false;
+
+			}
+			else {
+				itinButtonEnabled = true;
+			}
+			MenuItem itinBtn = menu.findItem(R.id.itineraries);
+			if (itinBtn != null) {
+				itinBtn.setVisible(itinButtonEnabled);
+				itinBtn.setEnabled(itinButtonEnabled);
+			}
+		}
+
 		DebugMenu.onPrepareOptionsMenu(this, menu);
 
 		mHockeyPuck.onPrepareOptionsMenu(menu);
@@ -102,6 +162,12 @@ public class LaunchActivity extends SherlockFragmentActivity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
+		case android.R.id.home:
+			gotoWaterfall();
+			return true;
+		case R.id.itineraries:
+			gotoItineraries();
+			return true;
 		case R.id.settings:
 			Intent intent = new Intent(this, ExpediaBookingPreferenceActivity.class);
 			startActivityForResult(intent, REQUEST_SETTINGS);
@@ -118,5 +184,39 @@ public class LaunchActivity extends SherlockFragmentActivity {
 		}
 
 		return super.onOptionsItemSelected(item);
+	}
+
+	private void gotoWaterfall() {
+		this.getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+		this.getSupportActionBar().setHomeButtonEnabled(false);
+		mViewPager.setCurrentItem(0);
+		supportInvalidateOptionsMenu();
+	}
+
+	private void gotoItineraries() {
+		mViewPager.setCurrentItem(1);
+		this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		this.getSupportActionBar().setHomeButtonEnabled(true);
+		supportInvalidateOptionsMenu();
+	}
+
+	public class PagerAdapter extends FragmentPagerAdapter {
+
+		private List<Fragment> fragments;
+
+		public PagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+			super(fm);
+			this.fragments = fragments;
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			return this.fragments.get(position);
+		}
+
+		@Override
+		public int getCount() {
+			return this.fragments.size();
+		}
 	}
 }
