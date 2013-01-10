1e8fa85dc6ba055cb79376e827da9059d134b417
Daniel Lew <dan@mobiata.com>
2013-01-10 13:15:48 -0600

    Created DateTime object
    This represents a point in time in a simpler manner than storing a
    Calendar.

---
+++ project/src/com/expedia/bookings/data/DateTime.java
@@ -0,0 +1,62 @@
+package com.expedia.bookings.data;
+
+import java.util.Calendar;
+import java.util.SimpleTimeZone;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.mobiata.android.json.JSONable;
+
+/**
+ * A simple way to store a milliseconds-from-epoch + static timezone offset.
+ * 
+ * Not mutable; it wouldn't make sense to allow someone to change one
+ * field without the other. 
+ */
+public class DateTime implements JSONable {
+
+	private long mMillisFromEpoch;
+	private int mTzOffsetMillis;
+
+	// Cached for speed
+	private Calendar mCal;
+
+	public DateTime(long millisFromEpoch, int tzOffsetMillis) {
+		mMillisFromEpoch = millisFromEpoch;
+		mTzOffsetMillis = tzOffsetMillis;
+	}
+
+	public Calendar getCalendar() {
+		if (mCal == null) {
+			mCal = Calendar.getInstance();
+			mCal.setTimeInMillis(mMillisFromEpoch);
+			mCal.setTimeZone(new SimpleTimeZone(mTzOffsetMillis, "GMT"));
+		}
+
+		return mCal;
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+	// JSONable
+
+	@Override
+	public JSONObject toJson() {
+		try {
+			JSONObject obj = new JSONObject();
+			obj.putOpt("millisFromEpoch", mMillisFromEpoch);
+			obj.putOpt("tzOffsetMillis", mTzOffsetMillis);
+			return obj;
+		}
+		catch (JSONException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public boolean fromJson(JSONObject obj) {
+		mMillisFromEpoch = obj.optLong("millisFromEpoch");
+		mTzOffsetMillis = obj.optInt("tzOffsetMillis");
+		return true;
+	}
+}
