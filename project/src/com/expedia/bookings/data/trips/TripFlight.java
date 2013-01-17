57a8893ddaeb677ab16e4b325f1edc72df122d9c
Daniel Lew <dan@mobiata.com>
2013-01-17 12:29:51 -0600

    Added TripFlight parsing

---
+++ project/src/com/expedia/bookings/data/trips/TripFlight.java
@@ -0,0 +1,70 @@
+package com.expedia.bookings.data.trips;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.expedia.bookings.data.FlightTrip;
+import com.expedia.bookings.data.Traveler;
+import com.mobiata.android.json.JSONUtils;
+
+public class TripFlight extends TripComponent {
+
+	private FlightTrip mFlightTrip;
+
+	private List<Traveler> mTravelers;
+
+	public TripFlight() {
+		super(Type.FLIGHT);
+	}
+
+	public void setFlightTrip(FlightTrip flightTrip) {
+		mFlightTrip = flightTrip;
+	}
+
+	public FlightTrip getFlightTrip() {
+		return mFlightTrip;
+	}
+
+	public void addTraveler(Traveler traveler) {
+		if (mTravelers == null) {
+			mTravelers = new ArrayList<Traveler>();
+		}
+
+		mTravelers.add(traveler);
+	}
+
+	public List<Traveler> getTravelers() {
+		return mTravelers;
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+	// JSONable
+
+	@Override
+	public JSONObject toJson() {
+		JSONObject obj = super.toJson();
+		if (obj == null) {
+			return null;
+		}
+
+		try {
+			JSONUtils.putJSONable(obj, "flightTrip", mFlightTrip);
+			JSONUtils.putJSONableList(obj, "travelers", mTravelers);
+			return obj;
+		}
+		catch (JSONException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public boolean fromJson(JSONObject obj) {
+		super.fromJson(obj);
+		mFlightTrip = JSONUtils.getJSONable(obj, "flightTrip", FlightTrip.class);
+		mTravelers = JSONUtils.getJSONableList(obj, "travelers", Traveler.class);
+		return true;
+	}
+}
--- project/src/com/expedia/bookings/server/TripParser.java
+++ project/src/com/expedia/bookings/server/TripParser.java
@@ -4,20 +4,31 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import android.text.TextUtils;
+
 import com.expedia.bookings.data.DateTime;
+import com.expedia.bookings.data.FlightLeg;
+import com.expedia.bookings.data.FlightTrip;
 import com.expedia.bookings.data.Location;
 import com.expedia.bookings.data.Media;
 import com.expedia.bookings.data.Property;
+import com.expedia.bookings.data.Traveler;
+import com.expedia.bookings.data.Traveler.Gender;
 import com.expedia.bookings.data.trips.BookingStatus;
 import com.expedia.bookings.data.trips.Trip;
 import com.expedia.bookings.data.trips.Trip.TimePeriod;
 import com.expedia.bookings.data.trips.TripComponent;
-import com.expedia.bookings.data.trips.TripComponent.Type;
+import com.expedia.bookings.data.trips.TripFlight;
 import com.expedia.bookings.data.trips.TripHotel;
+import com.mobiata.flightlib.data.Flight;
+import com.mobiata.flightlib.data.FlightCode;
+import com.mobiata.flightlib.data.Waypoint;
 
 /**
  * Common class for parsing trips, since the trip summary and trip details
@@ -27,17 +38,28 @@ import com.expedia.bookings.data.trips.TripHotel;
  */
 public class TripParser {
 
+	private Map<String, Waypoint> mWaypoints;
+
 	public Trip parseTrip(JSONObject tripJson) {
 		Trip trip = new Trip();
 		trip.setTripId(tripJson.optString("tripId"));
 		trip.setTripNumber(tripJson.optString("tripNumber"));
 		trip.setTitle(tripJson.optString("title"));
 		trip.setDescription(tripJson.optString("description"));
-		trip.setStartDate(parseDateTime(tripJson.optJSONObject("startTime")));
-		trip.setEndDate(parseDateTime(tripJson.optJSONObject("endTime")));
+		trip.setStartDate(parseDateTime(tripJson.opt("startTime")));
+		trip.setEndDate(parseDateTime(tripJson.opt("endTime")));
 
 		trip.setBookingStatus(parseBookingStatus(tripJson.optString("bookingStatus")));
 		trip.setTimePeriod(parseTimePeriod(tripJson.optString("timePeriod")));
+
+		// Parse waypoints (used for flights parsing, if flights details response)
+		JSONArray waypoints = tripJson.optJSONArray("waypoints");
+		if (waypoints != null) {
+			mWaypoints = new HashMap<String, Waypoint>();
+			for (int b = 0; b < waypoints.length(); b++) {
+				parseWaypoint(waypoints.optJSONObject(b));
+			}
+		}
 
 		// Parse hotels
 		JSONArray hotels = tripJson.optJSONArray("hotels");
@@ -51,7 +73,7 @@ public class TripParser {
 		JSONArray flights = tripJson.optJSONArray("flights");
 		if (flights != null) {
 			for (int b = 0; b < flights.length(); b++) {
-				trip.addTripComponent(parseTripComponent(flights.optJSONObject(b), Type.FLIGHT));
+				trip.addTripComponent(parseTripFlight(flights.optJSONObject(b)));
 			}
 		}
 
@@ -60,41 +82,34 @@ public class TripParser {
 		return trip;
 	}
 
-	private TripComponent parseTripComponent(JSONObject obj, Type type) {
-		TripComponent component = new TripComponent(type);
-
-		if (type == Type.HOTEL) {
-
+	private DateTime parseDateTime(Object obj) {
+		if (obj == null) {
+			return null;
 		}
-		else if (type == Type.FLIGHT) {
-			component.setStartDate(parseDateTime2(obj.optString("startDate")));
-			component.setEndDate(parseDateTime2(obj.optString("endDate")));
+		else if (obj instanceof JSONObject) {
+			JSONObject json = (JSONObject) obj;
+			return new DateTime(json.optLong("epochSeconds") * 1000, json.optInt("timeZoneOffsetSeconds"));
+		}
+		else if (obj instanceof String) {
+			// TODO: DELETE ONCE OBSELETE
+			//
+			// Parse with no timezone.  The current timezone code is buggy and we shouldn't
+			// be using this code anyways.
+
+			try {
+				String str = (String) obj;
+				Date date = DATE_FORMAT.parse(str);
+				return new DateTime(date.getTime(), 0);
+			}
+			catch (ParseException e) {
+				throw new RuntimeException(e);
+			}
 		}
 
-		component.setBookingStatus(parseBookingStatus(obj.optString("bookingStatus")));
-		return component;
-	}
-
-	private DateTime parseDateTime(JSONObject obj) {
-		return new DateTime(obj.optLong("epochSeconds") * 1000, obj.optInt("timeZoneOffsetSeconds"));
+		throw new RuntimeException("Could not parse date time: " + obj);
 	}
 
 	private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
-
-	private DateTime parseDateTime2(String str) {
-		// TODO: DELETE ONCE OBSELETE
-		//
-		// Parse with no timezone.  The current timezone code is buggy and we shouldn't
-		// be using this code anyways.
-
-		try {
-			Date date = DATE_FORMAT.parse(str);
-			return new DateTime(date.getTime(), 0);
-		}
-		catch (ParseException e) {
-			throw new RuntimeException(e);
-		}
-	}
 
 	private BookingStatus parseBookingStatus(String status) {
 		if ("SAVED".equals(status)) {
@@ -132,8 +147,8 @@ public class TripParser {
 
 		parseTripCommon(obj, hotel);
 
-		hotel.setStartDate(parseDateTime2(obj.optString("checkInDate")));
-		hotel.setEndDate(parseDateTime2(obj.optString("checkOutDate")));
+		hotel.setStartDate(parseDateTime(obj.opt("checkInDate")));
+		hotel.setEndDate(parseDateTime(obj.opt("checkOutDate")));
 
 		Property property = new Property();
 		property.setPropertyId(obj.optString("hotelId"));
@@ -159,7 +174,126 @@ public class TripParser {
 		return hotel;
 	}
 
+	private TripFlight parseTripFlight(JSONObject obj) {
+		TripFlight flight = new TripFlight();
+
+		parseTripCommon(obj, flight);
+
+		if (obj.has("startTime") && obj.has("endTime")) {
+			flight.setStartDate(parseDateTime(obj.opt("startTime")));
+			flight.setEndDate(parseDateTime(obj.opt("endTime")));
+		}
+		else {
+			flight.setStartDate(parseDateTime(obj.opt("startDate")));
+			flight.setEndDate(parseDateTime(obj.opt("endDate")));
+		}
+
+		// We're taking a lack of legs info to mean that this is a non-details call;
+		// short-circuit out of the info early
+		if (!obj.has("legs")) {
+			return flight;
+		}
+
+		FlightTrip flightTrip = new FlightTrip();
+		flight.setFlightTrip(flightTrip);
+
+		// Parse fares
+		JSONObject fareTotalJson = obj.optJSONObject("fareTotal");
+		String currency = fareTotalJson.optString("currency");
+		flightTrip.setBaseFare(ParserUtils.createMoney(fareTotalJson.optString("base"), currency));
+		flightTrip.setTaxes(ParserUtils.createMoney(fareTotalJson.optString("taxes"), currency));
+		flightTrip.setTotalFare(ParserUtils.createMoney(fareTotalJson.optString("total"), currency));
+
+		// Parse passengers
+		JSONArray passengersArr = obj.optJSONArray("passengers");
+		for (int a = 0; a < passengersArr.length(); a++) {
+			JSONObject passengerJson = passengersArr.optJSONObject(a);
+
+			Traveler traveler = new Traveler();
+			traveler.setFirstName(passengerJson.optString("firstName"));
+			traveler.setMiddleName(passengerJson.optString("middleName"));
+			traveler.setLastName(passengerJson.optString("lastName"));
+
+			String gender = passengerJson.optString("gender");
+			if ("Male".equals(gender)) {
+				traveler.setGender(Gender.MALE);
+			}
+			else if ("Female".equals(gender)) {
+				traveler.setGender(Gender.FEMALE);
+			}
+
+			// For now, just parse the first phone number
+			JSONArray phoneNumbersArr = passengerJson.optJSONArray("phoneNumbers");
+			if (phoneNumbersArr != null && phoneNumbersArr.length() > 0) {
+				JSONObject firstPhoneJson = phoneNumbersArr.optJSONObject(0);
+				traveler.setPhoneCountryCode(firstPhoneJson.optString("countryCode"));
+				traveler.setPhoneNumber(firstPhoneJson.optString("phone"));
+			}
+
+			flight.addTraveler(traveler);
+		}
+
+		// Parse the legs
+		JSONArray legsArr = obj.optJSONArray("legs");
+		for (int a = 0; a < legsArr.length(); a++) {
+			JSONObject legJson = legsArr.optJSONObject(a);
+			FlightLeg leg = new FlightLeg();
+
+			JSONArray segmentsArr = legJson.optJSONArray("segments");
+			for (int b = 0; b < segmentsArr.length(); b++) {
+				JSONObject segmentJson = segmentsArr.optJSONObject(b);
+
+				Flight segment = new Flight();
+
+				segment.mOrigin = mWaypoints.get(segmentJson.opt("departureWaypointId"));
+				segment.mDestination = mWaypoints.get(segmentJson.opt("arrivalWaypointId"));
+
+				FlightCode flightCode = new FlightCode();
+				flightCode.mAirlineCode = segmentJson.optString("airlineCode");
+				flightCode.mNumber = segmentJson.optString("flightNumber");
+				segment.addFlightCode(flightCode, Flight.F_PRIMARY_AIRLINE_CODE);
+
+				String operatedBy = segmentJson.optString("operatedByAirCarrierName", null);
+				if (!TextUtils.isEmpty(operatedBy)) {
+					FlightCode opFlightCode = new FlightCode();
+					opFlightCode.mAirlineName = operatedBy;
+					segment.addFlightCode(opFlightCode, Flight.F_OPERATING_AIRLINE_CODE);
+				}
+
+				segment.mAircraftType = segmentJson.optString("equipmentDescription", null);
+
+				// We assume all distances are in miles, throw a fit if that's not the case
+				if (!"mi".equals(segmentJson.optString("distanceUnits"))) {
+					throw new RuntimeException("Wasn't expecting non-miles unit");
+				}
+
+				segment.mDistanceToTravel = segmentJson.optInt("distance"); // Assumes "miles" here
+
+				leg.addSegment(segment);
+			}
+
+			flightTrip.addLeg(leg);
+		}
+
+		return flight;
+	}
+
 	private void parseTripCommon(JSONObject obj, TripComponent component) {
 		component.setBookingStatus(parseBookingStatus(obj.optString("bookingStatus")));
+	}
+
+	public void parseWaypoint(JSONObject obj) {
+		Waypoint waypoint = new Waypoint(obj.optString("type").equals("FLIGHT_DEPARTURE") ? Waypoint.F_DEPARTURE
+				: Waypoint.F_ARRIVAL);
+
+		JSONObject timeJson = obj.optJSONObject("time");
+		waypoint.addDateTime(Waypoint.POSITION_UNKNOWN, Waypoint.ACCURACY_SCHEDULED,
+				timeJson.optLong("epochSeconds") * 1000, timeJson.optInt("timeZoneOffsetSeconds"));
+
+		JSONObject airportJson = obj.optJSONObject("location");
+		waypoint.mAirportCode = airportJson.optString("airportCode");
+		waypoint.setTerminal(obj.optString("airportTerminal"));
+
+		mWaypoints.put(obj.optString("id"), waypoint);
 	}
 }
