ee9a76b84bb2c62eac38890798eb12afd39af28e
Doug Melton <doug@mobiata.com>
2013-01-18 16:32:35 -0800

    Added 'I Accept' button to flight checkout for POS that require it

--- project/src/com/expedia/bookings/fragment/FlightSlideToPurchaseFragment.java
+++ project/src/com/expedia/bookings/fragment/FlightSlideToPurchaseFragment.java
@@ -7,20 +7,27 @@ import android.support.v4.app.Fragment;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.expedia.bookings.R;
 import com.expedia.bookings.activity.FlightBookingActivity;
 import com.expedia.bookings.data.Db;
 import com.expedia.bookings.data.User;
+import com.expedia.bookings.data.pos.PointOfSale;
 import com.expedia.bookings.utils.Ui;
 import com.expedia.bookings.widget.SlideToWidget;
 import com.expedia.bookings.widget.SlideToWidget.ISlideToListener;
+import com.mobiata.android.Log;
 
 public class FlightSlideToPurchaseFragment extends Fragment {
 
+	private static final String HAS_ACCEPTED_TOS = "HAS_ACCEPTED_TOS";
+
 	private SlideToWidget mSlider;
+	private boolean mHasAcceptedTOS;
 
 	public static FlightSlideToPurchaseFragment newInstance() {
 		FlightSlideToPurchaseFragment fragment = new FlightSlideToPurchaseFragment();
@@ -34,6 +41,16 @@ public class FlightSlideToPurchaseFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_flight_slide_to_purchase, container, false);
 
+		// Click to accept TOS
+		if (savedInstanceState != null && savedInstanceState.containsKey(HAS_ACCEPTED_TOS)) {
+			mHasAcceptedTOS = savedInstanceState.getBoolean(HAS_ACCEPTED_TOS);
+		}
+		else {
+			mHasAcceptedTOS = !(PointOfSale.getPointOfSale().requiresRulesRestrictionsCheckbox());
+		}
+		showHideAcceptTOS(v, false);
+
+		// Slide To Purchase
 		TextView price = Ui.findView(v, R.id.trip_price);
 		String template = getResources().getString(R.string.your_card_will_be_charged_TEMPLATE);
 		String text = String.format(template, Db.getFlightSearch().getSelectedFlightTrip().getTotalFare()
@@ -93,6 +110,30 @@ public class FlightSlideToPurchaseFragment extends Fragment {
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
+		outState.putBoolean(HAS_ACCEPTED_TOS, mHasAcceptedTOS);
 	}
 
+	private void showHideAcceptTOS(final View view, final boolean animated) {
+		ViewGroup layoutConfirmTOS = Ui.findView(view, R.id.layout_confirm_tos);
+		ViewGroup layoutSlideToPurchase = Ui.findView(view, R.id.layout_slide_to_purchase);
+
+		if (mHasAcceptedTOS) {
+			layoutConfirmTOS.setVisibility(View.INVISIBLE);
+			layoutSlideToPurchase.setVisibility(View.VISIBLE);
+		}
+		else {
+			layoutConfirmTOS.setVisibility(View.VISIBLE);
+			layoutSlideToPurchase.setVisibility(View.INVISIBLE);
+
+			Button buttonIAccept = Ui.findView(layoutConfirmTOS, R.id.button_i_accept);
+			buttonIAccept.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					mHasAcceptedTOS = true;
+					showHideAcceptTOS(view, true);
+				}
+			});
+		}
+
+	}
 }
