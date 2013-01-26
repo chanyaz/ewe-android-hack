b14da40cb27462d7c0316e3b3875bdf1a19c717c
Daniel Lew <dan@mobiata.com>
2013-01-26 12:21:29 -0600

    Fix subtle logic problem

--- project/src/com/expedia/bookings/maps/HotelMapFragment.java
+++ project/src/com/expedia/bookings/maps/HotelMapFragment.java
@@ -428,7 +428,7 @@ public class HotelMapFragment extends SupportMapFragment {
 
 		// For some Expedia-suggested locations they do not return a latitude/longitude; in those
 		// cases, do not try to move the map around
-		if (latitude != 0 && longitude != 0) {
+		if (latitude != 0 || longitude != 0) {
 			animateCamera(CameraUpdateFactory.newLatLngZoom(
 					new LatLng(params.getSearchLatitude(), params.getSearchLongitude()), DEFAULT_ZOOM));
 		}
