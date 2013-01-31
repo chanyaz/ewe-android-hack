c78a1efa6aeb9e1c2df33bddec964b670f40d03a
Joel Drotos <jdrotos@mobiata.com>
2013-01-31 14:40:42 -0600

    Allow passing params to the Logout Confirmation Dialog

--- project/src/com/expedia/bookings/fragment/ConfirmLogoutDialogFragment.java
+++ project/src/com/expedia/bookings/fragment/ConfirmLogoutDialogFragment.java
@@ -12,6 +12,10 @@ public class ConfirmLogoutDialogFragment extends DialogFragment {
 
 	public static final String TAG = "ConfirmLogoutDialogFragment";
 
+	private static final String ARG_MESSAGE = "ARG_MESSAGE";
+	private static final String ARG_NEGATIVE_BTN_TEXT = "ARG_NEGATIVE_BTN_TEXT";
+	private static final String ARG_POSITIVE_BTN_TEXT = "ARG_POSITIVE_BTN_TEXT";
+
 	public static interface DoLogoutListener {
 		public void doLogout();
 	}
@@ -25,6 +29,25 @@ public class ConfirmLogoutDialogFragment extends DialogFragment {
 		return instance;
 	}
 
+	public static ConfirmLogoutDialogFragment getInstance(DoLogoutListener listener, String message) {
+		ConfirmLogoutDialogFragment instance = new ConfirmLogoutDialogFragment();
+		Bundle args = new Bundle();
+		args.putString(ARG_MESSAGE, message);
+		instance.setArguments(args);
+		return instance;
+	}
+
+	public static ConfirmLogoutDialogFragment getInstance(DoLogoutListener listener, String message,
+			String negativeBtnText, String positiveBtnText) {
+		ConfirmLogoutDialogFragment instance = new ConfirmLogoutDialogFragment();
+		Bundle args = new Bundle();
+		args.putString(ARG_MESSAGE, message);
+		args.putString(ARG_NEGATIVE_BTN_TEXT, negativeBtnText);
+		args.putString(ARG_POSITIVE_BTN_TEXT, positiveBtnText);
+		instance.setArguments(args);
+		return instance;
+	}
+
 	public void setDoLogoutListener(DoLogoutListener listener) {
 		mListener = listener;
 	}
@@ -32,8 +55,28 @@ public class ConfirmLogoutDialogFragment extends DialogFragment {
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
 		Builder builder = new Builder(getActivity());
-		builder.setMessage(R.string.logout_confirmation);
-		builder.setPositiveButton(R.string.log_out, new DialogInterface.OnClickListener() {
+
+		//Defaults
+		String messageText = getResources().getString(R.string.logout_confirmation);
+		String postiveBtnText = getResources().getString(R.string.log_out);
+		String negativeBtnText = getResources().getString(R.string.cancel);
+
+		//Args
+		Bundle args = getArguments();
+		if (args != null) {
+			if (args.containsKey(ARG_MESSAGE)) {
+				messageText = args.getString(ARG_MESSAGE);
+			}
+			if (args.containsKey(ARG_POSITIVE_BTN_TEXT)) {
+				postiveBtnText = args.getString(ARG_POSITIVE_BTN_TEXT);
+			}
+			if (args.containsKey(ARG_NEGATIVE_BTN_TEXT)) {
+				negativeBtnText = args.getString(ARG_NEGATIVE_BTN_TEXT);
+			}
+		}
+
+		builder.setMessage(messageText);
+		builder.setPositiveButton(postiveBtnText, new DialogInterface.OnClickListener() {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				if (mListener != null) {
@@ -42,7 +85,7 @@ public class ConfirmLogoutDialogFragment extends DialogFragment {
 				dismiss();
 			}
 		});
-		builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+		builder.setNegativeButton(negativeBtnText, new DialogInterface.OnClickListener() {
 
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
