7635565a1e95913d0ac919ff2f43d809f2f2451b
Brennan Taylor <brennan@mobiata.com>
2013-02-05 14:53:56 -0800

    Script to fix annoying string bugs

---
+++ tools/fix-strings.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ "$1" = "" ] ; then
+    echo "error: First arg must be path to res folder"
+    exit 1
+fi
+
+path=$1
+
+# Labels for sed fix-point detection loop
+start_loop=":repeat; "
+end_loop="; trepeat"
+
+# RE's to find and fix
+good_apos="\\\\\&apos;"
+fix_comment="$start_loop s/\(<!--.*\)’/\1'/ $end_loop"
+fix_string="$start_loop s/\(<string.*\)’/\1$good_apos/ $end_loop"
+fix_item="$start_loop s/\(<item.*\)’/\1$good_apos/ $end_loop"
+
+for i in $path/values*/strings.xml ; do
+    echo $i
+    gsed -i "$fix_comment" $i
+    gsed -i "$fix_string" $i
+    gsed -i "$fix_item" $i
+done
+
+# TESTING
+#echo "<!-- Can’t Sav’e Traveler -->" | gsed "$fix_comment"
+#echo "<!-- Can’t Sav’e Traveler -->" | gsed "$fix_string"
+#echo "<string name=\"test\">’’’’</string>" | gsed "$fix_string"
+#echo "<item name=\"test\">’’’’</string>" | gsed "$fix_item"
