0e9b9152d614a089cc63a947309f9d76d548f8b5
Joel Drotos <jdrotos@mobiata.com>
2013-02-05 12:53:37 -0600

    Removed unused interface/methods/logic/variables

--- project/src/com/expedia/bookings/activity/ItineraryGuestAddActivity.java
+++ project/src/com/expedia/bookings/activity/ItineraryGuestAddActivity.java
@@ -7,16 +7,13 @@ import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.expedia.bookings.R;
 import com.expedia.bookings.fragment.ItineraryGuestAddFragment;
-import com.expedia.bookings.fragment.LoginFragment.TitleSettable;
 import com.expedia.bookings.utils.Ui;
 
-public class ItineraryGuestAddActivity extends SherlockFragmentActivity implements TitleSettable {
+public class ItineraryGuestAddActivity extends SherlockFragmentActivity {
 
 	private static final String TAG_GUEST_ADD_FRAGMENT = "TAG_GUEST_ADD_FRAGMENT";
-	private static final String STATE_TITLE = "STATE_TITLE";
 
 	private ItineraryGuestAddFragment mAddGuestItinFragment;
-	private String mTitle;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -25,12 +22,6 @@ public class ItineraryGuestAddActivity extends SherlockFragmentActivity implemen
 
 		setContentView(R.layout.activity_itinerary_guest_add);
 
-		if (savedInstanceState != null) {
-			if (savedInstanceState.containsKey(STATE_TITLE)) {
-				setTitle(savedInstanceState.getString(STATE_TITLE));
-			}
-		}
-
 		// Actionbar
 		ActionBar actionBar = this.getSupportActionBar();
 		actionBar.setIcon(R.drawable.ic_logo_flights);
@@ -38,12 +29,8 @@ public class ItineraryGuestAddActivity extends SherlockFragmentActivity implemen
 		actionBar.setDisplayUseLogoEnabled(false);
 		actionBar.setDisplayShowHomeEnabled(true);
 		actionBar.setDisplayHomeAsUpEnabled(true);
-		if (mTitle != null) {
-			setTitle(mTitle);
-		}
-		else {
-			setTitle(getString(R.string.find_itinerary));
-		}
+
+		setTitle(getString(R.string.find_itinerary));
 
 		// Create/grab the login fragment
 		mAddGuestItinFragment = Ui.findSupportFragment(this, TAG_GUEST_ADD_FRAGMENT);
@@ -53,20 +40,5 @@ public class ItineraryGuestAddActivity extends SherlockFragmentActivity implemen
 			ft.add(R.id.fragment_container, mAddGuestItinFragment, TAG_GUEST_ADD_FRAGMENT);
 			ft.commit();
 		}
-	}
-
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		if (mTitle != null) {
-			outState.putString(STATE_TITLE, mTitle);
-		}
-	}
-
-	@Override
-	public void setActionBarTitle(String title) {
-		mTitle = title;
-		ActionBar actionBar = this.getSupportActionBar();
-		actionBar.setTitle(mTitle);
 	}
 }
