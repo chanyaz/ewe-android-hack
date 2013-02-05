9f3042d773e439e57f7365d18df2f5f0ec0adfe3
Brennan Taylor <brennan@mobiata.com>
2013-02-05 14:53:38 -0800

    Allow expedia.xml files to get dumped in to the EB res folder

--- tools/strings-import.py
+++ tools/strings-import.py
@@ -154,6 +154,10 @@ def move_files(in_dir, out_dir):
                     if new_dir is not None:
                         os.mkdir(new_dir)
                         shutil.copy(filepath, os.path.join(new_dir, "strings.xml"))
+                elif filename == "expedia.xml":
+                    new_dir = os.path.join(expedia_dir, "values-%s" % postfix)
+                    os.mkdir(new_dir)
+                    shutil.copy(filepath, os.path.join(new_dir, "strings.xml"))
                 elif filename == ".DS_Store":
                     # Ignore this annoying file
                     pass
