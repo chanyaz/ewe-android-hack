chanyaz/ewe-android-hack

commit 2950b384b733c234dc70d40fb3dd46b69017c452

TESTING HELPER - This adds another price change so that we can test when both the fees have changed and the price.
This doesnt need to be merged and should probably be removed shortly depending on usefulness to QA.

project/src/com/expedia/bookings/activity/FlightBookingActivity.java
@@ -335,6 +335,16 @@ public class FlightBookingActivity extends SherlockFragmentActivity implements C
 		case PRICE_CHANGE:
 			FlightTrip currentOffer = Db.getFlightSearch().getSelectedFlightTrip();
 			FlightTrip newOffer = response.getNewOffer();
+
+			// If the debug setting is made to fake a price change, then fake the price here too
+			// This is sort of a second price change, to help figure out testing when we have obfees and a price change...
+			if (!AndroidUtils.isRelease(this)) {
+				String val = SettingUtils.get(this,
+						getString(R.string.preference_flight_fake_price_change),
+						getString(R.string.preference_flight_fake_price_change_default));
+				newOffer.getTotalFare().add(new BigDecimal(val));
+			}
+
 			PriceChangeDialogFragment fragment = PriceChangeDialogFragment.newInstance(currentOffer, newOffer);
 			fragment.show(getSupportFragmentManager(), PriceChangeDialogFragment.TAG);
 

commit 91e6554abae152f7e21319a1ca6b7a75787341fa

TESTING HELPER - Fixed bug introduced by 2950b384b733c234dc70d40fb3dd46b69017c452 where diff == 0

project/src/com/expedia/bookings/activity/FlightBookingActivity.java
@@ -342,6 +342,7 @@ public class FlightBookingActivity extends SherlockFragmentActivity implements C
 				String val = SettingUtils.get(this,
 						getString(R.string.preference_flight_fake_price_change),
 						getString(R.string.preference_flight_fake_price_change_default));
+				currentOffer.getTotalFare().add(new BigDecimal(val));
 				newOffer.getTotalFare().add(new BigDecimal(val));
 			}
 