e73e1e16026407db919b12acd9810db69d8c4263
Brad McManus <brad@mobiata.com>
2013-03-08 14:40:09 -0800

    Add LineOfBusiness enum

---
+++ project/src/com/expedia/bookings/data/LineOfBusiness.java
@@ -0,0 +1,15 @@
+package com.expedia.bookings.data;
+
+/**
+ * This enum standardizes the various LineOfBusiness-like enums that have popped up in various sections of the code
+ * where we require different logic/params based on the LineOfBusiness.
+ * 
+ * Important Note: This is not entirely analogous the the Expedia line of business. For instance, ITIN will never
+ * be though of as a line of business to Expedia, although, the client teams like to refer to line of business for the 
+ * different "sub-projects" and code paths that we create in the app.
+ */
+public enum LineOfBusiness {
+	FLIGHTS,
+	HOTELS,
+	ITIN;
+}
