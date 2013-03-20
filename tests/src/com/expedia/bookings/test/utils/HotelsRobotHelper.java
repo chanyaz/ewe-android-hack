dbae9e829491509a6d2ee247251a8506822c077d
Kevin Carpenter <kevc@mobiata.com>
2013-03-20 15:56:45 -0400

    Remove repetitive "toUppercase()" from string selection in flights auto.

--- tests/src/com/expedia/bookings/test/utils/HotelsRobotHelper.java
+++ tests/src/com/expedia/bookings/test/utils/HotelsRobotHelper.java
@@ -1103,7 +1103,7 @@ public class HotelsRobotHelper {
 			mSolo.clickOnText(mRes.getString(R.string.sort_flights).toUpperCase());
 		}
 		catch (Error e) {
-			mSolo.clickOnText(mRes.getString(R.string.sort_flights).toUpperCase());
+			mSolo.clickOnText(mRes.getString(R.string.sort_flights));
 		}
 		screenshot("Sort fragment");
 		mSolo.goBack();
