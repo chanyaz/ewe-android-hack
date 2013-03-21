35fd1c4ddf5452fc92358a2d064718a281cbfe35
Michael Pardo <pardom@mobiata.com>
2013-03-21 14:13:29 -0400

    Add highlight to itin card.

--- project/src/com/expedia/bookings/widget/ItinHeaderImageView.java
+++ project/src/com/expedia/bookings/widget/ItinHeaderImageView.java
@@ -9,7 +9,9 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff.Mode;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 
 import com.expedia.bookings.R;
@@ -22,20 +24,21 @@ public class ItinHeaderImageView extends OptimizedImageView {
 	private int mMode = MODE_MINI;
 	private int mRadius = 10;
 
-	private RectF mBounds;
+	private Rect mBounds;
 	private Bitmap mCompositeBitmap;
 	private Canvas mCompositeCanvas;
 
 	private Paint mMaskPaint;
-
 	private Bitmap mTLMaskBitmap;
-	private Canvas mTLMaskCanvas;
 	private Bitmap mTRMaskBitmap;
-	private Canvas mTRMaskCanvas;
 	private Bitmap mBLMaskBitmap;
-	private Canvas mBLMaskCanvas;
 	private Bitmap mBRMaskBitmap;
+	private Canvas mTLMaskCanvas;
+	private Canvas mTRMaskCanvas;
+	private Canvas mBLMaskCanvas;
 	private Canvas mBRMaskCanvas;
+
+	private Drawable mHighlightDrawable;
 
 	public ItinHeaderImageView(Context context) {
 		super(context);
@@ -53,9 +56,11 @@ public class ItinHeaderImageView extends OptimizedImageView {
 	}
 
 	private void init(Context context, AttributeSet attrs, int defStyle) {
-		mBounds = new RectF();
+		mBounds = new Rect();
 		mMaskPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 		mMaskPaint.setXfermode(new PorterDuffXfermode(Mode.DST_OUT));
+
+		mHighlightDrawable = context.getResources().getDrawable(R.drawable.card_top_lighting);
 
 		TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ItinHeaderImageView);
 		setMode(a.getInteger(R.styleable.ItinHeaderImageView_mode, mMode));
@@ -82,7 +87,8 @@ public class ItinHeaderImageView extends OptimizedImageView {
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 		super.onSizeChanged(w, h, oldw, oldh);
 
-		mBounds.set(0.0f, 0.0f, w, h);
+		mBounds.set(0, 0, w, h);
+		mHighlightDrawable.setBounds(mBounds);
 
 		// Only create a new bitmap if it needs to be bigger
 		if (mCompositeBitmap == null || w > oldw) {
@@ -110,6 +116,9 @@ public class ItinHeaderImageView extends OptimizedImageView {
 			mCompositeCanvas.drawBitmap(mBLMaskBitmap, 0, mBounds.bottom - mRadius, mMaskPaint);
 			mCompositeCanvas.drawBitmap(mBRMaskBitmap, mBounds.right - mRadius, mBounds.bottom - mRadius, mMaskPaint);
 		}
+
+		// Draw highlight
+		mHighlightDrawable.draw(mCompositeCanvas);
 
 		// Draw composite (must use composite bitmap because original bitmap may not be ARGB_8888)
 		canvas.drawBitmap(mCompositeBitmap, 0, 0, null);
