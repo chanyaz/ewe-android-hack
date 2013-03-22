ea3521080a9217fa5301c4db70f0d701e0552d91
Kevin Carpenter <kevc@mobiata.com>
2013-03-22 12:34:08 -0400

    Create AirportSelectUtils 	* Get random pairs of US airports randomly

---
+++ tests/src/com/expedia/bookings/test/utils/AirportSelectUtils.java
@@ -0,0 +1,42 @@
+package com.expedia.bookings.test.utils;
+
+import java.util.Random;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.util.Pair;
+
+import com.expedia.bookings.activity.SearchActivity;
+
+public class AirportSelectUtils extends ActivityInstrumentationTestCase2<SearchActivity> {
+
+	public AirportSelectUtils() {
+		super("com.expedia.bookings", SearchActivity.class);
+	}
+
+	//20 busiest US Airports
+	public static String[] AIRPORT_LIST_USA = new String[] {
+			"ATL", "ORD", "LAX", "DFW", "DEN",
+			"JFK", "SFO", "LAS", "PHX", "IAH",
+			"CLT", "MIA", "MCO", "EWR", "SEA",
+			"MSP", "DTW", "PHL", "BOS", "LGA",
+	};
+
+	public Pair<String, String> getTwoAirports(String[] airportList){
+		return getTwoAirports(airportList, airportList);
+	}
+	
+	public Pair<String, String> getTwoAirports(String[] airportList1, String[] airportList2) {
+		Random numberGen = new Random();
+		String airport1 = airportList1[numberGen.nextInt(airportList1.length)];
+		String airport2 = airportList2[numberGen.nextInt(airportList2.length)];
+
+		while (airport1.equals(airport2)) {
+			airport2 = airportList2[numberGen.nextInt(airportList2.length)];
+		}
+
+		Pair<String, String> airports = new Pair<String, String>(airport1, airport2);
+		return airports;
+
+	}
+
+}
