902d1e9009ed7895442787abb4dbf3b56099edac
Joel Drotos <jdrotos@mobiata.com>
2013-03-25 14:22:07 -0500

    ITIN - 458, 785 - Fix improper pressed state.
    This is one of those bugs where I tried messing around with attributes for way too long.
    In the end overriding onPressed of the buttons parent container solves the problem.

---
+++ project/src/com/expedia/bookings/widget/NoPressLinearLayout.java
@@ -0,0 +1,39 @@
+package com.expedia.bookings.widget;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+
+/**
+ * This is a LinearLayout that wont get pressed state from its parent.
+ * Things within it can still be pressed
+ * 
+ * Inspired by this blog post: 
+ * http://cyrilmottier.com/2011/11/23/listview-tips-tricks-4-add-several-clickable-areas/
+ */
+public class NoPressLinearLayout extends LinearLayout {
+
+	public NoPressLinearLayout(Context context) {
+		super(context);
+	}
+
+	public NoPressLinearLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	@SuppressLint("NewApi")
+	public NoPressLinearLayout(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	@Override
+	public void setPressed(boolean pressed) {
+		if (pressed && getParent() instanceof View && ((View) getParent()).isPressed()) {
+			return;
+		}
+		super.setPressed(pressed);
+	}
+
+}
