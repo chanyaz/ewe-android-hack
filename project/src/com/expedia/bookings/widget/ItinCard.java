275e4446bc53f99099446c6724d0314b656b40f3
Michael Pardo <pardom@mobiata.com>
2013-03-25 21:35:49 -0400

    Fix bad merge.

--- project/src/com/expedia/bookings/widget/ItinCard.java
+++ project/src/com/expedia/bookings/widget/ItinCard.java
@@ -10,7 +10,6 @@ import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.MimeTypeMap;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
