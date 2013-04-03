e35c908e82da8f7183d80c57ae2af9e3d304d43a
Kevin Carpenter <kevc@mobiata.com>
2013-04-03 17:24:19 -0400

    Create IntegrationFailureError

---
+++ tests/src/com/expedia/bookings/test/utils/IntegrationFailureError.java
@@ -0,0 +1,13 @@
+package com.expedia.bookings.test.utils;
+
+public class IntegrationFailureError extends Error {
+	IntegrationFailureError() {
+		super();
+	}
+
+	IntegrationFailureError(String msg) {
+		super(msg);
+	}
+	
+	
+}
