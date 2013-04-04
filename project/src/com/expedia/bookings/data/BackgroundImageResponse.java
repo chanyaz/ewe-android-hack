45c944d33906c2a14f2922043e512b3a8f8d24be
Daniel Lew <dan@mobiata.com>
2013-04-04 10:38:09 -0500

    Removed unused fields from BackgroundImageResponse
    Was just confusing things a bit

--- project/src/com/expedia/bookings/data/BackgroundImageResponse.java
+++ project/src/com/expedia/bookings/data/BackgroundImageResponse.java
@@ -8,16 +8,10 @@ import com.mobiata.android.json.JSONable;
 public class BackgroundImageResponse extends Response implements JSONable {
 
 	private static final String JSON_KEY_IMAGEURL = "imageUrl";
-	private static final String JSON_KEY_BLURREDIMAGEURL = "blurredImageUrl";
 	private static final String JSON_KEY_CACHEKEY = "cacheKey";
 
 	private String mCacheKey;
 	private String mImageUrl;
-	private String mBlurredImageUrl;
-
-	public BackgroundImageResponse() {
-
-	}
 
 	public String getCacheKey() {
 		return mCacheKey;
@@ -35,21 +29,12 @@ public class BackgroundImageResponse extends Response implements JSONable {
 		this.mImageUrl = imageUrl;
 	}
 
-	public String getBlurredImageUrl() {
-		return mBlurredImageUrl;
-	}
-
-	public void setBlurredImageUrl(String blurredImageUrl) {
-		this.mBlurredImageUrl = blurredImageUrl;
-	}
-
 	@Override
 	public JSONObject toJson() {
 		JSONObject out = new JSONObject();
 		try {
 			out.putOpt(JSON_KEY_CACHEKEY, mCacheKey);
 			out.putOpt(JSON_KEY_IMAGEURL, mImageUrl);
-			out.putOpt(JSON_KEY_BLURREDIMAGEURL, mBlurredImageUrl);
 		}
 		catch (Exception ex) {
 			Log.e("Exception in toJson", ex);
@@ -60,7 +45,6 @@ public class BackgroundImageResponse extends Response implements JSONable {
 	@Override
 	public boolean fromJson(JSONObject obj) {
 		mImageUrl = obj.optString(JSON_KEY_IMAGEURL);
-		mBlurredImageUrl = obj.optString(JSON_KEY_BLURREDIMAGEURL);
 		mCacheKey = obj.optString(JSON_KEY_CACHEKEY);
 		return true;
 	}
