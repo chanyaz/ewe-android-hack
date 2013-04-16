1abb7e087813c77fa647bd3daa9c6e30a1efa44b
Kevin Carpenter <kevc@mobiata.com>
2013-04-16 09:14:39 -0400

    Create hotels rotate test for tablet!

---
+++ tests/src/com/expedia/bookings/test/tests/hotels/TabletHotelsRotateTest.java
@@ -0,0 +1,75 @@
+package com.expedia.bookings.test.tests.hotels;
+
+import android.content.res.Resources;
+import android.test.ActivityInstrumentationTestCase2;
+import android.util.DisplayMetrics;
+
+import com.expedia.bookings.R;
+import com.expedia.bookings.activity.SearchActivity;
+import com.expedia.bookings.test.utils.HotelsRobotHelper;
+import com.expedia.bookings.test.utils.HotelsUserData;
+import com.expedia.bookings.test.utils.TabletsRobotHelper;
+import com.jayway.android.robotium.solo.Solo;
+
+public class TabletHotelsRotateTest extends ActivityInstrumentationTestCase2<SearchActivity> {
+	public TabletHotelsRotateTest() {
+		super("com.expedia.bookings", SearchActivity.class);
+	}
+
+	private Solo mSolo;
+	private static final String TAG = "TabletHappyPath";
+	private Resources mRes;
+	DisplayMetrics mMetric;
+	private TabletsRobotHelper mDriver;
+	private HotelsUserData mUser;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		mRes = getActivity().getBaseContext().getResources();
+		mMetric = mRes.getDisplayMetrics();
+		mUser = new HotelsUserData();
+		mSolo = new Solo(getInstrumentation(), getActivity());
+		mDriver = new TabletsRobotHelper(mSolo, mRes, mUser);
+
+		mDriver.setScreenshotCount(1);
+		mDriver.setAllowScreenshots(false);
+		mDriver.setAllowOrientationChange(true);
+		mDriver.setWriteEventsToFile(false);
+
+		mUser.setHotelCityToRandomUSCity();
+	}
+
+	public void testMethod() {
+		mDriver.delay(5);
+		mDriver.setSpoofBookings();
+		mDriver.selectLocation(mUser.mHotelSearchCity);
+		mDriver.pressToSearch();
+
+		mDriver.selectHotel(3);
+		mDriver.delay();
+
+		mDriver.pressSeeDetails();
+		mSolo.scrollDown();
+		mSolo.scrollToTop();
+
+		mDriver.pressSelectRoom();
+		mDriver.pressBookingInfo();
+		mDriver.delay();
+
+		mDriver.pressLoginButton();
+		mDriver.enterLoginCredentials();
+		mDriver.delay(5);
+		mDriver.enterCCVAndZIP();
+		mDriver.pressToConfirmAndBook();
+		mDriver.delay(10);
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		//Robotium will finish all the activities that have been opened
+		mDriver.enterLog(TAG, "tearing down...");
+		mSolo.finishOpenedActivities();
+	}
+
+}
