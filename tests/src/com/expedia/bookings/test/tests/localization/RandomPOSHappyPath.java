2e1b7ac2815ce81c7d711012f78b9ca645a298f7
Kevin Carpenter <kevc@mobiata.com>
2013-04-19 15:47:57 -0400

    Create RandomPOSHappyPath test

---
+++ tests/src/com/expedia/bookings/test/tests/localization/RandomPOSHappyPath.java
@@ -0,0 +1,133 @@
+// POS/Locale Screenshot Sweep
+// Made for Expedia Hotels Android App.
+// Kevin Carpenter
+// Some code derived from Daniel Lew's LocalizationTests.java
+
+package com.expedia.bookings.test.tests.localization;
+
+import java.util.Locale;
+
+import android.content.res.Resources;
+import android.test.ActivityInstrumentationTestCase2;
+import android.util.DisplayMetrics;
+
+import com.expedia.bookings.R;
+import com.expedia.bookings.activity.SearchActivity;
+import com.expedia.bookings.test.utils.HotelsRobotHelper;
+import com.expedia.bookings.test.utils.HotelsUserData;
+import com.jayway.android.robotium.solo.Solo;
+
+public class RandomPOSHappyPath extends
+		ActivityInstrumentationTestCase2<SearchActivity> {
+
+	public RandomPOSHappyPath() { // Default constructor
+		super("com.expedia.bookings", SearchActivity.class);
+	}
+
+	private static final String TAG = "SearchTest";
+
+	private Solo mSolo;
+
+	private Resources mRes;
+	DisplayMetrics mMetric;
+	private HotelsRobotHelper mDriver;
+	private HotelsUserData mUser;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		mSolo = new Solo(getInstrumentation(), getActivity());
+		//Log.configureLogging("ExpediaBookings", true);
+
+		mRes = getActivity().getBaseContext().getResources();
+		mMetric = mRes.getDisplayMetrics();
+		mUser = new HotelsUserData();
+		mUser.setHotelCityToRandomUSCity();
+
+		mDriver = new HotelsRobotHelper(mSolo, mRes, mUser);
+	}
+
+	////////////////////////////////////////////////////////////////
+	// Test Drivers
+
+	// Run screenshot sweep on APAC locales
+	public void testBookingsAPAC() throws Exception {
+		testBookings(mDriver.mLocaleUtils.APAC_LOCALES);
+	}
+
+	// Run screenshot sweep on Western locales
+	public void testBookingsWestern() throws Exception {
+		testBookings(mDriver.mLocaleUtils.WESTERN_LOCALES);
+	}
+
+	// Run screenshot sweep on North, South, and Central American locales
+	public void testBookingsAmericas() throws Exception {
+		testBookings(mDriver.mLocaleUtils.AMERICAN_LOCALES);
+	}
+
+	private void testBookings(Locale[] localeList) throws Exception {
+		mDriver.setAllowScreenshots(true);
+		//change device locale and set variable
+		Locale testingLocale = mDriver.mLocaleUtils.setRandomLocale(localeList);
+		mDriver.enterLog(TAG, "Starting sweep of " + testingLocale.toString());
+
+		mDriver.changeAPI("Production");
+
+		//Reset screenshot file name to start with index 1
+		mDriver.setScreenshotCount(1);
+
+		mDriver.changePOS(testingLocale);
+		mDriver.setSpoofBookings();
+
+		mDriver.launchHotels();
+
+		//If the app is on a hotel confirmation screen, "NEW SEARCH"
+		//will be on that screen. Click it to get back to hotel search
+		if (mSolo.searchText(mRes.getString(R.string.NEW_SEARCH))) {
+			mSolo.clickOnText(mRes.getString(R.string.NEW_SEARCH));
+		}
+		// Open calendar and guest picker fragments
+		mDriver.delay();
+		mDriver.pressCalendar();
+		mDriver.pressGuestPicker();
+
+		//Enter and select San Francisco as destination
+		mDriver.selectLocation(mUser.mHotelSearchCity);
+
+		//Open sort dialog for hotels search
+		mDriver.pressSort();
+
+		//Filter fragment for generic string 
+		mDriver.filterFor("a");
+
+		//Select second hotel in list
+		mDriver.selectHotel(2);
+		mDriver.delay();
+
+		// From hotel info screen
+		// Check reviews and come back,
+		// Press to book room, and select 
+		// first room in list
+		mDriver.checkReviews();
+		mDriver.pressBookRoom();
+		mDriver.selectRoom(0);
+		mDriver.delay();
+
+		//Go through the log in and booking processes
+		mDriver.logInAndBook();
+		mDriver.mLocaleUtils.setLocale(testingLocale);
+		mDriver.delay(5);
+
+		//Look at the flights search screen
+		mDriver.checkFlightsScreen();
+
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		//Robotium will finish all the activities that have been opened
+		mDriver.enterLog(TAG, "tearing down...");
+
+		mSolo.finishOpenedActivities();
+	}
+
+}
