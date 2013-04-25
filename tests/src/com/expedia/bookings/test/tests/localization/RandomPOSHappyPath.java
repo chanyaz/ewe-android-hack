1099aca9cb4fc4a26cd9b0c120180da6b3b9c5f1
Kevin Carpenter <kevc@mobiata.com>
2013-04-25 14:47:01 -0400

    Wrap method's code in try-catch and implement method for appending locale to file

--- tests/src/com/expedia/bookings/test/tests/localization/RandomPOSHappyPath.java
+++ tests/src/com/expedia/bookings/test/tests/localization/RandomPOSHappyPath.java
@@ -7,10 +7,13 @@ package com.expedia.bookings.test.tests.localization;
 
 import java.util.Locale;
 
+import ErrorsAndExceptions.OutOfPOSException;
 import android.content.res.Resources;
 import android.os.Environment;
 import android.test.ActivityInstrumentationTestCase2;
+import junit.framework.AssertionFailedError;
 import android.util.DisplayMetrics;
+import android.util.Log;
 
 import com.expedia.bookings.R;
 import com.expedia.bookings.activity.SearchActivity;
@@ -53,60 +56,73 @@ public class RandomPOSHappyPath extends
 	// Test Drivers
 
 	public void testBookings() throws Exception {
-		
-		//Limit to two POSs at a time.
-		for (int i = 0; i < 2; i++) {
-			mDriver.setAllowScreenshots(true);
-			//change device locale and set variable
-			Locale testingLocale = mDriver.mLocaleUtils.selectNextLocaleFromInternalList(LOCALE_LIST_LOCATION);
-			mDriver.enterLog(TAG, "Starting sweep of " + testingLocale.toString());
+		try {
+			//Limit to two POSs at a time.
+			for (int i = 0; i < 2; i++) {
+				mDriver.setAllowScreenshots(true);
+				//change device locale and set variable
+				Locale testingLocale = mDriver.mLocaleUtils.selectNextLocaleFromInternalList(LOCALE_LIST_LOCATION);
+				mDriver.enterLog(TAG, "Starting sweep of " + testingLocale.toString());
 
-			mDriver.changeAPI("Production");
+				mDriver.changeAPI("Production");
 
-			//Reset screenshot file name to start with index 1
-			mDriver.setScreenshotCount(1);
+				//Reset screenshot file name to start with index 1
+				mDriver.setScreenshotCount(1);
 
-			mDriver.changePOS(testingLocale);
-			mDriver.setSpoofBookings();
+				mDriver.changePOS(testingLocale);
+				mDriver.setSpoofBookings();
 
-			mDriver.launchHotels();
+				mDriver.launchHotels();
 
-			//If the app is on a hotel confirmation screen, "NEW SEARCH"
-			//will be on that screen. Click it to get back to hotel search
-			if (mSolo.searchText(mRes.getString(R.string.NEW_SEARCH))) {
-				mSolo.clickOnText(mRes.getString(R.string.NEW_SEARCH));
+				//If the app is on a hotel confirmation screen, "NEW SEARCH"
+				//will be on that screen. Click it to get back to hotel search
+				if (mSolo.searchText(mRes.getString(R.string.NEW_SEARCH))) {
+					mSolo.clickOnText(mRes.getString(R.string.NEW_SEARCH));
+				}
+				// Open calendar and guest picker fragments
+				mDriver.delay();
+				mDriver.pressCalendar();
+				mDriver.pressGuestPicker();
+
+				//Enter and select San Francisco as destination
+				mDriver.selectLocation(mUser.mHotelSearchCity);
+
+				//Open sort dialog for hotels search
+				mDriver.pressSort();
+
+				//Filter fragment for generic string 
+				mDriver.filterFor("a");
+
+				//Select second hotel in list
+				mDriver.selectHotel(2);
+				mDriver.delay();
+
+				// From hotel info screen
+				// Check reviews and come back,
+				// Press to book room, and select 
+				// first room in list
+				mDriver.checkReviews();
+				mDriver.pressBookRoom();
+				mDriver.selectRoom(0);
+				mDriver.delay();
+
+				//Go through the log in and booking processes
+				mDriver.logInAndBook();
+				mDriver.mLocaleUtils.setLocale(testingLocale);
+				mDriver.delay(5);
 			}
-			// Open calendar and guest picker fragments
-			mDriver.delay();
-			mDriver.pressCalendar();
-			mDriver.pressGuestPicker();
-
-			//Enter and select San Francisco as destination
-			mDriver.selectLocation(mUser.mHotelSearchCity);
-
-			//Open sort dialog for hotels search
-			mDriver.pressSort();
-
-			//Filter fragment for generic string 
-			mDriver.filterFor("a");
-
-			//Select second hotel in list
-			mDriver.selectHotel(2);
-			mDriver.delay();
-
-			// From hotel info screen
-			// Check reviews and come back,
-			// Press to book room, and select 
-			// first room in list
-			mDriver.checkReviews();
-			mDriver.pressBookRoom();
-			mDriver.selectRoom(0);
-			mDriver.delay();
-
-			//Go through the log in and booking processes
-			mDriver.logInAndBook();
-			mDriver.mLocaleUtils.setLocale(testingLocale);
-			mDriver.delay(5);
+		}
+		catch (OutOfPOSException e) {
+			Log.e(TAG, "POSHappyPath out of POSs. Throwing exception");
+			throw e;
+		}
+		catch (Exception e) {
+			Log.e(TAG, "Caught exception. Rewriting locale list on device.");
+			mDriver.mLocaleUtils.appendCurrentLocaleBackOnToList(LOCALE_LIST_LOCATION);
+		}
+		catch (Error e) {
+			Log.e(TAG, "Caught error. Rewriting locale list on device.");
+			mDriver.mLocaleUtils.appendCurrentLocaleBackOnToList(LOCALE_LIST_LOCATION);
 		}
 	}
 
