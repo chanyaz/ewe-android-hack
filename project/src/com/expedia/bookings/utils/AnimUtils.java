e18aebd42098592731ab6a47d7d8b3f5ffddca7b
Daniel Lew <dan@mobiata.com>
2013-05-23 16:13:06 -0500

    Added new AnimUtil alpha fade for PVH

--- project/src/com/expedia/bookings/utils/AnimUtils.java
+++ project/src/com/expedia/bookings/utils/AnimUtils.java
@@ -8,6 +8,7 @@ import android.view.View;
 import com.nineoldandroids.animation.Animator;
 import com.nineoldandroids.animation.AnimatorSet;
 import com.nineoldandroids.animation.ObjectAnimator;
+import com.nineoldandroids.animation.PropertyValuesHolder;
 import com.nineoldandroids.animation.ValueAnimator;
 
 public class AnimUtils {
@@ -15,8 +16,26 @@ public class AnimUtils {
 	private static final float[] FADE_IN = new float[] { 0, 1 };
 	private static final float[] FADE_OUT = new float[] { 1, 0 };
 
+	private static PropertyValuesHolder sFadeEnterPvh;
+	private static PropertyValuesHolder sFadeExitPvh;
+
 	public static Animator createFadeAnimator(View v, boolean enter) {
 		return ObjectAnimator.ofFloat(v, "alpha", enter ? FADE_IN : FADE_OUT);
+	}
+
+	public static PropertyValuesHolder createFadePropertyValuesHolder(boolean enter) {
+		if (enter) {
+			if (sFadeEnterPvh == null) {
+				sFadeEnterPvh = PropertyValuesHolder.ofFloat("alpha", FADE_IN);
+			}
+			return sFadeEnterPvh;
+		}
+		else {
+			if (sFadeExitPvh == null) {
+				sFadeExitPvh = PropertyValuesHolder.ofFloat("alpha", FADE_OUT);
+			}
+			return sFadeExitPvh;
+		}
 	}
 
 	public static AnimatorSet playTogether(Collection<Animator> items) {
