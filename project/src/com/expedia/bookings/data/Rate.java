c89d4fdf76c5e9cc132ecafecdce55aa868555e4
Brad McManus <brad@mobiata.com>
2013-06-05 10:18:42 -0700

    Revert "Fix NPE from the dev server that is causing Susanna trouble"
    This reverts commit 8be294f242b2b3ce4ea1e11f0a68e5518b782dc1.

--- project/src/com/expedia/bookings/data/Rate.java
+++ project/src/com/expedia/bookings/data/Rate.java
@@ -98,14 +98,12 @@ public class Rate implements JSONable {
 	}
 
 	public String getFormattedBedNames() {
-		if (mBedTypes == null) {
-			return "";
-		}
-
 		ArrayList<String> bedNames = new ArrayList();
+
 		for (BedType bed : mBedTypes) {
 			bedNames.add(bed.getBedTypeDescription());
 		}
+
 		return StrUtils.joinWithoutEmpties(", ", bedNames);
 	}
 
