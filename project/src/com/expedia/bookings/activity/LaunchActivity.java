5f2d85b943f9bda4ae95692708cb3cccb119c276
Doug Melton <doug@mobiata.com>
2013-06-10 16:55:36 -0700

    Restored a missing import

--- project/src/com/expedia/bookings/activity/LaunchActivity.java
+++ project/src/com/expedia/bookings/activity/LaunchActivity.java
@@ -10,6 +10,7 @@ import org.json.JSONObject;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
