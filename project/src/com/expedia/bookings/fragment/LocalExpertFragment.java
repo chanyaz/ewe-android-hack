ad016719df8e56984a1e4c009b372125e73aedb0
Daniel Lew <dan@mobiata.com>
2013-06-13 11:11:29 -0500

    Added placeholder Fragment
    So that multiple people can work on this simultaneously without clobbering
    the files each other work on.

---
+++ project/src/com/expedia/bookings/fragment/LocalExpertFragment.java
@@ -0,0 +1,20 @@
+package com.expedia.bookings.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LocalExpertFragment extends Fragment {
+
+	public static LocalExpertFragment newInstance() {
+		return new LocalExpertFragment();
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+
+}
