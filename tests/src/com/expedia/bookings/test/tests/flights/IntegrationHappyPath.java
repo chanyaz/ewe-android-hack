d9db14b896cc8bf232cf2e30d190e7e9743fb67e
Kevin Carpenter <kevc@mobiata.com>
2013-06-28 15:29:53 -0700

    Add IntegrationHappyPath tests for flights and hotels

---
+++ tests/src/com/expedia/bookings/test/tests/flights/IntegrationHappyPath.java
@@ -0,0 +1,62 @@
+package com.expedia.bookings.test.tests.flights;
+
+import ErrorsAndExceptions.IntegrationFailureError;
+import android.content.res.Resources;
+import android.test.ActivityInstrumentationTestCase2;
+import android.util.DisplayMetrics;
+
+import com.expedia.bookings.activity.SearchActivity;
+import com.expedia.bookings.test.utils.HotelsRobotHelper;
+import com.expedia.bookings.test.utils.HotelsUserData;
+import com.jayway.android.robotium.solo.Solo;
+
+public class IntegrationHappyPath extends ActivityInstrumentationTestCase2<SearchActivity> {
+	public IntegrationHappyPath() { //Default constructor
+		super("com.expedia.bookings", SearchActivity.class);
+	}
+
+	private Solo mSolo;
+	private static final String TAG = "RotateHappyPath";
+	private Resources mRes;
+	DisplayMetrics mMetric;
+	private HotelsRobotHelper mDriver;
+	private HotelsUserData mUser;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		mSolo = new Solo(getInstrumentation(), getActivity());
+		mUser = new HotelsUserData();
+		mRes = getActivity().getBaseContext().getResources();
+		mMetric = mRes.getDisplayMetrics();
+		mDriver = new HotelsRobotHelper(mSolo, mRes, mUser);
+
+		mDriver.setScreenshotCount(1);
+		mDriver.setAllowOrientationChange(false);
+		mDriver.setWriteEventsToFile(false);
+		mUser.setAirportsToRandomUSAirports();
+	}
+
+	// This test goes through a prototypical flight booking
+	// UI flow, through checkout.
+	// It runs pulling from the Integration API
+
+	public void testMethod() throws Exception {
+		mSolo.clickOnScreen(50, 50);
+		mDriver.changePOS(mDriver.mLocaleUtils.FLIGHTS_LOCALES[2]);
+		mDriver.changeAPI("Integration");
+
+		try {
+			mDriver.flightsHappyPath(mUser.mDepartureAirport, mUser.mArrivalAirport, 1, false, false);
+		}
+		catch (IntegrationFailureError e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		//Robotium will finish all the activities that have been opened
+		mDriver.enterLog(TAG, "tearing down...");
+		mSolo.finishOpenedActivities();
+	}
+}
---
+++ tests/src/com/expedia/bookings/test/tests/hotels/IntegrationHappyPath.java
@@ -0,0 +1,65 @@
+package com.expedia.bookings.test.tests.hotels;
+
+import android.content.res.Resources;
+import android.test.ActivityInstrumentationTestCase2;
+import android.util.DisplayMetrics;
+
+import com.expedia.bookings.activity.SearchActivity;
+import com.expedia.bookings.test.utils.HotelsRobotHelper;
+import com.expedia.bookings.test.utils.HotelsUserData;
+import com.jayway.android.robotium.solo.Solo;
+
+public class IntegrationHappyPath extends ActivityInstrumentationTestCase2<SearchActivity> {
+
+	public IntegrationHappyPath() {
+		super("com.expedia.bookings", SearchActivity.class);
+	}
+
+	private static final String TAG = "SearchTest";
+
+	private Solo mSolo;
+
+	private Resources mRes;
+	DisplayMetrics mMetric;
+	private HotelsRobotHelper mDriver;
+	private HotelsUserData mUser;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		mSolo = new Solo(getInstrumentation(), getActivity());
+		//Log.configureLogging("ExpediaBookings", true);
+
+		mRes = getActivity().getBaseContext().getResources();
+		mMetric = mRes.getDisplayMetrics();
+		mDriver = new HotelsRobotHelper(mSolo, mRes);
+		mUser = new HotelsUserData();
+	}
+
+	// This test goes through a prototypical hotel booking
+	// UI flow, through check out.
+	// It runs pulling from the Production API
+	
+	public void testMethod() throws Exception {
+		mUser.setHotelCityToRandomUSCity();
+		mDriver.setAllowScreenshots(false);
+		mDriver.setAllowOrientationChange(false);
+		
+		mDriver.changePOS(mDriver.mLocaleUtils.AMERICAN_LOCALES[5]);
+		mDriver.changeAPI("Integration");
+		mDriver.clearPrivateData();
+		
+		mDriver.launchHotels();
+		mDriver.delay();
+		mDriver.browseRooms(4, mUser.mHotelSearchCity, true);
+
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		//Robotium will finish all the activities that have been opened
+		mDriver.enterLog(TAG, "tearing down...");
+
+		mSolo.finishOpenedActivities();
+	}
+
+}
