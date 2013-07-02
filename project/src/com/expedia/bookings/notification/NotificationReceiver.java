32c36fc00c7a33560445d13894bae7f1548c852a
Daniel Lew <dan@mobiata.com>
2013-07-02 08:39:15 -0500

    1457: Guard against recycled Bitmaps
    While somewhat of a narrow race condition, the problem seemed to be
    that the Bitmap being loaded by TwoLevelImageCache was being recycled
    before we could stuff it into a notification.  Now we make a copy of
    the Bitmap so that it can't be recycled without us knowing.

--- project/src/com/expedia/bookings/notification/NotificationReceiver.java
+++ project/src/com/expedia/bookings/notification/NotificationReceiver.java
@@ -222,7 +222,8 @@ public class NotificationReceiver extends BroadcastReceiver {
 
 			@Override
 			public void onImageLoaded(String url, Bitmap bitmap) {
-				mBitmap = bitmap;
+				// #1457 - We make a copy so that the TwoLevelImageCache can't recycle it from underneath us
+				mBitmap = bitmap.copy(bitmap.getConfig(), false);
 				display();
 			}
 		};
