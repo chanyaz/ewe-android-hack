4173dbb43a1ca66ea575ba452479f25f898ee94d
Kevin Carpenter <kevc@mobiata.com>
2013-07-18 16:53:27 -0700

    Implement takeScreenshotUponFailure in FullHappyPath test

--- tests/src/com/expedia/bookings/test/tests/full/FullHappyPathTest.java
+++ tests/src/com/expedia/bookings/test/tests/full/FullHappyPathTest.java
@@ -10,20 +10,19 @@ import com.expedia.bookings.test.utils.HotelsRobotHelper;
 import com.expedia.bookings.test.utils.HotelsUserData;
 import com.jayway.android.robotium.solo.Solo;
 
-
 public class FullHappyPathTest extends ActivityInstrumentationTestCase2<SearchActivity> {
-	
+
 	public FullHappyPathTest() { //Default constructor
 		super("com.expedia.bookings", SearchActivity.class);
 	}
-	
+
 	private Solo mSolo;
-	private static final String TAG = "RotateHappyPath";
+	private static final String TAG = "Full Happy Path Test";
 	private Resources mRes;
 	DisplayMetrics mMetric;
 	private HotelsRobotHelper mDriver;
 	private HotelsUserData mUser;
-	
+
 	protected void setUp() throws Exception {
 		super.setUp();
 		mSolo = new Solo(getInstrumentation(), getActivity());
@@ -31,11 +30,11 @@ public class FullHappyPathTest extends ActivityInstrumentationTestCase2<SearchAc
 
 		mRes = getActivity().getBaseContext().getResources();
 		mMetric = mRes.getDisplayMetrics();
-		
+
 		mDriver = new HotelsRobotHelper(mSolo, mRes);
 		mDriver.setScreenshotCount(1);
 		mDriver.setAllowOrientationChange(false);
-		
+
 		mUser = new HotelsUserData();
 		mUser.setHotelCityToRandomUSCity();
 		mUser.setAirportsToRandomUSAirports();
@@ -45,13 +44,23 @@ public class FullHappyPathTest extends ActivityInstrumentationTestCase2<SearchAc
 	// Test Driver 
 
 	public void testMethod() throws Exception {
-		mDriver.setSpoofBookings();
-		mDriver.clearPrivateData();
-		
-		mDriver.launchHotels();
-		mDriver.browseRooms(4, mUser.mHotelSearchCity, true);
-		
-		mDriver.flightsHappyPath(mUser.mDepartureAirport, mUser.mArrivalAirport, 1, true, false);
+		try {
+			mDriver.setSpoofBookings();
+			mDriver.clearPrivateData();
+
+			mDriver.launchHotels();
+			mDriver.browseRooms(4, mUser.mHotelSearchCity, true);
+
+			mDriver.flightsHappyPath(mUser.mDepartureAirport, mUser.mArrivalAirport, 1, true, false);
+		}
+		catch (Error e) {
+			mDriver.takeScreenshotUponFailure(e, TAG);
+			throw e;
+		}
+		catch (Exception e) {
+			mDriver.takeScreenshotUponFailure(e, TAG);
+			throw e;
+		}
 	}
 
 	@Override
@@ -62,5 +71,3 @@ public class FullHappyPathTest extends ActivityInstrumentationTestCase2<SearchAc
 		mSolo.finishOpenedActivities();
 	}
 }
-
-
