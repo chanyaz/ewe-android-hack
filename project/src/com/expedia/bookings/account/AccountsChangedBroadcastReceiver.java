3b04d7b66c19257190d5e16ba3d0766590c5da86
Joel Drotos <jdrotos@mobiata.com>
2013-07-19 12:14:00 -0500

    Added BroadcastReceiver for account changes.
    This will log us out if someone logs out of our account via AccountManager

---
+++ project/src/com/expedia/bookings/account/AccountsChangedBroadcastReceiver.java
@@ -0,0 +1,34 @@
+package com.expedia.bookings.account;
+
+import com.expedia.bookings.R;
+import com.expedia.bookings.data.User;
+import com.mobiata.android.Log;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * This receiver listens for LOGIN_ACCOUNTS_CHANGED_ACTION broadcast, 
+ * and ensures that if there are no Expedia accounts, that our user is
+ * logged out.
+ */
+public class AccountsChangedBroadcastReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		//This is only used for logging out users, so if the app thinks we are logged out, then we are good.
+		if (User.isLoggedInOnDisk(context)) {
+			String accountType = context.getString(R.string.expedia_account_type_identifier);
+			AccountManager manager = AccountManager.get(context);
+			Account[] accounts = manager.getAccountsByType(accountType);
+			if (accounts == null || accounts.length == 0) {
+				Log.d("AccountsChangedBroadcastReceiver signing out user.");
+				User.signOut(context);
+			}
+		}
+	}
+
+}
