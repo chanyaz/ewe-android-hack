e1ad7ca3e86e6ff39c77cdb1dc0405b9442a84ea
Kevin Carpenter <kevc@mobiata.com>
2013-07-22 12:10:17 -0700

    Add a delay before trying to set spoof/suppressed bookings on tablet

--- tests/src/com/expedia/bookings/test/utils/TabletsRobotHelper.java
+++ tests/src/com/expedia/bookings/test/utils/TabletsRobotHelper.java
@@ -41,12 +41,13 @@ public class TabletsRobotHelper extends HotelsRobotHelper {
 		boolean suppressFlightsDone = false;
 
 		screenshot("Tablet - main screen");
-		
+
 		mSolo.sendKey(Solo.MENU);
 		mSolo.clickOnText(mRes.getString(R.string.Settings));
 
 		screenshot("Tablet - settings");
 
+		delay(1);
 		mSolo.scrollDown();
 		ArrayList<View> a = mSolo.getCurrentViews();
 		for (int i = 0; i < a.size(); i++) {
@@ -101,7 +102,7 @@ public class TabletsRobotHelper extends HotelsRobotHelper {
 		catch (Error e) {
 			enterLog(TAG,
 					"Error when trying to press to search for hotels. " +
-					"Keyboard may be displayed, so trying to close it.");
+							"Keyboard may be displayed, so trying to close it.");
 			mSolo.goBack();
 			delay(1);
 			mSolo.clickOnView(searchForHotelsBtn);
