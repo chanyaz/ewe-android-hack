bee17fd2370301eb7ff631cfa7a44f496ef6282e
Supreeth <supreeth@mobiata.com>
2013-07-24 14:13:36 -0700

    1619. VSC Add train app link in hotel confirmation page

--- project/src/com/expedia/bookings/fragment/ConfirmationFragment.java
+++ project/src/com/expedia/bookings/fragment/ConfirmationFragment.java
@@ -1,11 +1,14 @@
 package com.expedia.bookings.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.expedia.bookings.R;
@@ -14,6 +17,7 @@ import com.expedia.bookings.activity.WebViewActivity;
 import com.expedia.bookings.data.Db;
 import com.expedia.bookings.data.pos.PointOfSale;
 import com.mobiata.android.SocialUtils;
+import com.mobiata.android.util.AndroidUtils;
 import com.mobiata.android.util.Ui;
 
 public abstract class ConfirmationFragment extends Fragment {
@@ -23,6 +27,8 @@ public abstract class ConfirmationFragment extends Fragment {
 	protected abstract int getActionsLayoutId();
 
 	protected abstract String getItinNumber();
+
+	private final static String PKG_VSC_VOYAGES = "com.vsct.vsc.mobile.horaireetresa.android";
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -38,9 +44,30 @@ public abstract class ConfirmationFragment extends Fragment {
 		inflater.inflate(getActionsLayoutId(), actionContainer, true);
 
 		// 1617. VSC Contact URL
+		// 1619. VSC Add VSC train app cross sell
 		if (ExpediaBookingApp.IS_VSC) {
 			TextView actionTextView = Ui.findView(v, R.id.call_action_text_view);
 			actionTextView.setText(R.string.vsc_customer_support);
+
+			View vscAppDivider = Ui.findView(v, R.id.vsc_app_divider);
+			vscAppDivider.setVisibility(View.VISIBLE);
+
+			LinearLayout vscAppCrossSellLayout = Ui.findView(v, R.id.vscAppCrossSellLayout);
+			vscAppCrossSellLayout.setVisibility(View.VISIBLE);
+			vscAppCrossSellLayout.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					SocialUtils.openSite(getActivity(), AndroidUtils.getGooglePlayAppLink(PKG_VSC_VOYAGES));
+				}
+			});
+
+			TextView rowTitleView = Ui.findView(v, R.id.row_title);
+			rowTitleView.setText(R.string.VSC_Voyages_SNF);
+			TextView description_view = Ui.findView(v, R.id.row_description);
+			description_view.setText(R.string.VSC_Voyages_SNF_description);
+			ImageView imageView = Ui.findView(v, R.id.image);
+			imageView.setImageResource(R.drawable.ic_vsc_train_app);
 		}
 
 		Ui.setOnClickListener(v, R.id.call_action_text_view, new OnClickListener() {
