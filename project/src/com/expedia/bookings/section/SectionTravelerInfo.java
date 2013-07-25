ee083d21fd1290350f1e928e9a2051a31c3bacae
Supreeth <supreeth@mobiata.com>
2013-07-24 17:41:37 -0700

    1597. VSC Set phone country code to France, in travelerInfo screen

--- project/src/com/expedia/bookings/section/SectionTravelerInfo.java
+++ project/src/com/expedia/bookings/section/SectionTravelerInfo.java
@@ -31,6 +31,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.expedia.bookings.R;
+import com.expedia.bookings.activity.ExpediaBookingApp;
 import com.expedia.bookings.data.Date;
 import com.expedia.bookings.data.Phone;
 import com.expedia.bookings.data.Traveler;
@@ -998,8 +999,12 @@ public class SectionTravelerInfo extends LinearLayout implements ISection<Travel
 				}
 			}
 			else {
-				final String targetCountry = mContext.getString(PointOfSale.getPointOfSale()
+				String targetCountry = mContext.getString(PointOfSale.getPointOfSale()
 						.getCountryNameResId());
+				// 1597. VSC Explicity set phone country code to France.
+				if (ExpediaBookingApp.IS_VSC) {
+					targetCountry = mContext.getString(R.string.country_fr);
+				}
 				for (int i = 0; i < adapter.getCount(); i++) {
 					if (targetCountry.equalsIgnoreCase(adapter.getCountryName(i))) {
 						getField().setSelection(i);
