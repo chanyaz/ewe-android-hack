e10c320e8fd59fce589489cd626f18d52e103a47
Kevin Carpenter <kevc@mobiata.com>
2013-07-31 16:03:46 -0700

    Add Mock data tests for EHad on phones

---
+++ tests/src/com/expedia/bookings/test/tests/hotels/MockDataTests.java
@@ -0,0 +1,130 @@
+package com.expedia.bookings.test.tests.hotels;
+
+import android.content.res.Resources;
+import android.test.ActivityInstrumentationTestCase2;
+import android.util.DisplayMetrics;
+import android.widget.ListView;
+
+import com.expedia.bookings.R;
+import com.expedia.bookings.activity.SearchActivity;
+import com.expedia.bookings.test.utils.HotelsRobotHelper;
+import com.expedia.bookings.test.utils.HotelsUserData;
+import com.jayway.android.robotium.solo.Solo;
+
+public class MockDataTests extends ActivityInstrumentationTestCase2<SearchActivity> {
+
+	public MockDataTests() {
+		super("com.expedia.bookings", SearchActivity.class);
+	}
+
+	private static final String TAG = "Mock Data Tests";
+
+	private Solo mSolo;
+
+	private Resources mRes;
+	DisplayMetrics mMetric;
+	private HotelsRobotHelper mDriver;
+	private HotelsUserData mUser;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		mSolo = new Solo(getInstrumentation(), getActivity());
+		mRes = getActivity().getBaseContext().getResources();
+		mMetric = mRes.getDisplayMetrics();
+		mDriver = new HotelsRobotHelper(mSolo, mRes);
+		mUser = new HotelsUserData();
+		mDriver.setUpMockServer("172.17.251.115");
+		mDriver.clearPrivateData();
+	}
+
+	// Verify that hotel room with raised price shows proper error message
+	public void testHotelRateUp() throws Exception {
+		mDriver.launchHotels();
+		mSolo.clickOnText("rateup");
+		mSolo.clickOnButton(0);
+		mDriver.selectRoom(0);
+		String alertText = (String) mSolo.getText(0).getText();
+		String expectedText = mRes.getString(R.string.the_hotel_raised_the_total_price_TEMPLATE, "$400.65", "$440.65");
+		mDriver.enterLog(TAG, "Expected text: " + expectedText);
+		mDriver.enterLog(TAG, "Alert text: " + alertText);
+		if (!expectedText.equals(alertText)) {
+			fail();
+		}
+	}
+
+	// Verify that hotel room with lowered price shows proper error message
+	public void testHotelRateDown() throws Exception {
+		mDriver.launchHotels();
+		mSolo.clickOnText("ratedown");
+		mSolo.clickOnButton(0);
+		mDriver.selectRoom(0);
+		String alertText = (String) mSolo.getText(0).getText();
+		String expectedText = mRes.getString(R.string.the_hotel_lowered_the_total_price_TEMPLATE, "$400.65", "$360.65");
+		mDriver.enterLog(TAG, "Expected text: " + expectedText);
+		mDriver.enterLog(TAG, "Alert text: " + alertText);
+		if (!expectedText.equals(alertText)) {
+			fail();
+		}
+	}
+
+	// Verify sold out room shows correct error message
+	public void testRoomNoLongerAvailable() throws Exception {
+		mDriver.launchHotels();
+		mSolo.clickOnText("no_rooms");
+		mSolo.clickOnButton(0);
+		mDriver.selectRoom(0);
+		String alertText = (String) mSolo.getText(0).getText();
+		String expectedText = mRes.getString(R.string.error_hotel_is_now_sold_out);
+		mDriver.enterLog(TAG, "Expected text: " + expectedText);
+		mDriver.enterLog(TAG, "Alert text: " + alertText);
+		if (!expectedText.equals(alertText)) {
+			fail();
+		}
+		String ok = mRes.getString(R.string.ok);
+		mSolo.clickOnText(ok);
+	}
+
+	// Go through each room in the list, verify the sold out message
+	// then verify that the hotel is sold out
+	public void testRoomsAndRatesNoMoreRooms() throws Exception {
+		mDriver.launchHotels();
+		mSolo.clickOnText("multi_room_unavailable");
+		mSolo.clickOnButton(0);
+		mDriver.delay();
+		ListView lv = (ListView) mSolo.getView(android.R.id.list);
+		int upperBound = lv.getChildCount();
+		mDriver.enterLog(TAG, "Upper bounds: " + upperBound);
+		String ok;
+		for (int i = 0; i < upperBound; i++) {
+			mSolo.clickInList(0);
+			mDriver.delay();
+			String alertText = (String) mSolo.getText(0).getText();
+			String expectedText = mRes.getString(R.string.e3_error_checkout_hotel_room_unavailable);
+			mDriver.enterLog(TAG, "Expected text: " + expectedText);
+			mDriver.enterLog(TAG, "Alert text: " + alertText);
+			if (!expectedText.equals(alertText)) {
+				fail();
+			}
+			ok = mRes.getString(R.string.ok);
+			mSolo.clickOnText(ok);
+			mDriver.delay(1);
+		}
+		String alertText = (String) mSolo.getText(0).getText();
+		String expectedText = mRes.getString(R.string.error_hotel_is_now_sold_out);
+		mDriver.enterLog(TAG, "Expected text: " + expectedText);
+		mDriver.enterLog(TAG, "Alert text: " + alertText);
+		if (!expectedText.equals(alertText)) {
+			fail();
+		}
+		ok = mRes.getString(R.string.ok);
+		mSolo.clickOnText(ok);
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		//Robotium will finish all the activities that have been opened
+		mDriver.enterLog(TAG, "tearing down...");
+		mSolo.finishOpenedActivities();
+	}
+
+}
