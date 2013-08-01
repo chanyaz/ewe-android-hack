c6c95c71b4e6de96b610be1b596ab473b7e2b0b6
Kevin Carpenter <kevc@mobiata.com>
2013-08-01 14:08:58 -0700

    Update MockDataTests IP and implement waitForStringToBeGone

--- tests/src/com/expedia/bookings/test/tests/hotels/MockDataTests.java
+++ tests/src/com/expedia/bookings/test/tests/hotels/MockDataTests.java
@@ -26,6 +26,9 @@ public class MockDataTests extends ActivityInstrumentationTestCase2<SearchActivi
 	private HotelsRobotHelper mDriver;
 	private HotelsUserData mUser;
 
+	//SF-slave-android-3 is perpetually running E3MockServer at this IP
+	private static final String SERVER_IP = "172.17.249.246";
+
 	protected void setUp() throws Exception {
 		super.setUp();
 		mSolo = new Solo(getInstrumentation(), getActivity());
@@ -33,16 +36,18 @@ public class MockDataTests extends ActivityInstrumentationTestCase2<SearchActivi
 		mMetric = mRes.getDisplayMetrics();
 		mDriver = new HotelsRobotHelper(mSolo, mRes);
 		mUser = new HotelsUserData();
-		mDriver.setUpMockServer("172.17.251.115");
+		mDriver.setUpMockServer(SERVER_IP);
 		mDriver.clearPrivateData();
 	}
 
 	// Verify that hotel room with raised price shows proper error message
 	public void testHotelRateUp() throws Exception {
 		mDriver.launchHotels();
+		mDriver.waitForStringToBeGone(R.string.progress_searching_hotels);
 		mSolo.clickOnText("rateup");
 		mSolo.clickOnButton(0);
 		mDriver.selectRoom(0);
+		mDriver.waitForStringToBeGone(R.string.calculating_taxes_and_fees);
 		String alertText = (String) mSolo.getText(0).getText();
 		String expectedText = mRes.getString(R.string.the_hotel_raised_the_total_price_TEMPLATE, "$400.65", "$440.65");
 		mDriver.enterLog(TAG, "Expected text: " + expectedText);
@@ -55,9 +60,11 @@ public class MockDataTests extends ActivityInstrumentationTestCase2<SearchActivi
 	// Verify that hotel room with lowered price shows proper error message
 	public void testHotelRateDown() throws Exception {
 		mDriver.launchHotels();
+		mDriver.waitForStringToBeGone(R.string.progress_searching_hotels);
 		mSolo.clickOnText("ratedown");
 		mSolo.clickOnButton(0);
 		mDriver.selectRoom(0);
+		mDriver.waitForStringToBeGone(R.string.calculating_taxes_and_fees);
 		String alertText = (String) mSolo.getText(0).getText();
 		String expectedText = mRes.getString(R.string.the_hotel_lowered_the_total_price_TEMPLATE, "$400.65", "$360.65");
 		mDriver.enterLog(TAG, "Expected text: " + expectedText);
@@ -70,9 +77,11 @@ public class MockDataTests extends ActivityInstrumentationTestCase2<SearchActivi
 	// Verify sold out room shows correct error message
 	public void testRoomNoLongerAvailable() throws Exception {
 		mDriver.launchHotels();
+		mDriver.waitForStringToBeGone(R.string.progress_searching_hotels);
 		mSolo.clickOnText("no_rooms");
 		mSolo.clickOnButton(0);
 		mDriver.selectRoom(0);
+		mDriver.waitForStringToBeGone(R.string.calculating_taxes_and_fees);
 		String alertText = (String) mSolo.getText(0).getText();
 		String expectedText = mRes.getString(R.string.error_hotel_is_now_sold_out);
 		mDriver.enterLog(TAG, "Expected text: " + expectedText);
@@ -88,16 +97,17 @@ public class MockDataTests extends ActivityInstrumentationTestCase2<SearchActivi
 	// then verify that the hotel is sold out
 	public void testRoomsAndRatesNoMoreRooms() throws Exception {
 		mDriver.launchHotels();
+		mDriver.waitForStringToBeGone(R.string.progress_searching_hotels);
 		mSolo.clickOnText("multi_room_unavailable");
 		mSolo.clickOnButton(0);
-		mDriver.delay();
+		mDriver.waitForStringToBeGone(R.string.room_rates_loading);
 		ListView lv = (ListView) mSolo.getView(android.R.id.list);
 		int upperBound = lv.getChildCount();
 		mDriver.enterLog(TAG, "Upper bounds: " + upperBound);
 		String ok;
 		for (int i = 0; i < upperBound; i++) {
 			mSolo.clickInList(0);
-			mDriver.delay();
+			mDriver.waitForStringToBeGone(R.string.calculating_taxes_and_fees);
 			String alertText = (String) mSolo.getText(0).getText();
 			String expectedText = mRes.getString(R.string.e3_error_checkout_hotel_room_unavailable);
 			mDriver.enterLog(TAG, "Expected text: " + expectedText);
@@ -109,6 +119,7 @@ public class MockDataTests extends ActivityInstrumentationTestCase2<SearchActivi
 			mSolo.clickOnText(ok);
 			mDriver.delay(1);
 		}
+		mDriver.delay(3);
 		String alertText = (String) mSolo.getText(0).getText();
 		String expectedText = mRes.getString(R.string.error_hotel_is_now_sold_out);
 		mDriver.enterLog(TAG, "Expected text: " + expectedText);
