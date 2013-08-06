9e87f7f49cdff4c2d26007b6274abb4142bb6512
Daniel Lew <dan@mobiata.com>
2013-08-06 11:34:26 -0500

    Added support for home deep link

--- project/src/com/expedia/bookings/activity/DeepLinkRouterActivity.java
+++ project/src/com/expedia/bookings/activity/DeepLinkRouterActivity.java
@@ -1,7 +1,12 @@
 package com.expedia.bookings.activity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+
+import com.expedia.bookings.utils.NavUtils;
+import com.mobiata.android.util.Ui;
 
 /**
  * This class acts as a router for incoming deep links.  It seems a lot
@@ -15,7 +20,16 @@ public class DeepLinkRouterActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		// TODO: Handle incoming intents
+		// Handle incoming intents
+		Intent intent = getIntent();
+		Uri data = intent.getData();
+		String host = data.getHost();
+		if (host.equals("home")) {
+			NavUtils.goToLaunchScreen(this, true);
+		}
+		else {
+			Ui.showToast(this, "Cannot yet handle data: " + data);
+		}
 
 		// This Activity should never fully launch
 		finish();
