543947363b673b9f8b47a96ee6a24a13edeb6690
Daniel Lew <dan@mobiata.com>
2013-08-07 14:33:24 -0500

    Added more JodaUtils stuff
    - Convenience method for Days Between
    
    - Formatters for using DateUtils with Joda time objects

--- project/src/com/expedia/bookings/utils/JodaUtils.java
+++ project/src/com/expedia/bookings/utils/JodaUtils.java
@@ -1,14 +1,20 @@
 package com.expedia.bookings.utils;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.Days;
 import org.joda.time.LocalDate;
+import org.joda.time.ReadablePartial;
 import org.joda.time.base.AbstractPartial;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.content.Context;
+import android.text.TextUtils;
+import android.text.format.DateUtils;
+
 import com.expedia.bookings.data.Date;
 import com.mobiata.android.json.JSONUtils;
-
-import android.text.TextUtils;
 
 public class JodaUtils {
 
@@ -18,6 +24,19 @@ public class JodaUtils {
 
 	public static boolean isBeforeOrEquals(AbstractPartial first, AbstractPartial second) {
 		return first.isBefore(second) || first.isEqual(second);
+	}
+	
+	public static int daysBetween(ReadablePartial start, ReadablePartial end) {
+		return Days.daysBetween(start, end).getDays();
+	}
+
+	public static String formatLocalDate(Context context, LocalDate localDate, int flags) {
+		return formatDateTime(context, localDate.toDateTimeAtStartOfDay(), flags);
+	}
+
+	public static String formatDateTime(Context context, DateTime dateTime, int flags) {
+		DateTime utcDateTime = dateTime.withZoneRetainFields(DateTimeZone.UTC);
+		return DateUtils.formatDateTime(context, utcDateTime.getMillis(), flags | DateUtils.FORMAT_UTC);
 	}
 
 	public static void putLocalDateInJson(JSONObject obj, String key, LocalDate localDate) throws JSONException {
