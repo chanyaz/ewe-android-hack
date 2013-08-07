7ef3657019cc8027b5aac183894e40f9daf8b838
Daniel Lew <dan@mobiata.com>
2013-08-07 10:29:07 -0500

    Added JodaUtils

---
+++ project/src/com/expedia/bookings/utils/JodaUtils.java
@@ -0,0 +1,15 @@
+package com.expedia.bookings.utils;
+
+import org.joda.time.base.AbstractPartial;
+
+public class JodaUtils {
+
+	public static boolean isAfterOrEquals(AbstractPartial first, AbstractPartial second) {
+		return first.isAfter(second) || first.isEqual(second);
+	}
+
+	public static boolean isBeforeOrEquals(AbstractPartial first, AbstractPartial second) {
+		return first.isBefore(second) || first.isEqual(second);
+	}
+
+}
