5d8cf270971253dde335eb234f266682ad75b1b6
Brad McManus <brad@mobiata.com>
2013-08-08 10:43:17 -0700

    1708 add share via FT to flight itin

--- project/src/com/expedia/bookings/dialog/SocialMessageChooserDialogFragment.java
+++ project/src/com/expedia/bookings/dialog/SocialMessageChooserDialogFragment.java
@@ -1,5 +1,8 @@
 package com.expedia.bookings.dialog;
 
+import java.util.List;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
@@ -8,13 +11,23 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 
 import com.expedia.bookings.R;
+import com.expedia.bookings.data.FlightTrip;
+import com.expedia.bookings.data.trips.ItinCardDataFlight;
 import com.expedia.bookings.data.trips.TripComponent;
+import com.expedia.bookings.data.trips.TripFlight;
 import com.expedia.bookings.tracking.OmnitureTracking;
+import com.expedia.bookings.utils.NavUtils;
+import com.expedia.bookings.widget.itin.FlightItinContentGenerator;
 import com.expedia.bookings.widget.itin.ItinContentGenerator;
 import com.mobiata.android.SocialUtils;
 import com.mobiata.android.util.Ui;
+import com.mobiata.flightlib.data.Flight;
+import com.mobiata.flightlib.utils.AddFlightsIntentUtils;
 
 public class SocialMessageChooserDialogFragment extends DialogFragment {
+
+	private ItinContentGenerator mItinContentGenerator;
+
 	private String mSubject;
 	private String mShortMessage;
 	private String mLongMessage;
@@ -27,12 +40,14 @@ public class SocialMessageChooserDialogFragment extends DialogFragment {
 		String longMessage = generator.getShareTextLong();
 		TripComponent.Type type = generator.getType();
 
-		return newInstance(subject, shortMessage, longMessage, type);
+		return newInstance(generator, subject, shortMessage, longMessage, type);
 	}
 
-	private static SocialMessageChooserDialogFragment newInstance(String subject, String shortMessage,
-			String longMessage, TripComponent.Type type) {
+	private static SocialMessageChooserDialogFragment newInstance(ItinContentGenerator<?> generator, String subject,
+			String shortMessage, String longMessage, TripComponent.Type type) {
 		SocialMessageChooserDialogFragment fragment = new SocialMessageChooserDialogFragment();
+
+		fragment.mItinContentGenerator = generator;
 		fragment.mSubject = subject;
 		fragment.mShortMessage = shortMessage;
 		fragment.mLongMessage = longMessage;
@@ -70,6 +85,27 @@ public class SocialMessageChooserDialogFragment extends DialogFragment {
 				OmnitureTracking.trackItinShare(getActivity(), mType, false);
 			}
 		});
+
+		// Share with FlightTrack
+		if (mItinContentGenerator instanceof FlightItinContentGenerator) {
+			// Grab the Flight segments for the given leg/itin
+			ItinCardDataFlight cardData = (ItinCardDataFlight) mItinContentGenerator.getItinCardData();
+			FlightTrip flightTrip = ((TripFlight) cardData.getTripComponent()).getFlightTrip();
+			List<Flight> flights = flightTrip.getLeg(cardData.getLegNumber()).getSegments();
+			final Intent intent = AddFlightsIntentUtils.getIntent(flights);
+
+			if (NavUtils.canHandleIntent(getActivity(), intent)) {
+				View ft = Ui.findView(view, R.id.flighttrack_button);
+				ft.setVisibility(View.VISIBLE);
+				ft.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						startActivity(intent);
+						dismiss();
+					}
+				});
+			}
+		}
 
 		return view;
 	}
