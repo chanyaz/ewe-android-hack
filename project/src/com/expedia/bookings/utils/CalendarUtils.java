6cd5e28bc6d4df94571f4e2034f9d0a57f49650c
Daniel Lew <dan@mobiata.com>
2013-08-09 12:31:22 -0500

    Deleted sad getDaysBetween() method
    I don't want anyone to use it when Joda's better methods exist

--- project/src/com/expedia/bookings/utils/CalendarUtils.java
+++ project/src/com/expedia/bookings/utils/CalendarUtils.java
@@ -29,38 +29,6 @@ public class CalendarUtils {
 		return now < timestamp || timestamp + cutoff < now;
 	}
 
-	/**
-	 * This ACCURATELY calculates the difference (in days) between two Calendars.
-	 *
-	 * WARNING: IT IS NOT REMOTELY FAST.  It is VERY slow.  You should only use this
-	 * if you know that the difference in days has a low bound (a couple months max).
-	 * @param cal1 the first calendar
-	 * @param cal2 the second calendar
-	 * @return positive number of days between if cal1 < cal2, otherwise negative days between
-	 */
-	public static long getDaysBetween(Calendar start, Calendar end) {
-		boolean reverse = false;
-		if (start.get(Calendar.YEAR) > end.get(Calendar.YEAR)
-				|| (start.get(Calendar.YEAR) == end.get(Calendar.YEAR) && start.get(Calendar.DAY_OF_YEAR) > end
-						.get(Calendar.DAY_OF_YEAR))) {
-			Calendar tmp = end;
-			end = start;
-			start = tmp;
-			reverse = true;
-		}
-
-		Calendar cal = (Calendar) start.clone();
-		int endDay = end.get(Calendar.DAY_OF_YEAR);
-		int endYear = end.get(Calendar.YEAR);
-		long daysBetween = 0;
-		while (cal.get(Calendar.DAY_OF_YEAR) != endDay || cal.get(Calendar.YEAR) != endYear) {
-			cal.add(Calendar.DAY_OF_MONTH, 1);
-			daysBetween++;
-		}
-
-		return (reverse) ? -daysBetween : daysBetween;
-	}
-
 	public static TimeZone getFormatTimeZone() {
 		TimeZone tz = TimeZone.getTimeZone("UTC");
 		if (tz == null) {
