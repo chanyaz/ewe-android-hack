5aa60601676d0d3619e1d52076c8f5fc95a3e914
Brad McManus <brad@mobiata.com>
2013-08-12 12:19:23 -0700

    1785 prevent crash on rotation with filter

--- project/src/com/expedia/bookings/fragment/HotelListFragment.java
+++ project/src/com/expedia/bookings/fragment/HotelListFragment.java
@@ -337,8 +337,10 @@ public class HotelListFragment extends ListFragment {
 		else if (mAdapter.getCount() == 0) {
 			showPlaceholder();
 			setHeaderVisibility(View.GONE);
-			mSearchProgressBar.setText(R.string.no_filter_results);
-			mSearchProgressBar.setShowProgress(false);
+			if (mSearchProgressBar != null) {
+				mSearchProgressBar.setText(R.string.no_filter_results);
+				mSearchProgressBar.setShowProgress(false);
+			}
 		}
 		else {
 			hidePlaceholder();
