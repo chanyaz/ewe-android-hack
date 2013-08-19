18f25e9754c6a82bca1f469c7e856ec63cc18545
Kevin Carpenter <kevc@mobiata.com>
2013-08-19 16:12:23 -0700

    Change DataUtils methods to return objects, and add new methods

--- tests/src/com/expedia/bookings/test/utils/DataUtils.java
+++ tests/src/com/expedia/bookings/test/utils/DataUtils.java
@@ -4,46 +4,73 @@ import org.joda.time.LocalDate;
 
 import com.expedia.bookings.data.BillingInfo;
 import com.expedia.bookings.data.CreditCardType;
+import com.expedia.bookings.data.FlightSearchParams;
 import com.expedia.bookings.data.Location;
+import com.expedia.bookings.data.Money;
 import com.expedia.bookings.utils.CurrencyUtils;
 
 public class DataUtils {
 
-	public static void setUpBillingInfo(BillingInfo b, String emailAddress,
+	public static BillingInfo setUpBillingInfo(String emailAddress,
 			String phoneNumber, String phoneCountryCode, String firstName,
 			String lastName, String cardNumber, String cardCVV,
 			LocalDate ccExpirationDate, Location location) {
-		b.setEmail(emailAddress);
-		b.setFirstName(firstName);
-		b.setLastName(lastName);
-		b.setNameOnCard(b.getFirstName() + " " + b.getLastName());
+		BillingInfo billingInfo = new BillingInfo();
+		billingInfo.setEmail(emailAddress);
+		billingInfo.setFirstName(firstName);
+		billingInfo.setLastName(lastName);
+		billingInfo.setNameOnCard(billingInfo.getFirstName() + " " + billingInfo.getLastName());
 
-		b.setLocation(location);
+		billingInfo.setLocation(location);
 
-		b.setNumber(cardNumber);
+		billingInfo.setNumber(cardNumber);
 		CreditCardType type = CurrencyUtils
-				.detectCreditCardBrand(b.getNumber());
-		b.setBrandCode(type.getCode());
-		b.setBrandName(type.name());
+				.detectCreditCardBrand(billingInfo.getNumber());
+		billingInfo.setBrandCode(type.getCode());
+		billingInfo.setBrandName(type.name());
 
-		b.setExpirationDate(ccExpirationDate);
+		billingInfo.setExpirationDate(ccExpirationDate);
 
-		b.setSecurityCode(cardCVV);
-		b.setTelephone(phoneNumber);
-		b.setTelephoneCountryCode(phoneCountryCode);
+		billingInfo.setSecurityCode(cardCVV);
+		billingInfo.setTelephone(phoneNumber);
+		billingInfo.setTelephoneCountryCode(phoneCountryCode);
+
+		return billingInfo;
 	}
 
-	public static void setUpLocation(Location l, String city,
+	public static Location setUpLocation(String city,
 			String countryCode, String description, String addressLine,
 			String postalCode, String stateCode, double latitude,
-			double longitude) {
-		l.setCity(city);
-		l.setCountryCode(countryCode);
-		l.setDescription(description);
-		l.addStreetAddressLine(addressLine);
-		l.setPostalCode(postalCode);
-		l.setStateCode(stateCode);
-		l.setLatitude(latitude);
-		l.setLongitude(longitude);
+			double longitude, String destinationId) {
+		Location location = new Location();
+		location.setCity(city);
+		location.setCountryCode(countryCode);
+		location.setDescription(description);
+		location.addStreetAddressLine(addressLine);
+		location.setPostalCode(postalCode);
+		location.setStateCode(stateCode);
+		location.setLatitude(latitude);
+		location.setLongitude(longitude);
+		location.setDestinationId(destinationId);
+		return location;
+	}
+
+	public static FlightSearchParams setUpFlightSearchParams(Location depLoc,
+			Location arrLoc,
+			LocalDate depDate, LocalDate retDate, int numAdults) {
+		FlightSearchParams params = new FlightSearchParams();
+		params.setDepartureLocation(depLoc);
+		params.setArrivalLocation(arrLoc);
+		params.setDepartureDate(depDate);
+		params.setReturnDate(retDate);
+		params.setNumAdults(numAdults);
+		return params;
+	}
+
+	public static Money setUpMoney(String amount, String currencyType) {
+		Money money = new Money();
+		money.setAmount(amount);
+		money.setCurrency(currencyType);
+		return money;
 	}
 }
