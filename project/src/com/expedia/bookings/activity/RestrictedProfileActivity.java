b47ae6cb4324bfffad05114d794b8f95fd92d81e
Joel Drotos <jdrotos@mobiata.com>
2013-08-20 08:50:23 -0500

    1690 p1 - add restricted accounts message activity

---
+++ project/src/com/expedia/bookings/activity/RestrictedProfileActivity.java
@@ -0,0 +1,46 @@
+package com.expedia.bookings.activity;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.MenuItem;
+import com.expedia.bookings.R;
+import com.expedia.bookings.utils.FontCache;
+import com.expedia.bookings.utils.FontCache.Font;
+import com.mobiata.android.util.Ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class RestrictedProfileActivity extends SherlockFragmentActivity {
+
+	public static Intent createIntent(Context context) {
+		Intent restrictedProfileIntent = new Intent(context, RestrictedProfileActivity.class);
+		return restrictedProfileIntent;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_restricted_profile);
+
+		ActionBar actionBar = this.getSupportActionBar();
+		actionBar.setDisplayShowHomeEnabled(true);
+		actionBar.setDisplayHomeAsUpEnabled(true);
+
+		TextView messageTv = Ui.findView(this, R.id.restricted_profile_message);
+		FontCache.setTypeface(messageTv, Font.ROBOTO_LIGHT);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			onBackPressed();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+}
