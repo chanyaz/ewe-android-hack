0bab5fb05db23174860e7e5f6e427948ac37938d
Kevin Carpenter <kevc@mobiata.com>
2013-08-20 15:10:32 -0700

    Add tests for checkInDates across timezones and dates

--- tests/src/com/expedia/bookings/test/tests/unit/ItinContentGeneratorTest.java
+++ tests/src/com/expedia/bookings/test/tests/unit/ItinContentGeneratorTest.java
@@ -1,6 +1,7 @@
 package com.expedia.bookings.test.tests.unit;
 
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 
 import android.content.res.Resources;
 import android.test.AndroidTestCase;
@@ -12,18 +13,17 @@ import com.expedia.bookings.data.trips.TripHotel;
 import com.expedia.bookings.utils.JodaUtils;
 import com.expedia.bookings.widget.itin.HotelItinContentGenerator;
 import com.expedia.bookings.widget.itin.ItinContentGenerator;
+import com.mobiata.android.Log;
 
 public class ItinContentGeneratorTest extends AndroidTestCase {
 
 	static final DateTime mNow = DateTime.now();
-	Resources mRes;
 	DateTime mCheckInDate;
 	DateTime mCheckOutDate;
 	HotelItinContentGenerator mItinGen;
 	TripHotel mTripHotel = new TripHotel();
 
 	public void setUp() {
-		mRes = getContext().getResources();
 	}
 
 	private ItinContentGenerator<?> getUpdatedItinGenerator(DateTime checkIn, DateTime checkOut, TripHotel trip) {
@@ -82,5 +82,54 @@ public class ItinContentGeneratorTest extends AndroidTestCase {
 				| DateUtils.FORMAT_NO_YEAR | DateUtils.FORMAT_ABBREV_MONTH);
 		String formattedDateString = getContext().getString(R.string.Title_Date_TEMPLATE, "", dateString);
 		assertEquals(formattedDateString, headerText);
+	}
+
+	private static final int MILLIS_IN_HOUR = 3600000;
+	private static final DateTimeZone mDTZDefault = DateTimeZone.getDefault();
+
+	public void testHotelHeaderTextTomorrowNextTimeZone() {
+		//Today at 23:59:59
+		mCheckInDate = mNow.withHourOfDay(23).withMinuteOfHour(59).withSecondOfMinute(59);
+		mItinGen = (HotelItinContentGenerator) getUpdatedItinGenerator(mCheckInDate, mCheckOutDate, mTripHotel);
+		String headerText = mItinGen.getHeaderTextDate();
+		String dateString = getContext().getString(R.string.Title_Date_TEMPLATE, "",
+				getContext().getString(R.string.Today));
+		assertEquals(dateString, headerText);
+
+		// Make a DateTimeZone that is +1 closer to UTC than default (local)
+		// and update itinGenerator
+		DateTimeZone dtz = DateTimeZone.forOffsetMillis(mDTZDefault.getOffset(mCheckInDate.getMillis())
+				+ MILLIS_IN_HOUR);
+		mCheckInDate = mCheckInDate.withZone(dtz);
+		mItinGen = (HotelItinContentGenerator) getUpdatedItinGenerator(mCheckInDate, mCheckOutDate,
+				mTripHotel);
+
+		headerText = mItinGen.getHeaderTextDate();
+		dateString = getContext().getString(R.string.Title_Date_TEMPLATE, "",
+				getContext().getString(R.string.Today));
+		assertEquals(dateString, headerText);
+	}
+
+	public void testHotelHeaderTextYesterdayPreviousTimeZone() {
+		//Today at 00:00:01
+		mCheckInDate = mNow.withHourOfDay(0).withMinuteOfHour(0).withSecondOfMinute(1);
+		mItinGen = (HotelItinContentGenerator) getUpdatedItinGenerator(mCheckInDate, mCheckOutDate, mTripHotel);
+		String headerText = mItinGen.getHeaderTextDate();
+		String dateString = getContext().getString(R.string.Title_Date_TEMPLATE, "",
+				getContext().getString(R.string.Today));
+		assertEquals(dateString, headerText);
+
+		// Make a DateTimeZone that is 1 hour farther from UTC than default (local)
+		// and update itinGenerator
+		DateTimeZone dtz = DateTimeZone.forOffsetMillis(mDTZDefault.getOffset(mCheckInDate.getMillis())
+				- MILLIS_IN_HOUR);
+		mCheckInDate = mCheckInDate.withZone(dtz);
+		mItinGen = (HotelItinContentGenerator) getUpdatedItinGenerator(mCheckInDate, mCheckOutDate,
+				mTripHotel);
+
+		headerText = mItinGen.getHeaderTextDate();
+		dateString = getContext().getString(R.string.Title_Date_TEMPLATE, "",
+				getContext().getString(R.string.yesterday));
+		assertEquals(dateString, headerText);
 	}
 }
