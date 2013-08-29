26caa0b0b53321e8fca749e5e3c0f346cf994a2b
Daniel Lew <dan@mobiata.com>
2013-08-29 11:14:28 -0500

    Made search header prettier
    It's not the best but it's quite a bit easier on the eyes now.

--- project/src/com/expedia/bookings/fragment/TabletSearchFragment.java
+++ project/src/com/expedia/bookings/fragment/TabletSearchFragment.java
@@ -186,7 +186,7 @@ public class TabletSearchFragment extends MeasurableFragment implements OnClickL
 	// Manipulation
 
 	public void setInitialTranslationY(int translationY) {
-		mInitialTranslationY = translationY - (mHeaderTopContainer.getHeight() / 2.0f);
+		mInitialTranslationY = translationY - (mHeaderTopContainer.getHeight() / 2.0f) - mHeader.getPaddingTop();
 	}
 
 	//////////////////////////////////////////////////////////////////////////
