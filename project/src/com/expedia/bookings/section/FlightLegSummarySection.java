f23f8924f3cd54392d8eaa573c5687c5811458c4
Doug Melton <doug@mobiata.com>
2013-09-03 16:02:33 -0700

    Layout drawableLeft optimization

--- project/src/com/expedia/bookings/section/FlightLegSummarySection.java
+++ project/src/com/expedia/bookings/section/FlightLegSummarySection.java
@@ -44,7 +44,6 @@ public class FlightLegSummarySection extends RelativeLayout {
 
 	private LinearLayout mAirlineContainer;
 	private TextView mAirlineTextView;
-	private ImageView mOperatingCarrierImageView;
 	private TextView mOperatingCarrierTextView;
 	private TextView mPriceTextView;
 	private TextView mDepartureTimeTextView;
@@ -63,7 +62,6 @@ public class FlightLegSummarySection extends RelativeLayout {
 		// Cache views
 		mAirlineContainer = Ui.findView(this, R.id.airline_container);
 		mAirlineTextView = Ui.findView(this, R.id.airline_text_view);
-		mOperatingCarrierImageView = Ui.findView(this, R.id.operating_carrier_image_view);
 		mOperatingCarrierTextView = Ui.findView(this, R.id.operating_carrier_text_view);
 		mPriceTextView = Ui.findView(this, R.id.price_text_view);
 		mDepartureTimeTextView = Ui.findView(this, R.id.departure_time_text_view);
@@ -139,8 +137,6 @@ public class FlightLegSummarySection extends RelativeLayout {
 			}
 
 			mOperatingCarrierTextView.setText(context.getString(R.string.operated_by_TEMPLATE, operatedBy));
-
-			mOperatingCarrierImageView.setVisibility(View.VISIBLE);
 			mOperatingCarrierTextView.setVisibility(View.VISIBLE);
 
 			belowTarget = mOperatingCarrierTextView.getId();
