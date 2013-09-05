497e0eca93ad8f2dba5e8bb1baa340bf9d1b4094
Kevin Carpenter <kevc@mobiata.com>
2013-09-05 13:02:50 -0700

    Adding HotelsReviewScreen to page model club

--- tests/src/com/expedia/bookings/test/utils/HotelsTestDriver.java
+++ tests/src/com/expedia/bookings/test/utils/HotelsTestDriver.java
@@ -5,12 +5,14 @@ import android.app.Instrumentation;
 import android.content.res.Resources;
 
 import com.expedia.bookings.test.tests.pageModels.hotels.HotelsDetailsScreen;
+import com.expedia.bookings.test.tests.pageModels.hotels.HotelsReviewsScreen;
 import com.expedia.bookings.test.tests.pageModels.hotels.HotelsSearchScreen;
 
 public class HotelsTestDriver extends TestDriver {
 
 	private HotelsSearchScreen mHotelsSearchScreen;
 	private HotelsDetailsScreen mHotelsDetailsScreen;
+	private HotelsReviewsScreen mHotelsReviewsScreen;
 
 	public HotelsTestDriver(Instrumentation instrumentation, Activity activity, Resources res,
 			TestPreferences preferences) {
@@ -29,6 +31,13 @@ public class HotelsTestDriver extends TestDriver {
 			mHotelsDetailsScreen = new HotelsDetailsScreen(mInstrumentation, getCurrentActivity(), mRes, mPreferences);
 		}
 		return mHotelsDetailsScreen;
+	}
+
+	public HotelsReviewsScreen hotelsReviewsScreen() {
+		if (mHotelsReviewsScreen == null) {
+			mHotelsReviewsScreen = new HotelsReviewsScreen(mInstrumentation, getCurrentActivity(), mRes, mPreferences);
+		}
+		return mHotelsReviewsScreen;
 	}
 
 }
