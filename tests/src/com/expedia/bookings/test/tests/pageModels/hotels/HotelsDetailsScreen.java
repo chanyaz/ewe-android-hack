de5eb69d1b9571f515c1c66ccbcf9471056d62a7
Kevin Carpenter <kevc@mobiata.com>
2013-09-05 12:26:48 -0700

    Add HotelsDetailsScreen to the page model family.

---
+++ tests/src/com/expedia/bookings/test/tests/pageModels/hotels/HotelsDetailsScreen.java
@@ -0,0 +1,99 @@
+package com.expedia.bookings.test.tests.pageModels.hotels;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.res.Resources;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.expedia.bookings.R;
+import com.expedia.bookings.test.tests.pageModels.common.ScreenActions;
+import com.expedia.bookings.test.utils.TestPreferences;
+
+public class HotelsDetailsScreen extends ScreenActions {
+
+	private static int sTitleViewID = R.id.title;
+	private static int sRatingViewID = R.id.rating;
+	private static int sBookNowButtonID = R.id.book_now_button;
+	private static int sBookByPhoneButtonID = R.id.book_by_phone_button;
+	private static int sReviewsTitleViewID = R.id.user_rating_text_view;
+	private static int sBannerViewID = R.id.banner_message_text_view;
+	private static int sReadMoreViewID = R.id.read_more;
+	private static int sSelectButtonID = R.id.menu_select_hotel;
+	private static int sBackButtonID = android.R.id.home;
+
+	public HotelsDetailsScreen(Instrumentation instrumentation, Activity activity, Resources res,
+			TestPreferences preferences) {
+		super(instrumentation, activity, res, preferences);
+	}
+
+	// Object access
+
+	public TextView titleView() {
+		return (TextView) getView(sTitleViewID);
+	}
+
+	public RatingBar ratingBar() {
+		return (RatingBar) getView(sRatingViewID);
+	}
+
+	public View bookNowButton() {
+		return getView(sBookNowButtonID);
+	}
+
+	public View bookByPhoneButton() {
+		return getView(sBookByPhoneButtonID);
+	}
+
+	public TextView reviewsTitle() {
+		return (TextView) getView(sReviewsTitleViewID);
+	}
+
+	public TextView bannerTextView() {
+		return (TextView) getView(sBannerViewID);
+	}
+
+	public View readMore() {
+		return getView(sReadMoreViewID);
+	}
+
+	public View selectButton() {
+		return getView(sSelectButtonID);
+	}
+
+	public View backButton() {
+		return getView(sBackButtonID);
+	}
+
+	// Object interaction
+
+	public void clickBookNowButton() {
+		clickOnView(bookNowButton());
+	}
+
+	public void clickBookByPhoneButton() {
+		clickOnView(bookByPhoneButton());
+	}
+
+	public void clickReviewsTitle() {
+		clickOnView(reviewsTitle());
+	}
+
+	public void clickBannerView() {
+		clickOnView(bannerTextView());
+	}
+
+	public void clickReadMore() {
+		clickOnView(readMore());
+	}
+
+	public void clickSelectButton() {
+		clickOnView(selectButton());
+	}
+
+	public void clickBackButton() {
+		clickOnView(backButton());
+	}
+
+}
--- tests/src/com/expedia/bookings/test/utils/HotelsTestDriver.java
+++ tests/src/com/expedia/bookings/test/utils/HotelsTestDriver.java
@@ -4,11 +4,13 @@ import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.res.Resources;
 
+import com.expedia.bookings.test.tests.pageModels.hotels.HotelsDetailsScreen;
 import com.expedia.bookings.test.tests.pageModels.hotels.HotelsSearchScreen;
 
 public class HotelsTestDriver extends TestDriver {
 
 	private HotelsSearchScreen mHotelsSearchScreen;
+	private HotelsDetailsScreen mHotelsDetailsScreen;
 
 	public HotelsTestDriver(Instrumentation instrumentation, Activity activity, Resources res,
 			TestPreferences preferences) {
@@ -20,6 +22,13 @@ public class HotelsTestDriver extends TestDriver {
 			mHotelsSearchScreen = new HotelsSearchScreen(mInstrumentation, getCurrentActivity(), mRes, mPreferences);
 		}
 		return mHotelsSearchScreen;
+	}
+
+	public HotelsDetailsScreen hotelsDetailsScreen() {
+		if (mHotelsDetailsScreen == null) {
+			mHotelsDetailsScreen = new HotelsDetailsScreen(mInstrumentation, getCurrentActivity(), mRes, mPreferences);
+		}
+		return mHotelsDetailsScreen;
 	}
 
 }
