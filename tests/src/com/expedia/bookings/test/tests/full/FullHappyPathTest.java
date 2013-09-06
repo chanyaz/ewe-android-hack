ddf2a265be5adc4a3c010d05660d7a07472a6634
Kevin Carpenter <kevc@mobiata.com>
2013-09-06 16:37:38 -0700

    Convert FullHappyPathTest to new format

--- tests/src/com/expedia/bookings/test/tests/full/FullHappyPathTest.java
+++ tests/src/com/expedia/bookings/test/tests/full/FullHappyPathTest.java
@@ -4,71 +4,48 @@ import android.content.res.Resources;
 import android.test.ActivityInstrumentationTestCase2;
 import android.util.DisplayMetrics;
 
-import com.expedia.bookings.R;
 import com.expedia.bookings.activity.SearchActivity;
-import com.expedia.bookings.test.utils.HotelsRobotHelper;
+import com.expedia.bookings.test.tests.flights.FlightsHappyPath;
+import com.expedia.bookings.test.tests.hotels.HotelsHappyPath;
+import com.expedia.bookings.test.utils.FlightsTestDriver;
+import com.expedia.bookings.test.utils.HotelsTestDriver;
 import com.expedia.bookings.test.utils.HotelsUserData;
-import com.jayway.android.robotium.solo.Solo;
+import com.expedia.bookings.test.utils.TestPreferences;
 
 public class FullHappyPathTest extends ActivityInstrumentationTestCase2<SearchActivity> {
 
 	public FullHappyPathTest() { //Default constructor
-		super("com.expedia.bookings", SearchActivity.class);
+		super(SearchActivity.class);
 	}
 
-	private Solo mSolo;
 	private static final String TAG = "Full Happy Path Test";
+
 	private Resources mRes;
 	DisplayMetrics mMetric;
-	private HotelsRobotHelper mDriver;
+	private HotelsTestDriver mHotelsDriver;
+	private FlightsTestDriver mFlightsDriver;
 	private HotelsUserData mUser;
+	private TestPreferences mPreferences;
 
 	protected void setUp() throws Exception {
 		super.setUp();
-		mSolo = new Solo(getInstrumentation(), getActivity());
-		//Log.configureLogging("ExpediaBookings", true);
-
 		mRes = getActivity().getBaseContext().getResources();
-		mMetric = mRes.getDisplayMetrics();
-
-		mDriver = new HotelsRobotHelper(mSolo, mRes);
-		mDriver.setScreenshotCount(1);
-		mDriver.setAllowOrientationChange(false);
-
+		mPreferences = TestPreferences.generateTestPreferences().setRotationPermission(false)
+				.setScreenshotPermission(false);
+		mHotelsDriver = new HotelsTestDriver(getInstrumentation(), getActivity(), mRes, mPreferences);
+		mFlightsDriver = new FlightsTestDriver(getInstrumentation(), getActivity(), mRes, mPreferences);
 		mUser = new HotelsUserData();
 		mUser.setHotelCityToRandomUSCity();
-		mUser.setAirportsToRandomUSAirports();
 	}
 
-	////////////////////////////////////////////////////////////////
-	// Test Driver 
-
 	public void testMethod() throws Exception {
-		try {
-			mDriver.ignoreSweepstakesActivity();
-			mDriver.setSpoofBookings();
-			mDriver.clearPrivateData();
-
-			mDriver.launchHotels();
-			mDriver.browseRooms(4, mUser.mHotelSearchCity, true);
-
-			mDriver.flightsHappyPath(mUser.mDepartureAirport, mUser.mArrivalAirport, 1, true, false);
-		}
-		catch (Error e) {
-			mDriver.takeScreenshotUponFailure(e, TAG);
-			throw e;
-		}
-		catch (Exception e) {
-			mDriver.takeScreenshotUponFailure(e, TAG);
-			throw e;
-		}
+		HotelsHappyPath.execute(mHotelsDriver, mUser, 1);
+		FlightsHappyPath.execute(mFlightsDriver, mUser);
 	}
 
 	@Override
 	protected void tearDown() throws Exception {
-		//Robotium will finish all the activities that have been opened
-		mDriver.enterLog(TAG, "tearing down...");
-
-		mSolo.finishOpenedActivities();
+		mHotelsDriver.enterLog(TAG, "tearing down...");
+		mHotelsDriver.finishOpenedActivities();
 	}
 }
