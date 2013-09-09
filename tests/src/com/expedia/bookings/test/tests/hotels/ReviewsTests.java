2ed7ecfec10ec8fa6449fd36ec8eed481c07d243
Kevin Carpenter <kevc@mobiata.com>
2013-09-09 13:11:28 -0700

    Update ReviewsTest to use page models
    The conversion is complete!

--- tests/src/com/expedia/bookings/test/tests/hotels/ReviewsTests.java
+++ tests/src/com/expedia/bookings/test/tests/hotels/ReviewsTests.java
@@ -4,164 +4,124 @@ import android.content.res.Resources;
 import android.test.ActivityInstrumentationTestCase2;
 import android.util.DisplayMetrics;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.RatingBar;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.expedia.bookings.R;
 import com.expedia.bookings.activity.SearchActivity;
-import com.expedia.bookings.test.utils.HotelsRobotHelper;
+import com.expedia.bookings.test.utils.HotelsTestDriver;
 import com.expedia.bookings.test.utils.HotelsUserData;
-import com.jayway.android.robotium.solo.Solo;
+import com.expedia.bookings.test.utils.TestPreferences;
 
 public class ReviewsTests extends ActivityInstrumentationTestCase2<SearchActivity> {
-
-	private Solo mSolo;
-	private static final String TAG = "Reviews Tests";
-	private Resources mRes;
-	DisplayMetrics mMetric;
-	private HotelsRobotHelper mDriver;
-	private HotelsUserData mUser;
 
 	public ReviewsTests() { //Default constructor
 		super("com.expedia.bookings", SearchActivity.class);
 	}
 
+	private static final String TAG = "Hotels Browsing Test";
+
+	private Resources mRes;
+	DisplayMetrics mMetric;
+	private HotelsTestDriver mDriver;
+	private HotelsUserData mUser;
+	private TestPreferences mPreferences;
+
 	protected void setUp() throws Exception {
 		super.setUp();
-		mSolo = new Solo(getInstrumentation(), getActivity());
-		mUser = new HotelsUserData();
 		mRes = getActivity().getBaseContext().getResources();
-		mMetric = mRes.getDisplayMetrics();
-		mDriver = new HotelsRobotHelper(mSolo, mRes, mUser);
-
-		mDriver.setScreenshotCount(1);
-		mDriver.setAllowOrientationChange(false);
-		mDriver.setWriteEventsToFile(false);
+		mPreferences = TestPreferences.generateTestPreferences().setRotationPermission(false)
+				.setScreenshotPermission(false);
+		mDriver = new HotelsTestDriver(getInstrumentation(), getActivity(), mRes, mPreferences);
+		mUser = new HotelsUserData();
 		mUser.setHotelCityToRandomUSCity();
+
+		try {
+			mDriver.sweepstakesScreen().clickNoThanksButton();
+		}
+		catch (Throwable e) {
+			mDriver.enterLog(TAG, "No sweepstakes activity to interact with!");
+		}
 	}
 
 	public void testNumberOfReviews() throws Exception {
-		View userRatingTextView;
-		TextView reviewsTitleView;
-		String reviewsTitleString;
-		String reviewsScreenNumber;
-
-		TextView detailsReviewTextView;
-		String detailsReviewString;
-		String hotelDetailsNumber;
-
-		mDriver.ignoreSweepstakesActivity();
-
 		String hotelSoldOut = mRes.getString(R.string.error_hotel_is_now_sold_out);
-		mDriver.launchHotels();
-		mDriver.selectLocation(mUser.mHotelSearchCity);
+		mDriver.launchScreen().launchHotels();
+		mDriver.hotelsSearchScreen().clickSearchEditText();
+		mDriver.hotelsSearchScreen().clickToClearSearchEditText();
+		mDriver.hotelsSearchScreen().enterSearchText(mUser.mHotelSearchCity);
+		mDriver.hotelsSearchScreen().selectHotelFromList(0);
+		mDriver.waitForStringToBeGone(mDriver.hotelsSearchScreen().searchingForHotels());
 
 		//Number of times to scroll down the results list for another set of hotels
 		int numberOfHotelSets = 4;
 		for (int i = 0; i < numberOfHotelSets; i++) {
-			for (int j = 2; j <= 4; j++) {
-				mSolo.clickInList(j);
+			for (int j = 1; j <= 4; j++) {
+				mDriver.delay(1);
+				mDriver.hotelsSearchScreen().selectHotelFromList(j);
 				mDriver.delay(5);
 				//Go back to search list if hotel sold out
-				if (!mSolo.searchText(hotelSoldOut, 1, false, true)) {
-					userRatingTextView =
-							mSolo.getCurrentActivity().findViewById(R.id.user_rating_text_view);
-					mSolo.clickOnView(userRatingTextView);
-					mDriver.delay();
+				if (!mDriver.searchText(hotelSoldOut, 1, false, true)) {
+					mDriver.hotelsDetailsScreen().clickBannerView();
+					mDriver.waitForStringToBeGone(mDriver.hotelsReviewsScreen().loadingUserReviews());
 
 					// Go to Reviews screen for hotel and get the review count
 					verifyReviewsActionBarContentsExist();
-					reviewsTitleView = (TextView) mSolo.getView(R.id.title);
-					reviewsTitleString = (String) reviewsTitleView.getText();
-					reviewsScreenNumber = reviewsTitleString.substring(0, reviewsTitleString.indexOf(' '));
-					mSolo.goBack();
+
+					String reviewsTitleString = (String) mDriver.hotelsReviewsScreen().titleView().getText();
+					String reviewsScreenNumber = reviewsTitleString.substring(0, reviewsTitleString.indexOf(' '));
+					mDriver.hotelsReviewsScreen().clickBackButton();
 
 					// Go back to the hotel details screen and get the review count
-					detailsReviewTextView = (TextView) mSolo.getView(R.id.user_rating_text_view);
-					detailsReviewString = (String) detailsReviewTextView.getText();
-					hotelDetailsNumber = detailsReviewString.substring(0, detailsReviewString.indexOf(' '));
+					String detailsReviewString = (String) mDriver.hotelsDetailsScreen().reviewsTitle().getText();
+					String hotelDetailsNumber = detailsReviewString.substring(0, detailsReviewString.indexOf(' '));
 
 					// test fails if the review counts are not equal
-					if (!reviewsScreenNumber.equals(hotelDetailsNumber)) {
-						mDriver.enterLog(TAG, "Test failed: hotel reviews screen showed " + detailsReviewString
-								+ ". Hotel details showed " + hotelDetailsNumber);
-						fail();
-					}
-					mSolo.goBack();
+					mDriver.enterLog(TAG, "Hotel reviews showed: " + reviewsScreenNumber
+							+ " Hotel details showed: " + hotelDetailsNumber);
+					assertEquals(reviewsScreenNumber, hotelDetailsNumber);
+					mDriver.goBack();
 				}
 				else {
-					mSolo.clickOnButton(0);
+					mDriver.clickOnButton(0);
 				}
 			}
-			mSolo.scrollDown();
+			mDriver.scrollDown();
 		}
-		mSolo.goBack();
-		mSolo.goBack();
+		mDriver.goBack();
+		mDriver.goBack();
 	}
 
 	public void testSelectButton() throws Exception {
-		mDriver.ignoreSweepstakesActivity();
+		mDriver.delay(1);
+		mDriver.launchScreen().launchHotels();
+		mDriver.hotelsSearchScreen().clickSearchEditText();
+		mDriver.hotelsSearchScreen().clickToClearSearchEditText();
+		mDriver.hotelsSearchScreen().enterSearchText(mUser.mHotelSearchCity);
+		mDriver.clickInList(1);
+		mDriver.waitForStringToBeGone(mDriver.hotelsSearchScreen().searchingForHotels());
+		mDriver.hotelsSearchScreen().selectHotelFromList(1);
 
-		String selectARoom = mRes.getString(R.string.select_a_room_instruction);
-		mDriver.launchHotels();
-		mDriver.selectLocation(mUser.mHotelSearchCity);
-		mSolo.clickInList(2);
-		mDriver.delay();
+		mDriver.hotelsDetailsScreen().clickReviewsTitle();
 
-		View userRatingTextView =
-				mSolo.getCurrentActivity().findViewById(R.id.user_rating_text_view);
-		if (userRatingTextView != null) {
-			mSolo.clickOnView(userRatingTextView);
-		}
-		else {
-			mDriver.delay();
-			mSolo.clickOnView(userRatingTextView);
-		}
-		mSolo.clickOnText(mRes.getString(R.string.select));
-		mDriver.delay();
-		if (!mSolo.searchText(selectARoom, 1, false, true)) {
-			mDriver.enterLog(TAG, "Couldn't find 'Select A Room' string");
+		mDriver.hotelsDetailsScreen().clickSelectButton();
+		mDriver.waitForStringToBeGone(mDriver.hotelsRoomsRatesScreen().findingAvailableRooms());
+		if (!mDriver.hotelsRoomsRatesScreen().hotelRatingBar().isShown()) {
+			mDriver.enterLog(TAG, "Did not make it to rooms and rates screen.");
 			fail();
 		}
-		mSolo.goBack();
-		mSolo.goBack();
-		mSolo.goBack();
-		mSolo.goBack();
 	}
 
-	private void verifyReviewsActionBarContentsExist() {
-		String critical = mRes.getString(R.string.user_review_sort_button_critical);
-		String favorable = mRes.getString(R.string.user_review_sort_button_favorable);
-		String recent = mRes.getString(R.string.user_review_sort_button_recent);
-		String select = mRes.getString(R.string.select);
-
+	private void verifyReviewsActionBarContentsExist() throws Exception {
 		mDriver.delay();
-		if (!mSolo.searchText(critical, 1, false, true)) {
-			mDriver.enterLog(TAG, "Couldn't find 'Critical' string");
-			fail();
-		}
-		if (!mSolo.searchText(favorable, 1, false, true)) {
-			mDriver.enterLog(TAG, "Couldn't find 'Favorable' string");
-			fail();
-		}
-		if (!mSolo.searchText(recent, 1, false, true)) {
-			mDriver.enterLog(TAG, "Couldn't find 'Recent' string");
-			fail();
-		}
-		if (!mSolo.searchText(select, 1, false, true)) {
-			mDriver.enterLog(TAG, "Couldn't find 'Select' string");
-			fail();
-		}
+		mDriver.waitForStringToBeGone(mDriver.hotelsReviewsScreen().loadingUserReviews());
+		mDriver.hotelsReviewsScreen().clickCriticalTab();
+		mDriver.hotelsReviewsScreen().clickFavorableTab();
+		mDriver.hotelsReviewsScreen().clickRecentTab();
 	}
 
 	@Override
 	protected void tearDown() throws Exception {
-		//Robotium will finish all the activities that have been opened
 		mDriver.enterLog(TAG, "tearing down...");
-
-		mSolo.finishOpenedActivities();
+		mDriver.finishOpenedActivities();
 	}
 }
