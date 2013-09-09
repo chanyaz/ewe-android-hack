9768055722b70aeb5633343cc94a4c58b9965e3b
Kevin Carpenter <kevc@mobiata.com>
2013-09-09 14:09:46 -0700

    Fix MockDataTests by delaying before accessing strings

--- tests/src/com/expedia/bookings/test/tests/hotels/MockDataTests.java
+++ tests/src/com/expedia/bookings/test/tests/hotels/MockDataTests.java
@@ -79,6 +79,7 @@ public class MockDataTests extends ActivityInstrumentationTestCase2<SearchActivi
 		mDriver.hotelsDetailsScreen().clickSelectButton();
 		mDriver.waitForStringToBeGone(mDriver.hotelsRoomsRatesScreen().findingAvailableRooms());
 		mDriver.hotelsRoomsRatesScreen().selectRoom(0);
+		mDriver.delay(3);
 		String alertText = (String) mDriver.getText(0).getText();
 		String expectedText = mRes.getString(R.string.the_hotel_raised_the_total_price_TEMPLATE, "$400.65", "$440.65");
 		mDriver.enterLog(TAG, "Expected text: " + expectedText);
@@ -98,6 +99,7 @@ public class MockDataTests extends ActivityInstrumentationTestCase2<SearchActivi
 		mDriver.hotelsDetailsScreen().clickSelectButton();
 		mDriver.waitForStringToBeGone(mDriver.hotelsRoomsRatesScreen().findingAvailableRooms());
 		mDriver.hotelsRoomsRatesScreen().selectRoom(0);
+		mDriver.delay(3);
 		String alertText = (String) mDriver.getText(0).getText();
 		String expectedText = mRes.getString(R.string.the_hotel_lowered_the_total_price_TEMPLATE, "$400.65", "$360.65");
 		mDriver.enterLog(TAG, "Expected text: " + expectedText);
@@ -117,6 +119,7 @@ public class MockDataTests extends ActivityInstrumentationTestCase2<SearchActivi
 		mDriver.hotelsDetailsScreen().clickSelectButton();
 		mDriver.waitForStringToBeGone(mDriver.hotelsRoomsRatesScreen().findingAvailableRooms());
 		mDriver.hotelsRoomsRatesScreen().selectRoom(0);
+		mDriver.delay(3);
 		String alertText = (String) mDriver.getText(0).getText();
 		String expectedText = mRes.getString(R.string.e3_error_checkout_hotel_room_unavailable);
 		mDriver.enterLog(TAG, "Expected text: " + expectedText);
@@ -145,6 +148,7 @@ public class MockDataTests extends ActivityInstrumentationTestCase2<SearchActivi
 		String ok;
 		for (int i = 0; i < upperBound; i++) {
 			mDriver.hotelsRoomsRatesScreen().selectRoom(0);
+			mDriver.delay(3);
 			String alertText = (String) mDriver.getText(0).getText();
 			String expectedText = mRes.getString(R.string.e3_error_checkout_hotel_room_unavailable);
 			mDriver.enterLog(TAG, "Expected text: " + expectedText);
