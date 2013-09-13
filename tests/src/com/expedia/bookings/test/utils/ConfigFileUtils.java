e220ab7ee41bd70c0e85ad4dc8e48f4f599fe645
Kevin Carpenter <kevc@mobiata.com>
2013-09-13 15:18:24 -0700

    Update ConfigFileUtils to read JSON

--- tests/src/com/expedia/bookings/test/utils/ConfigFileUtils.java
+++ tests/src/com/expedia/bookings/test/utils/ConfigFileUtils.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Iterator;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.os.Environment;
 
@@ -19,37 +22,32 @@ import com.mobiata.android.Log;
 public class ConfigFileUtils {
 	private static final String TAG = "ConfigFileUtils";
 	private static final String CONFIG_FILE_PATH = Environment.getExternalStorageDirectory().getAbsolutePath();
-	private static final String CONFIG_FILE_NAME = "config.data";
+	private static final String CONFIG_FILE_NAME = "config.json";
 	private File mConfigDataFile;
-	private HashMap<String, String> mConfigDataHashMap = new HashMap<String, String>();
+	private JSONObject mJsonObject;
 
-	public ConfigFileUtils() throws IOException {
-		String fullFilePath = CONFIG_FILE_PATH + "/" + CONFIG_FILE_NAME;
-		mConfigDataFile = new File(fullFilePath);
-		createVariableHashFromConfigFile(mConfigDataFile);
+	public ConfigFileUtils() throws IOException, JSONException {
+		mConfigDataFile = new File(CONFIG_FILE_PATH, CONFIG_FILE_NAME);
+		createJSONObjectFromFile();
 	}
 
-	public static void getFilePath() {
-		Log.d("TAG", CONFIG_FILE_PATH + '/' + CONFIG_FILE_NAME);
+	public String getFilePath() {
+		return mConfigDataFile.toString();
 	}
 
-	private void createVariableHashFromConfigFile(File file) throws IOException {
+	private String readFileToString(File f) throws IOException {
 		BufferedReader br = null;
 		try {
-			br = new BufferedReader(new FileReader(mConfigDataFile));
+			br = new BufferedReader(new FileReader(f));
 			String line;
-			String keyword;
-			String value;
-			int tabIndex;
+			StringBuffer buffer = new StringBuffer();
 			while ((line = br.readLine()) != null) {
-				if (!line.isEmpty() && (line.indexOf('\t') != -1)) {
-					tabIndex = line.indexOf('\t');
-					keyword = line.substring(0, tabIndex);
-					value = line.substring(tabIndex + 1, line.length());
-					Log.v(TAG, "Keyword: " + keyword + " -> value: " + value);
-					mConfigDataHashMap.put(keyword, value);
+				if (!line.isEmpty()) {
+					buffer.append(line);
 				}
 			}
+			Log.v("TAG", "Returned JSON string: " + buffer.toString());
+			return buffer.toString();
 		}
 		catch (IOException e) {
 			throw e;
@@ -59,7 +57,20 @@ public class ConfigFileUtils {
 		}
 	}
 
-	public String getConfigValue(String keyword) {
-		return mConfigDataHashMap.get(keyword);
+	private void createJSONObjectFromFile() throws IOException, JSONException {
+		String jsonString = readFileToString(mConfigDataFile);
+		mJsonObject = new JSONObject(jsonString);
+		Iterator<?> keys = mJsonObject.keys();
+		String key;
+		String val;
+		while (keys.hasNext()) {
+			key = (String) keys.next();
+			val = mJsonObject.getString(key);
+			Log.d(TAG, "Config file produced: key " + key + " -> value " + val);
+		}
+	}
+
+	public String getConfigValue(String keyword) throws JSONException {
+		return mJsonObject.getString(keyword);
 	}
 }
