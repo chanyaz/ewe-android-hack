09f0f37aa1ff2336492683412e8f9dfb8ee09048
Daniel Lew <dan@mobiata.com>
2013-09-19 15:41:23 -0500

    Setup styleable for CalendarPicker
    Also setup a default style, so you don't need to define everything

--- project/src/com/expedia/bookings/widget/CalendarPicker.java
+++ project/src/com/expedia/bookings/widget/CalendarPicker.java
@@ -3,6 +3,7 @@ package com.expedia.bookings.widget;
 import org.joda.time.YearMonth;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.util.AttributeSet;
@@ -13,37 +14,56 @@ import com.expedia.bookings.R;
 import com.mobiata.android.util.Ui;
 
 /**
- * TODO: Setup style that can change things in CalendarPicker
  * TODO: Scale all views based on size of CalendarPicker itself
- *
  */
 public class CalendarPicker extends LinearLayout {
 
 	private static final String INSTANCE_SUPER_STATE = "INSTANCE_SUPER_STATE";
 	private static final String INSTANCE_DISPLAY_YEAR_MONTH = "INSTANCE_DISPLAY_YEAR_MONTH";
+	private static final String INSTANCE_BASE_COLOR = "INSTANCE_BASE_COLOR";
+	private static final String INSTANCE_HIGHLIGHT_COLOR = "INSTANCE_HIGHLIGHT_COLOR";
 
+	// State
 	private YearMonth mDisplayYearMonth;
 
+	// Style
+	private int mBaseColor;
+	private int mHighlightColor;
+
+	// Subviews
 	private TextView mPreviousMonthTextView;
 	private TextView mCurrentMonthTextView;
 	private TextView mNextMonthTextView;
 
 	public CalendarPicker(Context context) {
-		super(context);
-		init(context);
+		this(context, null);
 	}
 
 	public CalendarPicker(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		init(context);
+		this(context, attrs, R.style.V2_Widget_CalendarPicker);
 	}
 
 	public CalendarPicker(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		init(context);
-	}
 
-	private void init(Context context) {
+		// Load attributes
+		TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.CalendarPicker, 0, defStyle);
+		int n = ta.getIndexCount();
+		for (int i = 0; i < n; i++) {
+			int attr = ta.getIndex(i);
+
+			switch (attr) {
+			case R.styleable.CalendarPicker_calendarBaseColor:
+				mBaseColor = ta.getColor(attr, mBaseColor);
+				break;
+
+			case R.styleable.CalendarPicker_calendarHighlightColor:
+				mHighlightColor = ta.getColor(attr, mHighlightColor);
+				break;
+			}
+		}
+		ta.recycle();
+
 		// Configure some layout params (so you don't have to do it in XML)
 		setOrientation(LinearLayout.VERTICAL);
 
@@ -68,6 +88,8 @@ public class CalendarPicker extends LinearLayout {
 		Bundle bundle = (Bundle) state;
 		super.onRestoreInstanceState(bundle.getParcelable(INSTANCE_SUPER_STATE));
 		mDisplayYearMonth = YearMonth.parse(bundle.getString(INSTANCE_DISPLAY_YEAR_MONTH));
+		mBaseColor = bundle.getInt(INSTANCE_BASE_COLOR);
+		mHighlightColor = bundle.getInt(INSTANCE_HIGHLIGHT_COLOR);
 	}
 
 	@Override
@@ -76,6 +98,7 @@ public class CalendarPicker extends LinearLayout {
 
 		// Wait until here to start manipulating sub-Views; that way we can
 		// restore the instance state properly first.
+		updateColors();
 		updateHeader();
 	}
 
@@ -84,11 +107,40 @@ public class CalendarPicker extends LinearLayout {
 		Bundle bundle = new Bundle();
 		bundle.putParcelable(INSTANCE_SUPER_STATE, super.onSaveInstanceState());
 		bundle.putString(INSTANCE_DISPLAY_YEAR_MONTH, mDisplayYearMonth.toString());
+		bundle.putInt(INSTANCE_BASE_COLOR, mBaseColor);
+		bundle.putInt(INSTANCE_HIGHLIGHT_COLOR, mHighlightColor);
 		return bundle;
 	}
 
 	//////////////////////////////////////////////////////////////////////////
+	// Configuration
+
+	public void setBaseColor(int color) {
+		mBaseColor = color;
+		updateColors();
+	}
+
+	public void setHighlightColor(int color) {
+		mHighlightColor = color;
+		updateColors();
+	}
+
+	//////////////////////////////////////////////////////////////////////////
 	// Display
+
+	private void updateColors() {
+		if (mPreviousMonthTextView != null) {
+			mPreviousMonthTextView.setTextColor(mHighlightColor);
+		}
+
+		if (mCurrentMonthTextView != null) {
+			mCurrentMonthTextView.setTextColor(mBaseColor);
+		}
+
+		if (mNextMonthTextView != null) {
+			mNextMonthTextView.setTextColor(mHighlightColor);
+		}
+	}
 
 	private void updateHeader() {
 		mPreviousMonthTextView.setText(mDisplayYearMonth.minusMonths(1).monthOfYear().getAsText());
