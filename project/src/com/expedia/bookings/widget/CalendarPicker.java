aa2791254eeed31f8aa0bc254da43736f169eb86
Daniel Lew <dan@mobiata.com>
2013-09-19 14:59:44 -0500

    Setup currently displayed month
    - Make sure to save the state of it correctly
    
    - Update the header based on the displayed month

--- project/src/com/expedia/bookings/widget/CalendarPicker.java
+++ project/src/com/expedia/bookings/widget/CalendarPicker.java
@@ -1,6 +1,10 @@
 package com.expedia.bookings.widget;
 
+import org.joda.time.YearMonth;
+
 import android.content.Context;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,6 +18,11 @@ import com.mobiata.android.util.Ui;
  *
  */
 public class CalendarPicker extends LinearLayout {
+
+	private static final String INSTANCE_SUPER_STATE = "INSTANCE_SUPER_STATE";
+	private static final String INSTANCE_DISPLAY_YEAR_MONTH = "INSTANCE_DISPLAY_YEAR_MONTH";
+
+	private YearMonth mDisplayYearMonth;
 
 	private TextView mPreviousMonthTextView;
 	private TextView mCurrentMonthTextView;
@@ -40,6 +49,9 @@ public class CalendarPicker extends LinearLayout {
 
 		// Inflate the widget
 		inflate(context, R.layout.widget_calendar_picker, this);
+
+		// Default to showing current year/month
+		mDisplayYearMonth = YearMonth.now();
 	}
 
 	@Override
@@ -49,16 +61,39 @@ public class CalendarPicker extends LinearLayout {
 		mPreviousMonthTextView = Ui.findView(this, R.id.previous_month);
 		mCurrentMonthTextView = Ui.findView(this, R.id.current_month);
 		mNextMonthTextView = Ui.findView(this, R.id.next_month);
+	}
 
+	@Override
+	protected void onRestoreInstanceState(Parcelable state) {
+		Bundle bundle = (Bundle) state;
+		super.onRestoreInstanceState(bundle.getParcelable(INSTANCE_SUPER_STATE));
+		mDisplayYearMonth = YearMonth.parse(bundle.getString(INSTANCE_DISPLAY_YEAR_MONTH));
+	}
+
+	@Override
+	protected void onAttachedToWindow() {
+		super.onAttachedToWindow();
+
+		// Wait until here to start manipulating sub-Views; that way we can
+		// restore the instance state properly first.
 		updateHeader();
+	}
+
+	@Override
+	protected Parcelable onSaveInstanceState() {
+		Bundle bundle = new Bundle();
+		bundle.putParcelable(INSTANCE_SUPER_STATE, super.onSaveInstanceState());
+		bundle.putString(INSTANCE_DISPLAY_YEAR_MONTH, mDisplayYearMonth.toString());
+		return bundle;
 	}
 
 	//////////////////////////////////////////////////////////////////////////
 	// Display
 
 	private void updateHeader() {
-		mPreviousMonthTextView.setText("PREVIOUS");
-		mCurrentMonthTextView.setText("CURRENT");
-		mNextMonthTextView.setText("NEXT");
+		mPreviousMonthTextView.setText(mDisplayYearMonth.minusMonths(1).monthOfYear().getAsText());
+		mCurrentMonthTextView.setText(mDisplayYearMonth.monthOfYear().getAsText());
+		mNextMonthTextView.setText(mDisplayYearMonth.plusMonths(1).monthOfYear().getAsText());
 	}
+
 }
