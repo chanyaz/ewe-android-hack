38c668e0839b45b0e3a7afb0856cbd58718f3bc2
Daniel Lew <dan@mobiata.com>
2013-09-30 13:58:35 -0500

    2018: Disallow honeycomb tablets from tablet UI

--- project/src/com/expedia/bookings/activity/ExpediaBookingApp.java
+++ project/src/com/expedia/bookings/activity/ExpediaBookingApp.java
@@ -10,7 +10,6 @@ import android.app.Application;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.text.format.DateUtils;
@@ -268,9 +267,10 @@ public class ExpediaBookingApp extends Application implements UncaughtExceptionH
 	//////////////////////////////////////////////////////////////////////////
 	// All-app utilities
 
-	// #11076 - for Android 3.0, we still use the phone version of the app due to crippling bugs.
+	// Due to a low number of users and a desire to use latest APIs,
+	// we only use tablet UI on ICS+
 	public static boolean useTabletInterface(Context context) {
-		return AndroidUtils.getSdkVersion() >= 12
+		return AndroidUtils.getSdkVersion() >= 14
 				&& (context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_LARGE;
 	}
 
