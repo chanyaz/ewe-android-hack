d0df347b32c499b31a6fc3dcd7f637a4430f33fd
Daniel Lew <dan@mobiata.com>
2013-09-30 14:40:49 -0500

    Lowered date range to match hotel max
    I know this is a cop out for now but we'll figure out a better solution later

--- project/src/com/expedia/bookings/fragment/DatesFragment.java
+++ project/src/com/expedia/bookings/fragment/DatesFragment.java
@@ -61,7 +61,7 @@ public class DatesFragment extends Fragment implements DateSelectionChangedListe
 		mCalendarPicker = Ui.findView(view, R.id.calendar_picker);
 
 		mCalendarPicker.setSelectableDateRange(LocalDate.now(), LocalDate.now().plusDays(330));
-		mCalendarPicker.setMaxSelectableDateRange(30);
+		mCalendarPicker.setMaxSelectableDateRange(28);
 		mCalendarPicker.setSelectedDates(mStartDate, mEndDate);
 		mCalendarPicker.setDateChangedListener(this);
 
