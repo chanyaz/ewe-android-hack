56a8ee33d6a7992df6eb58362cd1b822fd43e38d
Kevin Carpenter <kevc@mobiata.com>
2013-10-07 14:29:06 -0700

    Add logging and clean imports for HotelsHappyPath

--- tests/src/com/expedia/bookings/test/tests/hotels/HotelsHappyPath.java
+++ tests/src/com/expedia/bookings/test/tests/hotels/HotelsHappyPath.java
@@ -5,7 +5,7 @@ import com.expedia.bookings.test.utils.HotelsUserData;
 
 public class HotelsHappyPath {
 
-	private static final String TAG = "Hotels Happy Path";
+	private static final String TAG = HotelsHappyPath.class.getName();
 
 	public static void execute(HotelsTestDriver driver, HotelsUserData user, int numberOfHotelsToLookAt)
 			throws Exception {
@@ -27,12 +27,14 @@ public class HotelsHappyPath {
 			driver.settingsScreen().clickOKString();
 			driver.settingsScreen().setSpoofBookings();
 			driver.settingsScreen().goBack();
+			driver.enterLog(TAG, "Cleared private data and set spoof bookings");
 
 			driver.launchScreen().openMenuDropDown();
 			driver.launchScreen().pressSettings();
 			driver.settingsScreen().clickSelectAPIString();
 			driver.settingsScreen().scrollUp();
 			driver.settingsScreen().clickOnText(user.getBookingServer());
+			driver.enterLog(TAG, "Set booking server to: " + user.getBookingServer());
 			if (user.getBookingServer().equals("Proxy") || user.getBookingServer().equals("Mock Server")) {
 				driver.settingsScreen().clickServerProxyAddressString();
 				driver.settingsScreen().clearServerEditText();
@@ -43,15 +45,18 @@ public class HotelsHappyPath {
 
 			// Launch screen
 			driver.screenshot("Launch Screen");
+			driver.enterLog(TAG, "Launching hotels application");
 			driver.launchScreen().launchHotels();
 
 			// Search screen
 			driver.delay(1);
 			driver.screenshot("Search Screen");
+
 			driver.landscape();
 			driver.portrait();
 			driver.hotelsSearchScreen().clickSearchEditText();
 			driver.hotelsSearchScreen().clickToClearSearchEditText();
+			driver.enterLog(TAG, "Setting hotel search city to: " + user.getHotelSearchCity());
 			driver.hotelsSearchScreen().enterSearchText(user.getHotelSearchCity());
 			driver.clickInList(1);
 
@@ -61,10 +66,13 @@ public class HotelsHappyPath {
 			driver.screenshot("Calendar fragment");
 			driver.landscape();
 			driver.portrait();
-			driver.hotelsSearchScreen().clickDate(2);
+			int dateOffset = 2;
+			driver.enterLog(TAG, "Selecting date with offset from current date of: " + dateOffset);
+			driver.hotelsSearchScreen().clickDate(dateOffset);
 
 			// Guest Picker
 			driver.hotelsSearchScreen().clickOnGuestsButton();
+			driver.enterLog(TAG, "Incrementing number of adults and number of children by 1");
 			driver.hotelsSearchScreen().guestPicker().clickIncrementAdultsButton();
 			driver.hotelsSearchScreen().guestPicker().clickIncrementChildrenButton();
 			driver.screenshot("Guest Picker fragment");
@@ -75,9 +83,11 @@ public class HotelsHappyPath {
 			// Sort
 			driver.waitForStringToBeGone(driver.hotelsSearchScreen().searchingForHotels());
 			driver.delay();
+			driver.enterLog(TAG, "Hotel search results loaded");
 			driver.hotelsSearchScreen().clickOnSortButton();
 			driver.delay(1);
 			driver.screenshot("Sort fragment");
+			driver.enterLog(TAG, "Opened sort fragment");
 			if (driver.searchText(driver.hotelsSearchScreen().sortMenu().getSortByPopularityString())) {
 				driver.hotelsSearchScreen().sortMenu().clickSortByPopularityString();
 			}
@@ -89,20 +99,26 @@ public class HotelsHappyPath {
 
 			// Filter
 			driver.hotelsSearchScreen().clickOnFilterButton();
+			driver.enterLog(TAG, "Opened filter fragment");
 			driver.screenshot("Filter fragment");
 			driver.landscape();
 			driver.portrait();
 			driver.goBack();
 
 			// Iterate through hotels
+			driver.enterLog(TAG, "Starting hotel iteration. Will iterate through: " + numberOfHotelsToLookAt
+					+ " hotels");
 			for (int i = 0; i < numberOfHotelsToLookAt / 4; i++) {
 				for (int j = 0; j < 4; j++) {
 					driver.waitForStringToBeGone(driver.hotelsSearchScreen().searchingForHotels());
 					driver.hotelsSearchScreen().selectHotelFromList(j);
 					driver.landscape();
 					driver.portrait();
+					String hotelName = driver.hotelsDetailsScreen().titleView().getText().toString();
+					driver.enterLog(TAG, "Selected hotel: " + hotelName);
 					if (!driver.hotelsDetailsScreen().reviewsTitle().getText().toString()
 							.equals(driver.hotelsDetailsScreen().noReviews())) {
+						driver.enterLog(TAG, "Looking at hotel's reviews.");
 						driver.hotelsDetailsScreen().clickReviewsTitle();
 						driver.waitForStringToBeGone(driver.hotelsReviewsScreen().loadingUserReviews());
 						driver.screenshot("Reviews screen");
@@ -120,20 +136,26 @@ public class HotelsHappyPath {
 			driver.hotelsSearchScreen().selectHotelFromList(0);
 			driver.delay(1);
 			driver.screenshot("Hotel Details");
+			String hotelName = driver.hotelsDetailsScreen().titleView().getText().toString();
+			driver.enterLog(TAG, "Reselecting hotel: " + hotelName);
 			driver.hotelsDetailsScreen().clickSelectButton();
 
 			// Rooms and rates
 			driver.waitForStringToBeGone(driver.hotelsRoomsRatesScreen().findingAvailableRooms());
+			driver.enterLog(TAG, "On rooms and rates screen for hotel: " + hotelName);
 			driver.screenshot("Rooms and rates");
 			driver.landscape();
 			driver.portrait();
+			driver.enterLog(TAG, "Selecting first room listed for this hotel.");
 			driver.hotelsRoomsRatesScreen().selectRoom(0);
+			driver.delay();
 			driver.waitForStringToBeGone(driver.commonCheckout().calculatingTaxesAndFees());
 
 			// Checkout
 			driver.commonCheckout().clickCheckoutButton();
 			driver.delay();
 			if (user.getLogInForCheckout()) {
+				driver.enterLog(TAG, "Logging in for this booking using email " + user.getLoginEmail());
 				// Log in
 				driver.commonCheckout().clickLogInButton();
 				driver.screenshot("Login Screen");
@@ -145,18 +167,23 @@ public class HotelsHappyPath {
 				// Enter payment as logged in user
 				driver.commonCheckout().clickSelectPaymentButton();
 				driver.scrollToBottom();
+				driver.enterLog(TAG, "Using new credit card");
 				driver.commonPaymentMethodScreen().clickOnAddNewCardTextView();
 				driver.screenshot("Card info");
 				driver.landscape();
 				driver.portrait();
+				driver.enterLog(TAG, "Entering credit card with number: " + user.getCreditCardNumber());
 				driver.cardInfoScreen().typeTextCreditCardEditText(user.getCreditCardNumber());
+				driver.enterLog(TAG, "Entering postal code: " + user.getAddressPostalCode());
 				driver.billingAddressScreen().typeTextPostalCode(user.getAddressPostalCode());
+				driver.enterLog(TAG, "Entering cardholder name: " + user.getFirstName() + " " + user.getLastName());
 				driver.cardInfoScreen().typeTextNameOnCardEditText(user.getFirstName() + " " + user.getLastName());
 				driver.cardInfoScreen().clickOnExpirationDateButton();
 				driver.delay(1);
 				driver.screenshot("Expiration date fragment");
 				driver.landscape();
 				driver.portrait();
+				driver.enterLog(TAG, "Incrementing credit card exp. month and year by 1");
 				driver.cardInfoScreen().clickMonthUpButton();
 				driver.cardInfoScreen().clickYearUpButton();
 				driver.cardInfoScreen().clickSetButton();
@@ -165,6 +192,7 @@ public class HotelsHappyPath {
 			}
 			else {
 				// Manually add traveler
+				driver.enterLog(TAG, "Manually entering traveler information");
 				driver.screenshot("Add traveler screen");
 				driver.landscape();
 				driver.portrait();
@@ -173,8 +201,11 @@ public class HotelsHappyPath {
 				driver.screenshot("Traveler information");
 				driver.landscape();
 				driver.portrait();
+				driver.enterLog(TAG, "Entering first name: " + user.getFirstName());
 				driver.travelerInformationScreen().enterFirstName(user.getFirstName());
+				driver.enterLog(TAG, "Entering last name: " + user.getLastName());
 				driver.travelerInformationScreen().enterLastName(user.getLastName());
+				driver.enterLog(TAG, "Entering phone number: " + user.getPhoneNumber());
 				driver.travelerInformationScreen().enterPhoneNumber(user.getPhoneNumber());
 				driver.travelerInformationScreen().clickDoneButton();
 
@@ -189,11 +220,15 @@ public class HotelsHappyPath {
 				driver.screenshot("Credit card expiration fragment");
 				driver.landscape();
 				driver.portrait();
+				driver.enterLog(TAG, "Incrementing credit card exp. month and year by 1");
 				driver.cardInfoScreen().clickMonthUpButton();
 				driver.cardInfoScreen().clickYearUpButton();
 				driver.cardInfoScreen().clickSetButton();
+				driver.enterLog(TAG, "Entering postal code: " + user.getAddressPostalCode());
 				driver.cardInfoScreen().typeTextPostalCode(user.getAddressPostalCode());
+				driver.enterLog(TAG, "Entering cardholder name: " + user.getFirstName() + " " + user.getLastName());
 				driver.cardInfoScreen().typeTextNameOnCardEditText(user.getFirstName() + " " + user.getLastName());
+				driver.enterLog(TAG, "Entering user email address: " + user.getLoginEmail());
 				driver.cardInfoScreen().typeTextEmailEditText(user.getLoginEmail());
 				driver.cardInfoScreen().clickOnDoneButton();
 			}
@@ -208,11 +243,13 @@ public class HotelsHappyPath {
 			driver.screenshot("Slide to checkout");
 			driver.landscape();
 			driver.portrait();
+			driver.enterLog(TAG, "Sliding to checkout");
 			driver.commonCheckout().slideToCheckout();
 			driver.delay();
 
 			// CVV Entry
 			driver.screenshot("CVV Entry");
+			driver.enterLog(TAG, "Entering CCV: " + user.getCCV());
 			driver.cvvEntryScreen().parseAndEnterCVV(user.getCCV());
 			driver.cvvEntryScreen().clickBookButton();
 			driver.delay(1);
@@ -221,7 +258,9 @@ public class HotelsHappyPath {
 			driver.screenshot("Confirmation Screen");
 			driver.landscape();
 			driver.portrait();
+			driver.enterLog(TAG, "Going back to launch screen.");
 			driver.hotelsConfirmationScreen().clickDoneButton();
+			driver.enterLog(TAG, "Clicking shop tab");
 			driver.launchScreen().pressShop();
 		}
 		catch (Error e) {
