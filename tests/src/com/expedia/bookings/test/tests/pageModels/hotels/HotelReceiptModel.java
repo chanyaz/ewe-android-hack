15b1eb2b9b556f7afd756c0e490a0880821388ba
Kevin Carpenter <kevc@mobiata.com>
2013-10-10 14:19:11 -0700

    Update HotelReceiptModel and its implementation so it works like other POMs

--- tests/src/com/expedia/bookings/test/tests/pageModels/hotels/HotelReceiptModel.java
+++ tests/src/com/expedia/bookings/test/tests/pageModels/hotels/HotelReceiptModel.java
@@ -3,15 +3,12 @@ package com.expedia.bookings.test.tests.pageModels.hotels;
 import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.res.Resources;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.expedia.bookings.R;
 import com.expedia.bookings.test.tests.pageModels.common.ScreenActions;
 import com.expedia.bookings.test.utils.TestPreferences;
-import com.expedia.bookings.utils.Ui;
 
 public class HotelReceiptModel extends ScreenActions {
 
@@ -27,71 +24,60 @@ public class HotelReceiptModel extends ScreenActions {
 	private static final int GRAND_TOTAL_TEXT_VIEW_ID = R.id.grand_total_text;
 	private static final int WALLET_PROMO_APPLIED_TEXT_VIEW_ID = R.id.wallet_promo_applied_text_view;
 
-	private ImageView mHeaderImageView;
-	private TextView mRoomTypeDesciptionTextView;
-	private TextView mBedTypeNameTextView;
-	private TextView mRoomLongDescriptionTextView;
-
-	private TextView mNightsTextView;
-	private TextView mDateRangeTextView;
-	private TextView mGuestsTextView;
-	private TextView mPriceTextView;
-	private TextView mGrandTotalTextView;
-	private TextView mWalletPromoAppliedTextView;
+	private static final int COST_SUMMARY_STRING_ID = R.string.cost_summary;
 
 	public HotelReceiptModel(Instrumentation instrumentation, Activity activity, Resources res,
 			TestPreferences preferences) {
 		super(instrumentation, activity, res, preferences);
-		mHeaderImageView = (ImageView) getView(HEADER_IMAGE_VIEW_ID);
-		mRoomLongDescriptionTextView = (TextView) getView(ROOM_TYPE_DESCRIPTION_TEXT_VIEW_ID);
-		mBedTypeNameTextView = (TextView) getView(BED_TYPE_NAME_TEXT_VIEW_ID);
-		mRoomLongDescriptionTextView = (TextView) getView(ROOM_LONG_DESCRIPTION_TEXT_VIEW_ID);
-
-		mNightsTextView = (TextView) getView(NIGHTS_TEXT_VIEW_ID);
-		mDateRangeTextView = (TextView) getView(DATE_RANGE_TEXT_VIEW_ID);
-		mGuestsTextView = (TextView) getView(GUESTS_TEXT_VIEW_ID);
-		mPriceTextView = (TextView) getView(PRICE_TEXT_VIEW_ID);
-		mGrandTotalTextView = (TextView) getView(GRAND_TOTAL_TEXT_VIEW_ID);
-		mWalletPromoAppliedTextView = (TextView) getView(WALLET_PROMO_APPLIED_TEXT_VIEW_ID);
 	}
 
 	public ImageView headerImageView() {
-		return mHeaderImageView;
+		return (ImageView) getView(HEADER_IMAGE_VIEW_ID);
 	}
 
 	public TextView roomTypeDescriptionTextView() {
-		return mRoomTypeDesciptionTextView;
+		return (TextView) getView(ROOM_TYPE_DESCRIPTION_TEXT_VIEW_ID);
 	}
 
 	public TextView bedTypeTextView() {
-		return mBedTypeNameTextView;
+		return (TextView) getView(BED_TYPE_NAME_TEXT_VIEW_ID);
 	}
 
 	public TextView roomDescriptionTextView() {
-		return mRoomLongDescriptionTextView;
+		return (TextView) getView(ROOM_LONG_DESCRIPTION_TEXT_VIEW_ID);
 	}
 
 	public TextView nightsTextView() {
-		return mNightsTextView;
+		return (TextView) getView(NIGHTS_TEXT_VIEW_ID);
 	}
 
 	public TextView dateRangeTextView() {
-		return mDateRangeTextView;
+		return (TextView) getView(DATE_RANGE_TEXT_VIEW_ID);
 	}
 
 	public TextView guestsTextView() {
-		return mGuestsTextView;
+		return (TextView) getView(GUESTS_TEXT_VIEW_ID);
 	}
 
 	public TextView priceTextView() {
-		return mPriceTextView;
+		return (TextView) getView(PRICE_TEXT_VIEW_ID);
 	}
 
 	public TextView grandTotalTextView() {
-		return mGrandTotalTextView;
+		return (TextView) getView(GRAND_TOTAL_TEXT_VIEW_ID);
 	}
 
 	public TextView walletPromoAppliedTextView() {
-		return mWalletPromoAppliedTextView;
+		return (TextView) getView(WALLET_PROMO_APPLIED_TEXT_VIEW_ID);
+	}
+
+	public String costSummaryString() {
+		return getString(COST_SUMMARY_STRING_ID);
+	}
+
+	// Object interaction
+
+	public void clickGrandTotalTextView() {
+		clickOnView(grandTotalTextView());
 	}
 }
--- tests/src/com/expedia/bookings/test/tests/pageModels/hotels/HotelsCheckoutScreen.java
+++ tests/src/com/expedia/bookings/test/tests/pageModels/hotels/HotelsCheckoutScreen.java
@@ -3,11 +3,18 @@ package com.expedia.bookings.test.tests.pageModels.hotels;
 import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.res.Resources;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
+import com.expedia.bookings.R;
 import com.expedia.bookings.test.tests.pageModels.common.CommonCheckoutScreen;
 import com.expedia.bookings.test.utils.TestPreferences;
 
 public class HotelsCheckoutScreen extends CommonCheckoutScreen {
+
+	private static final int TITLE_VIEW_ID = R.id.title;
+	private static final int RATING_VIEW_ID = R.id.rating;
+	private HotelReceiptModel mHotelReceiptModel;
 
 	public HotelsCheckoutScreen(Instrumentation instrumentation, Activity activity, Resources res,
 			TestPreferences preferences) {
@@ -15,8 +22,21 @@ public class HotelsCheckoutScreen extends CommonCheckoutScreen {
 	}
 
 	public HotelReceiptModel hotelReceiptModel() {
-		return new HotelReceiptModel(mInstrumentation, getCurrentActivity(), mRes,
-				mPreferences);
+		if (mHotelReceiptModel == null) {
+			mHotelReceiptModel = new HotelReceiptModel(mInstrumentation, getCurrentActivity(), mRes,
+					mPreferences);
+		}
+		return mHotelReceiptModel;
+	}
+
+	// Object access
+
+	public TextView hotelNameView() {
+		return (TextView) getView(TITLE_VIEW_ID);
+	}
+
+	public RatingBar ratingBar() {
+		return (RatingBar) getView(RATING_VIEW_ID);
 	}
 
 }
