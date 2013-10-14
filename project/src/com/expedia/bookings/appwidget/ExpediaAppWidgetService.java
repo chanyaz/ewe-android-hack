f64641661a16eb2f66ad35b1f484f7f1241f3b8a
Daniel Lew <dan@mobiata.com>
2013-10-14 12:25:26 -0500

    Added basic search to widget

--- project/src/com/expedia/bookings/appwidget/ExpediaAppWidgetService.java
+++ project/src/com/expedia/bookings/appwidget/ExpediaAppWidgetService.java
@@ -1,7 +1,11 @@
 package com.expedia.bookings.appwidget;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
+import org.joda.time.DateTime;
 import org.joda.time.Hours;
 import org.joda.time.Minutes;
 import org.joda.time.ReadablePeriod;
@@ -14,13 +18,18 @@ import android.os.Bundle;
 import android.os.IBinder;
 
 import com.expedia.bookings.data.HotelSearchParams;
+import com.expedia.bookings.data.HotelSearchResponse;
 import com.expedia.bookings.data.Property;
+import com.expedia.bookings.server.ExpediaServices;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
 import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
+import com.mobiata.android.BackgroundDownloader;
+import com.mobiata.android.BackgroundDownloader.Download;
+import com.mobiata.android.BackgroundDownloader.OnDownloadComplete;
 import com.mobiata.android.Log;
 
 public class ExpediaAppWidgetService extends Service implements ConnectionCallbacks, OnConnectionFailedListener,
@@ -32,6 +41,9 @@ public class ExpediaAppWidgetService extends Service implements ConnectionCallba
 	public static final String TAG = "ExpediaWidget";
 
 	private static final String WIDGET_KEY_SEARCH = "WIDGET_KEY_SEARCH";
+
+	// The maximum # of properties to show in the widget
+	private static final int MAX_DEALS = 5;
 
 	// How often we should update the widget, regardless of anything else
 	private static final ReadablePeriod UPDATE_INTERVAL = Hours.ONE;
@@ -59,8 +71,9 @@ public class ExpediaAppWidgetService extends Service implements ConnectionCallba
 
 	private LocationClient mLocationClient;
 
-	private HotelSearchParams mSearchParams;
-	private List<Property> mDeals;
+	private HotelSearchParams mSearchParams = new HotelSearchParams();
+	private List<Property> mDeals = new ArrayList<Property>();
+	private DateTime mLastUpateTimestamp;
 
 	//////////////////////////////////////////////////////////////////////////
 	// Lifecycle
@@ -101,6 +114,78 @@ public class ExpediaAppWidgetService extends Service implements ConnectionCallba
 	}
 
 	//////////////////////////////////////////////////////////////////////////
+	// Search
+
+	private void startNewSearch() {
+		BackgroundDownloader bd = BackgroundDownloader.getInstance();
+
+		if (bd.isDownloading(WIDGET_KEY_SEARCH)) {
+			Log.w(TAG, "Widget was already doing a search when a new one was started!");
+			bd.cancelDownload(WIDGET_KEY_SEARCH);
+		}
+
+		bd.startDownload(WIDGET_KEY_SEARCH, mSearchDownload, mSearchCallback);
+	}
+
+	private Download<HotelSearchResponse> mSearchDownload = new Download<HotelSearchResponse>() {
+		@Override
+		public HotelSearchResponse doDownload() {
+			ExpediaServices services = new ExpediaServices(getApplicationContext());
+			BackgroundDownloader.getInstance().addDownloadListener(WIDGET_KEY_SEARCH, services);
+			return services.search(mSearchParams, ExpediaServices.F_FROM_WIDGET);
+		}
+	};
+
+	private OnDownloadComplete<HotelSearchResponse> mSearchCallback = new OnDownloadComplete<HotelSearchResponse>() {
+		@Override
+		public void onDownload(HotelSearchResponse results) {
+			mDeals.clear();
+			mDeals.addAll(getDeals(results));
+		}
+	};
+
+	// Filters through a full response to find the best deals
+	private List<Property> getDeals(HotelSearchResponse response) {
+		List<Property> deals = new ArrayList<Property>();
+
+		if (response != null && !response.hasErrors() && response.getPropertiesCount() != 0) {
+			List<Property> properties = new ArrayList<Property>(response.getProperties());
+
+			// Sort by rating, so we only show the best
+			Collections.sort(properties, Property.RATING_COMPARATOR);
+
+			// Find hotels on sale first
+			Iterator<Property> propertyIterator = properties.iterator();
+			while (propertyIterator.hasNext() && deals.size() < MAX_DEALS) {
+				Property property = propertyIterator.next();
+				if (property.getLowestRate().isOnSale()) {
+					deals.add(property);
+					propertyIterator.remove();
+				}
+			}
+
+			// Then add any highly-rated hotels
+			propertyIterator = properties.iterator();
+			while (propertyIterator.hasNext() && deals.size() < MAX_DEALS) {
+				Property property = propertyIterator.next();
+				if (property.isHighlyRated()) {
+					deals.add(property);
+					propertyIterator.remove();
+				}
+			}
+
+			// Add the rest until we get to the max
+			propertyIterator = properties.iterator();
+			while (propertyIterator.hasNext() && deals.size() < MAX_DEALS) {
+				Property property = propertyIterator.next();
+				deals.add(property);
+			}
+		}
+
+		return deals;
+	}
+
+	//////////////////////////////////////////////////////////////////////////
 	// ConnectionCallbacks
 
 	@Override
@@ -138,6 +223,7 @@ public class ExpediaAppWidgetService extends Service implements ConnectionCallba
 
 		// Due to the way we setup LocationRequest, this should only be called
 		// when we actually want to do a new search (due to the user moving).
+		startNewSearch();
 	}
 
 	//////////////////////////////////////////////////////////////////////////
