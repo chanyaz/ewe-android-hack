8c21a634a2cb511d41082fd3f2f31f9c8b46b968
Joel Drotos <jdrotos@mobiata.com>
2013-10-15 12:56:06 -0500

    Tablet Results - Add new measurement interfaces to replace parts of ITabletResultsController.

---
+++ project/src/com/expedia/bookings/interfaces/IMeasurementListener.java
@@ -0,0 +1,14 @@
+package com.expedia.bookings.interfaces;
+
+/**
+ * This is implemented by consumers of IMeasurementProvider.updateContentSize 
+ */
+public interface IMeasurementListener {
+	/**
+	 * Fired when the content size has changed.
+	 * 
+	 * @param totalWidth
+	 * @param totalHeight
+	 */
+	public void onContentSizeUpdated(int totalWidth, int totalHeight);
+}
