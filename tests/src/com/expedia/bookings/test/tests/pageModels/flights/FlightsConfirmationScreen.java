4246649b6c10f1bacddd87bea8f224e4af8c79e1
Kevin Carpenter <kevc@mobiata.com>
2013-10-29 14:15:50 -0700

    Update variable names in FlightsConfirmationScreen

--- tests/src/com/expedia/bookings/test/tests/pageModels/flights/FlightsConfirmationScreen.java
+++ tests/src/com/expedia/bookings/test/tests/pageModels/flights/FlightsConfirmationScreen.java
@@ -11,9 +11,9 @@ import com.expedia.bookings.widget.TextView;
 
 public class FlightsConfirmationScreen extends ConfirmationScreen {
 
-	private static final int sGoingToTextViewID = R.id.going_to_text_view;
-	private static final int sGetARoomTextViewID = R.id.hotels_action_text_view;
-	private static final int sFlightTrackActionViewID = R.id.flighttrack_action_text_view;
+	private static final int GOING_TO_TEXT_VIEW_ID = R.id.going_to_text_view;
+	private static final int GET_A_ROOM_TEXT_VIEW_ID = R.id.hotels_action_text_view;
+	private static final int FLIGHT_TRACK_ACTION_VIEW_ID = R.id.flighttrack_action_text_view;
 
 	public FlightsConfirmationScreen(Instrumentation instrumentation, Activity activity, Resources res,
 			TestPreferences preferences) {
@@ -23,15 +23,15 @@ public class FlightsConfirmationScreen extends ConfirmationScreen {
 	// Object access
 
 	public TextView goingToTextView() {
-		return (TextView) getView(sGoingToTextViewID);
+		return (TextView) getView(GOING_TO_TEXT_VIEW_ID);
 	}
 
 	public TextView getARoomTextView() {
-		return (TextView) getView(sGetARoomTextViewID);
+		return (TextView) getView(GET_A_ROOM_TEXT_VIEW_ID);
 	}
 
 	public TextView trackWithFlightTrackTextView() {
-		return (TextView) getView(sFlightTrackActionViewID);
+		return (TextView) getView(FLIGHT_TRACK_ACTION_VIEW_ID);
 	}
 
 	// Object interaction
