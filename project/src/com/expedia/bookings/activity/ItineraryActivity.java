a4360343cf5ef1a18ea6160cb76a73f7b08af338
Brennan Taylor <brennan@mobiata.com>
2013-11-26 14:15:10 -0800

    Use onBackPressed() to handle home button for ItineraryActivity

--- project/src/com/expedia/bookings/activity/ItineraryActivity.java
+++ project/src/com/expedia/bookings/activity/ItineraryActivity.java
@@ -334,21 +334,8 @@ public class ItineraryActivity extends SherlockFragmentActivity implements ItinI
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home: {
-			Intent upIntent = NavUtils.getParentActivityIntent(this);
-			if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
-				// This activity is NOT part of this app's task, so create a new task
-				// when navigating up, with a synthesized back stack.
-				TaskStackBuilder.create(this)
-						// Add all of this activity's parents to the back stack
-						.addNextIntentWithParentStack(upIntent)
-						// Navigate up to the closest parent
-						.startActivities();
-			}
-			else {
-				// This activity is part of this app's task, so simply
-				// navigate up to the logical parent activity.
-				NavUtils.navigateUpTo(this, upIntent);
-			}
+			// Let onBackPressed handle the task creation logic for us
+			onBackPressed();
 			return true;
 		}
 		case R.id.menu_add_guest_itinerary: {
