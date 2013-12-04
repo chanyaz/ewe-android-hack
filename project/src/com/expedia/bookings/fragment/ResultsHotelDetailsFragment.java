3ee668dbd5786f883df4276e72a044af5263133e
Doug Melton <doug@mobiata.com>
2013-12-04 10:31:06 -0800

    2060: Added hotel amenities section

--- project/src/com/expedia/bookings/fragment/ResultsHotelDetailsFragment.java
+++ project/src/com/expedia/bookings/fragment/ResultsHotelDetailsFragment.java
@@ -11,10 +11,16 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.HorizontalScrollView;
 
 import com.expedia.bookings.R;
+import com.expedia.bookings.data.Db;
+import com.expedia.bookings.data.Property;
 import com.expedia.bookings.interfaces.IAddToTripListener;
+import com.expedia.bookings.utils.LayoutUtils;
 import com.expedia.bookings.utils.ScreenPositionUtils;
+import com.expedia.bookings.widget.ScrollView;
+import com.mobiata.android.Log;
 import com.mobiata.android.util.Ui;
 
 /**
@@ -61,6 +67,17 @@ public class ResultsHotelDetailsFragment extends Fragment {
 		return mRootC;
 	}
 
+	@Override
+	public void onResume() {
+		super.onResume();
+		Db.getHotelSearch().getSelectedProperty()
+		setupViews(mRootC);
+	}
+
+	private void setupViews(View view) {
+		setupAmenities(view, Db.getHotelSearch().getSelectedProperty());
+	}
+
 	public void setTransitionToAddTripPercentage(float percentage) {
 		//TODO
 		//		if (mAddToTripButton != null) {
@@ -83,4 +100,28 @@ public class ResultsHotelDetailsFragment extends Fragment {
 		return ScreenPositionUtils.getGlobalScreenPosition(mHotelHeaderContainer);
 	}
 
+	private void setupAmenities(View view, Property property) {
+		if (property == null) {
+			return;
+		}
+
+		// Disable some aspects of the horizontal scrollview so it looks pretty
+		HorizontalScrollView amenitiesScrollView = (HorizontalScrollView) view.findViewById(R.id.amenities_scroll_view);
+		amenitiesScrollView.setHorizontalScrollBarEnabled(false);
+		amenitiesScrollView.setOverScrollMode(ScrollView.OVER_SCROLL_NEVER);
+
+		ViewGroup amenitiesContainer = (ViewGroup) view.findViewById(R.id.amenities_table_row);
+		amenitiesContainer.removeAllViews();
+		LayoutUtils.addAmenities(getActivity(), property, amenitiesContainer);
+
+		// Hide the text that indicated no amenities because there are amenities
+		view.findViewById(R.id.amenities_none_text).setVisibility(View.GONE);
+		if (property.hasAmenities()) {
+			view.findViewById(R.id.amenities_scroll_view).setVisibility(View.VISIBLE);
+		}
+		else {
+			view.findViewById(R.id.amenities_scroll_view).setVisibility(View.GONE);
+		}
+
+	}
 }
