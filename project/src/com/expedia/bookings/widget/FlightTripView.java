007fe15b51264ddada5db8d759fa4b5d5bba90b9
Brennan Taylor <brennan@mobiata.com>
2013-12-23 11:12:43 -0800

    2391: Use good minTime and maxTime not a naughty null one
    It looks like it was accounted for but not used throughout the code.

--- project/src/com/expedia/bookings/widget/FlightTripView.java
+++ project/src/com/expedia/bookings/widget/FlightTripView.java
@@ -298,8 +298,8 @@ public class FlightTripView extends View {
 				endMillis = flight2.mOrigin.getBestSearchDateTime().getTimeInMillis();
 			}
 			//Ensure we dont go over/under our max/min (this can happen if one of our intermediate waypoints gets rerouted or the max min values provided are bunk)
-			startMillis = startMillis < mMinTime.getTimeInMillis() ? mMinTime.getTimeInMillis() : startMillis;
-			endMillis = endMillis > mMaxTime.getTimeInMillis() ? mMaxTime.getTimeInMillis() : endMillis;
+			startMillis = startMillis < minTime.getTimeInMillis() ? minTime.getTimeInMillis() : startMillis;
+			endMillis = endMillis > maxTime.getTimeInMillis() ? maxTime.getTimeInMillis() : endMillis;
 
 			long durationMillis = endMillis - startMillis;
 			widths[a] = (durationMillis / (float) totalRangeMillis) * totalAvailableWidth;
