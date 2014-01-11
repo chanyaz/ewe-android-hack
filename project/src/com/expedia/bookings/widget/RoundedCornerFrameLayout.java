07e345d56a2321db24b04882ae163adcee830464
Doug Melton <doug@mobiata.com>
2014-01-10 17:06:27 -0800

    2407: Gave Hotel Details fragment rounded corners

---
+++ project/src/com/expedia/bookings/widget/RoundedCornerFrameLayout.java
@@ -0,0 +1,94 @@
+package com.expedia.bookings.widget;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.expedia.bookings.R;
+
+/**
+ * A FrameLayout that can apply rounded corners to its children, with a couple caveats:
+ *
+ * 1. Do not use this if you simply want an image with rounded corners. In that case, use a
+ * (more performant) BitmapShader.
+ *
+ * 2. This does not perform antialiasing on the rounded corners, due to API limitations. So make
+ * sure they look ok.
+ *
+ * This works quite nicely in other cases.
+ *
+ * @author doug
+ * @see { http://stackoverflow.com/questions/12358350/rendering-rounded-corners-for-imageview-on-android }
+ */
+public class RoundedCornerFrameLayout extends FrameLayout {
+
+    private Path roundedPath;
+    private float mRadius;
+
+    public RoundedCornerFrameLayout(Context context) {
+        this(context, null);
+    }
+
+    public RoundedCornerFrameLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public RoundedCornerFrameLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context, attrs);
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void init(Context context, AttributeSet attrs) {
+
+        if (attrs != null) {
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.RoundedCornerFrameLayout);
+            // Set the rounded rect radius (default 0)
+            mRadius = a.getDimension(R.styleable.RoundedCornerFrameLayout_radius, 0);
+            a.recycle();
+        }
+
+        setRadius(mRadius);
+
+        // If the application is hardware accelerated,
+        // must disable it for this view.
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        if (w != oldw || h != oldh) {
+            roundedPath = new Path();
+            roundedPath.addRoundRect(new RectF(0, 0, w, h),
+                    mRadius, mRadius, Path.Direction.CW);
+        }
+    }
+
+    public float getRadius() {
+
+        return mRadius;
+    }
+
+    public void setRadius(float radius) {
+        mRadius = radius;
+        roundedPath = new Path();
+        roundedPath.addRoundRect(new RectF(0, 0, getWidth(), getHeight()),
+                radius, radius, Path.Direction.CW);
+    }
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        //Apply the clip
+        if (mRadius != 0) {
+            canvas.clipPath(roundedPath);
+        }
+        //Let the view draw as normal
+        super.dispatchDraw(canvas);
+    }
+}
