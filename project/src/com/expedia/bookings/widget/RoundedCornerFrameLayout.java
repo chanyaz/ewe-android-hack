4201e5cb152a522f7f514c675223d6b92d46440f
Doug Melton <doug@mobiata.com>
2014-01-10 21:02:22 -0800

    Tried to code cleanup

--- project/src/com/expedia/bookings/widget/RoundedCornerFrameLayout.java
+++ project/src/com/expedia/bookings/widget/RoundedCornerFrameLayout.java
@@ -14,13 +14,13 @@ import com.expedia.bookings.R;
 
 /**
  * A FrameLayout that can apply rounded corners to its children, with a couple caveats:
- *
+ * <p/>
  * 1. Do not use this if you simply want an image with rounded corners. In that case, use a
  * (more performant) BitmapShader.
- *
+ * <p/>
  * 2. This does not perform antialiasing on the rounded corners, due to API limitations. So make
  * sure they look ok.
- *
+ * <p/>
  * This works quite nicely in other cases.
  *
  * @author doug
@@ -28,67 +28,67 @@ import com.expedia.bookings.R;
  */
 public class RoundedCornerFrameLayout extends FrameLayout {
 
-    private Path roundedPath;
-    private float mRadius;
+	private Path roundedPath;
+	private float mRadius;
 
-    public RoundedCornerFrameLayout(Context context) {
-        this(context, null);
-    }
+	public RoundedCornerFrameLayout(Context context) {
+		this(context, null);
+	}
 
-    public RoundedCornerFrameLayout(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
+	public RoundedCornerFrameLayout(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
 
-    public RoundedCornerFrameLayout(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        init(context, attrs);
-    }
+	public RoundedCornerFrameLayout(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init(context, attrs);
+	}
 
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    private void init(Context context, AttributeSet attrs) {
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void init(Context context, AttributeSet attrs) {
 
-        if (attrs != null) {
-            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.RoundedCornerFrameLayout);
-            // Set the rounded rect radius (default 0)
-            mRadius = a.getDimension(R.styleable.RoundedCornerFrameLayout_radius, 0);
-            a.recycle();
-        }
+		if (attrs != null) {
+			TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.RoundedCornerFrameLayout);
+			// Set the rounded rect radius (default 0)
+			mRadius = a.getDimension(R.styleable.RoundedCornerFrameLayout_radius, 0);
+			a.recycle();
+		}
 
-        setRadius(mRadius);
+		setRadius(mRadius);
 
-        // If the application is hardware accelerated,
-        // must disable it for this view.
-        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-    }
+		// If the application is hardware accelerated,
+		// must disable it for this view.
+		setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+	}
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        if (w != oldw || h != oldh) {
-            roundedPath = new Path();
-            roundedPath.addRoundRect(new RectF(0, 0, w, h),
-                    mRadius, mRadius, Path.Direction.CW);
-        }
-    }
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		if (w != oldw || h != oldh) {
+			roundedPath = new Path();
+			roundedPath.addRoundRect(new RectF(0, 0, w, h),
+					mRadius, mRadius, Path.Direction.CW);
+		}
+	}
 
-    public float getRadius() {
+	public float getRadius() {
 
-        return mRadius;
-    }
+		return mRadius;
+	}
 
-    public void setRadius(float radius) {
-        mRadius = radius;
-        roundedPath = new Path();
-        roundedPath.addRoundRect(new RectF(0, 0, getWidth(), getHeight()),
-                radius, radius, Path.Direction.CW);
-    }
+	public void setRadius(float radius) {
+		mRadius = radius;
+		roundedPath = new Path();
+		roundedPath.addRoundRect(new RectF(0, 0, getWidth(), getHeight()),
+				radius, radius, Path.Direction.CW);
+	}
 
-    @Override
-    protected void dispatchDraw(Canvas canvas) {
-        //Apply the clip
-        if (mRadius != 0) {
-            canvas.clipPath(roundedPath);
-        }
-        //Let the view draw as normal
-        super.dispatchDraw(canvas);
-    }
+	@Override
+	protected void dispatchDraw(Canvas canvas) {
+		//Apply the clip
+		if (mRadius != 0) {
+			canvas.clipPath(roundedPath);
+		}
+		//Let the view draw as normal
+		super.dispatchDraw(canvas);
+	}
 }
