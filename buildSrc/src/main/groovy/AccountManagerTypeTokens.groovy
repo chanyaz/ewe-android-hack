f54387f4a662370ca93a681d10816dd8ca2dd992
Brennan Taylor <brennan@mobiata.com>
2014-01-23 00:23:39 -0800

    Add Travelocity data for build

--- buildSrc/src/main/groovy/AccountManagerTypeTokens.groovy
+++ buildSrc/src/main/groovy/AccountManagerTypeTokens.groovy
@@ -4,8 +4,11 @@ class AccountManagerTypeTokens {
     // logic required for production app compatibility purposes.
 
     // Production apps before the dawn of this class refer to "expedia.tuid" and "vsc.tuid"
-    static def packageNameTokenPairsThatCannotChange = ['com.expedia.bookings' : 'expedia.tuid',
-                                                            'com.expedia.bookings.vsc' : 'vsc.tuid']
+    static def packageNameTokenPairsThatCannotChange = [
+        'com.expedia.bookings' : 'expedia.tuid',
+        'com.expedia.bookings.vsc' : 'vsc.tuid',
+        'com.travelocity.android' : 'travelocity.tuid',
+    ]
 
     static final def TOKEN_SUFFIX = ".tuid"
 
