6768156292cc1878232144b23eca44a45b24b1c7
Brennan Taylor <brennan@mobiata.com>
2014-01-29 11:12:28 -0800

    Override app name for release and latest builds

--- buildSrc/src/main/groovy/AppNameLabels.groovy
+++ buildSrc/src/main/groovy/AppNameLabels.groovy
@@ -14,6 +14,7 @@ class AppNameLabels {
             'com.expedia.bookings.vsc.latest' : 'VSC Latest',
 
             'com.travelocity.android.debug' : 'Travelocity Debug',
+            'com.travelocity.android.latest' : 'Travelocity Latest',
     ]
 
     static def getAppName(buildVariant) {
