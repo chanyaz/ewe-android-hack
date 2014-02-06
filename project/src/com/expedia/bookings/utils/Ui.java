79bcce1f58ce8bd5f1181b29792742c7df701485
Brennan Taylor <brennan@mobiata.com>
2014-02-06 13:35:13 -0800

    Change Ui.obtain attribute methods to take a Context
    I also put a very helpful warning comment next to the methods. I'm not
    too worried about these methods being called from an unsupported
    context since they're called from Activities, Fragments and Views which
    all support Theme information.

--- project/src/com/expedia/bookings/utils/Ui.java
+++ project/src/com/expedia/bookings/utils/Ui.java
@@ -179,14 +179,10 @@ public class Ui extends com.mobiata.android.util.Ui {
 		runOnNextLayout(fragment.getView(), runnable);
 	}
 
-	private static TypedArray obtainTypedArray(Activity activity, int attr) {
-		TypedArray a = activity.obtainStyledAttributes(new int[] {attr});
-		if (!a.hasValue(0)) {
-			throw new RuntimeException("Theme attribute not defined for attr=" + Integer.toHexString(attr));
-		}
-		return a;
-	}
 
+	//
+	// Be careful that the passed Context supports Theme information.
+	//
 	private static TypedArray obtainTypedArray(Context context, int attr) {
 		TypedArray a = context.obtainStyledAttributes(new int[] {attr});
 		if (!a.hasValue(0)) {
@@ -195,28 +191,20 @@ public class Ui extends com.mobiata.android.util.Ui {
 		return a;
 	}
 
-	public static int obtainThemeColor(Activity activity, int attr) {
-		TypedArray a = obtainTypedArray(activity, attr);
+	public static int obtainThemeColor(Context context, int attr) {
+		TypedArray a = obtainTypedArray(context, attr);
 		int color = a.getColor(0, 0);
 		a.recycle();
 
 		return color;
 	}
 
-	public static Drawable obtainThemeDrawable(Activity activity, int attr) {
-		TypedArray a = obtainTypedArray(activity, attr);
+	public static Drawable obtainThemeDrawable(Context context, int attr) {
+		TypedArray a = obtainTypedArray(context, attr);
 		Drawable drawable = a.getDrawable(0);
 		a.recycle();
 
 		return drawable;
-	}
-
-	public static int obtainThemeResID(Activity activity, int attr) {
-		TypedArray a = obtainTypedArray(activity, attr);
-		int resID = a.getResourceId(0, -1);
-		a.recycle();
-
-		return resID;
 	}
 
 	public static int obtainThemeResID(Context context, int attr) {
