c646cabdd1778cc8e399c69669231adb920f9464
Subodh Gupta <sugupta@expedia.com>
2014-02-13 10:09:16 -0800

    148: Tvly homescreen images rotate
    Summary: The images now rotate about every 8 seconds through a preset
    list of drawables.
    
    Reviewers: brennan
    
    Reviewed By: brennan
    
    Differential Revision: https://phabricator.mobiata.com/D108

--- project/src/com/expedia/bookings/fragment/TravelocityLauncherFragment.java
+++ project/src/com/expedia/bookings/fragment/TravelocityLauncherFragment.java
@@ -1,10 +1,12 @@
 package com.expedia.bookings.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.expedia.bookings.R;
 import com.expedia.bookings.tracking.OmnitureTracking;
@@ -19,32 +21,61 @@ public class TravelocityLauncherFragment extends Fragment {
 
     public static final String TAG = TravelocityLauncherFragment.class.getName();
     private boolean mLaunchingActivity = false;
+    private ImageView mSlidingImage;
+    private int mCurrentImageIndex = 0;
+    private static final int IMAGE_ROTATION_PERIOD = 8000;
+    private Handler mHandler;
+    private Runnable mUpdateResults;
 
     public static TravelocityLauncherFragment newInstance() {
         return new TravelocityLauncherFragment();
     }
 
-    // Background images
-    private static final Integer BACKGROUND_RES_IDS = R.drawable.bg_launch_london;
+    private static final Integer[] BACKGROUND_RES_IDS = new Integer[] {
+            R.drawable.bg_launch_sf_placeholder,
+            R.drawable.bg_launch_london,
+            R.drawable.bg_launch_ny,
+            R.drawable.bg_launch_paris,
+            R.drawable.bg_launch_sea,
+            R.drawable.bg_launch_sf,
+            R.drawable.bg_launch_toronto,
+            R.drawable.bg_launch_hongkong,
+            R.drawable.bg_launch_petronas,
+            R.drawable.bg_launch_vegas
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
        final View view = inflater.inflate(R.layout.fragment_tvly_launch, container, false);
        Ui.findView(view, R.id.tvly_flights).setOnClickListener(mHeaderItemOnClickListener);
        Ui.findView(view, R.id.tvly_hotels).setOnClickListener(mHeaderItemOnClickListener);
-       return view;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
+       mSlidingImage = Ui.findView(view, R.id.tvly_home_bg_view);
+       mHandler = new Handler();
+       mUpdateResults = new Runnable() {
+            public void run() {
+                animateAndSlideShow();
+                mHandler.postDelayed(this, IMAGE_ROTATION_PERIOD);
+            }
+        };
+        return view;
     }
 
     @Override
     public void onResume() {
         super.onResume();
-
         mLaunchingActivity = false;
+        mHandler.postDelayed(mUpdateResults, IMAGE_ROTATION_PERIOD);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mHandler.removeCallbacks(mUpdateResults);
+    }
+
+    private void animateAndSlideShow() {
+        mCurrentImageIndex = (mCurrentImageIndex + 1) % BACKGROUND_RES_IDS.length;
+        mSlidingImage.setImageResource(BACKGROUND_RES_IDS[mCurrentImageIndex]);
     }
 
     private final View.OnClickListener mHeaderItemOnClickListener = new View.OnClickListener() {
