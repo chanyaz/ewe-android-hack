26e48a33f9bdbf5973ebdea4ec067a29a32ba689
Kevin Carpenter <kevc@mobiata.com>
2014-02-18 09:36:15 -0800

    Update SearchResults POM

--- project/tests/src/com/expedia/bookings/test/tests/pageModels/tablet/SearchResults.java
+++ project/tests/src/com/expedia/bookings/test/tests/pageModels/tablet/SearchResults.java
@@ -1,32 +1,41 @@
 package com.expedia.bookings.test.tests.pageModels.tablet;
 
+import com.expedia.bookings.R;
+import com.expedia.bookings.section.HotelSummarySection;
+import com.google.android.apps.common.testing.ui.espresso.DataInteraction;
+import com.google.android.apps.common.testing.ui.espresso.ViewInteraction;
+
+import static com.expedia.bookings.test.utils.EspressoUtils.swipeUp;
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onData;
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withContentDescription;
-import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
-import static org.hamcrest.Matchers.instanceOf;
-import static com.expedia.bookings.test.utils.EspressoUtils.*;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.allOf;
-
-import com.expedia.bookings.R;
-import com.google.android.apps.common.testing.ui.espresso.ViewInteraction;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.core.AllOf.allOf;
 
 public class SearchResults {
 
 	public static ViewInteraction hotelList() {
-		return onView(withId(R.id.trip_bucket_hotel_trip_swipeout));
+		return onView(withContentDescription("Hotel Search Results"));
 	}
 
 	public static ViewInteraction flightList() {
-		return onView(withId(R.id.trip_bucket_flight_trip_swipeout));
+		return onView(withContentDescription("Flight Search Results"));
 	}
 
 	public static void swipeUpHotelList() {
-		onData(is(instanceOf(com.expedia.bookings.section.HotelSummarySection.class))).inAdapterView(allOf(
-				withId(android.R.id.list), withContentDescription("Hotel List"))).atPosition(0)
-				.perform(click());
+		hotelList().perform(swipeUp());
+	}
+
+	public static ViewInteraction sortAndFilterButton() {
+		return onView(allOf(withText(R.string.Sort_and_Filter), isCompletelyDisplayed()));
+	}
+
+	public static void swipeUpFlightList() {
+		flightList().perform(swipeUp());
 	}
 
 	public static ViewInteraction actionUpButton() {
@@ -37,4 +46,7 @@ public class SearchResults {
 		return TripBucket.getInstance();
 	}
 
+	public static DataInteraction getHotelFromList(int i) {
+		return onData(not(is(HotelSummarySection.class))).inAdapterView(withContentDescription("Hotel Search Results")).atPosition(i);
+	}
 }
