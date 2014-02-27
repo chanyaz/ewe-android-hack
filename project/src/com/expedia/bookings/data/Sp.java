2afd3612e5dab23d955c1d21da6c67b9c4b60871
Joel Drotos <jdrotos@mobiata.com>
2014-02-27 11:40:27 -0600

    TabResRef - Lets give this whole otto thing a try wrt search param updates

---
+++ project/src/com/expedia/bookings/data/Sp.java
@@ -0,0 +1,38 @@
+package com.expedia.bookings.data;
+
+import com.squareup.otto.Bus;
+
+/**
+ * Created by jdrotos on 2/27/14.
+ */
+public class Sp {
+
+	private static SearchParams sSearchParams;
+	private static Bus sBus;
+
+	public static Bus getBus(){
+		if(sBus == null){
+			sBus = new Bus();
+		}
+		return sBus;
+	}
+
+	public static SearchParams getParams(){
+		if(sSearchParams == null){
+			sSearchParams = new SearchParams();
+		}
+		return sSearchParams;
+	}
+
+	public static void reportSpUpdate(){
+		getBus().post(new SpUpdateEvent());
+	}
+
+	public static class SpUpdateEvent{
+		public SpUpdateEvent(){}
+	}
+
+	private Sp(){
+
+	}
+}
