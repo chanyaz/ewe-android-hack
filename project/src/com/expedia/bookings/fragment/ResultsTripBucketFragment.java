0267b6a1d95db5d38909a20bc298362fc8f7d21f
Joel Drotos <jdrotos@mobiata.com>
2014-03-05 10:10:09 -0600

    TabResRef - Create a results page specific trip bucket wrapper fragment

---
+++ project/src/com/expedia/bookings/fragment/ResultsTripBucketFragment.java
@@ -0,0 +1,93 @@
+package com.expedia.bookings.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.expedia.bookings.R;
+import com.expedia.bookings.data.Db;
+import com.expedia.bookings.data.TripBucket;
+import com.expedia.bookings.utils.FragmentAvailabilityUtils;
+import com.expedia.bookings.widget.FrameLayoutTouchController;
+import com.mobiata.android.util.Ui;
+
+/**
+ * ResultsTripBucketFragment designed for Tablet 2014
+ */
+public class ResultsTripBucketFragment extends Fragment
+	implements FragmentAvailabilityUtils.IFragmentAvailabilityProvider {
+
+	private static final String FTAG_BUCKET = "FTAG_BUCKET";
+
+	private TripBucketFragment mBucketFrag;
+
+	private FrameLayoutTouchController mTripBucketC;
+	private ViewGroup mEmptyTripC;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_tablet_results_trip_bucket, container, false);
+
+		mTripBucketC = Ui.findView(view, R.id.trip_bucket_container);
+		mEmptyTripC = Ui.findView(view, R.id.empty_bucket_container);
+
+		FragmentManager manager = getChildFragmentManager();
+		FragmentTransaction transaction = manager.beginTransaction();
+		mBucketFrag = FragmentAvailabilityUtils
+			.setFragmentAvailability(true, FTAG_BUCKET, manager, transaction, this, R.id.trip_bucket_container,
+				true);
+		transaction.commit();
+
+		return view;
+	}
+
+	public void bindToDb() {
+		bind(Db.getTripBucket());
+	}
+
+	public void bind(TripBucket bucket) {
+		boolean showBucket = bucket != null && bucket.size() > 0;
+		if (showBucket) {
+			mEmptyTripC.setVisibility(View.GONE);
+			mTripBucketC.setVisibility(View.VISIBLE);
+		}
+		else {
+			mEmptyTripC.setVisibility(View.VISIBLE);
+			mTripBucketC.setVisibility(View.INVISIBLE);
+		}
+
+		if (mBucketFrag != null && mBucketFrag.isAdded()) {
+			mBucketFrag.bind(bucket);
+		}
+	}
+
+
+	/**
+	 * IFragmentAvailabilityProvider
+	 */
+
+	@Override
+	public Fragment getExisitingLocalInstanceFromTag(String tag) {
+		if (tag == FTAG_BUCKET) {
+			return mBucketFrag;
+		}
+		return null;
+	}
+
+	@Override
+	public Fragment getNewFragmentInstanceFromTag(String tag) {
+		if (tag == FTAG_BUCKET) {
+			return new TripBucketFragment();
+		}
+		return null;
+	}
+
+	@Override
+	public void doFragmentSetup(String tag, Fragment frag) {
+
+	}
+}
