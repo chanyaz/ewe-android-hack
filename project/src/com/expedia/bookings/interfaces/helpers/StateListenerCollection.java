5aa901c5d4cbadf383df9f411dabe527507002c0
Joel Drotos <jdrotos@mobiata.com>
2014-03-06 14:47:47 -0600

    TabResRef - StateListenerCollection improvements.
    Use HashSet instead of arraylist for inactive listeners - should get a small perf improvement.
    
    More importantly, this solves the following issue: Say one of our fragments has child fragments and it chooses
    to remove one of them in onStateFinalized. This is totally acceptable behavior, except if the fragment we are
    removing happens to be an IStateListener, it tries to remove itself while we are iterating over the Listener list
    and then we end up throwing a concurrent modification exception. Now we just wait to change the lists until
    we are done iterating. Note also that we are keying on the listener itself, so if we try to perform two actions
    on the same listener, the most recent will take precedence.

--- project/src/com/expedia/bookings/interfaces/helpers/StateListenerCollection.java
+++ project/src/com/expedia/bookings/interfaces/helpers/StateListenerCollection.java
@@ -1,22 +1,37 @@
 package com.expedia.bookings.interfaces.helpers;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import com.expedia.bookings.interfaces.IStateListener;
 import com.mobiata.android.Log;
 
 /**
  * This class is designed to ease the implementation of IStateProvider.
- * 
+ * <p/>
  * It allows for easy registration/notification/deactivation of IStateListeners
  *
  * @param <T>
  */
 public class StateListenerCollection<T> {
+	//These are the persistant lists of our class
 	private ArrayList<IStateListener<T>> mStateChangeListeners = new ArrayList<IStateListener<T>>();
-	private ArrayList<IStateListener<T>> mInactiveStateChangeListeners = new ArrayList<IStateListener<T>>();
-	private T mLastFinalizedState;
+	private HashSet<IStateListener<T>> mInactiveStateChangeListeners = new HashSet<IStateListener<T>>();
 
+	//These are temporary stores that help us add and remove things while iterating.
+	private boolean mIsIterating = false;
+	private LinkedHashMap<IStateListener<T>, ListenerAction> mPendingActions = new LinkedHashMap<IStateListener<T>, ListenerAction>();
+
+	private enum ListenerAction {
+		ADD,
+		ADD_AND_FIRE,
+		REMOVE
+	}
+
+	private T mLastFinalizedState;
 	private T mTransStartState;
 	private T mTransEndState;
 
@@ -38,23 +53,27 @@ public class StateListenerCollection<T> {
 
 	public void startStateTransition(T stateOne, T stateTwo) {
 		if (mTransStartState != null || mTransEndState != null) {
-			Log.e("startStateTransition may not be called until after endStateTransition has been called on the previous transition.");
+			Log.e(
+				"startStateTransition may not be called until after endStateTransition has been called on the previous transition.");
 		}
 		else {
 			mTransStartState = stateOne;
 			mTransEndState = stateTwo;
+			preIterate();
 			for (IStateListener<T> listener : mStateChangeListeners) {
 				if (!mInactiveStateChangeListeners.contains(listener)) {
 					listener.onStateTransitionStart(stateOne, stateTwo);
 				}
 			}
+			postIterate();
 		}
 	}
 
 	public void updateStateTransition(T stateOne, T stateTwo, float percentage) {
 		if (mTransStartState == null || mTransEndState == null || stateOne != mTransStartState
-				|| stateTwo != mTransEndState) {
-			Log.e("updateStateTransition must be called after startStateTransition. The argument states of startStateTransition must match the argument states of updateStateTransition. startStateOne:"
+			|| stateTwo != mTransEndState) {
+			Log.e(
+				"updateStateTransition must be called after startStateTransition. The argument states of startStateTransition must match the argument states of updateStateTransition. startStateOne:"
 					+ mTransStartState
 					+ " startStateTwo:"
 					+ mTransEndState
@@ -64,18 +83,21 @@ public class StateListenerCollection<T> {
 					+ stateTwo);
 		}
 		else {
+			preIterate();
 			for (IStateListener<T> listener : mStateChangeListeners) {
 				if (!mInactiveStateChangeListeners.contains(listener)) {
 					listener.onStateTransitionUpdate(stateOne, stateTwo, percentage);
 				}
 			}
+			postIterate();
 		}
 	}
 
 	public void endStateTransition(T stateOne, T stateTwo) {
 		if (mTransStartState == null || mTransEndState == null || stateOne != mTransStartState
-				|| stateTwo != mTransEndState) {
-			Log.e("endStateTransition must be called after startStateTransition. The argument states of startStateTransition must match the argument states of updateStateTransition. startStateOne:"
+			|| stateTwo != mTransEndState) {
+			Log.e(
+				"endStateTransition must be called after startStateTransition. The argument states of startStateTransition must match the argument states of updateStateTransition. startStateOne:"
 					+ mTransStartState
 					+ " startStateTwo:"
 					+ mTransEndState
@@ -85,11 +107,13 @@ public class StateListenerCollection<T> {
 					+ stateTwo);
 		}
 		else {
+			preIterate();
 			for (IStateListener<T> listener : mStateChangeListeners) {
 				if (!mInactiveStateChangeListeners.contains(listener)) {
 					listener.onStateTransitionEnd(stateOne, stateTwo);
 				}
 			}
+			postIterate();
 			mTransStartState = null;
 			mTransEndState = null;
 		}
@@ -97,32 +121,77 @@ public class StateListenerCollection<T> {
 
 	public void finalizeState(T state) {
 		if (mTransStartState != null || mTransEndState != null) {
-			Log.e("finalizeState may not be called until after endStateTransition has been called on the transition. Transition that needs to end startStateOne:"
+			Log.e(
+				"finalizeState may not be called until after endStateTransition has been called on the transition. Transition that needs to end startStateOne:"
 					+ mTransStartState
 					+ " startStateTwo:"
 					+ mTransEndState);
 		}
 		else {
 			mLastFinalizedState = state;
+			preIterate();
 			for (IStateListener<T> listener : mStateChangeListeners) {
 				if (!mInactiveStateChangeListeners.contains(listener)) {
 					listener.onStateFinalized(state);
 				}
 			}
+			postIterate();
 		}
 	}
 
 	public void registerStateListener(IStateListener<T> listener, boolean fireFinalizeState) {
-		if (!mStateChangeListeners.contains(listener)) {
-			mStateChangeListeners.add(listener);
+		if (isIterating()) {
+			//If we are iterating, lets wait until we are done before we add our new listener
+			mPendingActions.put(listener, (fireFinalizeState ? ListenerAction.ADD_AND_FIRE : ListenerAction.ADD));
 		}
-		if (fireFinalizeState) {
-			listener.onStateFinalized(mLastFinalizedState);
+		else {
+			if (!mStateChangeListeners.contains(listener)) {
+				mStateChangeListeners.add(listener);
+			}
+			if (mInactiveStateChangeListeners.contains(listener)) {
+				mInactiveStateChangeListeners.remove(listener);
+			}
+			if (fireFinalizeState) {
+				listener.onStateFinalized(mLastFinalizedState);
+			}
 		}
 	}
 
 	public void unRegisterStateListener(IStateListener<T> listener) {
-		mStateChangeListeners.remove(listener);
-		mInactiveStateChangeListeners.remove(listener);
+		if (isIterating()) {
+			//If we are iterating, we set the listener inactive (so it stops receiving events)
+			mInactiveStateChangeListeners.add(listener);
+			//And we slate it for removal
+			mPendingActions.put(listener, ListenerAction.REMOVE);
+		}
+		else {
+			mStateChangeListeners.remove(listener);
+			mInactiveStateChangeListeners.remove(listener);
+		}
+	}
+
+	private boolean isIterating() {
+		return mIsIterating;
+	}
+
+	private void preIterate() {
+		mIsIterating = true;
+	}
+
+	private void postIterate() {
+		mIsIterating = false;
+
+		Iterator<Map.Entry<IStateListener<T>, ListenerAction>> iter = mPendingActions.entrySet().iterator();
+		while (!mIsIterating && iter.hasNext()) {
+			Map.Entry<IStateListener<T>, ListenerAction> entry = iter.next();
+			if (entry.getValue().compareTo(ListenerAction.REMOVE) == 0) {
+				unRegisterStateListener(entry.getKey());
+			}
+			else {
+				boolean fireFinalize = entry.getValue().compareTo(ListenerAction.ADD_AND_FIRE) == 0;
+				registerStateListener(entry.getKey(), fireFinalize);
+			}
+			iter.remove();
+		}
 	}
 }
