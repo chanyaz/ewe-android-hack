df6b91963998c81c57c96989ecf0c313e9ce6dcb
Brennan Taylor <brennan@mobiata.com>
2014-03-10 17:22:52 -0700

    Fix formatting of file

--- project/src/com/expedia/bookings/fragment/TravelocityLauncherFragment.java
+++ project/src/com/expedia/bookings/fragment/TravelocityLauncherFragment.java
@@ -14,22 +14,19 @@ import com.expedia.bookings.utils.AnimUtils;
 import com.expedia.bookings.utils.NavUtils;
 import com.mobiata.android.util.Ui;
 
-/**
- * Created by sugupta on 1/21/14.
- */
 public class TravelocityLauncherFragment extends Fragment {
 
-    public static final String TAG = TravelocityLauncherFragment.class.getName();
-    private boolean mLaunchingActivity = false;
-    private ImageView mSlidingImage;
-    private int mCurrentImageIndex = 0;
-    private static final int IMAGE_ROTATION_PERIOD = 8000;
-    private Handler mHandler;
-    private Runnable mUpdateResults;
+	public static final String TAG = TravelocityLauncherFragment.class.getName();
+	private boolean mLaunchingActivity = false;
+	private ImageView mSlidingImage;
+	private int mCurrentImageIndex = 0;
+	private static final int IMAGE_ROTATION_PERIOD = 8000;
+	private Handler mHandler;
+	private Runnable mUpdateResults;
 
-    public static TravelocityLauncherFragment newInstance() {
-        return new TravelocityLauncherFragment();
-    }
+	public static TravelocityLauncherFragment newInstance() {
+		return new TravelocityLauncherFragment();
+	}
 
 	private static final Integer[] BACKGROUND_RES_IDS = new Integer[] {
 		R.drawable.bg_launch_tvly_sf,
@@ -43,61 +40,63 @@ public class TravelocityLauncherFragment extends Fragment {
 		R.drawable.bg_launch_tvly_maldives
 	};
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-       final View view = inflater.inflate(R.layout.fragment_tvly_launch, container, false);
-       Ui.findView(view, R.id.tvly_flights).setOnClickListener(mHeaderItemOnClickListener);
-       Ui.findView(view, R.id.tvly_hotels).setOnClickListener(mHeaderItemOnClickListener);
-       mSlidingImage = Ui.findView(view, R.id.tvly_home_bg_view);
-       mHandler = new Handler();
-       mUpdateResults = new Runnable() {
-            public void run() {
-                animateAndSlideShow();
-                mHandler.postDelayed(this, IMAGE_ROTATION_PERIOD);
-            }
-        };
-        return view;
-    }
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		final View view = inflater.inflate(R.layout.fragment_tvly_launch, container, false);
+		Ui.findView(view, R.id.tvly_flights).setOnClickListener(mHeaderItemOnClickListener);
+		Ui.findView(view, R.id.tvly_hotels).setOnClickListener(mHeaderItemOnClickListener);
+		mSlidingImage = Ui.findView(view, R.id.tvly_home_bg_view);
+		mHandler = new Handler();
+		mUpdateResults = new Runnable() {
+			public void run() {
+				animateAndSlideShow();
+				mHandler.postDelayed(this, IMAGE_ROTATION_PERIOD);
+			}
+		};
+		return view;
+	}
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        mLaunchingActivity = false;
-        mHandler.postDelayed(mUpdateResults, IMAGE_ROTATION_PERIOD);
-    }
+	@Override
+	public void onResume() {
+		super.onResume();
+		mLaunchingActivity = false;
+		mHandler.postDelayed(mUpdateResults, IMAGE_ROTATION_PERIOD);
+	}
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        mHandler.removeCallbacks(mUpdateResults);
-    }
+	@Override
+	public void onPause() {
+		super.onPause();
+		mHandler.removeCallbacks(mUpdateResults);
+	}
 
-    private void animateAndSlideShow() {
-        mCurrentImageIndex = (mCurrentImageIndex + 1) % BACKGROUND_RES_IDS.length;
-        mSlidingImage.setImageResource(BACKGROUND_RES_IDS[mCurrentImageIndex]);
-    }
+	private void animateAndSlideShow() {
+		mCurrentImageIndex = (mCurrentImageIndex + 1) % BACKGROUND_RES_IDS.length;
+		mSlidingImage.setImageResource(BACKGROUND_RES_IDS[mCurrentImageIndex]);
+	}
 
-    private final View.OnClickListener mHeaderItemOnClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Bundle animOptions = AnimUtils.createActivityScaleBundle(v);
+	private final View.OnClickListener mHeaderItemOnClickListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Bundle animOptions = AnimUtils.createActivityScaleBundle(v);
 
-            switch (v.getId()) {
-                case R.id.tvly_hotels:
-                    if (!mLaunchingActivity) {
-                        mLaunchingActivity = true;
-                        NavUtils.goToHotels(getActivity(), animOptions);
-                        OmnitureTracking.trackLinkLaunchScreenToHotels(getActivity());
-                    }
-                    break;
-                case R.id.tvly_flights:
-                    if (!mLaunchingActivity) {
-                        mLaunchingActivity = true;
-                        NavUtils.goToFlights(getActivity(), animOptions);
-                        OmnitureTracking.trackLinkLaunchScreenToFlights(getActivity());
-                    }
-                    break;
-            }
-        }
-    };
+			switch (v.getId()) {
+			case R.id.tvly_hotels: {
+				if (!mLaunchingActivity) {
+					mLaunchingActivity = true;
+					NavUtils.goToHotels(getActivity(), animOptions);
+					OmnitureTracking.trackLinkLaunchScreenToHotels(getActivity());
+				}
+				break;
+			}
+			case R.id.tvly_flights: {
+				if (!mLaunchingActivity) {
+					mLaunchingActivity = true;
+					NavUtils.goToFlights(getActivity(), animOptions);
+					OmnitureTracking.trackLinkLaunchScreenToFlights(getActivity());
+				}
+				break;
+			}
+			}
+		}
+	};
 }
