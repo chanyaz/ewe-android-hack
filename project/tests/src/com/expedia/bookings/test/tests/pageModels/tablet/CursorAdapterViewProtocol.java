c792ffb224645dfafd816e6691aef12ed82029a8
Brennan Taylor <brennan@mobiata.com>
2014-03-13 14:30:15 -0700

    Change CursorAdapterViewProtocol to wrap standard protocol
    Where it can. Saves some code duplication.

--- project/tests/src/com/expedia/bookings/test/tests/pageModels/tablet/CursorAdapterViewProtocol.java
+++ project/tests/src/com/expedia/bookings/test/tests/pageModels/tablet/CursorAdapterViewProtocol.java
@@ -3,17 +3,14 @@ package com.expedia.bookings.test.tests.pageModels.tablet;
 import java.util.List;
 
 import android.database.Cursor;
-import android.os.Build;
 import android.support.v4.widget.CursorAdapter;
 import android.view.View;
-import android.widget.AbsListView;
 import android.widget.Adapter;
 import android.widget.AdapterView;
-import android.widget.AdapterViewAnimator;
-import android.widget.AdapterViewFlipper;
 
 import com.google.android.apps.common.testing.ui.espresso.action.AdapterViewProtocol;
 import com.google.android.apps.common.testing.ui.espresso.action.AdapterViewProtocol.AdaptedData;
+import com.google.android.apps.common.testing.ui.espresso.action.AdapterViewProtocols;
 import com.google.common.base.Optional;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Range;
@@ -22,8 +19,6 @@ import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMat
 import static com.google.common.base.Preconditions.checkArgument;
 
 public class CursorAdapterViewProtocol implements AdapterViewProtocol {
-	private static final int FULLY_RENDERED_PERCENTAGE_CUTOFF = 90;
-
 	public Object getDataFromCursor(CursorAdapter cursorAdapter, Cursor cursor) {
 		return cursorAdapter.convertToString(cursor);
 	}
@@ -61,62 +56,12 @@ public class CursorAdapterViewProtocol implements AdapterViewProtocol {
 
 	@Override
 	public void makeDataRenderedWithinAdapterView(AdapterView<? extends Adapter> adapterView, AdaptedData data) {
-		checkArgument(data.opaqueToken instanceof Integer, "Not my data: %s", data);
-		int position = ((Integer) data.opaqueToken).intValue();
-
-		boolean moved = false;
-		// set selection should always work, we can give a little better experience if per subtype
-		// though.
-		if (Build.VERSION.SDK_INT > 7) {
-			if (adapterView instanceof AbsListView) {
-				if (Build.VERSION.SDK_INT > 10) {
-					((AbsListView) adapterView).smoothScrollToPositionFromTop(position,
-						adapterView.getPaddingTop(), 0);
-				} else {
-					((AbsListView) adapterView).smoothScrollToPosition(position);
-				}
-				moved = true;
-			}
-			if (Build.VERSION.SDK_INT > 10) {
-				if (adapterView instanceof AdapterViewAnimator) {
-					if (adapterView instanceof AdapterViewFlipper) {
-						((AdapterViewFlipper) adapterView).stopFlipping();
-					}
-					((AdapterViewAnimator) adapterView).setDisplayedChild(position);
-					moved = true;
-				}
-			}
-		}
-		if (!moved) {
-			adapterView.setSelection(position);
-		}
+		AdapterViewProtocols.standardProtocol().makeDataRenderedWithinAdapterView(adapterView, data);
 	}
 
 	@SuppressWarnings("deprecation")
 	@Override
 	public boolean isDataRenderedWithinAdapterView(AdapterView<? extends Adapter> adapterView, AdaptedData adaptedData) {
-		checkArgument(adaptedData.opaqueToken instanceof Integer, "Not my data: %s", adaptedData);
-		int dataPosition = ((Integer) adaptedData.opaqueToken).intValue();
-
-		if (Range.closed(adapterView.getFirstVisiblePosition(), adapterView.getLastVisiblePosition()).contains(dataPosition)) {
-			if (adapterView.getFirstVisiblePosition() == adapterView.getLastVisiblePosition()) {
-				// thats a huge element.
-				return true;
-			} else {
-				return isElementFullyRendered(adapterView, dataPosition - adapterView.getFirstVisiblePosition());
-			}
-		} else {
-			return false;
-		}
-	}
-
-	private boolean isElementFullyRendered(AdapterView<? extends Adapter> adapterView, int childAt) {
-		View element = adapterView.getChildAt(childAt);
-		// Occassionally we'll have to fight with smooth scrolling logic on our definition of when
-		// there is extra scrolling to be done. In particular if the element is the first or last
-		// element of the list, the smooth scroller may decide that no work needs to be done to scroll
-		// to the element if a certain percentage of it is on screen. Ugh. Sigh. Yuck.
-
-		return isDisplayingAtLeast(FULLY_RENDERED_PERCENTAGE_CUTOFF).matches(element);
+		return AdapterViewProtocols.standardProtocol().isDataRenderedWithinAdapterView(adapterView, adaptedData);
 	}
 }
