a8920a07970bed41172a9e234688413ab7e4d500
Joel Drotos <jdrotos@mobiata.com>
2014-03-19 10:16:28 -0500

    Update StateListenerLogger to include state type in log statements.

--- project/src/com/expedia/bookings/interfaces/helpers/StateListenerLogger.java
+++ project/src/com/expedia/bookings/interfaces/helpers/StateListenerLogger.java
@@ -13,24 +13,31 @@ public class StateListenerLogger<T> implements IStateListener<T> {
 
 	@Override
 	public void onStateTransitionStart(T stateOne, T stateTwo) {
-		Log.d("IStateChangeLogger - onPrepareStateTransition - stateOne:" + stateOne + " stateTwo:" + stateTwo);
+		Log.d("IStateChangeLogger - " + getStateTypeStr(stateOne) + " - onPrepareStateTransition - stateOne:" + stateOne
+			+ " stateTwo:" + stateTwo);
 	}
 
 	@Override
 	public void onStateTransitionUpdate(T stateOne, T stateTwo, float percentage) {
-		Log.d("IStateChangeLogger - onStateTransitionPercentageChange - stateOne:" + stateOne + " stateTwo:" + stateTwo
-				+ " percentage:" + percentage);
+		Log.d("IStateChangeLogger - " + getStateTypeStr(stateOne) + " - onStateTransitionPercentageChange - stateOne:"
+			+ stateOne + " stateTwo:" + stateTwo
+			+ " percentage:" + percentage);
 
 	}
 
 	@Override
 	public void onStateTransitionEnd(T stateOne, T stateTwo) {
-		Log.d("IStateChangeLogger - onFinishStateTransition - stateOne:" + stateOne + " stateTwo:" + stateTwo);
+		Log.d("IStateChangeLogger - " + getStateTypeStr(stateOne) + " - onFinishStateTransition - stateOne:" + stateOne
+			+ " stateTwo:" + stateTwo);
 	}
 
 	@Override
 	public void onStateFinalized(T state) {
-		Log.d("IStateChangeLogger - onStateFinalized - state:" + state);
+		Log.d("IStateChangeLogger - " + getStateTypeStr(state) + " - onStateFinalized - state:" + state);
+	}
+
+	private String getStateTypeStr(T state) {
+		return state.getClass().getSimpleName();
 	}
 
 }
