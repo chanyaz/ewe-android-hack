7b2ee9a0735b49a05dd8989e402ef3d7fb491cdf
Supreeth <supreeth@mobiata.com>
2014-03-19 12:28:46 -0700

    2603. Tablet Confirmation Page - Hotel
    This is the remainder of the commits for this story.
    This commit mainly comprises of the minor UI tweat for the button bar, at the bottom.

--- project/src/com/expedia/bookings/fragment/TabletConfirmationFragment.java
+++ project/src/com/expedia/bookings/fragment/TabletConfirmationFragment.java
@@ -6,24 +6,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.expedia.bookings.R;
-import com.expedia.bookings.activity.ExpediaBookingApp;
-import com.expedia.bookings.activity.WebViewActivity;
 import com.expedia.bookings.data.Db;
-import com.expedia.bookings.data.LineOfBusiness;
 import com.expedia.bookings.data.pos.PointOfSale;
-import com.expedia.bookings.enums.CheckoutState;
 import com.expedia.bookings.otto.Events;
 import com.expedia.bookings.utils.NavUtils;
 import com.mobiata.android.SocialUtils;
-import com.mobiata.android.util.AndroidUtils;
 import com.mobiata.android.util.CalendarAPIUtils;
 import com.mobiata.android.util.Ui;
-import com.squareup.otto.Subscribe;
 
 public abstract class TabletConfirmationFragment extends Fragment {
 
@@ -32,11 +23,11 @@ public abstract class TabletConfirmationFragment extends Fragment {
 	protected abstract int getActionsLayoutId();
 
 	protected abstract String getItinNumber();
-	
+
 	protected abstract String getConfirmationSummaryText();
-	
+
 	protected abstract void shareItinerary();
-	
+
 	protected abstract void addItineraryToCalendar();
 
 	private ViewGroup mBookNextContainer;
@@ -45,7 +36,7 @@ public abstract class TabletConfirmationFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = inflater.inflate(getLayoutId(), container, false);
-		
+
 		Ui.setText(v, R.id.confirmation_summary_text, getConfirmationSummaryText());
 
 		Ui.setText(v, R.id.confirmation_itinerary_text_view, getString(R.string.tablet_itinerary_confirmation_TEMPLATE, getItinNumber(), Db.getBillingInfo().getEmail()));
@@ -53,13 +44,6 @@ public abstract class TabletConfirmationFragment extends Fragment {
 		// Inflate the custom actions layout id
 		ViewGroup actionContainer = Ui.findView(v, R.id.custom_actions_container);
 		inflater.inflate(getActionsLayoutId(), actionContainer, true);
-
-		Ui.setOnClickListener(v, R.id.call_action_text_view, new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				SocialUtils.call(getActivity(), PointOfSale.getPointOfSale().getSupportPhoneNumber());
-			}
-		});
 
 		mBookNextContainer = Ui.findView(v, R.id.confirmation_book_next_container);
 		mBookNextContainer.setOnClickListener(new OnClickListener() {
@@ -72,6 +56,7 @@ public abstract class TabletConfirmationFragment extends Fragment {
 
 		if (Db.getTripBucket().getFlight() == null) {
 			mBookNextContainer.setVisibility(View.GONE);
+			Ui.findView(v, R.id.confirmation_booking_bar_separator).setVisibility(View.GONE);
 		}
 
 		mDoneBookingContainer = Ui.findView(v, R.id.confirmation_done_booking_container);
@@ -81,6 +66,13 @@ public abstract class TabletConfirmationFragment extends Fragment {
 			public void onClick(View v) {
 				NavUtils.goToItin(getActivity());
 				getActivity().finish();
+			}
+		});
+
+		Ui.setOnClickListener(v, R.id.call_action_text_view, new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				SocialUtils.call(getActivity(), PointOfSale.getPointOfSale().getSupportPhoneNumber());
 			}
 		});
 
