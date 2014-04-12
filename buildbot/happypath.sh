173988826864dbaa98ad806f12ee8ef1d03b7d6d
Brennan Taylor <brennan@mobiata.com>
2014-04-11 20:31:42 -0700

    Change happypath.sh class that it runs
    This isn't the final class that'll be in change of running the happy
    paths but it is the most useful right now.

--- buildbot/happypath.sh
+++ buildbot/happypath.sh
@@ -16,7 +16,7 @@ java \
     -jar lib/TestUtils/jars/spoon-runner-1.1.1-jar-with-dependencies.jar \
     --apk  "$APK" \
     --test-apk "$TEST_APK" \
-    --class-name com.expedia.bookings.test.tests.full.FullHappyPathTest \
+    --class-name com.expedia.bookings.test.tests.tablet.TabletHappyPath \
     --no-animations \
     --fail-on-failure \
     --output "$OUTPUT_DIR"
