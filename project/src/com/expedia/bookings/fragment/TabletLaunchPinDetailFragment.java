2b5b39f354b734b32cd65b9869cee84537faf5ab
Brad McManus <brad@mobiata.com>
2014-06-12 16:29:05 -0700

    Use real images in PinDetailFragment

--- project/src/com/expedia/bookings/fragment/TabletLaunchPinDetailFragment.java
+++ project/src/com/expedia/bookings/fragment/TabletLaunchPinDetailFragment.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.expedia.bookings.R;
+import com.expedia.bookings.bitmaps.L2ImageCache;
 import com.expedia.bookings.data.LaunchLocation;
 import com.expedia.bookings.enums.LaunchState;
 import com.expedia.bookings.fragment.base.Fragment;
@@ -54,7 +55,7 @@ public class TabletLaunchPinDetailFragment extends Fragment {
 		mScaleOrigin = (float) origin.width() / (float) getResources().getDimensionPixelSize(R.dimen.launch_pin_detail_size);
 
 		ImageView roundImage = Ui.findView(mRootC, R.id.round_image);
-		roundImage.setImageDrawable(new RoundBitmapDrawable(getActivity(), R.drawable.mappin_madrid));
+		L2ImageCache.sGeneralPurpose.loadImage(metadata.getImageUrl(), roundImage);
 
 		TextView textTitle = Ui.findView(mRootC, R.id.text_title);
 		textTitle.setText(metadata.title);
