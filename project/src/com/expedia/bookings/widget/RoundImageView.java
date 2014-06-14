c3cd53fcfaad06c6ff76c780e13389d2b45d35b5
Brad McManus <brad@mobiata.com>
2014-06-13 17:25:34 -0700

    Add RoundImageView
    Rounding code gratuitously stolen from RoundBitmapDrawable...

---
+++ project/src/com/expedia/bookings/widget/RoundImageView.java
@@ -0,0 +1,77 @@
+package com.expedia.bookings.widget;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class RoundImageView extends ImageView {
+
+	public RoundImageView(Context context) {
+		super(context);
+	}
+
+	public RoundImageView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public RoundImageView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	private Bitmap mBitmap;
+	private Matrix mMatrix;
+	private Paint mPaint;
+
+	@Override
+	public void setImageBitmap(Bitmap bm) {
+		updateDrawingVars(bm);
+		super.setImageBitmap(bm);
+	}
+
+	private void updateDrawingVars(Bitmap bm) {
+		mBitmap = bm;
+		if (mBitmap != null) {
+			BitmapShader shader;
+			shader = new BitmapShader(mBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+
+			mPaint = new Paint();
+			mPaint.setAntiAlias(true);
+			mPaint.setShader(shader);
+
+			mMatrix = new Matrix();
+
+			RectF src = new RectF();
+			src.left = 0;
+			src.top = 0;
+			src.right = mBitmap.getWidth();
+			src.bottom = mBitmap.getHeight();
+
+			RectF dst = new RectF(0, 0, getWidth(), getHeight());
+			mMatrix.setRectToRect(src, dst, Matrix.ScaleToFit.CENTER);
+		}
+	}
+
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		updateDrawingVars(mBitmap);
+		super.onSizeChanged(w, h, oldw, oldh);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		if (mBitmap != null) {
+			canvas.save();
+			canvas.concat(mMatrix);
+			canvas.drawCircle(mBitmap.getWidth() / 2, mBitmap.getHeight() / 2, mBitmap.getWidth() / 2, mPaint);
+			canvas.restore();
+		}
+	}
+
+}
