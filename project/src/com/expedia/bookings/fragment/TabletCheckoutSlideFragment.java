81f2b74bcb8c3033ce3931edba90c72512fcdf22
Doug Melton <doug@mobiata.com>
2014-07-10 16:53:20 -0700

    Fixed slide-to-purchase background color

--- project/src/com/expedia/bookings/fragment/TabletCheckoutSlideFragment.java
+++ project/src/com/expedia/bookings/fragment/TabletCheckoutSlideFragment.java
@@ -84,7 +84,7 @@ public class TabletCheckoutSlideFragment extends LobableFragment implements IChe
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		mRootC = Ui.inflate(inflater, R.layout.fragment_tablet_slide_to_purchase, container, false);
 
-		mRootC.setBackgroundColor(Db.getFullscreenAverageColor());
+		Ui.findView(mRootC, R.id.slide_content_container).setBackgroundColor(Db.getFullscreenAverageColor());
 
 		if (savedInstanceState != null && savedInstanceState.containsKey(HAS_ACCEPTED_TOS)) {
 			mHasAcceptedTOS = savedInstanceState.getBoolean(HAS_ACCEPTED_TOS);
