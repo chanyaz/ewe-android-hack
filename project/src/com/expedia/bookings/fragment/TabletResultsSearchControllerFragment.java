285c8aaea9c8895be34b91fdbc834c7dd9db1165
Kevin Carpenter <kevc@mobiata.com>
2014-07-29 12:28:41 -0700

    Stop trying to set the ResultsSearchState conditionally

--- project/src/com/expedia/bookings/fragment/TabletResultsSearchControllerFragment.java
+++ project/src/com/expedia/bookings/fragment/TabletResultsSearchControllerFragment.java
@@ -150,11 +150,6 @@ public class TabletResultsSearchControllerFragment extends Fragment implements I
 				mLocalParams = savedInstanceState.getParcelable(INSTANCE_LOCAL_PARAMS);
 			}
 		}
-
-		Db.loadTripBucket(getActivity());
-		if (!Db.getTripBucket().isEmpty()) {
-			mSearchStateManager.setState(ResultsSearchState.DEFAULT, false);
-		}
 	}
 
 	@Override
