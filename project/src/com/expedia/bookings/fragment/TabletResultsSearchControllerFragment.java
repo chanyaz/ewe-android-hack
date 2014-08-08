chanyaz/ewe-android-hack

commit f37a1d9e8d356e36ec452d72e8e497d6443e2e3e

3233: Re-arranged search controls layouts

project/src/com/expedia/bookings/fragment/TabletResultsSearchControllerFragment.java
@@ -1,8 +1,6 @@
 package com.expedia.bookings.fragment;
 
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.joda.time.LocalDate;
 import org.joda.time.YearMonth;
@@ -15,7 +13,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.text.Html;
 import android.text.TextUtils;
 import android.text.format.DateUtils;
 import android.view.LayoutInflater;
@@ -95,7 +92,6 @@ public class TabletResultsSearchControllerFragment extends Fragment implements I
 	//Containers
 	private ViewGroup mRootC;
 	private FrameLayoutTouchController mSearchBarC;
-	private ViewGroup mRightButtonsC;
 	private FrameLayoutTouchController mBottomRightC;
 	private FrameLayoutTouchController mBottomCenterC;
 
@@ -163,7 +159,6 @@ public class TabletResultsSearchControllerFragment extends Fragment implements I
 
 		mRootC = Ui.findView(view, R.id.root_layout);
 		mSearchBarC = Ui.findView(view, R.id.search_bar_container);
-		mRightButtonsC = Ui.findView(view, R.id.right_buttons_container);
 		mBottomRightC = Ui.findView(view, R.id.bottom_right_container);
 		mBottomCenterC = Ui.findView(view, R.id.bottom_center_container);
 		mWaypointC = Ui.findView(view, R.id.waypoint_container);
@@ -828,11 +823,17 @@ public class TabletResultsSearchControllerFragment extends Fragment implements I
 				// This is only to ensure the search controls shift left of the overflow menu. This
 				// overflow menu is only present for HockeyApp builds.
 				if (!AndroidUtils.isRelease(getActivity())) {
-					mRightButtonsC.setTranslationX(percentage * -searchBarHeight);
+					float transX = percentage * -searchBarHeight;
+					mOrigBtn.setTranslationX(transX);
+					mCalBtn.setTranslationX(transX);
+					mTravBtn.setTranslationX(transX);
 				}
 			}
 			else {
-				mRightButtonsC.setTranslationY(percentage * searchBarHeight);
+				float transX = percentage * searchBarHeight;
+				mOrigBtn.setTranslationX(transX);
+				mCalBtn.setTranslationX(transX);
+				mTravBtn.setTranslationX(transX);
 			}
 		}
 
@@ -843,6 +844,9 @@ public class TabletResultsSearchControllerFragment extends Fragment implements I
 
 		private void setSlideUpHotelsOnlyAnimationPercentage(float percentage) {
 			mOrigBtn.setAlpha(1f - percentage);
+			if (mLocalParams.getStartDate() == null) {
+				mCalBtn.setAlpha(1f - percentage);
+			}
 		}
 
 		private void setActionbarShowingState(ResultsSearchState state) {
@@ -914,11 +918,6 @@ public class TabletResultsSearchControllerFragment extends Fragment implements I
 					mTravPickWhiteSpace.setVisibility(View.VISIBLE);
 				}
 			}
-
-			mCalBtn.setVisibility(
-				(state == ResultsSearchState.HOTELS_UP && mLocalParams.getStartDate() == null) ? View.GONE
-					: View.VISIBLE
-			);
 		}
 
 		private void resetWidgetTranslations() {

project/res/layout-sw600dp-port/snippet_search_controls.xml
@@ -1,0 +1,63 @@
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout
+    xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:orientation="vertical">
+
+    <com.mobiata.android.widget.AutoResizeTextView
+        android:id="@+id/dest_btn"
+        android:layout_width="wrap_content"
+        android:layout_height="0dp"
+        android:layout_weight="1"
+        android:textSize="@dimen/search_params_destination_text_size"
+        android:textColor="@color/tablet_search_params_text"
+        android:gravity="center_vertical"
+        android:paddingLeft="16dp"
+        android:paddingRight="16dp"
+        android:drawablePadding="8dp"
+        android:layout_gravity="center"
+        android:singleLine="true"
+        android:drawableLeft="@drawable/ic_action_bar_magnifying_glass"
+        android:background="?android:attr/selectableItemBackground"
+        app:minTextSize="14sp"
+        app:textStyle="light"/>
+
+    <LinearLayout
+        android:id="@+id/right_buttons_container"
+        android:layout_width="wrap_content"
+        android:layout_height="0dp"
+        android:layout_weight="1"
+        android:layout_gravity="center"
+        android:orientation="horizontal"
+        android:gravity="center_vertical">
+
+        <com.expedia.bookings.widget.TextView
+            android:id="@+id/origin_btn"
+            style="@style/V2.Widget.TextView.SearchParams"
+            android:layout_width="0dp"
+            android:layout_height="match_parent"
+            android:layout_weight="1"
+            android:drawableLeft="@drawable/ic_param_origin"
+            android:ellipsize="end"
+            android:singleLine="true" />
+
+        <com.expedia.bookings.widget.TextView
+            android:id="@+id/calendar_btn"
+            style="@style/V2.Widget.TextView.SearchParams"
+            android:layout_width="wrap_content"
+            android:layout_height="match_parent"
+            android:drawableLeft="@drawable/ic_param_date" />
+
+
+        <com.expedia.bookings.widget.TextView
+            android:id="@+id/traveler_btn"
+            style="@style/V2.Widget.TextView.SearchParams"
+            android:layout_width="wrap_content"
+            android:layout_height="match_parent"
+            android:drawableLeft="@drawable/ic_param_travelers" />
+
+
+    </LinearLayout>
+</LinearLayout>

project/res/layout-sw600dp/snippet_search_controls.xml
@@ -1,0 +1,68 @@
+<?xml version="1.0" encoding="utf-8"?>
+<RelativeLayout
+    xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:clipChildren="false">
+
+    <com.expedia.bookings.widget.TextView
+        android:id="@+id/traveler_btn"
+        style="@style/V2.Widget.TextView.SearchParams"
+        android:layout_width="wrap_content"
+        android:layout_height="match_parent"
+        android:layout_alignParentRight="true"
+        android:drawableLeft="@drawable/ic_param_travelers" />
+
+    <com.expedia.bookings.widget.TextView
+        android:id="@+id/calendar_btn"
+        style="@style/V2.Widget.TextView.SearchParams"
+        android:layout_width="wrap_content"
+        android:layout_height="match_parent"
+        android:layout_toLeftOf="@id/traveler_btn"
+        android:drawableLeft="@drawable/ic_param_date" />
+
+    <FrameLayout
+        android:id="@+id/dest_btn_frame"
+        android:layout_width="wrap_content"
+        android:layout_height="match_parent"
+        android:layout_alignParentLeft="true"
+        android:layout_marginLeft="16dp">
+
+        <com.mobiata.android.widget.AutoResizeTextView
+            android:id="@+id/dest_btn"
+            android:layout_width="wrap_content"
+            android:layout_height="match_parent"
+            android:layout_gravity="center_vertical|left"
+            android:background="?android:attr/selectableItemBackground"
+            android:drawableLeft="@drawable/ic_action_bar_magnifying_glass"
+            android:drawablePadding="8dp"
+            android:gravity="center_vertical"
+            android:maxWidth="400dp"
+            android:paddingLeft="16dp"
+            android:paddingRight="16dp"
+            android:singleLine="true"
+            android:textSize="@dimen/search_params_destination_text_size"
+            android:textColor="@color/tablet_search_params_text"
+            app:minTextSize="@dimen/search_params_right_container_text_size"
+            app:textStyle="light" />
+    </FrameLayout>
+
+    <FrameLayout
+        android:layout_width="wrap_content"
+        android:layout_height="match_parent"
+        android:layout_toLeftOf="@id/calendar_btn"
+        android:layout_toRightOf="@id/dest_btn_frame">
+
+        <com.expedia.bookings.widget.TextView
+            android:id="@+id/origin_btn"
+            style="@style/V2.Widget.TextView.SearchParams"
+            android:layout_width="wrap_content"
+            android:layout_height="match_parent"
+            android:layout_gravity="center_vertical|right"
+            android:drawableLeft="@drawable/ic_param_origin"
+            android:ellipsize="end"/>
+
+    </FrameLayout>
+
+</RelativeLayout>

project/res/layout/fragment_tablet_results_search.xml
@@ -1,7 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <FrameLayout xmlns:android="http://schemas.android.com/apk/res/android"
     xmlns:app="http://schemas.android.com/apk/res-auto"
-    xmlns:tools="http://schemas.android.com/tools"
     android:id="@+id/root_layout"
     android:layout_width="match_parent"
     android:layout_height="match_parent">
@@ -16,49 +15,10 @@
     <com.expedia.bookings.widget.FrameLayoutTouchController
         android:id="@+id/search_bar_container"
         android:layout_width="match_parent"
-        android:layout_height="?android:attr/actionBarSize"
-        tools:ignore="NewApi">
+        android:layout_height="?android:attr/actionBarSize">
 
-        <LinearLayout style="@style/V2.Widget.LinearLayout.TabletSearchControls">
+        <include layout="@layout/snippet_search_controls" />
 
-            <com.mobiata.android.widget.AutoResizeTextView
-                android:id="@+id/dest_btn"
-                style="@style/V2.Widget.TextView.TabletSearchDestination" />
-
-
-            <LinearLayout
-                android:id="@+id/right_buttons_container"
-                style="@style/V2.Widget.LinearLayout.TabletSearchRightButtonsContainer">
-
-                <com.expedia.bookings.widget.TextView
-                    android:id="@+id/origin_btn"
-                    style="@style/V2.Widget.TextView.SearchParams"
-                    android:layout_width="0dp"
-                    android:layout_height="match_parent"
-                    android:layout_weight="1"
-                    android:drawableLeft="@drawable/ic_param_origin"
-                    android:ellipsize="end"
-                    android:singleLine="true" />
-
-
-                <com.expedia.bookings.widget.TextView
-                    android:id="@+id/calendar_btn"
-                    style="@style/V2.Widget.TextView.SearchParams"
-                    android:layout_width="wrap_content"
-                    android:layout_height="match_parent"
-                    android:drawableLeft="@drawable/ic_param_date" />
-
-
-                <com.expedia.bookings.widget.TextView
-                    android:id="@+id/traveler_btn"
-                    style="@style/V2.Widget.TextView.SearchParams"
-                    android:layout_width="wrap_content"
-                    android:layout_height="match_parent"
-                    android:drawableLeft="@drawable/ic_param_travelers" />
-
-
-            </LinearLayout>
-        </LinearLayout>
     </com.expedia.bookings.widget.FrameLayoutTouchController>
 
     <FrameLayout

project/res/values-port/tablet_themes.xml
@@ -1,37 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<resources>
-
-    <style name="V2.Widget.LinearLayout.TabletSearchControls">
-        <item name="android:layout_width">match_parent</item>
-        <item name="android:layout_height">match_parent</item>
-        <item name="android:orientation">vertical</item>
-    </style>
-
-    <style name="V2.Widget.TextView.TabletSearchDestination">
-        <item name="android:layout_width">wrap_content</item>
-        <item name="android:layout_height">0dp</item>
-        <item name="android:layout_weight">1</item>
-        <item name="android:textSize">@dimen/search_params_destination_text_size</item>
-        <item name="minTextSize">14dp</item>
-        <item name="android:textColor">#FFFFFF</item>
-        <item name="android:gravity">center_vertical</item>
-        <item name="android:paddingLeft">16dp</item>
-        <item name="android:paddingRight">16dp</item>
-        <item name="android:drawablePadding">8dp</item>
-        <item name="android:layout_gravity">center</item>
-        <item name="android:singleLine">true</item>
-        <item name="android:drawableLeft">@drawable/ic_action_bar_magnifying_glass</item>
-        <item name="android:background">?android:attr/selectableItemBackground</item>
-        <item name="textStyle">light</item>
-    </style>
-
-    <style name="V2.Widget.LinearLayout.TabletSearchRightButtonsContainer">
-        <item name="android:layout_width">wrap_content</item>
-        <item name="android:layout_height">0dp</item>
-        <item name="android:layout_weight">1</item>
-        <item name="android:orientation">horizontal</item>
-        <item name="android:gravity">center_vertical</item>
-        <item name="android:layout_gravity">center</item>
-    </style>
-
-</resources>

project/res/values-sw600dp/colors.xml
@@ -19,6 +19,7 @@
     <color name="sliding_group_text_bright">#f4f5f7</color>
     <color name="sliding_group_text_dim">#4a4f54</color>
     <color name="tablet_results_search_blue">#3d6a99</color>
+    <color name="tablet_search_params_text">#ffffff</color>
 
     <!-- Tablet Hotels -->
     <color name="hotel_details_sticky_header_background">#4e4e52</color>

project/res/values/tablet_themes.xml
@@ -166,42 +166,9 @@
         <item name="android:showDividers" tools:targetApi="11">middle</item>
     </style>
 
-    <style name="V2.Widget.LinearLayout.TabletSearchControls">
-        <item name="android:layout_width">match_parent</item>
-        <item name="android:layout_height">match_parent</item>
-        <item name="android:orientation">horizontal</item>
-    </style>
-
-    <style name="V2.Widget.LinearLayout.TabletSearchRightButtonsContainer">
-        <item name="android:layout_width">wrap_content</item>
-        <item name="android:layout_height">match_parent</item>
-        <item name="android:orientation">horizontal</item>
-        <item name="android:gravity">center_vertical</item>
-        <item name="android:layout_weight">0</item>
-        <item name="android:layout_gravity">center_vertical|right</item>
-    </style>
-
     <!-- TextView styling -->
 
     <style name="V2.Widget.TextView" />
-
-    <style name="V2.Widget.TextView.TabletSearchDestination">
-        <item name="android:layout_width">wrap_content</item>
-        <item name="android:layout_height">match_parent</item>
-        <item name="android:textSize">@dimen/search_params_destination_text_size</item>
-        <item name="minTextSize">14dp</item>
-        <item name="android:textColor">#FFFFFF</item>
-        <item name="android:gravity">center_vertical</item>
-        <item name="android:paddingLeft">32dp</item>
-        <item name="android:paddingRight">16dp</item>
-        <item name="android:drawablePadding">8dp</item>
-        <item name="android:layout_gravity">center_vertical|left</item>
-        <item name="android:layout_weight">1</item>
-        <item name="android:singleLine">true</item>
-        <item name="android:drawableLeft">@drawable/ic_action_bar_magnifying_glass</item>
-        <item name="android:background">?android:attr/selectableItemBackground</item>
-        <item name="textStyle">light</item>
-    </style>
 
     <style name="V2.Widget.TextView.TabletSearch" />
 
@@ -305,12 +272,13 @@
 
     <style name="V2.Widget.TextView.SearchParams">
         <item name="android:textSize">@dimen/search_params_right_container_text_size</item>
-        <item name="android:textColor">#FFFFFF</item>
+        <item name="android:textColor">@color/tablet_search_params_text</item>
         <item name="android:paddingLeft">16dp</item>
         <item name="android:paddingRight">16dp</item>
         <item name="android:drawablePadding">8dp</item>
         <item name="android:background">?android:attr/selectableItemBackground</item>
         <item name="android:gravity">center_vertical</item>
+        <item name="android:singleLine">true</item>
         <item name="textStyle">light</item>
     </style>
 

commit 480335b97b48e4565b5218d2b1684232d2de1f86

Fixed a regression introduced in f37a1d9

project/src/com/expedia/bookings/fragment/TabletResultsSearchControllerFragment.java
@@ -837,10 +837,10 @@ public class TabletResultsSearchControllerFragment extends Fragment implements I
 				}
 			}
 			else {
-				float transX = percentage * searchBarHeight;
-				mOrigBtn.setTranslationX(transX);
-				mCalBtn.setTranslationX(transX);
-				mTravBtn.setTranslationX(transX);
+				float transY = percentage * searchBarHeight;
+				mOrigBtn.setTranslationY(transY);
+				mCalBtn.setTranslationY(transY);
+				mTravBtn.setTranslationY(transY);
 			}
 		}
 