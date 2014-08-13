a5651adc1fab5742b6398381f2be1ab450c77da7
Kevin Carpenter <kevc@mobiata.com>
2014-08-13 12:19:49 -0700

    3293 & 3150: Nuke "Remove (Hotel/Flight)" button

--- project/src/com/expedia/bookings/fragment/BookingUnavailableFragment.java
+++ project/src/com/expedia/bookings/fragment/BookingUnavailableFragment.java
@@ -13,7 +13,6 @@ import com.expedia.bookings.data.LineOfBusiness;
 import com.expedia.bookings.enums.TripBucketItemState;
 import com.expedia.bookings.fragment.base.LobableFragment;
 import com.expedia.bookings.utils.FontCache;
-import com.expedia.bookings.utils.NavUtils;
 import com.expedia.bookings.utils.Ui;
 import com.expedia.bookings.widget.TextView;
 
@@ -26,7 +25,6 @@ public class BookingUnavailableFragment extends LobableFragment {
 
 	private View mRootC;
 	private TextView mSoldOutText;
-	private Button mRemoveItemButton;
 	private Button mSelectNewItemButton;
 
 	private BookingUnavailableFragmentListener mListener;
@@ -40,11 +38,8 @@ public class BookingUnavailableFragment extends LobableFragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		mRootC = inflater.inflate(R.layout.fragment_sold_out, container, false);
 		mSoldOutText = Ui.findView(mRootC, R.id.sold_out_text_view);
-		mRemoveItemButton = Ui.findView(mRootC, R.id.remove_sold_out_button);
 		mSelectNewItemButton = Ui.findView(mRootC, R.id.select_new_item_button);
-		mRemoveItemButton.setOnClickListener(mClickListener);
 		mSelectNewItemButton.setOnClickListener(mClickListener);
-		FontCache.setTypeface(mRemoveItemButton, R.id.remove_sold_out_button, FontCache.Font.ROBOTO_LIGHT);
 		FontCache.setTypeface(mSelectNewItemButton, R.id.select_new_item_button, FontCache.Font.ROBOTO_LIGHT);
 		updateViews();
 		return mRootC;
@@ -69,22 +64,8 @@ public class BookingUnavailableFragment extends LobableFragment {
 		public void onClick(View v) {
 			LineOfBusiness lob = getLob();
 			Db.getTripBucket().clear(lob);
-			switch (v.getId()) {
-			case R.id.remove_sold_out_button:
-				if (mListener != null) {
-					if (Db.getTripBucket().isEmpty()) {
-						mListener.onSelectNewTripItem(lob);
-					}
-					else {
-						mListener.onTripBucketItemRemoved(lob);
-					}
-				}
-				break;
-			case R.id.select_new_item_button:
-				if (mListener != null) {
-					mListener.onSelectNewTripItem(lob);
-				}
-				break;
+			if (mListener != null) {
+				mListener.onSelectNewTripItem(lob);
 			}
 		}
 	};
@@ -97,7 +78,6 @@ public class BookingUnavailableFragment extends LobableFragment {
 			else {
 				mSoldOutText.setText(getString(R.string.tablet_sold_out_summary_text_hotel));
 			}
-			mRemoveItemButton.setText(getString(R.string.tablet_sold_out_remove_hotel));
 			mSelectNewItemButton.setText(getString(R.string.tablet_sold_out_select_hotel));
 		}
 		else {
@@ -119,7 +99,6 @@ public class BookingUnavailableFragment extends LobableFragment {
 				}
 			}
 
-			mRemoveItemButton.setText(getString(R.string.tablet_sold_out_remove_flight));
 			mSelectNewItemButton.setText(getString(R.string.tablet_sold_out_select_flight));
 		}
 	}
