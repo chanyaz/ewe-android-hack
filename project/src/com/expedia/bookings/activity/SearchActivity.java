ead2de0e1f34f988c1472cd1440c010d6def18d2
Brennan Taylor <brennan@mobiata.com>
2014-09-16 11:27:44 -0700

    Remove unused import

--- project/src/com/expedia/bookings/activity/SearchActivity.java
+++ project/src/com/expedia/bookings/activity/SearchActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.crashlytics.android.Crashlytics;
 import com.expedia.bookings.server.ExpediaServices;
 import com.expedia.bookings.tracking.AdTracker;
 import com.expedia.bookings.tracking.OmnitureTracking;
