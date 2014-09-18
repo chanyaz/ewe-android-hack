d9eee0328b735020514e17ef26031088637356c2
Nandan Upadhyay <v-nupadyay@expedia.com>
2014-09-17 17:51:52 -0700

    changed calender text for all POS using attributes
    Reviewers: brennan
    
    Reviewed By: brennan
    
    Subscribers: sugupta
    
    Differential Revision: https://phabricator.mobiata.com/D637

--- project/src/com/expedia/bookings/utils/ConfirmationUtils.java
+++ project/src/com/expedia/bookings/utils/ConfirmationUtils.java
@@ -3,7 +3,6 @@ package com.expedia.bookings.utils;
 import android.content.Context;
 
 import com.expedia.bookings.R;
-import com.expedia.bookings.activity.ExpediaBookingApp;
 import com.expedia.bookings.data.pos.PointOfSale;
 
 public class ConfirmationUtils {
@@ -12,13 +11,10 @@ public class ConfirmationUtils {
 	// Miscellaneous
 
 	public static String determineContactText(Context context) {
-		if (ExpediaBookingApp.IS_TRAVELOCITY) {
-			return context.getString(R.string.contact_phone_template_tvly, PointOfSale.getPointOfSale()
-				.getSupportPhoneNumber());
-		}
 
-		return context.getString(R.string.contact_phone_template, PointOfSale.getPointOfSale()
+		return context.getString(Ui.obtainThemeResID(context, R.attr.addToCalenderMessage), PointOfSale.getPointOfSale()
 			.getSupportPhoneNumber());
+
 	}
 
 }
