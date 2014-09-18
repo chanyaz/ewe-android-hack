994762ec10f32c8336d0791a388c3571a341aaf3
Kevin Carpenter <kevc@mobiata.com>
2014-09-18 13:12:21 -0700

    Add buildbot script for crashlytics beta

---
+++ buildbot/crashlytics_beta.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PROPERTIES_FILE="properties.json"
+CHANGE_LOG_FILE="changelog.txt"
+
+if [ -e "$PROPERTIES_FILE" ] ; then
+    echo "=== Properties ==="
+    cat $PROPERTIES_FILE | python -m json.tool
+    echo "=== End Properties ==="
+
+    ./buildbot/CreateChangelog.py $PROPERTIES_FILE | sed -e 's/^/    /' > $CHANGE_LOG_FILE
+fi
+
+./gradlew --info --stacktrace --no-daemon -DdisablePreDex clean assembleExpediaLatest
+
+./gradlew crashlyticsUploadDistributionExpediaLatest
