ceabe3ed6616565fe709da55f7b4d60e0f8633aa
Deepanshu Madan <dmadan@expedia.com>
2014-10-04 02:49:16 +0530

    Click hotel and flight buttons by content description on phone launch screen.

--- project/src/androidTestExpediaAutomation/java/com/expedia/bookings/test/phone/pagemodels/common/LaunchScreen.java
+++ project/src/androidTestExpediaAutomation/java/com/expedia/bookings/test/phone/pagemodels/common/LaunchScreen.java
@@ -1,31 +1,22 @@
 package com.expedia.bookings.test.phone.pagemodels.common;
 
-import com.expedia.bookings.R;
 import com.google.android.apps.common.testing.ui.espresso.ViewInteraction;
 
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
-
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withContentDescription;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
 
 /**
  * Created by dmadan on 4/7/14.
  */
 public class LaunchScreen extends LaunchActionBar {
-	private static final int sHotelsButtonID = R.id.hotels_button;
-	private static final int sFlightsButtonID = R.id.flights_button;
-	public LaunchActionBar mLaunchActionBar;
 
 	public static ViewInteraction hotelLaunchButton() {
-		return onView(withId(sHotelsButtonID));
+		return onView(withContentDescription("Launch Hotels"));
 	}
 
 	public static ViewInteraction flightLaunchButton() {
-		return onView(withId(sFlightsButtonID));
+		return onView(withContentDescription("Launch Flights"));
 	}
 
 	public static void launchHotels() {
@@ -35,6 +26,5 @@ public class LaunchScreen extends LaunchActionBar {
 	public static void launchFlights() {
 		flightLaunchButton().perform(click());
 	}
-
 }
 
