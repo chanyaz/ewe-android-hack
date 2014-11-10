42df5e38d0c6d69bc2444cee2436d83bed7417d4
Kevin Carpenter <kevc@mobiata.com>
2014-11-10 13:54:11 -0800

    FTC changes: Resort fee info on phone

---
+++ project/src/main/java/com/expedia/bookings/section/HotelReceiptExtraSection.java
@@ -0,0 +1,55 @@
+package com.expedia.bookings.section;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.expedia.bookings.R;
+import com.expedia.bookings.data.Rate;
+import com.expedia.bookings.utils.Ui;
+import com.expedia.bookings.widget.FrameLayout;
+
+public class HotelReceiptExtraSection extends RelativeLayout {
+
+	private TextView mTitle;
+	private TextView mPrice;
+
+	public HotelReceiptExtraSection(Context context) {
+		super(context);
+	}
+
+	public HotelReceiptExtraSection(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	@Override
+	protected void onFinishInflate() {
+		super.onFinishInflate();
+		mTitle = Ui.findView(this, R.id.price_title);
+		mPrice = Ui.findView(this, R.id.price_text_view);
+	}
+
+	public void bind(CharSequence title, String rateString) {
+		if (!TextUtils.isEmpty(title)) {
+			mTitle.setText(title);
+		}
+		else {
+			throw new RuntimeException("You probably shouldn't be binding a HotelReceiptExtraSection without a title.");
+		}
+		if (!TextUtils.isEmpty(title)) {
+			mPrice.setText(rateString);
+			mPrice.setVisibility(View.VISIBLE);
+		}
+		else {
+			mPrice.setVisibility(View.GONE);
+		}
+	}
+
+
+
+
+}
