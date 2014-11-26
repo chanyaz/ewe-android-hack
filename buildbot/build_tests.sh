94e0389c618baa945267223faea3e45f7e56c449
Brennan Taylor <brennan@mobiata.com>
2014-11-25 16:52:18 -0800

    Change how APKs are built for CI
    Don't build twice. Build once and use the apk in the crashlytics_beta.sh
    script.

--- buildbot/build_tests.sh
+++ buildbot/build_tests.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-./gradlew --info --no-daemon clean assembleExpediaLatest assembleExpediaLatestTest
+
+TARGET=$(echo ${BUILDER_NAME} | perl -ne 'print ucfirst($_)')
+
+if [ -z "${TARGET}" ] ; then
+    # Just default to expedia
+    TARGET="Expedia"
+fi
+
+echo "target=$TARGET"
+
+TERM=dumb
+./gradlew --no-daemon "clean" "assemble${TARGET}Latest" "assemble${TARGET}LatestTest"
