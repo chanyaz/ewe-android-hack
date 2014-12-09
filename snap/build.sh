0421feb5d0a74724fe3ff72fafb481db126965e3
Brennan Taylor <brennan@mobiata.com>
2014-12-08 18:26:35 -0800

    Update snap script for new dependencies
    And check if the tools already exist so we don't waste time downloading

--- snap/build.sh
+++ snap/build.sh
@@ -3,18 +3,27 @@
 set -e
 
 function get() {
-    START=$(date +%s)
-    echo y | android update sdk --no-ui --filter "${1}" --all > /dev/null
-    END=$(date +%s)
-    DIFF=$(echo "$END - $START" | bc)
-    echo $1 "took" $DIFF "seconds"
+    local cachedir="$1"
+    local package="$2"
+
+    if [ -d "${ANDROID_HOME}/${cachedir}" ] ; then
+        echo "${package} already there"
+        return 0
+    fi
+
+    local START=$(date +%s)
+    echo y | android update sdk --no-ui --filter "${package}" --all > /dev/null
+    local END=$(date +%s)
+
+    local DIFF=$(echo "$END - $START" | bc)
+    echo "${package} took ${DIFF} seconds"
 }
 
-get "platform-tools"
-get "android-19"
-get "extra-android-m2repository"
-get "extra-google-m2repository"
-get "build-tools-19.1.0"
+get "platform-tools" "platform-tools"
+get "platforms/android-21" "android-21"
+get "build-tools/21.1.1" "build-tools-21.1.1"
+get "extras/android/m2repository" "extra-android-m2repository"
+get "extras/google/m2repository" "extra-google-m2repository"
 
 function build() {
     # Disable gradle's fancy log outputting
@@ -23,7 +32,8 @@ function build() {
     ./gradlew --no-daemon $*
 }
 
-build assembleExpediaDebug
-build assembleTravelocityDebug
-build assembleAirAsiaGoDebug
+build assembleExpediaLatest assembleExpediaLatestTest
+build assembleTravelocityLatest
+build assembleAirAsiaGoLatest
+build assembleVoyagesLatest
 
