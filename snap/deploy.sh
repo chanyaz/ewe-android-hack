60b99ca11f34e85bc87535fa3a87458b66c2b2ce
Brennan Taylor <brennan@mobiata.com>
2014-12-21 21:22:23 -0800

    Use android-sdk-manager plugin
    Makes it so:
    
    - Devs don't get burned when their local tools are out of date
    - CI systems don't need to be updated by hand before running builds
    - New builds people building the repo don't have to ask what to download

--- snap/deploy.sh
+++ snap/deploy.sh
@@ -7,12 +7,6 @@ test -z "${SNAP_PULL_REQUEST_NUMBER}" || exit 0
 
 source snap/common.sh
 
-getAndroid "platform-tools" "platform-tools"
-getAndroid "platforms/android-21" "android-21"
-getAndroid "build-tools/21.1.1" "build-tools-21.1.1"
-getAndroid "extras/android/m2repository" "extra-android-m2repository"
-getAndroid "extras/google/m2repository" "extra-google-m2repository"
-
 echo """
 SNAP_PIPELINE_COUNTER: ${SNAP_PIPELINE_COUNTER}
 SNAP_BRANCH: ${SNAP_BRANCH}
