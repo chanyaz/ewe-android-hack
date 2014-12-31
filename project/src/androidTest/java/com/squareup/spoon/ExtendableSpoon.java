38890b34a37c83a2d80081a7ac3c70b66d9ad561
Brennan Taylor <brennan@mobiata.com>
2014-12-30 22:23:36 -0800

    checkstyle: fix RedundantImports

--- project/src/androidTest/java/com/squareup/spoon/ExtendableSpoon.java
+++ project/src/androidTest/java/com/squareup/spoon/ExtendableSpoon.java
@@ -18,7 +18,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.lang.String;
 import java.util.concurrent.CountDownLatch;
 import java.util.regex.Pattern;
 
