46084f69cbce8b53f945d8193cee0ed730e4b7cc
Kevin Carpenter <kevc@mobiata.com>
2015-01-08 11:11:57 -0800

    Move CarSearchParams to lib-ExpediaBookings

---
+++ lib/ExpediaBookings/src/main/java/com/expedia/bookings/utils/data/cars/CarSearchParams.java
@@ -0,0 +1,11 @@
+package com.expedia.bookings.utils.data.cars;
+
+import org.joda.time.DateTime;
+
+public class CarSearchParams {
+
+    public DateTime startTime;
+    public DateTime endTime;
+
+    public String origin;
+}
