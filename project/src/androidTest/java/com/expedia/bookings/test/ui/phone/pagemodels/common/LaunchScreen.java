848e88fd6e6ae438a5789d8904556d474c0b0fe2
Deepanshu Madan <dmadan@expedia.com>
2015-01-07 17:46:02 -0800

    Update localization screenshot sweep.
    Read locale from config.json and change the app POS from the test.

--- project/src/androidTest/java/com/expedia/bookings/test/ui/phone/pagemodels/common/LaunchScreen.java
+++ project/src/androidTest/java/com/expedia/bookings/test/ui/phone/pagemodels/common/LaunchScreen.java
@@ -1,22 +1,34 @@
 package com.expedia.bookings.test.ui.phone.pagemodels.common;
 
+import android.content.res.Resources;
 import android.support.test.espresso.ViewInteraction;
+
+import com.expedia.bookings.R;
 
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * Created by dmadan on 4/7/14.
  */
 public class LaunchScreen extends LaunchActionBar {
 
+	public static ViewInteraction hotelLaunchWithContentDescription(Resources res) {
+		return onView(withContentDescription(res.getString(R.string.cd_launch_hotels)));
+	}
+
+	public static ViewInteraction flightLaunchWithContentDescription(Resources res) {
+		return onView(withContentDescription(res.getString(R.string.cd_launch_flights)));
+	}
+
 	public static ViewInteraction hotelLaunchButton() {
-		return onView(withContentDescription("Launch Hotels"));
+		return onView(withId(R.id.hotels_button));
 	}
 
 	public static ViewInteraction flightLaunchButton() {
-		return onView(withContentDescription("Launch Flights"));
+		return onView(withId(R.id.flights_button));
 	}
 
 	public static void launchHotels() {
