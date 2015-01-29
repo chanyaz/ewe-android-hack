bc22a0df992855e3f367556d133c03ea2982dc58
Brennan Taylor <brennan@mobiata.com>
2015-01-28 17:03:44 -0800

    Simplify CarsActivity

--- project/src/main/java/com/expedia/bookings/activity/CarsActivity.java
+++ project/src/main/java/com/expedia/bookings/activity/CarsActivity.java
@@ -1,10 +1,7 @@
 package com.expedia.bookings.activity;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.LayoutRes;
 
 import com.expedia.bookings.R;
 import com.expedia.bookings.enums.CarsState;
@@ -15,21 +12,13 @@ import butterknife.InjectView;
 
 public class CarsActivity extends Activity {
 
-	private static final String KEY_LAYOUT_RES = "KEY_LAYOUT_RES";
-
 	@InjectView(R.id.cars_presenter)
 	CarsPresenter carsPresenter;
-
-	public static Intent createIntent(Context context, @LayoutRes int layoutResId) {
-		Intent intent = new Intent(context, CarsActivity.class);
-		intent.putExtra(KEY_LAYOUT_RES, layoutResId);
-		return intent;
-	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(getIntent().getIntExtra(KEY_LAYOUT_RES, R.layout.activity_cars));
+		setContentView(R.layout.activity_cars);
 
 		ButterKnife.inject(this);
 		carsPresenter.show(CarsState.SEARCH);
