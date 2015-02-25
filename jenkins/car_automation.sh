9bee98661e5a27192d3793b27328f61305235842
Brad McManus <brad@mobiata.com>
2015-02-25 10:59:26 -0800

    Car search error regression tests
    - add to car automation script

--- jenkins/car_automation.sh
+++ jenkins/car_automation.sh
@@ -12,6 +12,7 @@ OUTPUT_DIR="spoon/cars"
 
 add_test "com.expedia.bookings.test.component.cars.CarSearchParamsTests"
 add_test "com.expedia.bookings.test.ui.happy.CarPhoneHappyPath"
+add_test "com.expedia.bookings.test.ui.phone.tests.cars.CarSearchErrorTests"
 
 rm -rf "spoon"
 java \
