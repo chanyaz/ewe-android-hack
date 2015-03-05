3d5c2db7e0b398b575d172dbd7d1406d475d2eef
Rajan Kochhar <rkochhar@expedia.com>
2015-03-05 23:43:11 +0530

    Fix LX tests. Try to make Android-Phone-EB-LX job green.

--- project/src/androidTest/java/com/expedia/bookings/test/ui/happy/LxPhoneHappyPath.java
+++ project/src/androidTest/java/com/expedia/bookings/test/ui/happy/LxPhoneHappyPath.java
@@ -2,7 +2,7 @@ package com.expedia.bookings.test.ui.happy;
 
 import java.util.concurrent.TimeUnit;
 
-import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
 
 import android.widget.ProgressBar;
 
@@ -24,12 +24,15 @@ public class LxPhoneHappyPath extends PhoneTestCase {
 	}
 
 	public void testLxPhoneHappyPath() throws Throwable {
-		final DateTime startDateTime = DateTime.now().withTimeAtStartOfDay();
-		final DateTime endDateTime = startDateTime.plusDays(3);
+		final LocalDate startDateTime = LocalDate.now();
+		String expectedLocationDisplayName = "San Francisco, CA";
 		screenshot("LX_search");
-		LXViewModel.location().perform(typeText("Seattle"));
+		LXViewModel.location().perform(typeText("San"));
+
+		LXViewModel.selectLocation(getInstrumentation(), expectedLocationDisplayName);
+
 		LXViewModel.selectDateButton().perform(click());
-		LXViewModel.selectDates(startDateTime.toLocalDate(), endDateTime.toLocalDate());
+		LXViewModel.selectDates(startDateTime, null);
 		screenshot("LX_Search_Params_Entered");
 		LXViewModel.searchButton().perform(click());
 		onView(withId(R.id.loading_results)).perform(waitFor(10L, TimeUnit.SECONDS, ProgressBar.class));
