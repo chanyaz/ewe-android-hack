a19c5259d12710f1f64b24fad4dac6f7e3e34eb6
Brennan Taylor <brennan@mobiata.com>
2015-03-06 16:43:36 -0800

    Add more tests to buildbot/happypath.sh
    Its worth having all of these stop the QA build right now

--- buildbot/happypath.sh
+++ buildbot/happypath.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+TESTS=""
+function add_test() {
+    TESTS+="$1,"
+}
+
 # Run tests
 APK="project/build/outputs/apk/project-expedia-debug-unaligned.apk"
 TEST_APK="project/build/outputs/apk/project-expedia-debug-test-unaligned.apk"
@@ -11,12 +16,26 @@ else
     export OUTPUT_DIR="spoon/happy"
 fi
 
+# Happypath
+add_test "com.expedia.bookings.test.ui.happy.TabletHappyPath"
+add_test "com.expedia.bookings.test.ui.happy.PhoneHappyPath"
+add_test "com.expedia.bookings.test.ui.happy.CarPhoneHappyPath"
+add_test "com.expedia.bookings.test.ui.happy.LxPhoneHappyPath"
+
+# Cars
+add_test "com.expedia.bookings.test.component.cars.CarSearchParamsTests"
+add_test "com.expedia.bookings.test.ui.phone.tests.cars.CarSearchErrorTests"
+
+# LX
+add_test "com.expedia.bookings.test.component.lx.LXSearchParamsTest"
+add_test "com.expedia.bookings.test.component.lx.LXDetailsPresenterTests"
+add_test "com.expedia.bookings.test.component.lx.LXResultsPresenterTests"
 
 java \
     -jar "jars/spoon-runner-1.1.3-EXP-jar-with-dependencies.jar" \
     --apk  "$APK" \
     --test-apk "$TEST_APK" \
-    --class-name "com.expedia.bookings.test.ui.happy.TabletHappyPath,com.expedia.bookings.test.ui.happy.PhoneHappyPath,com.expedia.bookings.test.ui.happy.CarPhoneHappyPath" \
+    --class-name "$TESTS" \
     --no-animations \
     --fail-on-failure \
     --output "$OUTPUT_DIR"
