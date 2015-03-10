a2e274318cb4af20b720a2ce705f1fbc06208cf2
Nakul Pandey <napandey@DELC02H63K9DV7M.sea.corp.expecn.com>
2015-03-10 16:22:54 +0530

    Added a new test case to help the localization testing
    Fixed checkstyle error

---
+++ project/src/androidTest/java/com/expedia/bookings/test/ui/phone/tests/lx/LocalisationTestCase.java
@@ -0,0 +1,66 @@
+package com.expedia.bookings.test.ui.phone.tests.lx;
+
+import java.util.concurrent.TimeUnit;
+
+import org.joda.time.LocalDate;
+
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.widget.ProgressBar;
+
+import com.expedia.bookings.R;
+import com.expedia.bookings.activity.LXBaseActivity;
+import com.expedia.bookings.test.component.lx.LXViewModel;
+import com.expedia.bookings.test.ui.utils.PhoneTestCase;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static com.expedia.bookings.test.ui.espresso.ViewActions.waitFor;
+/*
+	A test that would run through all the screens of the LX flow in the Landscape and Portrait mode"
+*/
+public class LocalisationTestCase extends PhoneTestCase {
+
+	public LocalisationTestCase() {
+		super(LXBaseActivity.class);
+	}
+
+	public void testScreenShotThroughLandscapeMode() throws Throwable {
+		//rotate the screen so that we are ready for the flow
+		rotateScreen();
+		screenshot("BEFORE_SEARCH");
+		LXViewModel.location().perform(typeText("San"));
+		screenshot("AFTER_SEARCH_SUGGESTIONS_POPULATE");
+		LXViewModel.selectLocation(getInstrumentation(), "San Francisco, CA");
+		screenshot("BEFORE_DATE_SELECTION");
+		LXViewModel.selectDateButton().perform(click());
+		LXViewModel.selectDates(LocalDate.now(), null);
+		screenshot("AFTER_DATE_SELECTION");
+		LXViewModel.searchButton().perform(click());
+		screenshot("LOADING_SCREEN");
+		onView(withId(R.id.loading_results)).perform(waitFor(10L, TimeUnit.SECONDS, ProgressBar.class));
+		screenshot("ACTIVITY_RESULT_SCREEN");
+		onView(withId(R.id.lx_search_results_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+		screenshot("INFO_SITE_PAGE");
+
+	}
+
+
+	public void testScreenShotThroughPortraitMode() throws Throwable {
+		screenshot("BEFORE_SEARCH");
+		LXViewModel.location().perform(typeText("San"));
+		screenshot("AFTER_SEARCH_SUGGESTIONS_POPULATE");
+		LXViewModel.selectLocation(getInstrumentation(), "San Francisco, CA");
+		screenshot("BEFORE_DATE_SELECTION");
+		LXViewModel.selectDateButton().perform(click());
+		LXViewModel.selectDates(LocalDate.now(), null);
+		screenshot("AFTER_DATE_SELECTION");
+		LXViewModel.searchButton().perform(click());
+		screenshot("LOADING_SCREEN");
+		onView(withId(R.id.loading_results)).perform(waitFor(10L, TimeUnit.SECONDS, ProgressBar.class));
+		screenshot("ACTIVITY_RESULT_SCREEN");
+		onView(withId(R.id.lx_search_results_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+		screenshot("INFO_SITE_PAGE");
+	}
+}
