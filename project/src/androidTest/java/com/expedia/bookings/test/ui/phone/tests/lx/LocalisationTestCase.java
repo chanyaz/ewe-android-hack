feed836b3f29967b2962b534fab04d0701e917ea
Nakul Pandey <napandey@DELC02H63K9DV7M.sea.corp.expecn.com>
2015-03-11 09:54:42 +0530

    As we dont support the landscape version on LX, I have removed the landscape version of the test case

--- project/src/androidTest/java/com/expedia/bookings/test/ui/phone/tests/lx/LocalisationTestCase.java
+++ project/src/androidTest/java/com/expedia/bookings/test/ui/phone/tests/lx/LocalisationTestCase.java
@@ -18,34 +18,14 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static com.expedia.bookings.test.ui.espresso.ViewActions.waitFor;
 /*
-	A test that would run through all the screens of the LX flow in the Landscape and Portrait mode"
+	A test that would run through all the screens of the LX flow in the Portrait mode"
 */
 public class LocalisationTestCase extends PhoneTestCase {
+
 
 	public LocalisationTestCase() {
 		super(LXBaseActivity.class);
 	}
-
-	public void testScreenShotThroughLandscapeMode() throws Throwable {
-		//rotate the screen so that we are ready for the flow
-		rotateScreen();
-		screenshot("BEFORE_SEARCH");
-		LXViewModel.location().perform(typeText("San"));
-		screenshot("AFTER_SEARCH_SUGGESTIONS_POPULATE");
-		LXViewModel.selectLocation(getInstrumentation(), "San Francisco, CA");
-		screenshot("BEFORE_DATE_SELECTION");
-		LXViewModel.selectDateButton().perform(click());
-		LXViewModel.selectDates(LocalDate.now(), null);
-		screenshot("AFTER_DATE_SELECTION");
-		LXViewModel.searchButton().perform(click());
-		screenshot("LOADING_SCREEN");
-		onView(withId(R.id.loading_results)).perform(waitFor(10L, TimeUnit.SECONDS, ProgressBar.class));
-		screenshot("ACTIVITY_RESULT_SCREEN");
-		onView(withId(R.id.lx_search_results_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-		screenshot("INFO_SITE_PAGE");
-
-	}
-
 
 	public void testScreenShotThroughPortraitMode() throws Throwable {
 		screenshot("BEFORE_SEARCH");
