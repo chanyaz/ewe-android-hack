127b3318c9bcea0b44b558a1f196bd14b9680b9d
Brennan Taylor <brennan@mobiata.com>
2015-03-24 16:08:07 -0700

    Add more screenshots to LxPhonHappyPath test
    It is so pretty. Why not show it off!

--- project/src/androidTest/java/com/expedia/bookings/test/ui/happy/LxPhoneHappyPath.java
+++ project/src/androidTest/java/com/expedia/bookings/test/ui/happy/LxPhoneHappyPath.java
@@ -22,47 +22,52 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.expedia.bookings.test.component.lx.LXViewModel.getBookNowButtonFromTicketName;
 import static com.expedia.bookings.test.component.lx.LXViewModel.getTicketAddButtonViewFromTicketName;
-import static com.expedia.bookings.test.component.lx.LXViewModel.itinNumberOnConfirmationScreen;
 import static com.expedia.bookings.test.ui.espresso.ViewActions.clickOnFirstEnabled;
 import static com.expedia.bookings.test.ui.espresso.ViewActions.waitFor;
 
 public class LxPhoneHappyPath extends PhoneTestCase {
 
-	private static final String TAG = LxPhoneHappyPath.class.getSimpleName();
-
 	public LxPhoneHappyPath() {
 		super(LXBaseActivity.class);
 	}
 
-	/*
-		A basic happy path
-	 */
 	public void testLxPhoneHappyPath() throws Throwable {
-		String expectedLocationDisplayName = "San Francisco, CA";
-		String ticketName = "2-Day";
-		screenshot("LX_search");
+		final String ticketName = "2-Day";
+
+		screenshot("LX Search");
 		LXViewModel.location().perform(typeText("San"));
-		LXViewModel.selectLocation(getInstrumentation(), expectedLocationDisplayName);
+		LXViewModel.selectLocation(getInstrumentation(), "San Francisco, CA");
 		LXViewModel.selectDateButton().perform(click());
 		LXViewModel.selectDates(LocalDate.now(), null);
-		screenshot("LX_Search_Params_Entered");
+		screenshot("LX Search Params Entered");
 		LXViewModel.searchButton().perform(click());
+
 		onView(withId(R.id.loading_results)).perform(waitFor(10L, TimeUnit.SECONDS));
-		onView(withId(R.id.lx_search_results_list)).perform(
-			RecyclerViewActions
-				.actionOnItemAtPosition(0, click()));
+		screenshot("LX Search Results");
+
+		onView(withId(R.id.lx_search_results_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 		onView(withId(R.id.loading_details)).perform(waitFor(10L, TimeUnit.SECONDS));
+		screenshot("LX Details");
+
 		LXViewModel.detailsDateContainer().perform(scrollTo(),clickOnFirstEnabled());
 		LXViewModel.selectTicketsButton("2-Day New York Pass").perform(scrollTo(),click());
 		getTicketAddButtonViewFromTicketName(ticketName, "Adult").perform(click());
-		getBookNowButtonFromTicketName(ticketName).perform(scrollTo(),click());
+		getBookNowButtonFromTicketName(ticketName).perform(scrollTo());
+		screenshot("LX Ticket Selection");
+		getBookNowButtonFromTicketName(ticketName).perform(click());
+
+		screenshot("LX Checkout Started");
 		CheckoutViewModel.enterTravelerInfo();
 		CheckoutViewModel.enterPaymentInfo();
 		CheckoutViewModel.clickDone();
+		screenshot("LX Checkout Ready");
 		CheckoutViewModel.performSlideToPurchase();
-		CVVEntryScreen.parseAndEnterCVV("111");
-		CVVEntryScreen.clickBookButton();
-		itinNumberOnConfirmationScreen().check(matches(withText("7672544862")));
-	}
 
+		CVVEntryScreen.parseAndEnterCVV("111");
+		screenshot("LX CVV");
+		CVVEntryScreen.clickBookButton();
+
+		screenshot("LX Checkout Started");
+		LXViewModel.itinNumberOnConfirmationScreen().check(matches(withText("7672544862")));
+	}
 }
