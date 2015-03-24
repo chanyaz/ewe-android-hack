5717b12d3b09b127c25d997b12ab0981edb0bfdb
Deepanshu Madan <dmadan@expedia.com>
2015-03-23 18:23:40 -0700

    Fix NPE crash on car search screen.

--- project/src/main/java/com/expedia/bookings/presenter/CarSearchPresenter.java
+++ project/src/main/java/com/expedia/bookings/presenter/CarSearchPresenter.java
@@ -101,7 +101,7 @@ public class CarSearchPresenter extends Presenter
 
 	@OnClick(R.id.pickup_location)
 	public void onPickupEditClicked() {
-		if (!getCurrentState().equals(CarParamsDefault.class.getName())) {
+		if (getCurrentState() != null && !getCurrentState().equals(CarParamsDefault.class.getName())) {
 			back();
 		}
 	}
