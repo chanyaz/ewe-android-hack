d7f2e33b9173395234cb31ae9832716848ef376c
Brad McManus <brad@mobiata.com>
2015-04-02 10:55:09 -0700

    Cleanup a few TODOs

--- lib/ExpediaBookings/src/main/java/com/expedia/bookings/data/cars/CarCheckoutResponse.java
+++ lib/ExpediaBookings/src/main/java/com/expedia/bookings/data/cars/CarCheckoutResponse.java
@@ -9,11 +9,7 @@ public class CarCheckoutResponse extends BaseCarResponse {
 	public CarTrackingData trackingData;
 	public TripInfo newTrip;
 	public String orderId;
-	public Money totalChargesPrice; // TODO: API needs to include currency code in this object.
-
-	//TODO: nuke these when the comment above is addressed.
-	public String currencyCode;
-	public String totalCharges;
+	public Money totalChargesPrice;
 
 	// NOTE: These fields look shockingly similar to the fields in CarCreateTripResponse. That is
 	// because a price change on checkout is communicated like the CreateTripResponse with price
