chanyaz/ewe-android-hack

commit 36ebe63aeb73e690a0fb81647eeaf3f81b2a38ce

TVLY: Rectify Branding in Strings for FTC

project/src/main/java/com/expedia/bookings/widget/HotelReceipt.java
@@ -319,18 +319,18 @@ public class HotelReceipt extends LinearLayout {
 		mExtrasLayout.setVisibility(View.VISIBLE);
 		mExtrasDivider.setVisibility(View.VISIBLE);
 
-		HotelReceiptExtraSection dueToExpediaRow = Ui.inflate(R.layout.snippet_hotel_receipt_price_extra, mExtrasLayout, false);
-		String totalDueToExpediaToday = getResources().getString(R.string.total_due_to_expedia_today);
-		dueToExpediaRow.bind(totalDueToExpediaToday, rate.getDepositAmount().getFormattedMoney());
+		HotelReceiptExtraSection dueToOurBrandRow = Ui.inflate(R.layout.snippet_hotel_receipt_price_extra, mExtrasLayout, false);
+		String totalDueToOurBrandToday = getResources().getString(R.string.total_due_to_our_brand_today);
+		dueToOurBrandRow.bind(totalDueToOurBrandToday, rate.getTotalAmountAfterTax().getFormattedMoney());
 
 		if (rate.getDepositAmount().isZero()) {
-			TextView labelView = (TextView) dueToExpediaRow.findViewById(R.id.price_title);
-			TextView rateView = (TextView) dueToExpediaRow.findViewById(R.id.price_text_view);
+			TextView labelView = (TextView) dueToOurBrandRow.findViewById(R.id.price_title);
+			TextView rateView = (TextView) dueToOurBrandRow.findViewById(R.id.price_text_view);
 			labelView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_etp_overview_checkmark, 0, 0, 0);
 			labelView.setTextColor(getResources().getColor(R.color.etp_text_color));
 			rateView.setTextColor(getResources().getColor(R.color.etp_text_color));
 		}
-		mExtrasLayout.addView(dueToExpediaRow);
+		mExtrasLayout.addView(dueToOurBrandRow);
 	}
 
 	private static final int MAX_AMENITY_ROWS = 3;

project/src/main/java/com/expedia/bookings/fragment/TripBucketHotelFragment.java
@@ -164,8 +164,8 @@ public class TripBucketHotelFragment extends TripBucketItemFragment {
 		String feesPaidAtHotel = getResources().getString(R.string.fees_paid_at_hotel);
 		addExtraRow(feesPaidAtHotel, rate.getTotalMandatoryFees().getFormattedMoney(), false);
 
-		String totalDueToExpediaToday = getResources().getString(R.string.total_due_to_expedia_today);
-		addExtraRow(totalDueToExpediaToday, rate.getTotalAmountAfterTax().getFormattedMoney(), false);
+		String totalDueToOurBrandToday = getResources().getString(R.string.total_due_to_our_brand_today);
+		addExtraRow(totalDueToOurBrandToday, rate.getTotalAmountAfterTax().getFormattedMoney(), false);
 	}
 
 	private static final int LANDSCAPE_EXTRAS_LIMIT = 3;

project/src/main/res/values/strings.xml
@@ -1281,7 +1281,7 @@
     <!-- Row header for when we show resort fees as its own line item -->
     <string name="fees_paid_at_hotel">Fees Paid at Hotel:</string>
     <!-- Row header for when the grand total of the user's hotel stay is more than what they owe Expedia at the time of reservation-->
-    <string name="total_due_to_expedia_today">Due to Expedia Today:</string>
+    <string name="total_due_to_our_brand_today">Due to Expedia Today:</string>
     <!-- Legal disclaimer for resort fees (at bottom of checkout) -->
     <string name="resort_fee_disclaimer_TEMPLATE">The &lt;b&gt;%1$s&lt;/b&gt; resort fee will be collected at the hotel. The total price for your stay will be &lt;b&gt;%2$s&lt;/b&gt;.</string>
     <!-- Pay later disclaimer at bottom of checkout -->

project/src/travelocity/res/values/strings.xml
@@ -87,4 +87,6 @@
     <string name="sorry_rooms_sold_out_tvly">Sorry, all rooms are sold out on Travelocity.</string>
     <!-- Travelocity App download link -->
     <string name="share_template_long_ad_tvly">Book hotels, flights and view &amp; share your itineraries. Download the free Travelocity mobile app today.\n%s</string>
+    <!-- Row header for when the grand total of the user's hotel stay is more than what they owe Expedia at the time of reservation-->
+    <string name="total_due_to_our_brand_today">Total Due to Travelocity Today:</string>
 </resources>

commit aa589a519b27d618b00a20663dae39db9dc76061

Fix total due amount on  hotel checkout summary.
This bug got introduced in commit 36ebe63aeb73e690a0fb81647eeaf3f81b2a38ce
for travelocity 7.1 while doing some renaming and string changes.

project/src/main/java/com/expedia/bookings/widget/HotelReceipt.java
@@ -321,7 +321,7 @@ public class HotelReceipt extends LinearLayout {
 
 		HotelReceiptExtraSection dueToOurBrandRow = Ui.inflate(R.layout.snippet_hotel_receipt_price_extra, mExtrasLayout, false);
 		String totalDueToOurBrandToday = getResources().getString(R.string.total_due_to_our_brand_today);
-		dueToOurBrandRow.bind(totalDueToOurBrandToday, rate.getTotalAmountAfterTax().getFormattedMoney());
+		dueToOurBrandRow.bind(totalDueToOurBrandToday, rate.getDepositAmount().getFormattedMoney());
 
 		if (rate.getDepositAmount().isZero()) {
 			TextView labelView = (TextView) dueToOurBrandRow.findViewById(R.id.price_title);