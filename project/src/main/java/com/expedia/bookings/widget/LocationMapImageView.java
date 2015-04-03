92eca8a6e6a81f4b9e7f6cd89f6caed3611f76ce
Brennan Taylor <brennan@mobiata.com>
2015-04-03 16:04:05 -0700

    Always use PicassoHelper

--- project/src/main/java/com/expedia/bookings/widget/LocationMapImageView.java
+++ project/src/main/java/com/expedia/bookings/widget/LocationMapImageView.java
@@ -7,6 +7,7 @@ import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
+import com.expedia.bookings.bitmaps.PicassoHelper;
 import com.expedia.bookings.bitmaps.PicassoTarget;
 import com.expedia.bookings.data.Location;
 import com.mobiata.android.Log;
@@ -81,7 +82,10 @@ public class LocationMapImageView extends ImageView {
 		Log.d("ITIN: mapUrl:" + mStaticMapUri);
 
 		if (!mStaticMapUri.equals(oldUri)) {
-			Picasso.with(getContext()).load(mStaticMapUri).into(callback);
+			new PicassoHelper.Builder(this)
+				.setTarget(callback)
+				.build()
+				.load(mStaticMapUri);
 		}
 	}
 
