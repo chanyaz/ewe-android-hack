bec86b6fc2a370f456a7a63d94d3d440638a3585
Pulkit Kothari <v-pkothari@expedia.com>
2015-04-14 17:37:42 +0530

    Added PhoneLaunchFragment in Wotif

--- project/src/wotif/java/com/expedia/bookings/fragment/PhoneLaunchFragment.java
+++ project/src/wotif/java/com/expedia/bookings/fragment/PhoneLaunchFragment.java
@@ -1,13 +1,10 @@
 package com.expedia.bookings.fragment;
 
-import java.util.Random;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.expedia.bookings.R;
 import com.expedia.bookings.interfaces.IPhoneLaunchActivityLaunchFragment;
@@ -20,37 +17,12 @@ public class PhoneLaunchFragment extends Fragment implements IPhoneLaunchActivit
 
 	public static final String TAG = PhoneLaunchFragment.class.getName();
 	private boolean mLaunchingActivity = false;
-	private ImageView mSlidingImage;
-	private int mCurrentImageIndex = 0;
-
-	private static final Integer[] BACKGROUND_RES_IDS = new Integer[] {
-		R.drawable.bg_launch_tvly_sf,
-		R.drawable.bg_launch_tvly_las_vegas,
-		R.drawable.bg_launch_tvly_london,
-		R.drawable.bg_launch_tvly_paris,
-		R.drawable.bg_launch_tvly_swiss,
-		R.drawable.bg_launch_tvly_japan,
-		R.drawable.bg_launch_tvly_rome,
-		R.drawable.bg_launch_tvly_venice,
-		R.drawable.bg_launch_tvly_seattle,
-		R.drawable.bg_launch_tvly_ny,
-		R.drawable.bg_launch_tvly_chicago,
-		R.drawable.bg_launch_tvly_china,
-		R.drawable.bg_launch_tvly_dallas,
-		R.drawable.bg_launch_tvly_arizona,
-		R.drawable.bg_launch_tvly_maldives,
-	};
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		final View view = inflater.inflate(R.layout.fragment_phone_launch, container, false);
-		Ui.findView(view, R.id.tvly_flights).setOnClickListener(mHeaderItemOnClickListener);
-		Ui.findView(view, R.id.tvly_hotels).setOnClickListener(mHeaderItemOnClickListener);
-		mSlidingImage = Ui.findView(view, R.id.tvly_home_bg_view);
-
-		//Randomly select an image to display
-		mCurrentImageIndex = new Random().nextInt(BACKGROUND_RES_IDS.length);
-		mSlidingImage.setImageResource(BACKGROUND_RES_IDS[mCurrentImageIndex]);
+		Ui.findView(view, R.id.wotif_flights).setOnClickListener(mHeaderItemOnClickListener);
+		Ui.findView(view, R.id.wotif_hotels).setOnClickListener(mHeaderItemOnClickListener);
 		return view;
 	}
 
@@ -66,7 +38,7 @@ public class PhoneLaunchFragment extends Fragment implements IPhoneLaunchActivit
 			Bundle animOptions = AnimUtils.createActivityScaleBundle(v);
 
 			switch (v.getId()) {
-			case R.id.tvly_hotels: {
+			case R.id.wotif_hotels: {
 				if (!mLaunchingActivity) {
 					mLaunchingActivity = true;
 					NavUtils.goToHotels(getActivity(), animOptions);
@@ -74,7 +46,7 @@ public class PhoneLaunchFragment extends Fragment implements IPhoneLaunchActivit
 				}
 				break;
 			}
-			case R.id.tvly_flights: {
+			case R.id.wotif_flights: {
 				if (!mLaunchingActivity) {
 					mLaunchingActivity = true;
 					NavUtils.goToFlights(getActivity(), animOptions);
