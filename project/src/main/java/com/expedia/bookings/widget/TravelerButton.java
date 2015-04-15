f5ba1e85d3a99ca8929715e312f10a83f606e35c
Rajan Kochhar <rkochhar@expedia.com>
2015-04-15 10:28:54 +0530

    Add LX primary color drawable for traveler initials while selecting saved traveler.

--- project/src/main/java/com/expedia/bookings/widget/TravelerButton.java
+++ project/src/main/java/com/expedia/bookings/widget/TravelerButton.java
@@ -19,6 +19,7 @@ import com.expedia.bookings.fragment.SimpleSupportDialogFragment;
 import com.expedia.bookings.section.TravelerAutoCompleteAdapter;
 import com.expedia.bookings.server.ExpediaServices;
 import com.expedia.bookings.utils.TravelerUtils;
+import com.expedia.bookings.utils.Ui;
 import com.mobiata.android.BackgroundDownloader;
 import com.mobiata.android.Log;
 
@@ -67,7 +68,7 @@ public class TravelerButton extends LinearLayout {
 		LayoutInflater inflater = LayoutInflater.from(getContext());
 		inflater.inflate(R.layout.checkout_traveler_button, this);
 		ButterKnife.inject(this);
-		mTravelerAdapter = new TravelerAutoCompleteAdapter(getContext(), false, R.drawable.car_driver_checkout_circle);
+		mTravelerAdapter = new TravelerAutoCompleteAdapter(getContext(), false, Ui.obtainThemeResID(getContext(), R.attr.traveler_checkout_circle_drawable));
 		BackgroundDownloader dl = BackgroundDownloader.getInstance();
 		if (dl.isDownloading(getTravelerDownloadKey())) {
 			dl.registerDownloadCallback(getTravelerDownloadKey(), mTravelerDetailsCallback);
