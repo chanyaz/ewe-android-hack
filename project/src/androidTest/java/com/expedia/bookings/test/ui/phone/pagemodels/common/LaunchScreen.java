a876db7778ccb071467ab8a10387ef6e71756eb7
Brennan Taylor <brennan@mobiata.com>
2015-04-16 16:42:09 -0700

    Remove strings that no longer exist
    Was unused code anyways

--- project/src/androidTest/java/com/expedia/bookings/test/ui/phone/pagemodels/common/LaunchScreen.java
+++ project/src/androidTest/java/com/expedia/bookings/test/ui/phone/pagemodels/common/LaunchScreen.java
@@ -1,6 +1,5 @@
 package com.expedia.bookings.test.ui.phone.pagemodels.common;
 
-import android.content.res.Resources;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.matcher.ViewMatchers;
 
@@ -8,21 +7,12 @@ import com.expedia.bookings.R;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
 public class LaunchScreen extends LaunchActionBar {
-
-	public static ViewInteraction hotelLaunchWithContentDescription(Resources res) {
-		return onView(withContentDescription(res.getString(R.string.cd_launch_hotels)));
-	}
-
-	public static ViewInteraction flightLaunchWithContentDescription(Resources res) {
-		return onView(withContentDescription(res.getString(R.string.cd_launch_flights)));
-	}
 
 	public static ViewInteraction hotelLaunchButton() {
 		return onView(allOf(withId(R.id.hotels_button), withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
