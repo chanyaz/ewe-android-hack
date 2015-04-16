847b1f63f68513642ae6834e23f02c7a4fd40de4
Kevin Carpenter <kevc@mobiata.com>
2015-04-16 10:59:42 -0400

    Add visibility check to LOB button clicks.

--- project/src/androidTest/java/com/expedia/bookings/test/ui/phone/pagemodels/common/LaunchScreen.java
+++ project/src/androidTest/java/com/expedia/bookings/test/ui/phone/pagemodels/common/LaunchScreen.java
@@ -2,14 +2,17 @@ package com.expedia.bookings.test.ui.phone.pagemodels.common;
 
 import android.content.res.Resources;
 import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.matcher.ViewMatchers;
 
 import com.expedia.bookings.R;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 public class LaunchScreen extends LaunchActionBar {
 
@@ -22,15 +25,15 @@ public class LaunchScreen extends LaunchActionBar {
 	}
 
 	public static ViewInteraction hotelLaunchButton() {
-		return onView(withId(R.id.hotels_button));
+		return onView(allOf(withId(R.id.hotels_button), withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
 	}
 
 	public static ViewInteraction flightLaunchButton() {
-		return onView(withId(R.id.flights_button));
+		return onView(allOf(withId(R.id.flights_button), withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
 	}
 
 	public static ViewInteraction carLaunchButton() {
-		return onView(withId(R.id.cars_button));
+		return onView(allOf(withId(R.id.cars_button), withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
 	}
 
 	public static ViewInteraction tripsButton() {
