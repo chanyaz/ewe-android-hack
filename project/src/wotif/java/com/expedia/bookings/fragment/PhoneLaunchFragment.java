f820b5dd8bc1ffd8043cd7b48894139b324a1b00
Pulkit Kothari <v-pkothari@expedia.com>
2015-04-17 11:24:00 +0530

    Wotif action bar themes and assets updated

--- project/src/wotif/java/com/expedia/bookings/fragment/PhoneLaunchFragment.java
+++ project/src/wotif/java/com/expedia/bookings/fragment/PhoneLaunchFragment.java
@@ -2,6 +2,8 @@ package com.expedia.bookings.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +18,10 @@ public class PhoneLaunchFragment extends Fragment implements IPhoneLaunchActivit
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_phone_launch, container, false);
+
+		ActionBar actionBar = ((ActionBarActivity)getActivity()).getSupportActionBar();
+		actionBar.setIcon(R.drawable.ic_action_bar_logo);
+		actionBar.setDisplayShowHomeEnabled(true);
 		return view;
 	}
 
