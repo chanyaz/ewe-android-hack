9ec24f8b0223b09d1afb3e931261ee4af9ae985c
Malcolm Nguyen <malcolm@mobiata.com>
2015-05-04 22:13:10 -0700

    Hotel redesign

--- project/src/main/java/com/expedia/bookings/ui/HotelActivity.kt
+++ project/src/main/java/com/expedia/bookings/ui/HotelActivity.kt
@@ -1,18 +1,42 @@
 package com.expedia.bookings.ui
 
+import com.expedia.bookings.R
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.expedia.bookings.R
-import org.jetbrains.anko.frameLayout
-import org.jetbrains.anko.textView
+import android.view.View
+import android.support.v7.widget.Toolbar
+import android.view.ViewManager
+import android.widget.LinearLayout
+import android.widget.ToggleButton
+import org.joda.time.LocalDate
+import com.expedia.bookings.widget.AlwaysFilterAutoCompleteTextView
+import com.mobiata.android.time.widget.CalendarPicker
 
 public class HotelActivity : AppCompatActivity() {
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		frameLayout {
-			textView("Get yer hotels")
-		}
-	}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.hotel_search)
+        var calendar: CalendarPicker = findViewById(R.id.calendar) as CalendarPicker
+        var traveler: LinearLayout = findViewById(R.id.traveler_view) as LinearLayout
+        var toolbar: Toolbar = findViewById(R.id.toolbar) as Toolbar
+        var selectDate: ToggleButton = findViewById(R.id.select_date) as ToggleButton
+        var selectTraveler: ToggleButton = findViewById(R.id.select_traveler) as ToggleButton
 
+        toolbar.inflateMenu(R.menu.cars_search_menu)
+        calendar.setSelectableDateRange(LocalDate.now(), LocalDate.now().plusDays(getResources().getInteger(R.integer.calendar_max_selectable_date_range)))
+        calendar.setMaxSelectableDateRange(getResources().getInteger(R.integer.calendar_max_days_hotel_stay))
+
+        selectDate.setOnClickListener {
+            calendar.setVisibility(View.VISIBLE)
+            traveler.setVisibility(View.GONE)
+        }
+
+        selectTraveler.setOnClickListener {
+            calendar.setVisibility(View.GONE)
+            traveler.setVisibility(View.VISIBLE)
+        }
+
+    }
 }
+
