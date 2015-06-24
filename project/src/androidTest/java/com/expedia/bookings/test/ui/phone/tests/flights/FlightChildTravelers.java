1696c519ca3894e4716615ee36dba3cde62f3eb5
Supreeth <supreeth@mobiata.com>
2015-06-24 13:19:29 -0700

    511. Child travelers - Flights
    Adding ui automation checks for pricebreakdown and checkout traveler labels.

--- project/src/androidTest/java/com/expedia/bookings/test/ui/phone/tests/flights/FlightChildTravelers.java
+++ project/src/androidTest/java/com/expedia/bookings/test/ui/phone/tests/flights/FlightChildTravelers.java
@@ -1,5 +1,6 @@
 package com.expedia.bookings.test.ui.phone.tests.flights;
 
+import org.joda.time.LocalDate;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,9 +10,19 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.expedia.bookings.activity.FlightSearchActivity;
+import com.expedia.bookings.test.rules.ExpediaMockWebServerRule;
+import com.expedia.bookings.test.ui.phone.pagemodels.common.CommonCheckoutScreen;
 import com.expedia.bookings.test.ui.phone.pagemodels.common.ScreenActions;
+import com.expedia.bookings.test.ui.phone.pagemodels.flights.FlightLegScreen;
+import com.expedia.bookings.test.ui.phone.pagemodels.flights.FlightsSearchResultsScreen;
 import com.expedia.bookings.test.ui.phone.pagemodels.flights.FlightsSearchScreen;
+import com.expedia.bookings.test.ui.phone.pagemodels.flights.FlightsTravelerInfoScreen;
 import com.expedia.bookings.test.ui.phone.pagemodels.flights.FlightsTravelerPicker;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasSibling;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class FlightChildTravelers {
@@ -23,6 +34,9 @@ public class FlightChildTravelers {
 	//Launch directly into the About/Info Screen
 	@Rule
 	public ActivityTestRule<FlightSearchActivity> mRule = new ActivityTestRule<>(FlightSearchActivity.class);
+
+	@Rule
+	public final ExpediaMockWebServerRule server = new ExpediaMockWebServerRule();
 
 	private int adultCount = 1;
 	private int childCount = 0;
@@ -116,7 +130,7 @@ public class FlightChildTravelers {
 		FlightsTravelerPicker.isInfantAlertShown(false);
 		FlightsTravelerPicker.isInfantInSeatChecked(true);
 
-		// 1 Adult, 1 10yr, 1 infant = show alert
+		// 1 Adult, 1 10yr, 2 infant = show alert
 		// add 10yr old (childCount#2)
 		incrementChildCountBy(1);
 		// add infant
@@ -144,5 +158,86 @@ public class FlightChildTravelers {
 		FlightsTravelerPicker.isInfantAlertShown(false);
 
 		Espresso.pressBack();
+	}
+
+	@Test
+	public void testInfantLapPriceBreakdownCheckoutOverview() throws Throwable {
+		FlightsSearchScreen.clickPassengerSelectionButton();
+		incrementChildCountBy(1);
+		// Select less than one year old (infant)
+		FlightsTravelerPicker.selectChildAge(mRule.getActivity(), childCount, 0);
+		FlightsTravelerPicker.isInfantInLapChecked(true);
+
+		FlightsSearchScreen.enterDepartureAirport("LAX");
+		FlightsSearchScreen.enterArrivalAirport("SFO");
+		FlightsSearchScreen.clickSelectDepartureButton();
+		LocalDate startDate = LocalDate.now().plusDays(35);
+		FlightsSearchScreen.clickDate(startDate);
+		FlightsSearchScreen.clickSearchButton();
+		FlightsSearchResultsScreen.clickListItem(4);
+		FlightLegScreen.clickSelectFlightButton();
+
+		FlightLegScreen.clickCostBreakdownButtonView();
+		onView(withText("Infant in lap 1 details")).check(matches(hasSibling(withText("$0.00"))));
+		FlightLegScreen.clickCostBreakdownDoneButton();
+
+		CommonCheckoutScreen.clickCheckoutButton();
+		FlightsTravelerInfoScreen.assertEmptyTravelerDetailsLabel(0, "Adult details");
+		FlightsTravelerInfoScreen.assertEmptyTravelerDetailsLabel(2, "Infant in lap details");
+	}
+
+	@Test
+	public void testInfantSeatPriceBreakdownCheckoutOverview() throws Throwable {
+		FlightsSearchScreen.clickPassengerSelectionButton();
+		incrementChildCountBy(1);
+		// Select less than one year old (infant)
+		FlightsTravelerPicker.selectChildAge(mRule.getActivity(), childCount, 0);
+		FlightsTravelerPicker.isInfantInLapChecked(true);
+
+		FlightsSearchScreen.enterDepartureAirport("LAX");
+		FlightsSearchScreen.enterArrivalAirport("SFO");
+		FlightsSearchScreen.clickSelectDepartureButton();
+		LocalDate startDate = LocalDate.now().plusDays(35);
+		FlightsSearchScreen.clickDate(startDate);
+		FlightsSearchScreen.clickSearchButton();
+		FlightsSearchResultsScreen.clickListItem(6);
+		FlightLegScreen.clickSelectFlightButton();
+
+		FlightLegScreen.clickCostBreakdownButtonView();
+		onView(withText("Infant in seat 1 details")).check(matches(hasSibling(withText("$589.00"))));
+		FlightLegScreen.clickCostBreakdownDoneButton();
+
+		CommonCheckoutScreen.clickCheckoutButton();
+		FlightsTravelerInfoScreen.assertEmptyTravelerDetailsLabel(0, "Adult details");
+		FlightsTravelerInfoScreen.assertEmptyTravelerDetailsLabel(2, "Infant in seat details");
+	}
+
+	@Test
+	public void testChildInfantLapPriceBreakdownOverview() throws Throwable {
+		FlightsSearchScreen.clickPassengerSelectionButton();
+		incrementChildCountBy(1);
+		incrementChildCountBy(1);
+		// Select less than one year old (infant)
+		FlightsTravelerPicker.selectChildAge(mRule.getActivity(), childCount, 0);
+		FlightsTravelerPicker.isInfantInLapChecked(true);
+
+		FlightsSearchScreen.enterDepartureAirport("LAX");
+		FlightsSearchScreen.enterArrivalAirport("SFO");
+		FlightsSearchScreen.clickSelectDepartureButton();
+		LocalDate startDate = LocalDate.now().plusDays(35);
+		FlightsSearchScreen.clickDate(startDate);
+		FlightsSearchScreen.clickSearchButton();
+		FlightsSearchResultsScreen.clickListItem(5);
+		FlightLegScreen.clickSelectFlightButton();
+
+		FlightLegScreen.clickCostBreakdownButtonView();
+		onView(withText("Infant in lap 1 details")).check(matches(hasSibling(withText("$0.00"))));
+		onView(withText("Child 1 details")).check(matches(hasSibling(withText("$589.00"))));
+		FlightLegScreen.clickCostBreakdownDoneButton();
+
+		CommonCheckoutScreen.clickCheckoutButton();
+		FlightsTravelerInfoScreen.assertEmptyTravelerDetailsLabel(0, "Adult details");
+		FlightsTravelerInfoScreen.assertEmptyTravelerDetailsLabel(2, "10 yr old details");
+		FlightsTravelerInfoScreen.assertEmptyTravelerDetailsLabel(4, "Infant in lap details");
 	}
 }
