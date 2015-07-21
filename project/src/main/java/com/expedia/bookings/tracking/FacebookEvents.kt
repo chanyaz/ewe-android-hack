43b23738f1b30cd2f2f13c6c3a574b11b26b936c
Deepanshu Madan <dmadan@expedia.com>
2015-07-21 16:35:16 -0700

    Fix phone hotel and flight happy path tests

--- project/src/main/java/com/expedia/bookings/tracking/FacebookEvents.kt
+++ project/src/main/java/com/expedia/bookings/tracking/FacebookEvents.kt
@@ -23,122 +23,138 @@ class FacebookEvents(val context: Context?) {
     }
 
     fun trackHotelSearch(search: HotelSearch) {
-        val searchParams = search.getSearchParams()
-        val location = search.getSearchResponse().getProperty(0).getLocation()
+        if (logger != null) {
+            val searchParams = search.getSearchParams()
+            val location = search.getSearchResponse().getProperty(0).getLocation()
 
-        var parameters = Bundle();
-        addCommonHotelParams(parameters, searchParams, location)
-        parameters.putString("Search_String", searchParams.getQuery())
-        parameters.putString("AvgSearch_Value", calculateAverageRateHotels(search.getSearchResponse().getProperties()));
-        parameters.putInt("Num_Rooms", search.getSearchResponse().getProperties().size())
+            var parameters = Bundle();
+            addCommonHotelParams(parameters, searchParams, location)
+            parameters.putString("Search_String", searchParams.getQuery())
+            parameters.putString("AvgSearch_Value", calculateAverageRateHotels(search.getSearchResponse().getProperties()));
+            parameters.putInt("Num_Rooms", search.getSearchResponse().getProperties().size())
 
-        logger?.logEvent(AppEventsConstants.EVENT_NAME_SEARCHED, parameters);
+            logger?.logEvent(AppEventsConstants.EVENT_NAME_SEARCHED, parameters);
+        }
     }
 
     fun trackHotelInfosite(search: HotelSearch) {
-        val searchParams = search.getSearchParams()
-        val location = search.getSearchResponse().getProperty(0).getLocation()
-        val property = search.getSelectedProperty()
+        if (logger != null) {
+            val searchParams = search.getSearchParams()
+            val location = search.getSearchResponse().getProperty(0).getLocation()
+            val property = search.getSelectedProperty()
 
-        var parameters = Bundle();
-        addCommonHotelParams(parameters, searchParams, location)
-        parameters.putString("Room_Value", property.getLowestRate().getAverageRate().getFormattedMoney());
-        parameters.putInt("Num_Rooms", search.getSearchResponse().getProperties().size())
-        parameters.putString("Content_ID", property.getPropertyId());
-        parameters.putString("Currency", property.getLowestRate().getAverageRate().currencyCode);
-        parameters.putString("ContentType", "product");
+            var parameters = Bundle();
+            addCommonHotelParams(parameters, searchParams, location)
+            parameters.putString("Room_Value", property.getLowestRate().getAverageRate().getFormattedMoney());
+            parameters.putInt("Num_Rooms", search.getSearchResponse().getProperties().size())
+            parameters.putString("Content_ID", property.getPropertyId());
+            parameters.putString("Currency", property.getLowestRate().getAverageRate().currencyCode);
+            parameters.putString("ContentType", "product");
 
-        logger?.logEvent(AppEventsConstants.EVENT_NAME_VIEWED_CONTENT, parameters);
+            logger?.logEvent(AppEventsConstants.EVENT_NAME_VIEWED_CONTENT, parameters);
+        }
     }
 
     fun trackHotelCheckout(hotel: TripBucketItemHotel, rate: Rate) {
-        val searchParams = hotel.getHotelSearch().getSearchParams()
-        val property = hotel.getProperty()
-        val location = property.getLocation()
+        if (logger != null) {
+            val searchParams = hotel.getHotelSearch().getSearchParams()
+            val property = hotel.getProperty()
+            val location = property.getLocation()
 
-        var parameters = Bundle();
-        addCommonHotelParams(parameters, searchParams, location)
-        parameters.putString("Booking_Value", rate.getTotalAmountAfterTax().getFormattedMoney());
-        parameters.putString("Content_ID", property.getPropertyId());
-        parameters.putString("Currency", property.getLowestRate().getAverageRate().currencyCode);
-        parameters.putString("ContentType", "product");
+            var parameters = Bundle();
+            addCommonHotelParams(parameters, searchParams, location)
+            parameters.putString("Booking_Value", rate.getTotalAmountAfterTax().getFormattedMoney());
+            parameters.putString("Content_ID", property.getPropertyId());
+            parameters.putString("Currency", property.getLowestRate().getAverageRate().currencyCode);
+            parameters.putString("ContentType", "product");
 
-        logger?.logEvent(AppEventsConstants.EVENT_NAME_ADDED_TO_CART, parameters);
+            logger?.logEvent(AppEventsConstants.EVENT_NAME_ADDED_TO_CART, parameters);
+        }
     }
 
     fun trackHotelConfirmation(hotel: TripBucketItemHotel, rate: Rate) {
-        val searchParams = hotel.getHotelSearch().getSearchParams()
-        val property = hotel.getProperty()
-        val location = property.getLocation()
+        if (logger != null) {
+            val searchParams = hotel.getHotelSearch().getSearchParams()
+            val property = hotel.getProperty()
+            val location = property.getLocation()
 
-        var parameters = Bundle();
-        addCommonHotelParams(parameters, searchParams, location)
-        parameters.putString("Booking_Value", rate.getTotalAmountAfterTax().getFormattedMoney());
-        parameters.putString("Content_ID", property.getPropertyId());
-        parameters.putString("Currency", property.getLowestRate().getAverageRate().currencyCode);
-        parameters.putString("ContentType", "product");
+            var parameters = Bundle();
+            addCommonHotelParams(parameters, searchParams, location)
+            parameters.putString("Booking_Value", rate.getTotalAmountAfterTax().getFormattedMoney());
+            parameters.putString("Content_ID", property.getPropertyId());
+            parameters.putString("Currency", property.getLowestRate().getAverageRate().currencyCode);
+            parameters.putString("ContentType", "product");
 
-        logger?.logEvent(AppEventsConstants.EVENT_NAME_PURCHASED, parameters);
+            logger?.logEvent(AppEventsConstants.EVENT_NAME_PURCHASED, parameters);
+        }
     }
 
     fun trackFlightSearch(search: FlightSearch) {
-        val searchParams = search.getSearchParams()
-        val location = searchParams.getArrivalLocation()
-        val destinationAirport = searchParams.getArrivalLocation().getDestinationId()
+        if (logger != null) {
+            val searchParams = search.getSearchParams()
+            val location = searchParams.getArrivalLocation()
+            val destinationAirport = searchParams.getArrivalLocation().getDestinationId()
 
-        var parameters = Bundle();
-        addCommonFlightParams(parameters, searchParams, location)
-        parameters.putString("Search_String", destinationAirport)
-        parameters.putString("AvgSearch_Value", calculateAverageRateFlights(search.getSearchResponse().getTrips()));
+            var parameters = Bundle();
+            addCommonFlightParams(parameters, searchParams, location)
+            parameters.putString("Search_String", destinationAirport)
+            parameters.putString("AvgSearch_Value", calculateAverageRateFlights(search.getSearchResponse().getTrips()));
 
-        logger?.logEvent(AppEventsConstants.EVENT_NAME_SEARCHED, parameters);
+            logger?.logEvent(AppEventsConstants.EVENT_NAME_SEARCHED, parameters);
+        }
     }
 
     fun trackFlightDetail(search: FlightSearch, flightTrip: FlightTrip) {
-        val searchParams = search.getSearchParams()
-        val location = searchParams.getArrivalLocation()
-        val money = flightTrip.getTotalFare()
+        if (logger != null) {
+            val searchParams = search.getSearchParams()
+            val location = searchParams.getArrivalLocation()
+            val money = flightTrip.getTotalFare()
 
-        var parameters = Bundle();
-        addCommonFlightParams(parameters, searchParams, location)
-        parameters.putString("Flight_Value", money.getFormattedMoney());
-        parameters.putString("Content_ID", flightTrip.getLegs().get(0).getFirstAirlineCode());
-        parameters.putString("Currency", money.getCurrency());
-        parameters.putString("ContentType", "product");
+            var parameters = Bundle();
+            addCommonFlightParams(parameters, searchParams, location)
+            parameters.putString("Flight_Value", money.getFormattedMoney());
+            parameters.putString("Content_ID", flightTrip.getLegs().get(0).getFirstAirlineCode());
+            parameters.putString("Currency", money.getCurrency());
+            parameters.putString("ContentType", "product");
 
-        logger?.logEvent(AppEventsConstants.EVENT_NAME_VIEWED_CONTENT, parameters);
+            logger?.logEvent(AppEventsConstants.EVENT_NAME_VIEWED_CONTENT, parameters);
+        }
     }
 
     fun trackFlightCheckout(flight: TripBucketItemFlight) {
-        val searchParams = flight.getFlightSearchParams()
-        val location = searchParams.getArrivalLocation()
-        val flightTrip = flight.getFlightTrip()
-        val money = flightTrip.getTotalFare()
+        if (logger != null) {
+            val searchParams = flight.getFlightSearchParams()
+            val location = searchParams.getArrivalLocation()
+            val flightTrip = flight.getFlightTrip()
+            val money = flightTrip.getTotalFare()
 
-        var parameters = Bundle();
-        addCommonFlightParams(parameters, searchParams, location)
-        parameters.putString("Booking_Value", money.getFormattedMoney());
-        parameters.putString("Content_ID", flightTrip.getLegs().get(0).getFirstAirlineCode());
-        parameters.putString("Currency", money.getCurrency());
-        parameters.putString("ContentType", "product");
+            var parameters = Bundle();
+            addCommonFlightParams(parameters, searchParams, location)
+            parameters.putString("Booking_Value", money.getFormattedMoney());
+            parameters.putString("Content_ID", flightTrip.getLegs().get(0).getFirstAirlineCode());
+            parameters.putString("Currency", money.getCurrency());
+            parameters.putString("ContentType", "product");
 
-        logger?.logEvent(AppEventsConstants.EVENT_NAME_ADDED_TO_CART, parameters);
+            logger?.logEvent(AppEventsConstants.EVENT_NAME_ADDED_TO_CART, parameters);
+        }
     }
 
     fun trackFlightConfirmation(flight: TripBucketItemFlight) {
-        val searchParams = flight.getFlightSearchParams()
-        val location = searchParams.getArrivalLocation()
-        val flightTrip = flight.getFlightTrip()
-        val money = flightTrip.getTotalFare()
+        if (logger != null) {
+            val searchParams = flight.getFlightSearchParams()
+            val location = searchParams.getArrivalLocation()
+            val flightTrip = flight.getFlightTrip()
+            val money = flightTrip.getTotalFare()
 
-        var parameters = Bundle();
-        addCommonFlightParams(parameters, searchParams, location)
-        parameters.putString("Booking_Value", money.getFormattedMoney());
-        parameters.putString("Content_ID", flightTrip.getLegs().get(0).getFirstAirlineCode());
-        parameters.putString("Currency", money.getCurrency());
-        parameters.putString("ContentType", "product");
+            var parameters = Bundle();
+            addCommonFlightParams(parameters, searchParams, location)
+            parameters.putString("Booking_Value", money.getFormattedMoney());
+            parameters.putString("Content_ID", flightTrip.getLegs().get(0).getFirstAirlineCode());
+            parameters.putString("Currency", money.getCurrency());
+            parameters.putString("ContentType", "product");
 
-        logger?.logEvent(AppEventsConstants.EVENT_NAME_PURCHASED, parameters);
+            logger?.logEvent(AppEventsConstants.EVENT_NAME_PURCHASED, parameters);
+        }
     }
 
     private fun getBookingWindow(time: LocalDate): Int {
