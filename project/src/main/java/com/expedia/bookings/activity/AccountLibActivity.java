fc96c69a4c213f35a139207602f0bd9d0082c3b2
Malcolm Nguyen <malcolm@mobiata.com>
2015-07-22 10:32:03 -0700

    delay activity finish by 300ms

--- project/src/main/java/com/expedia/bookings/activity/AccountLibActivity.java
+++ project/src/main/java/com/expedia/bookings/activity/AccountLibActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
@@ -259,12 +260,24 @@ public class AccountLibActivity extends AppCompatActivity
 			}
 		}
 
-		finish();
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				finish();
+			}
+		}, 300);
+
+
 	}
 
 	@Override
 	public void loginExtenderWorkComplete(LoginExtender extender) {
-		finish();
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				finish();
+			}
+		}, 300);
 	}
 
 	@Override
