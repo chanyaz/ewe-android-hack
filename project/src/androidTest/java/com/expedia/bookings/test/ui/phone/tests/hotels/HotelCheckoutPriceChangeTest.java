72d8bffc12e99d72dde518444090d4a684938cf0
girija <girija16@gmail.com>
2015-08-19 14:50:15 -0700

    Rename test file name to make it run with fork

--- project/src/androidTest/java/com/expedia/bookings/test/ui/phone/tests/hotels/HotelCheckoutPriceChangeTest.java
+++ project/src/androidTest/java/com/expedia/bookings/test/ui/phone/tests/hotels/HotelCheckoutPriceChangeTest.java
@@ -17,9 +17,9 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.containsString;
 
-public class HotelCheckoutPriceChangeTests extends PhoneTestCase {
+public class HotelCheckoutPriceChangeTest extends PhoneTestCase {
 
-	private static final String TAG = HotelCheckoutPriceChangeTests.class.getSimpleName();
+	private static final String TAG = HotelCheckoutPriceChangeTest.class.getSimpleName();
 
 	public void testCheckHotels() throws Exception {
 		LaunchScreen.launchHotels();
