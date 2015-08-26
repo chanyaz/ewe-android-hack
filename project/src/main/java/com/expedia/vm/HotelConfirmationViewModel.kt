cfcdd66314025793370e630578e9bf839d0d39ef
Chuning Luo <chuluo@expedia.com>
2015-08-26 16:49:04 -0700

    Material Hotel Confirmation Page
    a little refactoring
    
    Add actual viewModel for confirmationView
    
    remove unused imports

---
+++ project/src/main/java/com/expedia/vm/HotelConfirmationViewModel.kt
@@ -0,0 +1,158 @@
+package com.expedia.vm
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import com.expedia.bookings.R
+import com.expedia.bookings.data.Db
+import com.expedia.bookings.data.Location
+import com.expedia.bookings.data.Property
+import com.expedia.bookings.data.cars.CarSearchParamsBuilder
+import com.expedia.bookings.services.HotelCheckoutResponse
+import com.expedia.bookings.utils.AddToCalendarUtils
+import com.expedia.bookings.utils.DateFormatUtils
+import com.expedia.bookings.utils.NavUtils
+import org.joda.time.LocalDate
+import org.joda.time.format.DateTimeFormat
+import rx.Observable
+import rx.Observer
+import rx.exceptions.OnErrorNotImplementedException
+import rx.subjects.BehaviorSubject
+
+public class HotelConfirmationViewModel(checkoutResponseObservable: Observable<HotelCheckoutResponse>, context: Context) {
+
+    // output
+    val itineraryNumber = BehaviorSubject.create<String>()
+    val itineraryNumberLabel = BehaviorSubject.create<String>()
+    val formattedCheckInOutDate = BehaviorSubject.create<String>()
+    val checkInDate = BehaviorSubject.create<LocalDate>()
+    val checkOutDate = BehaviorSubject.create<LocalDate>()
+    val bigImageUrl = BehaviorSubject.create<String>()
+    val hotelName = BehaviorSubject.create<String>()
+    val addressLineOne = BehaviorSubject.create<String>()
+    val addressLineTwo = BehaviorSubject.create<String>()
+    val hotelCity = BehaviorSubject.create<String>()
+    val addCarBtnText = BehaviorSubject.create<String>()
+    val addFlightBtnText = BehaviorSubject.create<String>()
+    val customerEmail = BehaviorSubject.create<String>()
+    val hotelLocation = BehaviorSubject.create<Location>()
+
+    val dtf = DateTimeFormat.forPattern("yyyy-MM-dd")
+
+    init {
+        checkoutResponseObservable.subscribe({hotelCheckoutResponse ->
+            val product = hotelCheckoutResponse.checkoutResponse.productResponse
+            val itinNumber = hotelCheckoutResponse.checkoutResponse.bookingResponse.itineraryNumber
+            val checkInLocalDate = dtf.parseLocalDate(product.checkInDate)
+            val checkOutLocalDate = dtf.parseLocalDate(product.checkOutDate)
+            val location = Location()
+
+            itineraryNumber.onNext(itinNumber)
+            itineraryNumberLabel.onNext(context.getResources().getString(com.expedia.bookings.R.string.successful_checkout_TEMPLATE, itinNumber))
+            checkInDate.onNext(checkInLocalDate)
+            checkOutDate.onNext(checkOutLocalDate)
+            formattedCheckInOutDate.onNext(DateFormatUtils.formatDateRange(context, checkInLocalDate, checkOutLocalDate, DateFormatUtils.FLAGS_DATE_ABBREV_MONTH))
+            bigImageUrl.onNext("https://media.expedia.com" + product.bigImageUrl)
+            hotelName.onNext(product.localizedHotelName)
+            addressLineOne.onNext(product.hotelAddress)
+            addressLineTwo.onNext(context.getResources().getString(R.string.stay_summary_TEMPLATE, product.hotelCity, product.hotelStateProvince))
+            hotelCity.onNext(product.hotelCity)
+            addCarBtnText.onNext(context.getResources().getString(com.expedia.bookings.R.string.rent_a_car_TEMPLATE, product.hotelCity))
+            addFlightBtnText.onNext(context.getResources().getString(com.expedia.bookings.R.string.flights_to_TEMPLATE, product.hotelCity))
+            customerEmail.onNext(hotelCheckoutResponse.checkoutResponse.bookingResponse.email)
+
+            location.setCity(product.hotelCity)
+            location.setCountryCode(product.hotelCountry)
+            location.setStateCode(product.hotelStateProvince)
+            location.addStreetAddressLine(product.hotelAddress)
+            hotelLocation.onNext(location)
+        })
+    }
+
+    fun getAddFlightBtnObserver(context: Context): Observer<Unit> {
+        return object: Observer<Unit> {
+            override fun onNext(t: Unit?) {
+                val flightSearchParams = Db.getFlightSearch().getSearchParams()
+                flightSearchParams.reset()
+                val loc = Location()
+                loc.setDestinationId(hotelLocation.getValue().toShortFormattedString())
+                flightSearchParams.setArrivalLocation(loc)
+                flightSearchParams.setDepartureDate(checkInDate.getValue())
+                flightSearchParams.setReturnDate(checkOutDate.getValue())
+
+                NavUtils.goToFlights(context, true)
+            }
+
+            override fun onCompleted() {
+            }
+
+            override fun onError(e: Throwable?) {
+                throw OnErrorNotImplementedException(e)
+            }
+        }
+    }
+
+    fun getAddCarBtnObserver(context: Context): Observer<Unit> {
+        return object: Observer<Unit> {
+            override fun onNext(t: Unit?) {
+                val builder = CarSearchParamsBuilder()
+                val dateTimeBuilder = CarSearchParamsBuilder.DateTimeBuilder().startDate(checkInDate.getValue()).endDate(checkOutDate.getValue())
+                builder.origin(hotelLocation.getValue().toShortFormattedString())
+                builder.originDescription(hotelLocation.getValue().toShortFormattedString())
+                builder.dateTimeBuilder(dateTimeBuilder)
+                val carSearchParams = builder.build()
+
+                NavUtils.goToCars(context, null, carSearchParams, NavUtils.FLAG_OPEN_SEARCH)
+            }
+
+            override fun onCompleted() {
+            }
+
+            override fun onError(e: Throwable?) {
+                throw OnErrorNotImplementedException(e)
+            }
+        }
+    }
+
+    fun getAddToCalendarBtnObserver(context: Context): Observer<Unit> {
+        return object: Observer<Unit> {
+            override fun onNext(t: Unit?) {
+                // Go in reverse order, so that "check in" is shown to the user first
+                context.startActivity(generateHotelCalendarIntent(false))
+                context.startActivity(generateHotelCalendarIntent(true))
+            }
+
+            override fun onCompleted() {
+            }
+
+            override fun onError(e: Throwable?) {
+                throw OnErrorNotImplementedException(e)
+            }
+
+            private fun generateHotelCalendarIntent(checkIn: Boolean): Intent {
+                val property = Property()
+                property.setName(hotelName.getValue())
+                property.setLocation(hotelLocation.getValue())
+                val date = if (checkIn) checkInDate.getValue() else checkOutDate.getValue()
+
+                return AddToCalendarUtils.generateHotelAddToCalendarIntent(context, property, date, checkIn, null, itineraryNumber.getValue())
+            }
+        }
+    }
+
+    fun getDirectionsToHotelBtnObserver(context: Context): Observer<Unit> {
+        return object: Observer<Unit> {
+            override fun onNext(t: Unit?) {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://maps.google.com/maps?daddr=" + hotelLocation.getValue().toLongFormattedString()))
+                context.startActivity(intent)
+            }
+
+            override fun onCompleted() {
+            }
+
+            override fun onError(e: Throwable?) {
+                throw OnErrorNotImplementedException(e)
+            }
+        }
+    }
+}
