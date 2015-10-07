c284c26db3f99d80a1c15045c0fa93fc2d922c96
Pulkit Kothari <pkothari@expedia.com>
2015-10-07 12:29:44 +0530

    4995: Rating progress loading implemented

--- project/src/main/java/com/expedia/bookings/widget/HotelReviewsAdapter.kt
+++ project/src/main/java/com/expedia/bookings/widget/HotelReviewsAdapter.kt
@@ -5,13 +5,12 @@ import android.support.v4.view.PagerAdapter
 import android.support.v4.view.ViewPager
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TableLayout
 import com.expedia.bookings.R
 import com.expedia.bookings.data.hotels.HotelReviewsResponse.Review
 import com.expedia.bookings.data.hotels.ReviewSort
-import com.expedia.vm.HotelReviewsAdapterViewModel
 import com.expedia.vm.HotelReviewRowViewModel
+import com.expedia.vm.HotelReviewsAdapterViewModel
+import com.expedia.vm.HotelReviewsPageViewModel
 import com.expedia.vm.HotelReviewsSummaryViewModel
 
 public class HotelReviewsAdapter(val context: Context, val viewPager: ViewPager, val vm: HotelReviewsAdapterViewModel) : PagerAdapter() {
@@ -46,6 +45,9 @@ public class HotelReviewsAdapter(val context: Context, val viewPager: ViewPager,
 
     private fun addReviews(reviewSort: ReviewSort, reviews: List<Review>) {
         val hotelReviewsView = viewPager.findViewWithTag(reviewSort) as HotelReviewsPageView
+        hotelReviewsView.viewModel = HotelReviewsPageViewModel()
+        hotelReviewsView.viewModel.reviewObserver.onNext(reviews)
+
         val reviewsTable = hotelReviewsView.reviewsTable
         reviews.forEachIndexed { index, review ->
             val hotelReviewRowViewModel = HotelReviewRowViewModel(context)
@@ -79,7 +81,7 @@ public class HotelReviewsAdapter(val context: Context, val viewPager: ViewPager,
 
     override fun instantiateItem(container: ViewGroup?, position: Int): Any? {
         val hotelReviewsView = HotelReviewsPageView(context)
-        hotelReviewsView.addOnScrollListener { scrollView, x1, y1, x2, y2 ->
+        hotelReviewsView.reviewsScrollviewContainer.addOnScrollListener { scrollView, x1, y1, x2, y2 ->
             val view = scrollView.getChildAt(scrollView.getChildCount() - 1)
             val diff = (view.getBottom() - (scrollView.getHeight() + scrollView.getScrollY()));
             if ( diff == 0 ) {
