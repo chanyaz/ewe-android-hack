4c68fbdd5102d6817036c33ab2b0a42c2b16ce10
Deepanshu Madan <dmadan@expedia.com>
2015-10-20 16:04:53 -0700

    s/5306 Stars to Circles for AU and NZ

--- project/src/main/java/com/expedia/bookings/extension/hotel_extension.kt
+++ project/src/main/java/com/expedia/bookings/extension/hotel_extension.kt
@@ -6,3 +6,7 @@ import com.expedia.bookings.data.pos.PointOfSale
 public fun HotelRate.isAirAttached(): Boolean {
     return airAttached && PointOfSale.getPointOfSale().showHotelCrossSell() && isDiscountTenPercentOrBetter()
 }
+
+public fun shouldShowCircleForRatings(): Boolean {
+    return PointOfSale.getPointOfSale().shouldShowCircleForRatings()
+}
