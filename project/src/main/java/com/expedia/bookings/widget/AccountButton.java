8615a13bda1f562c20eff6ebd0fa782961430cca
aadhikari <v-aadhikari@expedia.com>
2015-11-27 14:13:22 +0530

    TVLY & Wotif: Sign in btn logo brand break on chckout screen fix

--- project/src/main/java/com/expedia/bookings/widget/AccountButton.java
+++ project/src/main/java/com/expedia/bookings/widget/AccountButton.java
@@ -147,6 +147,7 @@ public class AccountButton extends LinearLayout {
 	// Do some runtime styling, based on whether this is tablet or a white-labelled app
 	private void bindLoginContainer(LineOfBusiness lob) {
 		boolean isTablet = AndroidUtils.isTablet(getContext());
+		mLoginTextView.setCompoundDrawablesWithIntrinsicBounds(Ui.obtainThemeResID(mContext, R.attr.skin_phoneCheckoutLoginLogoDrawable), 0, 0, 0);
 
 		if (isTablet) {
 			LayoutParams lp = (LayoutParams) mLoginContainer.getLayoutParams();
@@ -176,7 +177,6 @@ public class AccountButton extends LinearLayout {
 				lpt.gravity = Gravity.CENTER_VERTICAL | Gravity.LEFT;
 				mLoginContainer.setBackgroundResource(R.drawable.card_background);
 				mLoginTextView.setTextColor(getResources().getColor(R.color.cars_lx_checkout_button_text_color));
-				mLoginTextView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.expedia, 0, 0, 0);
 				mLoginTextView.setBackgroundResource(bgResourceId);
 			}
 			mLoginTextView.setGravity(Gravity.LEFT);
