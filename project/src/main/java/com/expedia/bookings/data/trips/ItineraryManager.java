0acb2d0660fcb376b78f07ef65de49bee71cd31f
Malcolm Nguyen <malcolm@mobiata.com>
2015-12-04 12:45:31 -0800

    Fix cars api error by sending a valid version code Fix Itin NPE

--- project/src/main/java/com/expedia/bookings/data/trips/ItineraryManager.java
+++ project/src/main/java/com/expedia/bookings/data/trips/ItineraryManager.java
@@ -1348,15 +1348,18 @@ public class ItineraryManager implements JSONable {
 				// make request to the /me API
 				Response rep = Request.newMeRequest(session, null).executeAndWait();
 				GraphUser user = rep.getGraphObjectAs(GraphUser.class);
-				String fbUserId = user.getId();
+				if (user != null) {
+					String fbUserId = user.getId();
 
-				Session fbSession = Session.getActiveSession();
-				if (fbSession != null && !fbSession.isClosed()) {
-					FacebookLinkResponse linkResponse = mServices
-						.facebookAutoLogin(fbUserId, fbSession.getAccessToken());
-					if (linkResponse != null && linkResponse.getFacebookLinkResponseCode() != null && linkResponse
-						.isSuccess()) {
-						Log.w(LOGGING_TAG, "FB: Autologin success" + linkResponse.getFacebookLinkResponseCode().name());
+					Session fbSession = Session.getActiveSession();
+					if (fbSession != null && !fbSession.isClosed()) {
+						FacebookLinkResponse linkResponse = mServices
+							.facebookAutoLogin(fbUserId, fbSession.getAccessToken());
+						if (linkResponse != null && linkResponse.getFacebookLinkResponseCode() != null && linkResponse
+							.isSuccess()) {
+							Log.w(LOGGING_TAG,
+								"FB: Autologin success" + linkResponse.getFacebookLinkResponseCode().name());
+						}
 					}
 				}
 			}
@@ -1404,7 +1407,7 @@ public class ItineraryManager implements JSONable {
 				if (response == null || response.hasErrors()) {
 					if (response != null && response.hasErrors()) {
 						Log.w(LOGGING_TAG, "Error updating trip " + trip.getItineraryKey() + ": "
-								+ response.gatherErrorMessage(mContext));
+							+ response.gatherErrorMessage(mContext));
 
 						// If it's a guest trip, and we've never retrieved info on it, it may be invalid.
 						// As such, we should remove it (but don't remove a trip if it's ever been loaded
@@ -1811,7 +1814,8 @@ public class ItineraryManager implements JSONable {
 				boolean showAirAttach = itinCardDataFlight.showAirAttach();
 				// Check if user qualifies for air attach
 				if (isUserAirAttachQualified && showAirAttach) {
-					return HotelCrossSellUtils.generateHotelSearchParamsFromItinData((TripFlight) itinCardDataFlight.getTripComponent(),
+					return HotelCrossSellUtils.generateHotelSearchParamsFromItinData(
+						(TripFlight) itinCardDataFlight.getTripComponent(),
 						itinCardDataFlight.getFlightLeg(), itinCardDataFlight.getNextFlightLeg());
 				}
 			}
@@ -1896,7 +1900,8 @@ public class ItineraryManager implements JSONable {
 						notification = existing;
 					}
 
-					String time = com.expedia.bookings.utils.DateUtils.convertMilliSecondsForLogging(notification.getTriggerTimeMillis());
+					String time = com.expedia.bookings.utils.DateUtils.convertMilliSecondsForLogging(
+						notification.getTriggerTimeMillis());
 					Log.i(LOGGING_TAG, "Notification scheduled for " + time);
 
 					// This is just to get the notifications to show up frequently for development
