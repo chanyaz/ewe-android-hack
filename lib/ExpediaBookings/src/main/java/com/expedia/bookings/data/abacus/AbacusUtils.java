223d506a1973fe928cc730313320ba15d1f5cd9b
Chuning Luo <chuluo@expedia.com>
2015-12-18 11:30:06 -0800

    s/5934 Hotel checkout postal code AB test

--- lib/ExpediaBookings/src/main/java/com/expedia/bookings/data/abacus/AbacusUtils.java
+++ lib/ExpediaBookings/src/main/java/com/expedia/bookings/data/abacus/AbacusUtils.java
@@ -33,7 +33,7 @@ public class AbacusUtils {
 	public static final int EBAndroidAppHotelRecentSearchTest = 8692;
 	public static final int EBAndroidAppLXFirstActivityListingExpanded = 9467;
 	public static final int EBAndroidAppHotelResultMapTest = 9474;
-
+	public static final int EBAndroidAppHotelCKOPostalCodeTest = 9480;
 
 	public static final int ABTEST_IGNORE_DEBUG = -1;
 
@@ -91,6 +91,7 @@ public class AbacusUtils {
 		testIDs.add(EBAndroidAppHotelRecentSearchTest);
 		testIDs.add(EBAndroidAppLXFirstActivityListingExpanded);
 		testIDs.add(EBAndroidAppHotelResultMapTest);
+		testIDs.add(EBAndroidAppHotelCKOPostalCodeTest);
 		return testIDs;
 	}
 
