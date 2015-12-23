50032278a96d1b94263d59a8a556aa1776d314a8
Mohit <mohitsharmasjsu@gmail.com>
2015-12-22 16:08:09 -0800

    s/5724 hotel price breakdown AB test

--- project/src/main/java/com/expedia/bookings/widget/HotelCheckoutSummaryWidget.kt
+++ project/src/main/java/com/expedia/bookings/widget/HotelCheckoutSummaryWidget.kt
@@ -8,18 +8,23 @@ import android.graphics.drawable.Drawable
 import android.support.v4.content.ContextCompat
 import android.support.v7.graphics.Palette
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import com.expedia.bookings.BuildConfig
 import com.expedia.bookings.R
-import com.expedia.bookings.activity.ExpediaBookingApp
 import com.expedia.bookings.bitmaps.PicassoHelper
 import com.expedia.bookings.bitmaps.PicassoTarget
+import com.expedia.bookings.data.Db
 import com.expedia.bookings.data.HotelMedia
+import com.expedia.bookings.data.abacus.AbacusUtils
 import com.expedia.bookings.graphics.HeaderBitmapDrawable
 import com.expedia.bookings.tracking.HotelV2Tracking
 import com.expedia.bookings.utils.ColorBuilder
+import com.expedia.bookings.utils.FontCache
 import com.expedia.bookings.utils.Images
 import com.expedia.bookings.utils.bindView
 import com.expedia.util.subscribeText
@@ -53,6 +58,12 @@ public class HotelCheckoutSummaryWidget(context: Context, attrs: AttributeSet?, 
     val costSummary: LinearLayout by bindView(R.id.cost_summary)
     val priceChangeLayout: LinearLayout by bindView(R.id.price_change_container)
     val priceChange: android.widget.TextView by bindView(R.id.price_change_text)
+
+    val totalFeesContainer : RelativeLayout by bindView(R.id.total_fees_container)
+    val hotelFeesContainer : ViewGroup by bindView(R.id.hotel_fees_container)
+    val amountDueTodayContainer : ViewGroup by bindView(R.id.amount_due_today_container)
+    val dottedDivider : View by bindView(R.id.dotted_divider)
+
     val breakdown = HotelBreakDownView(context, null)
     val dialog: AlertDialog by lazy {
         val builder = AlertDialog.Builder(context)
@@ -65,6 +76,9 @@ public class HotelCheckoutSummaryWidget(context: Context, attrs: AttributeSet?, 
         })
         builder.create()
     }
+
+    val isUserBucketedForPriceBreakDownTest = Db.getAbacusResponse().isUserBucketedForTest(AbacusUtils.EBAndroidAppHotelPriceBreakDownTest)
+    val priceBreakDownTestVariate = Db.getAbacusResponse().variateForTest(AbacusUtils.EBAndroidAppHotelPriceBreakDownTest)
 
     init {
         setOrientation(LinearLayout.VERTICAL)
@@ -99,12 +113,48 @@ public class HotelCheckoutSummaryWidget(context: Context, attrs: AttributeSet?, 
         viewModel.isPayLaterOrResortCase.subscribeVisibility(totalWithTaxLabelWithInfoButton.compoundDrawables[2], false)
         viewModel.isPayLaterOrResortCase.subscribeVisibility(amountDueTodayLabel.compoundDrawables[2], true)
         viewModel.newDataObservable.subscribe {
-            amountDueTodayLabel.text = if(it.isDepositV2.value)
-                                            Phrase.from(getContext(), R.string.due_to_brand_today_today_TEMPLATE).put("brand", BuildConfig.brand).format()
-                                        else if (it.isPayLaterOrResortCase.value)
-                                            Phrase.from(getContext(), R.string.due_to_brand_today_TEMPLATE).put("brand", BuildConfig.brand).format()
-                                        else
-                                            resources.getString(R.string.total_with_tax)
+            resetViewsToInitialState()
+            if (isUserBucketedForPriceBreakDownTest) {
+                if ((it.isDepositV2.value || it.isResortCase.value)) {
+                    if (priceBreakDownTestVariate == AbacusUtils.HotelPriceBreakDownVariate.DUE_TODAY.ordinal) {
+                        FontCache.setTypeface(feesPaidLabel, FontCache.Font.ROBOTO_REGULAR)
+                        amountDueTodayLabel.text = context.resources.getString(R.string.car_cost_breakdown_due_today)
+                        changeTextSizeForTotalAndDueNow()
+                    } else if (priceBreakDownTestVariate == AbacusUtils.HotelPriceBreakDownVariate.TRIP_TOTAL.ordinal) {
+                        amountDueTodayLabel.text = context.resources.getString(R.string.old_room_rate_pay_later_due_now)
+                        feesPaidLabel.text = context.resources.getString(R.string.fees_due_at_hotel)
+                        costSummary.removeView(amountDueTodayContainer)
+                        costSummary.addView(amountDueTodayContainer, 0)
+                        costSummary.removeView(hotelFeesContainer)
+                        costSummary.addView(hotelFeesContainer, 1)
+                        costSummary.removeView(dottedDivider)
+                        costSummary.addView(dottedDivider, 2)
+                        costSummary.removeView(totalFeesContainer)
+                        costSummary.addView(totalFeesContainer, 3)
+                        dottedDivider.visibility = View.VISIBLE
+                        feesPaidLabel.setCompoundDrawables(null, null, null, null)
+                        amountDueTodayLabel.setTextColor(ContextCompat.getColor(context, R.color.hotels_primary_color))
+                        changeTextSizeForTotalAndDueNow()
+                        totalWithTaxLabelWithInfoButton.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14f)
+                        FontCache.setTypeface(amountDueTodayLabel, FontCache.Font.ROBOTO_REGULAR)
+                        FontCache.setTypeface(totalPriceWithTaxAndFees, FontCache.Font.ROBOTO_REGULAR)
+                        FontCache.setTypeface(feesPaidLabel, FontCache.Font.ROBOTO_REGULAR)
+
+                    }
+                } else {
+                    if (it.isPayLaterOrResortCase.value) {
+                        amountDueTodayLabel.text = Phrase.from(getContext(), R.string.due_to_brand_today_TEMPLATE).put("brand", BuildConfig.brand).format()
+                    } else
+                        amountDueTodayLabel.text = resources.getString(R.string.total_with_tax)
+                }
+            } else {
+                if (it.isDepositV2.value)
+                    amountDueTodayLabel.text = Phrase.from(getContext(), R.string.due_to_brand_today_today_TEMPLATE).put("brand", BuildConfig.brand).format()
+                else if (it.isPayLaterOrResortCase.value) {
+                    amountDueTodayLabel.text = Phrase.from(getContext(), R.string.due_to_brand_today_TEMPLATE).put("brand", BuildConfig.brand).format()
+                } else
+                    amountDueTodayLabel.text = resources.getString(R.string.total_with_tax)
+            }
         }
         viewModel.roomHeaderImage.subscribe {
             PicassoHelper.Builder(context)
@@ -115,6 +165,37 @@ public class HotelCheckoutSummaryWidget(context: Context, attrs: AttributeSet?, 
                     .load(HotelMedia(Images.getMediaHost() + it).getBestUrls(width/2))
         }
         breakdown.viewmodel = HotelBreakDownViewModel(context, viewModel)
+
+    }
+
+    private fun changeTextSizeForTotalAndDueNow() {
+        totalWithTaxLabelWithInfoButton.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f)
+        totalPriceWithTax.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f)
+        amountDueTodayLabel.setTextSize(TypedValue.COMPLEX_UNIT_SP, 11f)
+        totalPriceWithTaxAndFees.setTextSize(TypedValue.COMPLEX_UNIT_SP, 11f)
+    }
+
+    private fun resetViewsToInitialState() {
+        FontCache.setTypeface(feesPaidLabel, FontCache.Font.ROBOTO_MEDIUM)
+        FontCache.setTypeface(amountDueTodayLabel, FontCache.Font.ROBOTO_MEDIUM)
+        FontCache.setTypeface(totalPriceWithTaxAndFees, FontCache.Font.ROBOTO_MEDIUM)
+        FontCache.setTypeface(feesPaidLabel, FontCache.Font.ROBOTO_MEDIUM)
+        totalWithTaxLabelWithInfoButton.setTextSize(TypedValue.COMPLEX_UNIT_SP, 11f)
+        totalPriceWithTax.setTextSize(TypedValue.COMPLEX_UNIT_SP, 11f)
+        amountDueTodayLabel.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f)
+        totalPriceWithTaxAndFees.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f)
+        feesPaidLabel.setCompoundDrawablesWithIntrinsicBounds(ContextCompat.getDrawable(context, R.drawable.fees_paid), null, null, null)
+        costSummary.removeView(amountDueTodayContainer)
+        costSummary.addView(amountDueTodayContainer, 3)
+        costSummary.removeView(dottedDivider)
+        costSummary.addView(dottedDivider, 2)
+        costSummary.removeView(hotelFeesContainer)
+        costSummary.addView(hotelFeesContainer, 1)
+        costSummary.removeView(totalFeesContainer)
+        costSummary.addView(totalFeesContainer, 0)
+        amountDueTodayLabel.setTextColor(ContextCompat.getColor(context, R.color.hotelsv2_checkout_text_color))
+        dottedDivider.visibility = View.GONE
+        feesPaidLabel.text = context.resources.getString(R.string.fees_paid_at_hotel)
     }
 
     val picassoTarget = object : PicassoTarget() {
