f5dfe7743032d6718e8a0ecee8782e67440cab06
Mahak Swami <mswami@expedia.com>
2016-01-08 00:49:23 +0530

    Tests for #5952 - I Accept visibility issues

--- project/src/androidTest/java/com/expedia/bookings/test/phone/lx/LXCheckoutPresenterTest.java
+++ project/src/androidTest/java/com/expedia/bookings/test/phone/lx/LXCheckoutPresenterTest.java
@@ -1,14 +1,16 @@
 package com.expedia.bookings.test.phone.lx;
 
+import java.util.concurrent.TimeUnit;
 import org.joda.time.LocalDate;
 
 import android.support.test.espresso.contrib.RecyclerViewActions;
 
 import com.expedia.bookings.R;
+import com.expedia.bookings.data.pos.PointOfSaleId;
+import com.expedia.bookings.test.espresso.Common;
 import com.expedia.bookings.test.espresso.LxTestCase;
 import com.expedia.bookings.test.phone.pagemodels.common.CardInfoScreen;
 import com.expedia.bookings.test.phone.pagemodels.common.CheckoutViewModel;
-import com.expedia.bookings.test.espresso.Common;
 import com.expedia.bookings.utils.LXDataUtils;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -21,6 +23,8 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.expedia.bookings.test.espresso.ViewActions.waitFor;
+import static com.expedia.bookings.test.espresso.ViewActions.waitForViewToDisplay;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.not;
 
@@ -43,7 +47,8 @@ public class LXCheckoutPresenterTest extends LxTestCase {
 
 		final String ticketName = "2-Day";
 		LXInfositeScreen.selectOffer("2-Day New York Pass").perform(scrollTo(), click());
-		LXInfositeScreen.ticketAddButton(ticketName, "Adult").perform(scrollTo(), click());
+		LXInfositeScreen.ticketAddButton(ticketName, "Adult").perform(waitFor(isDisplayed(), 2L,
+			TimeUnit.SECONDS), scrollTo(), click());
 		LXInfositeScreen.ticketAddButton(ticketName, "Adult").perform(scrollTo(), click());
 		LXInfositeScreen.ticketAddButton(ticketName, "Child").perform(scrollTo(), click());
 		LXInfositeScreen.bookNowButton(ticketName).perform(scrollTo(), click());
@@ -96,5 +101,29 @@ public class LXCheckoutPresenterTest extends LxTestCase {
 		LXScreen.rulesWidgetPrivacyPolicy().check(matches(isDisplayed()));
 		LXScreen.rulesWidgetToolbar().check(matches(isDisplayed()));
 		LXScreen.rulesWidgetToolbar().check(matches(hasDescendant(withText(R.string.legal_information))));
+	}
+
+	public void testAcceptTermsVisibilityHappyPathGuestUser() throws Throwable {
+		Common.setPOS(PointOfSaleId.FRANCE);
+		goToCheckout();
+		screenshot("Checkout_Payment_Card");
+		CheckoutViewModel.enterTravelerInfo();
+		CheckoutViewModel.enterPaymentInfo();
+		CheckoutViewModel.clickDone();
+		LXScreen.acceptTermsWidget().check(matches(isDisplayed()));
+	}
+
+	public void testAcceptTermsVisibilityOnBack() throws Throwable {
+		Common.setPOS(PointOfSaleId.FRANCE);
+		goToCheckout();
+		screenshot("Checkout_Payment_Card");
+		CheckoutViewModel.enterTravelerInfo();
+		CheckoutViewModel.enterPaymentInfo();
+		CheckoutViewModel.clickDone();
+		Common.pressBack();
+		LXInfositeScreen.bookNowButton("2-Day").perform(scrollTo(), click());
+		CheckoutViewModel.driverInfo().perform(waitForViewToDisplay());
+		LXScreen.acceptTermsWidget().check(matches(not(isDisplayed())));
+
 	}
 }
