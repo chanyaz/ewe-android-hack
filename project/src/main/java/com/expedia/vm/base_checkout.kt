6c573a59121870319ea186eb0fa66495b6815f70
Malcolm Nguyen <malcolm@mobiata.com>
2016-01-22 16:33:32 -0800

    Show resort fees after getting createtrip

--- project/src/main/java/com/expedia/vm/base_checkout.kt
+++ project/src/main/java/com/expedia/vm/base_checkout.kt
@@ -1,14 +1,19 @@
 package com.expedia.vm
 
 import android.content.Context
+import android.text.Html
 import android.text.SpannableStringBuilder
 import android.text.Spanned
+import com.expedia.bookings.R
 import com.expedia.bookings.data.BillingInfo
 import com.expedia.bookings.data.LineOfBusiness
+import com.expedia.bookings.data.Money
 import com.expedia.bookings.data.Traveler
 import com.expedia.bookings.data.ValidPayment
+import com.expedia.bookings.data.packages.PackageCreateTripResponse
 import rx.subjects.BehaviorSubject
 import rx.subjects.PublishSubject
+import java.math.BigDecimal
 import java.util.ArrayList
 import kotlin.collections.isNotEmpty
 
@@ -16,20 +21,30 @@ public class BaseCheckoutViewModel(val context: Context) {
     val builder = BaseCheckoutParams.Builder()
 
     // Inputs
-    val validPayments = PublishSubject.create<List<ValidPayment>>()
     val lineOfBusiness = BehaviorSubject.create<LineOfBusiness>()
     val creditCardRequired = PublishSubject.create<Boolean>()
     val travelerCompleted = BehaviorSubject.create<Traveler>()
     val paymentCompleted = BehaviorSubject.create<BillingInfo>()
     val cvvCompleted = BehaviorSubject.create<String>()
-    val legalText = PublishSubject.create<SpannableStringBuilder>()
-    val depositPolicyText = PublishSubject.create<Spanned>()
+    val packageTripResponse = PublishSubject.create<PackageCreateTripResponse>()
 
     // Outputs
+    val depositPolicyText = PublishSubject.create<Spanned>()
+    val legalText = PublishSubject.create<SpannableStringBuilder>()
     val infoCompleted = PublishSubject.create<Boolean>()
     val checkout = PublishSubject.create<BaseCheckoutParams>()
 
     init {
+        packageTripResponse.subscribe {
+            val hotelRate = it.packageDetails.hotel.hotelRoomResponse.rateInfo.chargeableRateInfo
+            var depositText = Html.fromHtml("")
+            if (hotelRate.showResortFeeMessage) {
+                val resortFees = Money(BigDecimal(hotelRate.totalMandatoryFees.toDouble()), hotelRate.currencyCode).formattedMoney
+                depositText = Html.fromHtml(context.getString(R.string.resort_fee_disclaimer_TEMPLATE, resortFees, it.packageDetails.pricing.packageTotal));
+            }
+            depositPolicyText.onNext(depositText)
+        }
+
         travelerCompleted.subscribe {
             builder.travelers(it)
             infoCompleted.onNext(builder.hasValidTravelerAndBillingInfo())
