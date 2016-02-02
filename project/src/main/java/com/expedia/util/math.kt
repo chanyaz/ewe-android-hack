00d7a8f01cb3f59bab9adeed54ed998148eb3da9
Jonathan Garnaas-Holmes <jonathan@mobiata.com>
2016-02-02 15:13:11 -0800

    6170 Search screen animates nicely from overview to item search

---
+++ project/src/main/java/com/expedia/util/math.kt
@@ -0,0 +1,10 @@
+package com.expedia.util
+
+
+fun scaleValueToRange(oldMin: Float, oldMax: Float, newMin: Float, newMax: Float, value: Float): Float{
+    val startRange = (oldMax - oldMin)
+    if (startRange == 0f) {
+        return 0f;
+    }
+    return (((value - oldMin) * (newMax - newMin)) / (oldMax - oldMin)) + newMin
+}
