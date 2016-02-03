f742585a7a0ed8b887d3dec3158d0800dafa3fbc
Mahak Swami <mswami@expedia.com>
2016-02-03 12:13:39 +0530

    Tests for Price changes - pwp

--- project/src/androidTest/java/com/expedia/bookings/test/phone/pagemodels/common/PaymentOptionsScreen.java
+++ project/src/androidTest/java/com/expedia/bookings/test/phone/pagemodels/common/PaymentOptionsScreen.java
@@ -1,13 +1,18 @@
 package com.expedia.bookings.test.phone.pagemodels.common;
 
 import android.support.test.espresso.ViewInteraction;
+
 import com.expedia.bookings.R;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.containsString;
 
 /**
  * Created by mswami on 1/15/16.
@@ -51,7 +56,7 @@ public class PaymentOptionsScreen {
 	}
 
 	public static void enterAmountForPointsCalculation(String points) {
-		editAmountTextView().perform(typeText(points));
+		editAmountTextView().perform(replaceText(points));
 	}
 
 	public static void clearPoints() {
@@ -60,5 +65,39 @@ public class PaymentOptionsScreen {
 
 	public static void openCardPaymentSection() {
 		cardPaymentOption().perform(click());
+	}
+
+	public static void assertTextInEditAmountMatches(String amount) {
+		onView(withId(R.id.pwp_edit_box_container)).check(matches(hasDescendant(allOf(withId(R.id.edit_amount_view),
+			withText(amount)))));
+	}
+
+	public static void assertTotalDueAmountMatches(String amount) {
+		onView(withId(R.id.section_payment_options_container))
+			.check(matches(hasDescendant(allOf(withId(R.id.total_due_today), withText(containsString(amount))))));
+	}
+
+	public static void assertTotalPointsAvailableMatches(String points) {
+		onView(withId(R.id.section_payment_options_container))
+			.check(matches(
+				hasDescendant(allOf(withId(R.id.total_points_available_view), withText(containsString(points))))));
+	}
+
+	public static void assertPointsAppliedMatches(String points) {
+		onView(withId(R.id.section_payment_options_container))
+			.check(matches(
+				hasDescendant(allOf(withId(R.id.message_view), withText(containsString(points))))));
+	}
+
+	public static void assertRemainingDueMatches(String amount) {
+		onView(withId(R.id.section_payment_options_container))
+			.check(matches(hasDescendant(allOf(withId(R.id.remaining_balance), withText(containsString(amount))))));
+
+	}
+
+	public static void enterCardInfo() {
+		openCardPaymentSection();
+		CheckoutViewModel.enterPaymentDetails();
+		CheckoutViewModel.pressClose();
 	}
 }
