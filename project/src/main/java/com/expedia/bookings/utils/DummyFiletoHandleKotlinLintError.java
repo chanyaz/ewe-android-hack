88041a33aa9e997019a5d532621f28fcfaa83022
Chuning Luo <chuluo@expedia.com>
2016-02-05 16:54:44 -0800

    add test for package overview widget

--- project/src/main/java/com/expedia/bookings/utils/DummyFiletoHandleKotlinLintError.java
+++ project/src/main/java/com/expedia/bookings/utils/DummyFiletoHandleKotlinLintError.java
@@ -334,6 +334,6 @@ DummyFiletoHandleKotlinLintError {
 		R.string.car_does_not_accept_cardtype_TEMPLATE,
 		R.string.credit_debit_card_hint,
 		R.string.coupon_submit_button,
-		R.string.guests_with_rooms_TEMPLATE
+		R.string.guests_with_rooms_TEMPLATE 
 	};
 }
