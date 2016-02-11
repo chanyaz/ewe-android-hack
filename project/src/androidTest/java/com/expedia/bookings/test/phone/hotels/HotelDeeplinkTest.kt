4f453eacfc63f5c889e7b90d4a0e6daec3b6fb15
Scott Weber <scott@mobiata.com>
2016-02-11 13:45:24 -0500

    6469: handle lowercased deep links
    Google has decided to start lower-casing our deep links, so we need
    to be able to handle those just as well as the normal, camel-cased
    version.

--- project/src/androidTest/java/com/expedia/bookings/test/phone/hotels/HotelDeeplinkTest.kt
+++ project/src/androidTest/java/com/expedia/bookings/test/phone/hotels/HotelDeeplinkTest.kt
@@ -35,6 +35,23 @@ public class NewHotelDeepLinkTest: PhoneTestCase() {
         EspressoUtils.assertViewIsDisplayed(R.id.widget_hotel_results)
     }
 
+    // Google has decided to modify the deep links that we provide them, this test verifies that we properly handle
+    // links that have been "Google-ified"
+    @Throws(Throwable::class)
+    fun testHotelDeepLinkWithLocationFromGoogle() {
+        val intent = Intent()
+        val deepLinkText = Uri.parse("expda://hotelsearch/?location=Miami")
+        intent.setData(deepLinkText)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        intent.setComponent(ComponentName(BuildConfig.APPLICATION_ID,
+                "com.expedia.bookings.activity.DeepLinkRouterActivity"))
+        Common.getApplication().startActivity(intent)
+
+        Common.delay(3)
+        EspressoUtils.assertViewIsDisplayed(R.id.widget_hotel_results)
+    }
+
     @Throws(Throwable::class)
     fun testHotelDeepLinkWithHotelId() {
         val intent = Intent()
