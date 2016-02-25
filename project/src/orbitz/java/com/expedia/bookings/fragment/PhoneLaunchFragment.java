9a50626bc7f05acc00f586e57a991a6cc361c273
Vaidehi Deshpande <v-vdeshpande@expedia.com>
2016-02-25 18:52:17 +0530

    Fixed merge issues observed in CTX & Orbitz
    Removed styles that are not required from other whitelabels

--- project/src/orbitz/java/com/expedia/bookings/fragment/PhoneLaunchFragment.java
+++ project/src/orbitz/java/com/expedia/bookings/fragment/PhoneLaunchFragment.java
@@ -2,6 +2,7 @@ package com.expedia.bookings.fragment;
 
 import org.joda.time.LocalDate;
 import org.json.JSONException;
+
 import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -22,7 +23,7 @@ import com.expedia.bookings.R;
 import com.expedia.bookings.activity.ExpediaBookingApp;
 import com.expedia.bookings.data.Db;
 import com.expedia.bookings.data.HotelSearchParams;
-import com.expedia.bookings.data.cars.Suggestion;
+import com.expedia.bookings.data.collections.CollectionLocation;
 import com.expedia.bookings.data.trips.ItineraryManager;
 import com.expedia.bookings.interfaces.IPhoneLaunchActivityLaunchFragment;
 import com.expedia.bookings.location.CurrentLocationObservable;
@@ -163,7 +164,7 @@ public class PhoneLaunchFragment extends Fragment implements IPhoneLaunchActivit
 	// Hotel search in collection location
 	@Subscribe
 	public void onCollectionLocationSelected(Events.LaunchCollectionItemSelected event) throws JSONException {
-		Suggestion location = event.collectionLocation.location;
+		CollectionLocation.Location location = event.collectionLocation.location;
 		HotelSearchParams params = new HotelSearchParams();
 		params.setQuery(location.shortName);
 		params.setSearchType(HotelSearchParams.SearchType.valueOf(location.type));
