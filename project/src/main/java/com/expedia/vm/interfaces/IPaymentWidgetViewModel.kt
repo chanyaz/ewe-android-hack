93a8238e84c5e8e6efb7bfba769968a8ba109fbc
Pulkit Kothari <pkothari@expedia.com>
2016-03-16 16:06:16 +0530

    PaymentModel refactoring of function

--- project/src/main/java/com/expedia/vm/interfaces/IPaymentWidgetViewModel.kt
+++ project/src/main/java/com/expedia/vm/interfaces/IPaymentWidgetViewModel.kt
@@ -1,11 +1,12 @@
 package com.expedia.vm.interfaces
 
+import com.expedia.bookings.data.TripResponse
 import com.expedia.bookings.data.payment.PaymentModel
 import com.expedia.bookings.utils.ArrowXDrawableUtil
 import rx.Observable
 import rx.subjects.PublishSubject
 
-public interface IPaymentWidgetViewModel {
+interface IPaymentWidgetViewModel {
     //INLETS
     val navigatingOutOfPaymentOptions: PublishSubject<Unit>
     val hasPwpEditBoxFocus: PublishSubject<Boolean>
@@ -14,7 +15,7 @@ public interface IPaymentWidgetViewModel {
     val totalDueToday: Observable<String>
     val remainingBalanceDueOnCard: Observable<String>
     val remainingBalanceDueOnCardVisibility: Observable<Boolean>
-    val paymentSplitsAndTripResponse: Observable<PaymentModel.PaymentSplitsAndTripResponse>
+    val paymentSplitsAndTripResponse: Observable<PaymentModel.PaymentSplitsAndTripResponse<TripResponse>>
     val burnAmountApiCallResponsePending: PublishSubject<Boolean>
     val isPwpDirty: Observable<Boolean>
     val toolbarNavIcon: PublishSubject<ArrowXDrawableUtil.ArrowDrawableType>
