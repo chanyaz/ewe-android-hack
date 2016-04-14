5a7d41d036ee59a335314231e86be4d25cba029d
Thomas Harman <tharman@expedia.com>
2016-04-13 17:10:52 -0700

    d/7050 Stop sending 0 LatLong for region based Hotel searches

--- lib/ExpediaBookings/src/main/java/com/expedia/bookings/services/HotelApi.java
+++ lib/ExpediaBookings/src/main/java/com/expedia/bookings/services/HotelApi.java
@@ -31,8 +31,6 @@ public interface HotelApi {
 	@GET("/m/api/hotel/search?sortOrder=ExpertPicks&resultsPerPage=200&pageIndex=0&filterUnavailable=true&enableSponsoredListings=true")
 	Observable<HotelSearchResponse> search(
 		@Query("regionId") String gaiaId,
-		@Query("latitude") double lat,
-		@Query("longitude") double lng,
 		@Query("checkInDate") String checkIn,
 		@Query("checkOutDate") String checkOut,
 		@Query("room1") String guestString,
