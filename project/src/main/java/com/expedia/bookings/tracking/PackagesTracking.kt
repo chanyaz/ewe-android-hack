4c0a39d7d2b828c6acc76d5537e3f65098e2f8e6
Supreeth <supreeth@mobiata.com>
2016-04-20 15:54:42 -0700

    7233. Package Omniture Flights Round 2
    Adding following omniture events:
    - Baggage Fee click
    - All Sort and Filter related

--- project/src/main/java/com/expedia/bookings/tracking/PackagesTracking.kt
+++ project/src/main/java/com/expedia/bookings/tracking/PackagesTracking.kt
@@ -1,7 +1,9 @@
 package com.expedia.bookings.tracking
 
+import com.expedia.bookings.data.FlightFilter
 import com.expedia.bookings.data.packages.PackageCreateTripResponse
 import com.expedia.bookings.data.packages.PackageSearchResponse
+import com.expedia.vm.packages.PackageFlightFilterViewModel
 
 class PackagesTracking {
 
@@ -119,5 +121,36 @@ class PackagesTracking {
 
     fun trackSearchTravelerPickerChooserClick(text: String) {
         OmnitureTracking.trackPackagesSearchTravelerPickerChooser(text)
+    }
+
+    fun trackFlightBaggageFeeClick() {
+        OmnitureTracking.trackPackagesFlightBaggageFeeClick()
+    }
+
+    fun trackFlightSortFilterLoad() {
+        OmnitureTracking.trackPackagesFlightSortFilterLoad()
+    }
+
+    fun trackFlightSortBy(sortBy: FlightFilter.Sort) {
+        val sortedBy = ( when (sortBy) {
+            FlightFilter.Sort.PRICE -> "Price"
+            FlightFilter.Sort.ARRIVAL -> "Arrival"
+            FlightFilter.Sort.DEPARTURE -> "Departure"
+            FlightFilter.Sort.DURATION -> "Duration"
+        })
+        OmnitureTracking.trackPackagesFlightSortBy(sortedBy)
+    }
+
+    fun trackFlightFilterStops(stops: PackageFlightFilterViewModel.Stops) {
+        val stops = ( when (stops) {
+            PackageFlightFilterViewModel.Stops.NONSTOP -> "No Stops"
+            PackageFlightFilterViewModel.Stops.ONE_STOP -> "1 Stop"
+            PackageFlightFilterViewModel.Stops.TWO_PLUS_STOPS -> "2 Stops"
+        })
+        OmnitureTracking.trackPackagesFlightFilterStops(stops)
+    }
+
+    fun trackFlightFilterAirlines() {
+        OmnitureTracking.trackPackagesFlightFilterAirlines()
     }
 }
