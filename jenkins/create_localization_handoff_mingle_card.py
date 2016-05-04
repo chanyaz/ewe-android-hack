2d3ac7dc4818b7a1fca2e61c68b2b2465fcbc65e
Rajan <rkochhar@expedia.com>
2016-05-04 04:06:48 +0000

    7408 - Localization handoff automation scripts.
    - Copied HMACAuth.py from mergebot repo.
    - Creates a card with details of localization being sent.
    - Jenkins job has been configured : https://ewemobile.jenkins.test.expedia.com/job/EBad-Localization-Handoff/
    - We can select 'Brand' and 'Branch' for localization from the job.
    - Uploads the strings.xml sent to the mingle card as attachment.

---
+++ jenkins/create_localization_handoff_mingle_card.py
@@ -0,0 +1,47 @@
+from mingle_utils import createCard, uploadAttachment
+import sys
+import datetime
+import pytz
+import traceback
+from hipchat_send_message import sendHipchatMessageToRoom
+
+MINGLE_PROJECT='eb_ad_app'
+#TODO - update to Mingler's Mingle Token
+MINGLE_ACCESS_ID='rkochhar'
+MINGLE_ACCESS_SECRET="5ISptP8ZqZDO7YNh0ZbwADo2NYRNLRIDSIFDxT0qS+Q="
+
+UTC = pytz.utc
+handoffDate = datetime.datetime.now(tz=UTC).strftime('%A %d, %b %Y, %H:%M %p %Z')
+cardProperties = {'Team':'PF_US', 'Releases - Release':'(Current Release)', 'Theme':'Localizations', 'Schedule - Iteration':'(Current PF Iteration)'}
+brandName = sys.argv[1]
+branchName = sys.argv[2]
+fileLocation = sys.argv[3]
+hipchatAccessToken = sys.argv[4]
+#TODO - update this room name to Android team
+hipchatRoomName = 'TestLoc1'
+
+cardName = 'LOC DROP - {handoff_date}'.format(handoff_date=handoffDate)
+cardDescription = 'Loc handoff<br><br>Brand : {brand_name}<br>Branch : {branch_name}<br>Date : {date}' \
+                  .format(brand_name=brandName, branch_name=branchName, date=handoffDate)
+
+cardType = 'Story'
+
+cardNumber = createCard(MINGLE_PROJECT, MINGLE_ACCESS_ID, MINGLE_ACCESS_SECRET, cardName, cardDescription, cardType, cardProperties)
+if cardNumber == -1: sys.exit(-1)
+
+with open("build.properties", "w") as buildPropertiesFile:
+    print buildPropertiesFile.name
+    buildPropertiesFile.write('\nLOC_HANDOFF_CARD_CREATED='+cardNumber+"\n")
+
+#Upload string file as attachment to the card
+uploadStatus = uploadAttachment(MINGLE_PROJECT, MINGLE_ACCESS_ID, MINGLE_ACCESS_SECRET, cardNumber, fileLocation )
+if uploadStatus == -1: sys.exit(-1)
+
+hipchatMessage = "Loc Drop sent for {brand_name} from branch {branch_name}."\
+    "Mingle card : https://eiwork.mingle.thoughtworks.com/projects/{mingleProject}/cards/{card_number}"\
+    .format(brand_name=brandName, branch_name=branchName, card_number=cardNumber, mingleProject=MINGLE_PROJECT)
+
+try:
+    sendHipchatMessageToRoom(hipchatAccessToken, hipchatRoomName, hipchatMessage)
+except:
+    print "Exception encountered while trying to ping {hipchatRoomName}. Stack Trace: \n{stack_trace}".format(hipchatRoomName=hipchatRoomName, stack_trace=traceback.format_exc())
--- jenkins/hipchat_send_message.py
+++ jenkins/hipchat_send_message.py
@@ -5,3 +5,8 @@ def sendPrivateHipchatMessage(accessToken, userMailIdToSendMessageTo, message):
 	hipchat = HypChat(accessToken)
 	userToSendMessageTo = hipchat.get_user(userMailIdToSendMessageTo)
 	userToSendMessageTo.message(message, message_format='text', notify=True)
+
+def sendHipchatMessageToRoom(accessToken, roomName, message):
+	hipchat = HypChat(accessToken)
+	roomToSendMessageTo = hipchat.get_room(roomName)
+	roomToSendMessageTo.notification(message, notify=True)
