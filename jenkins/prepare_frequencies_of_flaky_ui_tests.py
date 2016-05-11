52897a7b59d9c8efa5d8745509c98788bcda8120
Narinder Beri <nberi@expedia.com>
2016-05-11 22:17:34 +0530

    Topmost Flaky UI Tests
    This script is in preparation for a job which will report on a daily
    basis the top flaky tests which led to developers resorting to 'retest
    uitests' and wasted effort. The larger goal is for teams and individuals
    to take ownership and accountability for Flaky Tests. Attempting to fix these
    tests will provide us an opportunity to revisit these tests and see if
    these can be implemented via Robolectric itself which will be a major
    win for the build pipeline and team productivity.

---
+++ jenkins/prepare_frequencies_of_flaky_ui_tests.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+import json
+import objectpath
+import glob
+import sys
+
+def main():
+	testToFailureCountMap = {}
+
+	forkOutputDir = sys.argv[1]
+	#process artifact and record failed tests
+	for forkTestRunOutputFileName in glob.glob(os.path.join(forkOutputDir, 'expedia/debug/summary/fork-*.json')):
+		print "Processing {forkTestRunOutputFileName}...".format(forkTestRunOutputFileName=forkTestRunOutputFileName)
+		with open(forkTestRunOutputFileName) as forkTestRunOutputFile:
+			forkTestRunOutputJsonData = json.load(forkTestRunOutputFile)
+
+			forkTestRunOutputJsonTree = objectpath.Tree(forkTestRunOutputJsonData)
+			forkTestRunFailureMetadataList = forkTestRunOutputJsonTree.execute('$.poolSummaries[0].testResults[@.testClass and @.failureTrace]')
+			for forkTestRunFailureMetadata in forkTestRunFailureMetadataList:
+				failedTest = forkTestRunFailureMetadata['testClass'] + "." + forkTestRunFailureMetadata['testMethod']
+				if failedTest in testToFailureCountMap:
+					testToFailureCountMap[failedTest] = testToFailureCountMap[failedTest] + 1
+				else:
+					testToFailureCountMap[failedTest] = 1
+
+	print "\n\n\nFinal List of Failed Tests:"
+	print testToFailureCountMap
+
+	if len(testToFailureCountMap) > 0:
+		filepathToWriteTo = sys.argv[2]
+		with open(filepathToWriteTo, "w") as fileToWriteTo:
+			fileToWriteTo.write(str(testToFailureCountMap))
+
+if __name__ == "__main__":
+	main()
