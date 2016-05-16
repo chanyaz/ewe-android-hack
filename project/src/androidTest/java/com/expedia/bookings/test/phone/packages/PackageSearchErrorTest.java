22c5d0f56d4ab5a9ec437ba0c7aa87df3714b28a
Deepanshu Madan <dmadan@expedia.com>
2016-05-16 16:10:47 -0700

    Test hotel offers error handling

--- project/src/androidTest/java/com/expedia/bookings/test/phone/packages/PackageSearchErrorTest.java
+++ project/src/androidTest/java/com/expedia/bookings/test/phone/packages/PackageSearchErrorTest.java
@@ -2,18 +2,22 @@ package com.expedia.bookings.test.phone.packages;
 
 import org.joda.time.LocalDate;
 
+import android.support.test.espresso.contrib.RecyclerViewActions;
+
 import com.expedia.bookings.R;
 import com.expedia.bookings.test.espresso.Common;
 import com.expedia.bookings.test.espresso.PackageTestCase;
 import com.expedia.bookings.test.espresso.ViewActions;
 import com.expedia.bookings.test.phone.pagemodels.common.SearchScreen;
-
+import com.expedia.bookings.test.phone.hotels.HotelScreen;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class PackageSearchErrorTest extends PackageTestCase {
 
@@ -36,4 +40,24 @@ public class PackageSearchErrorTest extends PackageTestCase {
 		SearchScreen.searchButton().check(matches(isDisplayed()));
 	}
 
+	public void testHotelOffersSearchError() throws Throwable {
+		SearchScreen.selectOriginAndDestination();
+
+		LocalDate startDate = LocalDate.now().plusDays(3);
+		LocalDate endDate = LocalDate.now().plusDays(8);
+		SearchScreen.selectDates(startDate, endDate);
+		SearchScreen.searchButton().perform(click());
+		PackageScreen.clickHotelBundle();
+		HotelScreen.hotelResultsList().perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("hotel offers error")), click()));
+
+		PackageScreen.assertErrorScreen("Edit Search", "We were unable to find any results. Please adjust your search.");
+		onView(withId(R.id.error_action_button)).perform(click());
+
+		//Error action button takes back to search screen
+		SearchScreen.searchButton().perform(click());
+		PackageScreen.clickHotelBundle();
+		HotelScreen.selectHotel("Package Happy Path");
+		PackageScreen.selectRoom();
+
+	}
 }
