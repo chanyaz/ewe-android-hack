e365fa22f9660df2f2f86827e84fbf895d1005b9
v-vsonawane <v-vsonawane@expedia.com>
2016-05-19 14:57:07 +0530

    Ebookers:Booking support tfn fix.

--- project/src/main/java/com/expedia/bookings/data/pos/PointOfSale.java
+++ project/src/main/java/com/expedia/bookings/data/pos/PointOfSale.java
@@ -590,6 +590,9 @@ public class PointOfSale {
 			Traveler traveler = usr.getPrimaryTraveler();
 			if (traveler.getIsLoyaltyMembershipActive()) {
 				switch (traveler.getLoyaltyMembershipTier()) {
+				case BASE:
+					number = getSupportPhoneNumberBaseTier();
+					break;
 				case MIDDLE:
 					number = getSupportPhoneNumberMiddleTier();
 					break;
