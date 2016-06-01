6b8ca47ee4b2e9e2ee3175875534b56e5fe24df9
Scott Weber <scott@mobiata.com>
2016-06-01 16:48:00 -0400

    7535: show taxes/fees when needed
    Apparently some hotels can have taxStatusType of INCLUDED, but also have
    a non-zero surchargeTotal. Previously, taxStatusType of INCLUDED forced
    the "Taxes & Fees" line to be "Included." Now, if there is a non-zero
    surcharge, that will be displayed on the Taxes & Fees line instead.

--- project/src/test/java/com/expedia/bookings/test/HotelBreakdownTest.kt
+++ project/src/test/java/com/expedia/bookings/test/HotelBreakdownTest.kt
@@ -6,8 +6,8 @@ import com.expedia.bookings.data.payment.PaymentModel
 import com.expedia.bookings.services.LoyaltyServices
 import com.expedia.bookings.test.robolectric.RobolectricRunner
 import com.expedia.bookings.testrule.ServicesRule
-import com.expedia.vm.HotelBreakDownViewModel.Breakdown
 import com.expedia.vm.HotelBreakDownViewModel
+import com.expedia.vm.HotelBreakDownViewModel.Breakdown
 import com.expedia.vm.HotelBreakDownViewModel.BreakdownItem
 import com.expedia.vm.HotelCheckoutSummaryViewModel
 import org.junit.Before
@@ -47,21 +47,17 @@ class HotelBreakdownTest {
     fun verifyCostBreakdownForUserWithNoRewardPoints() {
         givenHappyCreateTripResponse()
 
-        val latch = CountDownLatch(1)
-        vm.addRows.subscribe { latch.countDown() }
-        val testSubscriber = TestSubscriber<List<HotelBreakDownViewModel.Breakdown>>()
-        val expected = arrayListOf<List<HotelBreakDownViewModel.Breakdown>>()
+        val testSubscriber = TestSubscriber<List<Breakdown>>()
         vm.addRows.subscribe(testSubscriber)
 
-        paymentModel.createTripSubject.onNext(createTripResponse)
+        executeTest(testSubscriber)
+
+        val expected = arrayListOf<List<Breakdown>>()
         expected.add(arrayListOf(Breakdown("1 Night", "$99.00", BreakdownItem.OTHER),
                 Breakdown("3/22/2013", "$99.00", BreakdownItem.DATE),
                 Breakdown("Taxes & Fees", "$16.81", BreakdownItem.OTHER),
                 Breakdown("Trip Total", "$135.81", BreakdownItem.TRIPTOTAL)))
 
-        assertTrue(latch.await(10, TimeUnit.SECONDS))
-        vm.addRows.onCompleted()
-        testSubscriber.awaitTerminalEvent(10, TimeUnit.SECONDS)
         testSubscriber.assertReceivedOnNext(expected)
         testSubscriber.unsubscribe()
     }
@@ -70,19 +66,17 @@ class HotelBreakdownTest {
     fun verifyCostBreakdownForUserWithRedeemablePoints() {
         givenLoggedInUserWithEnoughRedeemablePointsResponse()
 
-        val latch = CountDownLatch(1)
-        vm.addRows.subscribe { latch.countDown() }
         val testSubscriber = TestSubscriber<List<Breakdown>>()
-        val expected = arrayListOf<List<Breakdown>>()
         vm.addRows.subscribe(testSubscriber)
 
-        paymentModel.createTripSubject.onNext(createTripResponse)
+        executeTest(testSubscriber, Runnable() {
+            val latch1 = CountDownLatch(1)
+            paymentModel.burnAmountToPointsApiResponse.subscribe { latch1.countDown() }
+            paymentModel.burnAmountSubject.onNext(BigDecimal(32))
+            latch1.await(10, TimeUnit.SECONDS)
+        })
 
-        val latch1 = CountDownLatch(1)
-        paymentModel.burnAmountToPointsApiResponse.subscribe { latch1.countDown() }
-        paymentModel.burnAmountSubject.onNext(BigDecimal(32))
-        latch1.await(10, TimeUnit.SECONDS)
-
+        val expected = arrayListOf<List<Breakdown>>()
         expected.add(arrayListOf(Breakdown("1 Night", "$99.00", BreakdownItem.OTHER),
                 Breakdown("3/22/2013", "$99.00", BreakdownItem.DATE),
                 Breakdown("Taxes & Fees", "$16.81", BreakdownItem.OTHER),
@@ -95,11 +89,126 @@ class HotelBreakdownTest {
                 Breakdown("14,005 points", "$100.00", BreakdownItem.DISCOUNT),
                 Breakdown("Trip Total", "$3.70", BreakdownItem.TRIPTOTAL)))
 
+        testSubscriber.assertReceivedOnNext(expected)
+        testSubscriber.unsubscribe()
+    }
+
+    @Test
+    fun verifyCostBreakDownForFeesWithIncludedTaxes() {
+        givenHotelWithFeesAndIncludedTaxesResponse()
+
+        val testSubscriber = TestSubscriber<List<Breakdown>>()
+        vm.addRows.subscribe(testSubscriber)
+
+        executeTest(testSubscriber)
+
+        val expected = arrayListOf<List<Breakdown>>()
+        expected.add(arrayListOf(Breakdown("3 Nights", "$110.43", BreakdownItem.OTHER),
+                Breakdown("7/05/2016", "$36.81", BreakdownItem.DATE),
+                Breakdown("7/06/2016", "$36.81", BreakdownItem.DATE),
+                Breakdown("7/07/2016", "$36.81", BreakdownItem.DATE),
+                Breakdown("Taxes & Fees", "$19.89", BreakdownItem.OTHER),
+                Breakdown("Trip Total", "$130.32", BreakdownItem.TRIPTOTAL)))
+
+        testSubscriber.assertReceivedOnNext(expected)
+        testSubscriber.unsubscribe()
+    }
+
+    @Test
+    fun verifyCostBreakDownForFeesPaidAtHotel() {
+        givenHotelWithFeesPaidAtHotelResponse()
+
+        val testSubscriber = TestSubscriber<List<Breakdown>>()
+        vm.addRows.subscribe(testSubscriber)
+
+        executeTest(testSubscriber)
+
+        val expected = arrayListOf<List<Breakdown>>()
+        expected.add(arrayListOf(Breakdown("1 Night", "$179.31", BreakdownItem.OTHER),
+                Breakdown("6/30/2016", "$179.31", BreakdownItem.DATE),
+                Breakdown("Taxes & Fees", "$8.97", BreakdownItem.OTHER),
+                Breakdown("Fees paid at hotel", "$2.49", BreakdownItem.OTHER),
+                Breakdown("Trip Total", "$190.77", BreakdownItem.TRIPTOTAL),
+                Breakdown("Due to Expedia", "$188.28", BreakdownItem.OTHER)))
+
+        testSubscriber.assertReceivedOnNext(expected)
+        testSubscriber.unsubscribe()
+    }
+
+    @Test
+    fun verifyCostBreakDownForGuestCharge() {
+        givenHotelWithGuestChargeResponse()
+
+        val testSubscriber = TestSubscriber<List<Breakdown>>()
+        vm.addRows.subscribe(testSubscriber)
+
+        executeTest(testSubscriber)
+
+        val expected = arrayListOf<List<Breakdown>>()
+        expected.add(arrayListOf(Breakdown("2 Nights", "$349.42", BreakdownItem.OTHER),
+                Breakdown("6/30/2016", "$135.96", BreakdownItem.DATE),
+                Breakdown("7/01/2016", "$213.46", BreakdownItem.DATE),
+                Breakdown("Taxes & Fees", "$59.34", BreakdownItem.OTHER),
+                Breakdown("Extra Guest Charge", "$120.00", BreakdownItem.OTHER),
+                Breakdown("Discount", "$25.00", BreakdownItem.DISCOUNT),
+                Breakdown("Fees paid at hotel", "$67.20", BreakdownItem.OTHER),
+                Breakdown("Trip Total", "$595.94", BreakdownItem.TRIPTOTAL),
+                Breakdown("Due to Expedia", "$528.74", BreakdownItem.OTHER)))
+
+        testSubscriber.assertReceivedOnNext(expected)
+        testSubscriber.unsubscribe()
+    }
+
+    @Test
+    fun verifyCostBreakDownForIncludedTaxes() {
+        givenHotelWithTaxesIncludedResponse()
+
+        val testSubscriber = TestSubscriber<List<Breakdown>>()
+        vm.addRows.subscribe(testSubscriber)
+
+        executeTest(testSubscriber)
+
+        val expected = arrayListOf<List<Breakdown>>()
+        expected.add(arrayListOf(Breakdown("1 Night", "$72.96", BreakdownItem.OTHER),
+                Breakdown("6/29/2016", "$72.96", BreakdownItem.DATE),
+                Breakdown("Taxes & Fees", "Included", BreakdownItem.OTHER),
+                Breakdown("Trip Total", "$72.96", BreakdownItem.TRIPTOTAL)))
+
+        testSubscriber.assertReceivedOnNext(expected)
+        testSubscriber.unsubscribe()
+    }
+
+    private fun executeTest(testSubscriber: TestSubscriber<List<HotelBreakDownViewModel.Breakdown>>, runBeforeComplete: Runnable? = null) {
+        val latch = CountDownLatch(1)
+        vm.addRows.subscribe { latch.countDown() }
+
+        paymentModel.createTripSubject.onNext(createTripResponse)
+
+        runBeforeComplete?.run()
+
         assertTrue(latch.await(10, TimeUnit.SECONDS))
         vm.addRows.onCompleted()
         testSubscriber.awaitTerminalEvent(10, TimeUnit.SECONDS)
-        testSubscriber.assertReceivedOnNext(expected)
-        testSubscriber.unsubscribe()
+    }
+
+    private fun givenHotelWithTaxesIncludedResponse() {
+        createTripResponse = mockHotelServiceTestRule.getHotelWithTaxesIncludedResponse()
+        createTripResponse.tripId = "happy"
+    }
+
+    private fun givenHotelWithGuestChargeResponse() {
+        createTripResponse = mockHotelServiceTestRule.getHotelWithGuestChargeResponse()
+        createTripResponse.tripId = "happy"
+    }
+
+    private fun givenHotelWithFeesPaidAtHotelResponse() {
+        createTripResponse = mockHotelServiceTestRule.getHotelWithFeesPaidAtHotelResponse()
+        createTripResponse.tripId = "happy"
+    }
+
+    private fun givenHotelWithFeesAndIncludedTaxesResponse() {
+        createTripResponse = mockHotelServiceTestRule.getHotelWithFeesAndIncludedTaxesResponse()
+        createTripResponse.tripId = "happy"
     }
 
     private fun givenLoggedInUserWithEnoughRedeemablePointsResponse() {
