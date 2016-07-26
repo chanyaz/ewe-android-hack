ba6afb2e1b062ad62911074f809d81fb2220878d
Thomas Harman <tharman@expedia.com>
2016-07-26 12:22:11 -0700

    s/8388 flights model refactor

--- project/src/main/java/com/expedia/bookings/presenter/flight/FlightInboundPresenter.kt
+++ project/src/main/java/com/expedia/bookings/presenter/flight/FlightInboundPresenter.kt
@@ -6,6 +6,14 @@ import com.expedia.bookings.tracking.FlightsV2Tracking
 
 class FlightInboundPresenter(context: Context, attrs: AttributeSet) : AbstractMaterialFlightResultsPresenter(context, attrs) {
 
+    override fun setupComplete() {
+        super.setupComplete()
+        flightOfferViewModel.confirmedOutboundFlightSelection.subscribe(resultsPresenter.outboundFlightSelectedSubject)
+        overviewPresenter.vm.selectedFlightClickedSubject.subscribe(flightOfferViewModel.confirmedInboundFlightSelection)
+        overviewPresenter.vm.selectedFlightLegSubject.subscribe(flightOfferViewModel.inboundSelected)
+        flightOfferViewModel.inboundResultsObservable.subscribe(resultsPresenter.resultsViewModel.flightResultsObservable)
+    }
+
     override fun isOutboundResultsPresenter(): Boolean {
         return false
     }
