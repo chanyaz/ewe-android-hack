54978b5c9b2c7430b1536d7ffaddfaaf0aa200d0
Malcolm Nguyen <malcolm@mobiata.com>
2016-08-16 13:48:42 -0700

    8285 center checkout text

--- project/src/main/java/com/expedia/bookings/widget/CheckoutHeaderBehavior.kt
+++ project/src/main/java/com/expedia/bookings/widget/CheckoutHeaderBehavior.kt
@@ -14,37 +14,44 @@ class CheckoutHeaderBehavior(val context: Context, attrs: AttributeSet) : Coordi
     var toolBarRightX = 0
     var textViewLeftX = 0
     var textViewWidth = 0
+    val toolbarHeight = Ui.getStatusBarHeight(context)
 
     override fun layoutDependsOn(parent: CoordinatorLayout, child: CheckoutOverviewHeader, dependency: View): Boolean {
-        return dependency is AppBarLayout;
+        return dependency is AppBarLayout
     }
 
     override fun onDependentViewChanged(parent: CoordinatorLayout, child: CheckoutOverviewHeader, dependency: View): Boolean {
-        val maxScroll = (dependency as AppBarLayout).totalScrollRange;
+        val maxScroll = (dependency as AppBarLayout).totalScrollRange
         if (maxScroll != 0) {
-            val percentage = Math.abs(dependency.getY()) / maxScroll;
-            var childHeight = dependency.height + dependency.getY() - Ui.getStatusBarHeight(context)
+            val percentage = Math.abs(dependency.getY()) / maxScroll
 
             child.checkInOutDates.alpha = 1 - (percentage * 2)
             child.travelers.alpha = 1 - (percentage * 2)
 
             if (textViewLeftX == 0 || textViewWidth == 0 || toolBarRightX == 0) {
                 setupValues(child, dependency)
-            } else {
-                val toolbarText = dependency.findViewById(R.id.checkout_overview_header_toolbar) as CheckoutOverviewHeader
-                val lp = child.layoutParams as CoordinatorLayout.LayoutParams
-                lp.height = childHeight.toInt()
-                lp.setMargins(lp.leftMargin, toolbarText.top, lp.rightMargin, lp.bottomMargin)
-                child.layoutParams = lp
-                child.destinationText.x = Math.max(toolBarRightX.toFloat(), textViewLeftX * (1 - percentage))
-                val scale = .75f + (1 - percentage) * (1f - .75f)
-                child.destinationText.pivotX = 0f
-                child.destinationText.pivotY = 0f
-                child.destinationText.scaleX = scale
-                child.destinationText.scaleY = scale
+            }
+
+            if (textViewLeftX != 0 && textViewWidth != 0 && toolBarRightX != 0) {
+                translateText(percentage, child, dependency)
             }
         }
-        return true;
+        return true
+    }
+
+    fun translateText(percentage: Float, child: CheckoutOverviewHeader, dependency: View) {
+        val childHeight = dependency.height + dependency.y - toolbarHeight
+        val toolbarText = dependency.findViewById(R.id.checkout_overview_header_toolbar) as CheckoutOverviewHeader
+        val lp = child.layoutParams as CoordinatorLayout.LayoutParams
+        lp.height = childHeight.toInt()
+        lp.setMargins(lp.leftMargin, toolbarText.top, lp.rightMargin, lp.bottomMargin)
+        child.layoutParams = lp
+        child.destinationText.x = Math.max(toolBarRightX.toFloat(), textViewLeftX * (1 - percentage))
+        val scale = .75f + (1 - percentage) * (1f - .75f)
+        child.destinationText.pivotX = 0f
+        child.destinationText.pivotY = 0f
+        child.destinationText.scaleX = scale
+        child.destinationText.scaleY = scale
     }
 
     private fun setupValues(child: CheckoutOverviewHeader, dependency: View) {
@@ -58,7 +65,7 @@ class CheckoutHeaderBehavior(val context: Context, attrs: AttributeSet) : Coordi
 
     private fun getLocation(view: View): IntArray {
         val location = IntArray(2)
-        view.getLocationInWindow(location);
-        return location;
+        view.getLocationInWindow(location)
+        return location
     }
 }
