7dd7a5b5a69b6e0e4222023aed037949d6eb3210
Chuning Luo <chuluo@expedia.com>
2016-08-25 11:34:24 -0700

    d/8792 fix flight cards after change hotel

--- project/src/androidTest/java/com/expedia/bookings/test/phone/packages/PackageChangePathBackNavTest.java
+++ project/src/androidTest/java/com/expedia/bookings/test/phone/packages/PackageChangePathBackNavTest.java
@@ -1,5 +1,10 @@
 package com.expedia.bookings.test.phone.packages;
 
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import android.support.test.espresso.matcher.ViewMatchers;
+
 import com.expedia.bookings.R;
 import com.expedia.bookings.test.espresso.Common;
 import com.expedia.bookings.test.espresso.PackageTestCase;
@@ -11,6 +16,7 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.allOf;
@@ -69,6 +75,34 @@ public class PackageChangePathBackNavTest extends PackageTestCase {
 
 	private void assertAfterChange() {
 		onView(withId(R.id.checkout_button)).check(matches(isEnabled()));
+
+		PackageScreen.hotelBundle().check(matches(hasDescendant(
+			allOf(isDisplayed(), withText("Package Happy Path")))));
+
+		final DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyy-MM-dd");
+		PackageScreen.hotelBundle().check(matches(hasDescendant(
+			allOf(isDisplayed(), withText(PackageScreen.getDatesGuestInfoText(dtf.parseLocalDate("2016-02-02"), dtf.parseLocalDate("2016-02-04")))))));
+		PackageScreen.hotelDetailsIcon().check(matches(isEnabled()));
+
+		PackageScreen.outboundFlight().check(matches(isEnabled()));
+		PackageScreen.inboundFLight().check(matches(isEnabled()));
+
+		PackageScreen.outboundFlightInfo().check(matches(hasDescendant(
+			allOf(isDisplayed(), withText("Flight to (DTW) Detroit")))));
+		PackageScreen.inboundFlightInfo().check(matches(hasDescendant(
+			allOf(isDisplayed(), withText("Flight to (SFO) San Francisco")))));
+
+		PackageScreen.outboundFlightDetailsIcon().check(matches(withEffectiveVisibility(
+			ViewMatchers.Visibility.VISIBLE)));
+		PackageScreen.outboundFlightDetailsIcon().perform(click());
+		PackageScreen.outboundFlightDetailsContainer().check(matches(withEffectiveVisibility(
+			ViewMatchers.Visibility.VISIBLE)));
+
+		PackageScreen.clickHotelBundle();
+		PackageScreen.hotelRoomImageView().check(matches(isDisplayed()));
+		PackageScreen.outboundFlightDetailsContainer().check(matches(withEffectiveVisibility(
+			ViewMatchers.Visibility.GONE)));
+
 	}
 }
 
