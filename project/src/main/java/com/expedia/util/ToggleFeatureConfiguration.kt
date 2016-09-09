b64c10091b3b80f9a9c67fb243dc7a0eff759eff
Malcolm Nguyen <malcolm@mobiata.com>
2016-09-09 11:47:46 -0700

    Move activies out of debug maifest.

--- project/src/main/java/com/expedia/util/ToggleFeatureConfiguration.kt
+++ project/src/main/java/com/expedia/util/ToggleFeatureConfiguration.kt
@@ -4,7 +4,6 @@ class ToggleFeatureConfiguration {
     companion object {
         @JvmField val SMART_LOCK_FEATURE: Boolean = false
         @JvmField val HOTEL_FAVORITE_FEATURE: Boolean = false
-        @JvmField val PACKAGES_FEATURE: Boolean = false
         @JvmField val RAIL_FEATURE: Boolean = false
     }
 }
