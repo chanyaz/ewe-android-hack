d46e614389879475469afc11c0bc022921676396
Thomas Harman <tharman@expedia.com>
2016-09-14 11:53:59 -0700

    8922 Card fees web view toolbar title and bg colour fix

--- project/src/main/java/com/expedia/bookings/widget/flights/PaymentFeeInfoWebView.kt
+++ project/src/main/java/com/expedia/bookings/widget/flights/PaymentFeeInfoWebView.kt
@@ -2,8 +2,12 @@ package com.expedia.bookings.widget.flights
 
 import android.content.Context
 import android.util.AttributeSet
+import com.expedia.bookings.R
 import com.expedia.bookings.widget.shared.AbstractWebViewWidget
 
 class PaymentFeeInfoWebView(context: Context, attrs: AttributeSet) : AbstractWebViewWidget(context, attrs) {
-
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        toolbar.setTitle(R.string.flights_flight_overview_payment_fees)
+    }
 }
