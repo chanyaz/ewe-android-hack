96a366e0483c2495e933c9b2d06ef1751a9aab36
MiaC <mclapham@expedia.com>
2016-09-21 15:15:57 -0700

    9001 only show airline fee when it is greater than zero

--- project/src/test/java/com/expedia/vm/packages/PackageCostSummaryBreakdownViewModelTest.kt
+++ project/src/test/java/com/expedia/vm/packages/PackageCostSummaryBreakdownViewModelTest.kt
@@ -1,8 +1,10 @@
 package com.expedia.vm.packages
 
 import android.content.Context
+import android.icu.util.CurrencyAmount
 import android.support.v4.content.ContextCompat
 import com.expedia.bookings.R
+import com.expedia.bookings.data.Money
 import com.expedia.bookings.data.packages.PackageCreateTripParams
 import com.expedia.bookings.data.packages.PackageCreateTripResponse
 import com.expedia.bookings.services.PackageServices
@@ -16,6 +18,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RuntimeEnvironment
 import rx.observers.TestSubscriber
+import java.math.BigDecimal
 import java.util.Arrays
 import java.util.concurrent.TimeUnit
 import kotlin.properties.Delegates
@@ -214,6 +217,84 @@ class PackageCostSummaryBreakdownViewModelTest {
         assertEquals(false, rows[4].separator)
     }
 
+    @Test
+    fun testAirlineFee() {
+        val rows = getCostSummaryBreakdownRowsWithAirlineFee(5.20)
+        assertEquals(6, rows.count())
+
+        assertEquals("Hotel + Flights", rows[0].title)
+        assertEquals("$2,230.34", rows[0].cost)
+        assertEquals(null, rows[0].color)
+        assertEquals(null, rows[0].typeface)
+        assertEquals(false, rows[0].separator)
+
+        assertEquals("1 room, 2 nights, 5 guests", rows[1].title)
+        assertEquals(null, rows[1].cost)
+        assertEquals(null, rows[1].color)
+        assertEquals(null, rows[1].typeface)
+        assertEquals(false, rows[1].separator)
+
+        assertEquals("Taxes & Fees Included ($333.08)", rows[2].title)
+        assertEquals(null, rows[2].cost)
+        assertEquals(null, rows[2].color)
+        assertEquals(null, rows[2].typeface)
+        assertEquals(false, rows[2].separator)
+
+        assertEquals("Airline Card Fee", rows[3].title)
+        assertEquals("$5.20", rows[3].cost)
+        assertEquals(null, rows[3].color)
+        assertEquals(null, rows[3].typeface)
+        assertEquals(false, rows[3].separator)
+
+        assertEquals(null, rows[4].title)
+        assertEquals(null, rows[4].cost)
+        assertEquals(null, rows[4].color)
+        assertEquals(null, rows[4].typeface)
+        assertEquals(true, rows[4].separator)
+
+        assertEquals("Bundle total", rows[5].title)
+        assertEquals("$2,202.34", rows[5].cost)
+        assertEquals(null, rows[5].color)
+        assertEquals(FontCache.Font.ROBOTO_MEDIUM, rows[5].typeface)
+        assertEquals(false, rows[5].separator)
+    }
+
+    @Test
+    fun testZeroAirlineFee() {
+        val rows = getCostSummaryBreakdownRowsWithAirlineFee(0.00)
+        assertEquals(5, rows.count())
+
+        assertEquals("Hotel + Flights", rows[0].title)
+        assertEquals("$2,230.34", rows[0].cost)
+        assertEquals(null, rows[0].color)
+        assertEquals(null, rows[0].typeface)
+        assertEquals(false, rows[0].separator)
+
+        assertEquals("1 room, 2 nights, 5 guests", rows[1].title)
+        assertEquals(null, rows[1].cost)
+        assertEquals(null, rows[1].color)
+        assertEquals(null, rows[1].typeface)
+        assertEquals(false, rows[1].separator)
+
+        assertEquals("Taxes & Fees Included ($333.08)", rows[2].title)
+        assertEquals(null, rows[2].cost)
+        assertEquals(null, rows[2].color)
+        assertEquals(null, rows[2].typeface)
+        assertEquals(false, rows[2].separator)
+
+        assertEquals(null, rows[3].title)
+        assertEquals(null, rows[3].cost)
+        assertEquals(null, rows[3].color)
+        assertEquals(null, rows[3].typeface)
+        assertEquals(true, rows[3].separator)
+
+        assertEquals("Bundle total", rows[4].title)
+        assertEquals("$2,202.34", rows[4].cost)
+        assertEquals(null, rows[4].color)
+        assertEquals(FontCache.Font.ROBOTO_MEDIUM, rows[4].typeface)
+        assertEquals(false, rows[4].separator)
+    }
+
     private fun getCostSummaryBreakdownRows(hasFees: Boolean): List<BaseCostSummaryBreakdownViewModel.CostSummaryBreakdownRow> {
         val viewModel = PackageCostSummaryBreakdownViewModel(getContext())
         val prodID = if (hasFees) "create_trip_with_resort_fee" else "create_trip_multitraveler"
@@ -249,6 +330,28 @@ class PackageCostSummaryBreakdownViewModelTest {
         val rowsObserver = TestSubscriber<List<BaseCostSummaryBreakdownViewModel.CostSummaryBreakdownRow>>()
         viewModel.addRows.subscribe(rowsObserver)
         viewModel.packageCostSummaryObservable.onNext(observer.onNextEvents[0])
+        return rowsObserver.onNextEvents[0]
+    }
+
+    private fun getCostSummaryBreakdownRowsWithAirlineFee(airlineFee: Double): List<BaseCostSummaryBreakdownViewModel.CostSummaryBreakdownRow> {
+        val viewModel = PackageCostSummaryBreakdownViewModel(getContext())
+        val prodID = "create_trip_with_negative_savings"
+
+        val observer = TestSubscriber<PackageCreateTripResponse>()
+        val params = PackageCreateTripParams(prodID, "6139057", 2, false, Arrays.asList(0, 8, 12))
+
+        packageServiceRule.services!!.createTrip(params).subscribe(observer)
+
+        observer.awaitTerminalEvent(10, TimeUnit.SECONDS)
+        observer.assertNoErrors()
+        observer.assertCompleted()
+
+        val createTrip = observer.onNextEvents[0]
+        createTrip.selectedCardFees = Money(BigDecimal(airlineFee), "USD")
+
+        val rowsObserver = TestSubscriber<List<BaseCostSummaryBreakdownViewModel.CostSummaryBreakdownRow>>()
+        viewModel.addRows.subscribe(rowsObserver)
+        viewModel.packageCostSummaryObservable.onNext(createTrip)
         return rowsObserver.onNextEvents[0]
     }
 }
