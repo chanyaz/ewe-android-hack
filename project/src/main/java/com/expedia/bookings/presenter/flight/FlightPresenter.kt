5a90c1d70af45fb53a8c0b5e14513306a99817f8
Mohit <mohitsharmasjsu@gmail.com>
2016-09-21 15:52:11 -0700

    d/9067 flight crasher for drodown pos

--- project/src/main/java/com/expedia/bookings/presenter/flight/FlightPresenter.kt
+++ project/src/main/java/com/expedia/bookings/presenter/flight/FlightPresenter.kt
@@ -13,7 +13,6 @@ import com.expedia.bookings.animation.TransitionElement
 import com.expedia.bookings.data.ApiError
 import com.expedia.bookings.data.BaseApiResponse
 import com.expedia.bookings.data.Db
-import com.expedia.bookings.data.User
 import com.expedia.bookings.data.abacus.AbacusUtils
 import com.expedia.bookings.data.flights.FlightCheckoutResponse
 import com.expedia.bookings.data.flights.FlightCreateTripParams
@@ -303,7 +302,7 @@ class FlightPresenter(context: Context, attrs: AttributeSet?) : Presenter(contex
 
     override fun onFinishInflate() {
         super.onFinishInflate()
-        addTransition(if (displayFlightDropDownRoutes()) restrictedSearchToOutbound else searchToOutbound)
+        addTransition(searchToOutbound)
         addTransition(inboundFlightToOverview)
         addTransition(outboundToInbound)
         addTransition(overviewToConfirmation)
@@ -451,10 +450,9 @@ class FlightPresenter(context: Context, attrs: AttributeSet?) : Presenter(contex
         }
     }
 
-    private val searchToOutbound = SearchToOutboundTransition(this, FlightSearchPresenter::class.java, FlightOutboundPresenter::class.java)
-    private val restrictedSearchToOutbound = SearchToOutboundTransition(this, FlightSearchAirportDropdownPresenter::class.java, FlightOutboundPresenter::class.java)
-    private val searchToInbound = SearchToOutboundTransition(this, FlightSearchPresenter::class.java, FlightInboundPresenter::class.java)
-
+    private val searchToOutbound = SearchToOutboundTransition(this, searchPresenter.javaClass, FlightOutboundPresenter::class.java)
+    private val searchToInbound = SearchToOutboundTransition(this, searchPresenter.javaClass, FlightInboundPresenter::class.java)
+ 
     private val defaultSearchTransition = object : Presenter.DefaultTransition(getDefaultSearchPresenterClassName()) {
         override fun endTransition(forward: Boolean) {
             searchPresenter.visibility = View.VISIBLE
