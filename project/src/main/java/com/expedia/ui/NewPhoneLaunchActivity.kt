a2bd9a7cc0974c14f78532849564bd1f096b8de2
Mohit <mohitsharmasjsu@gmail.com>
2016-09-27 13:55:24 -0700

    d/8908 fix extras sign in call - fix new launch screen sign in call when user go to account tab - flights and packages dont make refresh user call if user is not logged in.

--- project/src/main/java/com/expedia/ui/NewPhoneLaunchActivity.kt
+++ project/src/main/java/com/expedia/ui/NewPhoneLaunchActivity.kt
@@ -22,6 +22,7 @@ import com.expedia.bookings.activity.ExpediaBookingApp
 import com.expedia.bookings.activity.ExpediaBookingPreferenceActivity
 import com.expedia.bookings.data.Codes
 import com.expedia.bookings.data.LineOfBusiness
+import com.expedia.bookings.data.User
 import com.expedia.bookings.data.trips.ItinCardData
 import com.expedia.bookings.data.trips.ItineraryManager
 import com.expedia.bookings.dialog.ClearPrivateDataDialog
@@ -223,7 +224,9 @@ class NewPhoneLaunchActivity : AbstractAppCompatActivity(), NewPhoneLaunchFragme
                     }
                     PAGER_POS_ITIN -> gotoItineraries()
                     PAGER_POS_ACCOUNT -> {
-                        accountFragment?.refreshUserInfo()
+                        if (User.isLoggedIn(this@NewPhoneLaunchActivity)) {
+                            accountFragment?.refreshUserInfo()
+                        }
                         viewPager.currentItem = PAGER_POS_ACCOUNT
                         OmnitureTracking.trackAccountPageLoad()
                     }
