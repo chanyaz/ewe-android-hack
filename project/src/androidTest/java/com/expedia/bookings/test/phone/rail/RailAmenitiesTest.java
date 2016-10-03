6f12f88569d47ab88e68b81ead05dfcb2a306bd8
Danny Briggs <dbriggs@expedia.com>
2016-10-03 15:41:36 -0500

    9176 - Fare Rules/Amenities Modal Tab Missing
    Fixing layout
    Renaming view ids

--- project/src/androidTest/java/com/expedia/bookings/test/phone/rail/RailAmenitiesTest.java
+++ project/src/androidTest/java/com/expedia/bookings/test/phone/rail/RailAmenitiesTest.java
@@ -1,24 +1,28 @@
 package com.expedia.bookings.test.phone.rail;
 
 import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
 
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.view.View;
 
+import com.expedia.bookings.R;
 import com.expedia.bookings.test.espresso.RailTestCase;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.hasSibling;
+import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 public class RailAmenitiesTest extends RailTestCase {
 
 	public void testRailAmenities() throws Throwable {
 		navigateFromSearchToAmenities();
+		assertTabVisible();
 		assertAmenities();
 	}
 
@@ -29,6 +33,13 @@ public class RailAmenitiesTest extends RailTestCase {
 		RailScreen.clickAmenitiesLink("Any off-peak train");
 	}
 
+	private void assertTabVisible() {
+		onView(allOf(isDescendantOfA(withId(R.id.amenities_rules_tabs)),
+			withText(R.string.amenities))).check(matches(isDisplayed()));
+		onView(allOf(isDescendantOfA(withId(R.id.amenities_rules_tabs)),
+			withText(R.string.fare_rules))).check(matches(isDisplayed()));
+	}
+
 	private void assertAmenities() {
 		onView(withText("Manchester Piccadilly to London Euston")).check(matches(isDisplayed()));
 		assertAmenityIsDisplayed("• Buffet Service\n• WiFi Access (Additional Cost)\n",
@@ -37,7 +48,7 @@ public class RailAmenitiesTest extends RailTestCase {
 	}
 
 	private void assertAmenityIsDisplayed(String amenityString, String segmentName) {
-		Matcher<View> matcher = Matchers.allOf(
+		Matcher<View> matcher = allOf(
 			withText(amenityString),
 			withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE),
 			hasSibling(withText(segmentName)));
