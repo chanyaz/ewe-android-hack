c28012b992da1a24a09f69c1cdec1f4ef3e0992b
Thomas Harman <tharman@expedia.com>
2016-10-13 14:52:37 -0700

    i 9209 Shell script to print out logs since any given tag

---
+++ jenkins/rc_logs_since_last_release.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+git fetch --tags
+last_release_tag=$1
+output_file=$2
+
+if git rev-parse -q --verify $last_release_tag;
+then
+	echo -e "Commits since release with tag: $last_release_tag\n\n" > $output_file
+	git log $last_release_tag..HEAD >> $output_file
+else
+	echo "Cannot find release tag: $last_release_tag" > $output_file
+fi
+
