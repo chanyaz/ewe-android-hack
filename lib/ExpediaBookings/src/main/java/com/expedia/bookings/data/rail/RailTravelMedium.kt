81b49230dec097de51cc5e1c326d7bc13268f9f2
Danny Briggs <dbriggs@expedia.com>
2016-10-17 16:42:39 -0500

    9170 - Update Rail To Look At Travel Medium Instead of Mode

---
+++ lib/ExpediaBookings/src/main/java/com/expedia/bookings/data/rail/RailTravelMedium.kt
@@ -0,0 +1,27 @@
+package com.expedia.bookings.data.rail
+
+class RailTravelMedium(val travelMediumCode: String,
+                       val travelMediumName: String) {
+
+    companion object {
+        val AIRPORT = "APT"
+        val BRANCH_LINE_REGIONAL = "BLR"
+        val BUS = "BUS"
+        val COMMUTER = "CMT"
+        val FERRY = "FRY"
+        val HOVERCRAFT = "HOV"
+        val HIGH_SPEED = "HSP"
+        val INTER_CITY = "ICY"
+        val METRO_CITY_TRANSIT = "MCT"
+        val NAMED_TRAIN = "NAT"
+        val NIGHT_TRAIN = "NIT"
+        val PEDESTRIAN = "PED"
+        val PLATFORM_CHANGE = "PFC"
+        val SELF_TRANSFER = "STR"
+        val TAXI = "TAX"
+        val TRAM = "TRM"
+        val TRANSFER = "TRS"
+        val EXPRESS_BUS = "XBU"
+        val UNKNOWN = "UNK"
+    }
+}
