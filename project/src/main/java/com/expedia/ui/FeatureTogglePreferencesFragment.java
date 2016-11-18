38e4508dc091241ee8bf6218ef16f681c3f23edb
Danny Briggs <dbriggs@expedia.com>
2016-11-18 15:59:50 -0600

    9551 - Enable Rail Line of Business
    Gettin ready to ship it

--- project/src/main/java/com/expedia/ui/FeatureTogglePreferencesFragment.java
+++ project/src/main/java/com/expedia/ui/FeatureTogglePreferencesFragment.java
@@ -23,7 +23,6 @@ public class FeatureTogglePreferencesFragment extends BasePreferenceFragment {
 
 		// Login Features
 		initializeFeatureCheck(R.string.preference_enable_smart_lock);
-		initializeFeatureCheck(R.string.preference_enable_rail);
 		initializeFeatureCheck(R.string.preference_enable_activity_map);
 		initializeFeatureCheck(R.string.preference_enable_filter_text_search);
 		initializeFeatureCheck(R.string.preference_enable_gaia_current_location_suggestion);
