217da1c85a826d6fdbaacf79622e1e2b17e94e12
Mahima Pulastya <mpulastya@expedia.com>
2016-11-22 09:27:23 +0530

    Select Dates Label Fix

--- project/src/main/java/com/expedia/bookings/utils/DummyFiletoHandleKotlinLintError.java
+++ project/src/main/java/com/expedia/bookings/utils/DummyFiletoHandleKotlinLintError.java
@@ -793,6 +793,8 @@ public class DummyFiletoHandleKotlinLintError {
 		R.string.rail_unknown_error_message,
 		R.string.rail_cko_retry_error_message,
 		R.string.rail_error_title,
-		R.layout.itin_gallery
+		R.layout.itin_gallery,
+		R.string.select_start_date,
+		R.string.lx_search_start_date_cont_desc
 	};
 }
