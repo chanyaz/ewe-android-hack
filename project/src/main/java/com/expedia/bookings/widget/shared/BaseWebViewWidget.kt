63e8a592e0e4d814ed21fe73d7b8c92f2b6abc4b
Danny Briggs <dbriggs@expedia.com>
2016-12-07 17:53:08 -0600

    9598 Refresh rail link on each open
    Add progress bar to webviews

---
+++ project/src/main/java/com/expedia/bookings/widget/shared/BaseWebViewWidget.kt
@@ -0,0 +1,80 @@
+package com.expedia.bookings.widget.shared
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.support.v7.widget.Toolbar
+import android.util.AttributeSet
+import android.view.View
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import com.expedia.bookings.R
+import com.expedia.bookings.utils.Ui
+import com.expedia.bookings.utils.bindView
+import com.expedia.util.notNullAndObservable
+import com.expedia.vm.WebViewViewModel
+
+open class BaseWebViewWidget(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+    val toolbar: Toolbar by bindView(R.id.toolbar)
+    val webView: WebView by bindView(R.id.web_view)
+    val progressView: ProgressBar by bindView(R.id.webview_progress_view)
+    val statusBarHeight by lazy { Ui.getStatusBarHeight(context) }
+
+    var webClient = object : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+            return false
+        }
+
+        override fun onPageFinished(view: WebView, url: String) {
+            toggleLoading(false)
+        }
+
+        override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
+            toggleLoading(true)
+        }
+
+        override fun onReceivedError(view: WebView, request: WebResourceRequest, error: WebResourceError) {
+            super.onReceivedError(view, request, error)
+            toggleLoading(false)
+        }
+    }
+
+    init {
+        View.inflate(getContext(), R.layout.widget_web_view, this)
+        this.orientation = LinearLayout.VERTICAL
+        toolbar.setNavigationContentDescription(R.string.toolbar_nav_icon_cont_desc)
+        setToolbarPadding()
+
+        webView.setWebViewClient(webClient)
+        webView.settings.javaScriptEnabled = true
+    }
+
+    var viewModel: WebViewViewModel by notNullAndObservable { vm ->
+        vm.webViewURLObservable.subscribe { url ->
+            webView.loadUrl(url)
+        }
+    }
+
+    fun setExitButtonOnClickListener(listener: OnClickListener) {
+        toolbar.setNavigationOnClickListener(listener)
+    }
+
+    open fun setToolbarPadding() {
+        if (statusBarHeight > 0) {
+            toolbar.setPadding(0, statusBarHeight, 0, 0)
+        }
+    }
+
+    private fun toggleLoading(loading: Boolean) {
+        if (loading) {
+            webView.visibility = View.GONE
+            progressView.visibility = View.VISIBLE
+        } else {
+            webView.visibility = View.VISIBLE
+            progressView.visibility = View.GONE
+        }
+    }
+}
