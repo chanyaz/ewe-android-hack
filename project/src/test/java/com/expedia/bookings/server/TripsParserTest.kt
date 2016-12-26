6e2ae7323bbe391cd55820d9f9fc5f1c06d171c4
Silvy Aggarwal <siaggarwal@DELC02RRBN3G8WN.sea.corp.expecn.com>
2016-12-26 12:52:59 +0530

    Show Guest Info on LX itin Card

--- project/src/test/java/com/expedia/bookings/server/TripsParserTest.kt
+++ project/src/test/java/com/expedia/bookings/server/TripsParserTest.kt
@@ -1,9 +1,10 @@
 package com.expedia.bookings.server
 
 import com.expedia.bookings.data.trips.TripHotel
+import com.expedia.bookings.data.trips.Trip
+import com.expedia.bookings.data.trips.TripActivity
 import com.expedia.bookings.test.robolectric.RobolectricRunner
 import okio.Okio
-import org.joda.time.format.DateTimeFormatter
 import org.joda.time.format.ISODateTimeFormat
 import org.json.JSONObject
 import org.junit.Test
@@ -49,5 +50,71 @@ class TripsParserTest {
         hotelTripJsonObj.remove("checkOutDateTime")
 
         return hotelTripJsonObj
+    }
+
+    @Test
+    fun testLXTripParsingMultipleTravelerType() {
+        val data = Okio.buffer(Okio.source(File("../lib/mocked/templates/api/trips/lx_trip_details.json"))).readUtf8()
+        val jsonObject = JSONObject(data)
+        val jsonArray = jsonObject.getJSONArray("responseData")
+        val lxTripJsonObj = jsonArray.get(0) as JSONObject
+
+        val tripParser = TripParser()
+        var trip: Trip
+        try {
+            trip = tripParser.parseTrip(lxTripJsonObj)
+        }
+        catch (e: Exception) {
+            fail("Oops, we shouldn't have ended up here")
+        }
+        val tripActivity = trip.tripComponents.get(0) as TripActivity
+        val activity = tripActivity.getActivity()
+        assertEquals("200E974C-C7DA-445E-A392-DD12578A96A0_0_358734_358736", activity.id)
+        assertEquals("Day Trip to New York by Train with Hop-on Hop-Off Pass: Full-Day Excursion", activity.title)
+        assertEquals(5, activity.guestCount)
+    }
+
+    @Test
+    fun testLXTripParsingSingleTravelerType() {
+        val data = Okio.buffer(Okio.source(File("../lib/mocked/templates/api/trips/lx_trip_details.json"))).readUtf8()
+        val jsonObject = JSONObject(data)
+        val jsonArray = jsonObject.getJSONArray("responseData")
+        val lxTripJsonObj = jsonArray.get(1) as JSONObject
+
+        val tripParser = TripParser()
+        var trip: Trip
+        try {
+            trip = tripParser.parseTrip(lxTripJsonObj)
+        }
+        catch (e: Exception) {
+            fail("Oops, we shouldn't have ended up here")
+        }
+        val tripActivity = trip.tripComponents.get(0) as TripActivity
+        val activity = tripActivity.getActivity()
+        assertEquals("8AEE006B-E82D-40C1-A77D-5063EF3D47A9_0_224793_224797", activity.id)
+        assertEquals("Shared Shuttle: Detroit International Airport (DTW): Hotels to Airport in Detroit City Center", activity.title)
+        assertEquals(1, activity.guestCount)
+    }
+
+    @Test
+    fun testLXTripParsingNoTraveler() {
+        val data = Okio.buffer(Okio.source(File("../lib/mocked/templates/api/trips/lx_trip_details.json"))).readUtf8()
+        val jsonObject = JSONObject(data)
+        val jsonArray = jsonObject.getJSONArray("responseData")
+        val lxTripJsonObj = jsonArray.get(2) as JSONObject
+
+        val tripParser = TripParser()
+        var trip: Trip
+        try {
+            trip = tripParser.parseTrip(lxTripJsonObj)
+        }
+        catch (e: Exception) {
+            fail("Oops, we shouldn't have ended up here")
+        }
+        val tripActivity = trip.tripComponents.get(0) as TripActivity
+        val activity = tripActivity.getActivity()
+        assertEquals("8AEE006B-E82D-40C1-A77D-5063EF3D47A9_0_224793_224797", activity.id)
+        assertEquals("Shared Shuttle: Detroit International Airport (DTW): Hotels to Airport in Detroit City Center", activity.title)
+        assertEquals(0, activity.guestCount)
     }
 }
