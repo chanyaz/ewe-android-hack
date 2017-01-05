293161b4a26fc68f1e23455d57794ea25fda6594
Malcolm Nguyen <malcolm@mobiata.com>
2017-01-05 11:02:59 -0800

    Revert "Reset app module when changing POS"
    This reverts commit 833ff2f50c0c880bbed986c268cf45f3643ac3b7.

--- project/src/debug/java/com/expedia/bookings/utils/TestEncryptionUtil.kt
+++ project/src/debug/java/com/expedia/bookings/utils/TestEncryptionUtil.kt
@@ -1,6 +1,7 @@
 package com.expedia.bookings.utils
 
 import android.content.Context
+import android.provider.Settings
 import okio.Okio
 import org.bouncycastle.jce.X509Principal
 import org.bouncycastle.x509.X509V3CertificateGenerator
