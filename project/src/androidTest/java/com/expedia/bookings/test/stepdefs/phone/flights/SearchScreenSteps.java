39f8ed2ae0374a23b6b84d9add46654c165476aa
Lalit Sagar <lalitsagar0411@gmail.com>
2017-01-16 17:07:01 +0530

    Adding UI tests for calender widget for flights

--- project/src/androidTest/java/com/expedia/bookings/test/stepdefs/phone/flights/SearchScreenSteps.java
+++ project/src/androidTest/java/com/expedia/bookings/test/stepdefs/phone/flights/SearchScreenSteps.java
@@ -8,8 +8,16 @@ import com.expedia.bookings.R;
 import com.expedia.bookings.test.espresso.RecyclerViewAssertions;
 import com.expedia.bookings.test.phone.newflights.FlightsScreen;
 import com.expedia.bookings.test.phone.pagemodels.common.SearchScreen;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
+import android.support.test.espresso.matcher.ViewMatchers;
 
 import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
@@ -20,6 +28,8 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.hasSibling;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -84,6 +94,51 @@ public class SearchScreenSteps {
 	@And("^I select one way trip$")
 	public void selectOneWayTrip() throws Throwable {
 		FlightsScreen.selectOneWay();
+	}
+
+	@And("^I choose departure date for flights-roundtrip and validate the tool tip")
+	public void selectRoundTripDepartureDateAndValidate(Map<String, String> parameters) throws Throwable {
+		LocalDate stDate = LocalDate.now().plusDays(Integer.parseInt(parameters.get("start_date")));
+		Format dateFormatter = new SimpleDateFormat("MMM d", Locale.US);
+		String stDateStr = dateFormatter.format(stDate.toDate()).toString();
+
+		//choose departure date
+		SearchScreen.chooseDates(stDate, null);
+
+		//validate calender tooltip and subtilte
+		SearchScreen.validateDatesToolTip(stDateStr, "Next: Select return date");
+		validateCalenderSubtitle(stDateStr + " – Select return date");
+	}
+
+	@And("^I choose return date for flights-roundtrip and validate the tool tip")
+	public void selectRoundTripReturnDateAndValidate(Map<String, String> parameters) throws Throwable {
+		LocalDate stDate = LocalDate.now().plusDays(Integer.parseInt(parameters.get("start_date")));
+		LocalDate endDate = LocalDate.now().plusDays(Integer.parseInt(parameters.get("end_date")));
+		Format dateFormatter = new SimpleDateFormat("MMM d", Locale.US);
+		String stDateStr = dateFormatter.format(stDate.toDate()).toString();
+		String endDateStr = dateFormatter.format(endDate.toDate()).toString();
+
+		//choose return date
+		SearchScreen.chooseDates(stDate, endDate);
+
+		//validate calender tooltip and subtitle
+		SearchScreen.validateDatesToolTip(stDateStr + " - " + endDateStr, "Drag to modify");
+		validateCalenderSubtitle(stDateStr + " - " + endDateStr);
+	}
+
+	@And("^I choose departure date for flights-oneway and validate the tool tip")
+	public void selectOneWayDepartureDateAndValidate(Map<String, String> parameters) throws Throwable {
+		LocalDate stDate = LocalDate.now().plusDays(Integer.parseInt(parameters.get("start_date")));
+		Format dateFormatter = new SimpleDateFormat("MMM d", Locale.US);
+		String stDateStr = dateFormatter.format(stDate.toDate()).toString();
+
+		//choose departure date
+		SearchScreen.chooseDates(stDate, null);
+
+		//validate calender tooltip and subtitle
+		SearchScreen.validateDatesToolTip(stDateStr, "Drag to modify");
+		validateCalenderSubtitle(stDateStr + " (One Way)");
+
 	}
 
 	@And("^I pick departure date for flights$")
@@ -386,5 +441,114 @@ public class SearchScreenSteps {
 		else {
 			checkString(R.id.flight_duration_text_view, "Nonstop");
 		}
+	}
+
+	@Then("^Validate that Previous month arrow is displayed: (true|false)$")
+	public void validatePreviousMonthArrowVisibility(boolean isDisplayed) throws Throwable {
+		validateViewVisibility(R.id.previous_month, isDisplayed);
+	}
+
+	@Then("^Validate that Next month arrow is displayed: (true|false)$")
+	public void validateNextMonthArrowVisibility(boolean isDisplayed) throws Throwable {
+		validateViewVisibility(R.id.next_month, isDisplayed);
+	}
+
+	@Then("^Validate that Calender widget is displayed: (true|false)")
+	public void validateCalenderWidgetVisibility(boolean isDisplayed) throws Throwable {
+		validateViewVisibility(R.id.parentPanel, isDisplayed);
+	}
+
+	private void validateViewVisibility(int resId, boolean isDisplayed) {
+		ViewMatchers.Visibility visibility = ViewMatchers.Visibility.INVISIBLE;
+		if (isDisplayed) {
+			visibility = ViewMatchers.Visibility.VISIBLE;
+		}
+		onView(withId(resId)).check(matches(withEffectiveVisibility(visibility)));
+	}
+
+	@Then("^Validate that Done button is disabled$")
+	public void validateDoneButtonDisabled() throws Throwable {
+		onView(withId(R.id.parentPanel)).check(matches(hasDescendant(allOf(withId(android.R.id.button1),
+			withText("DONE"), not(isEnabled())))));
+	}
+
+	@Then("^Validate that Done button is enabled")
+	public void validateDoneButtonEnabled() throws Throwable {
+		onView(withId(R.id.parentPanel)).check(matches(hasDescendant(allOf(withId(android.R.id.button1),
+			withText("DONE"), isEnabled()))));
+	}
+
+	@Then("^Validate that Current Month calender is displayed$")
+	public void validateCurrentMonthYearInCalender() throws Throwable {
+		Format dateFormatter = new SimpleDateFormat("MMMM yyyy", Locale.US);
+		String todayDateStr = dateFormatter.format(new Date());
+		validateMonthYearOfCalender(todayDateStr);
+	}
+
+	private void validateMonthYearOfCalender(String monthYearString) throws Throwable {
+		onView(allOf(withId(R.id.current_month), withText(monthYearString))).check(matches(isDisplayed()));
+	}
+
+	@Then("^Validate that \"(.*?)\" text below calender title is displayed$")
+	public void validateCalenderSubtitle(String text) throws Throwable {
+		SearchScreen.calendarSubtitle().check(matches(allOf(withText(text), isDisplayed())));
+	}
+
+	@Given("^I Click on Select Dates button for flights$")
+	public void clickSelectDatedButton() throws Throwable {
+		SearchScreen.selectDateButton().perform(click());
+	}
+
+	@And("^I Click on Done button$")
+	public void clickDoneButton() throws Throwable {
+		SearchScreen.searchAlertDialogDone().perform(click());
+	}
+
+	@And("^Validate the selected date on calender button$")
+	public void validateCalenderButtonText(Map<String, String> parameters) throws Throwable {
+		Format dateFormatter = new SimpleDateFormat("MMM d", Locale.US);
+		LocalDate stDate = LocalDate.now().plusDays(Integer.parseInt(parameters.get("start_date")));
+		LocalDate endDate = null;
+		boolean isRoundTrip = true;
+
+		String stDateStr = dateFormatter.format(stDate.toDate()).toString().trim();
+
+		//Currently, we are using two different versions of hyphen in our code:
+		// When user has selected only departure date, Mar 23 – Select return date
+		// When user has selected both departure date and return date, Mar 23 - Apr 8
+		String endDateStr = " – Select return date";
+
+		if (parameters.get("end_date") != null) {
+			endDate = LocalDate.now().plusDays(Integer.parseInt(parameters.get("end_date")));
+			endDateStr = " - " + dateFormatter.format(endDate.toDate()).toString().trim();
+		}
+		if (parameters.get("isRoundTrip") != null) {
+			isRoundTrip = new Boolean(parameters.get("isRoundTrip"));
+		}
+
+		if (isRoundTrip) {
+			SearchScreen.calendarCard().check(matches(withText(stDateStr + endDateStr)));
+		}
+		else {
+			SearchScreen.calendarCard().check(matches(withText(stDateStr + " (One Way)")));
+		}
+	}
+
+	@Then("^I click on Next month button$")
+	public void clickNextMonthButton() throws Throwable {
+		SearchScreen.nextMonthButton().perform(click());
+
+	}
+	@Then("^I click on Previous month button$")
+	public void clickPreviosMonthButton() throws Throwable {
+		SearchScreen.previousMonthButton().perform(click());
+
+	}
+
+	@And("^Validate that next month calender is displayed$")
+	public void validateNextMonthCalenderTitle() throws Throwable {
+		Format dateFormatter = new SimpleDateFormat("MMMM yyyy", Locale.US);
+		String nextMonthYearStr = dateFormatter.format(LocalDate.now().plusMonths(1).toDate()).toString();
+		validateMonthYearOfCalender(nextMonthYearStr);
 	}
 }
