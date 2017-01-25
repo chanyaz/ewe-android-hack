dd3bd05659e3f79884130fceeb9ad8a6204293b6
Danny Briggs <dbriggs@expedia.com>
2017-01-25 13:14:48 -0600

    8757 - Updating to use vipV2 add supports for a much larger set of pos
    Adding a new version to ensure no breaking changes with iOS
    They are in the middle of a rewrite and a change like this would need to be confirmed for both old and new paths
    with a new version flag they can update when they are ready.

--- project/src/test/java/com/expedia/bookings/test/PointOfSaleTestConfiguration.java
+++ project/src/test/java/com/expedia/bookings/test/PointOfSaleTestConfiguration.java
@@ -11,6 +11,7 @@ import android.content.Context;
 
 import com.expedia.bookings.data.pos.PointOfSale;
 import com.expedia.bookings.data.pos.PointOfSaleConfigHelper;
+import com.expedia.bookings.data.pos.PointOfSaleId;
 
 public class PointOfSaleTestConfiguration {
 	private static final String ESS_LOCALES_JSON = "[\"en_US\"]";
@@ -34,17 +35,22 @@ public class PointOfSaleTestConfiguration {
 	 * @param isTablet true if in tablet UI mode, false if in phone UI mode
 	 */
 	public static void configurePointOfSale(Context context, String posConfigFilename, boolean isTablet) {
+		configurePOS(context, posConfigFilename, Integer.toString(PointOfSaleId.UNITED_STATES.getId()), isTablet);
+	}
+
+	public static void configurePOS(Context context, String posConfigFilename, String posKey, boolean isTablet) {
 		PointOfSaleConfigHelper mockConfigHelper = Mockito.mock(PointOfSaleConfigHelper.class);
 
 		Mockito.doAnswer(createInputStreamAnswerForString(ESS_LOCALES_JSON))
-				.when(mockConfigHelper).openExpediaSuggestSupportedLocalesConfig();
+			.when(mockConfigHelper).openExpediaSuggestSupportedLocalesConfig();
 		Mockito.doAnswer(createInputStreamAnswerForString(OPTIONAL_POSTCODE_COUNTRY_JSON))
-				.when(mockConfigHelper).openPaymentPostalCodeOptionalCountriesConfiguration();
+			.when(mockConfigHelper).openPaymentPostalCodeOptionalCountriesConfiguration();
 		Mockito.doAnswer(createInputStreamAnswerForAssetFile(context, posConfigFilename))
-				.when(mockConfigHelper).openPointOfSaleConfiguration();
+			.when(mockConfigHelper).openPointOfSaleConfiguration();
 
-		PointOfSale.init(mockConfigHelper, "29", true, isTablet);
+		PointOfSale.init(mockConfigHelper, posKey, true, isTablet);
 	}
+
 
 	private static Answer<InputStream> createInputStreamAnswerForString(final String str) {
 		return new Answer<InputStream>() {
