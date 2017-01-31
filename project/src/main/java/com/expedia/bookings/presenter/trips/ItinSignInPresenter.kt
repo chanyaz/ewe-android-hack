8663325a7d65347b0ed0c191ad65f837948a65c5
Malcolm Nguyen <malcolm@mobiata.com>
2017-01-31 10:50:13 -0800

    Add transition to be ready to show add guest itin

--- project/src/main/java/com/expedia/bookings/presenter/trips/ItinSignInPresenter.kt
+++ project/src/main/java/com/expedia/bookings/presenter/trips/ItinSignInPresenter.kt
@@ -1,28 +1,24 @@
 package com.expedia.bookings.presenter.trips
 
 import android.content.Context
-import android.support.v7.widget.CardView
 import android.util.AttributeSet
 import android.view.View
 import com.expedia.bookings.R
 import com.expedia.bookings.presenter.Presenter
 import com.expedia.bookings.utils.bindView
-import com.expedia.bookings.widget.TextView
-import com.expedia.util.subscribeOnClick
-import com.expedia.vm.itin.ItinSignInViewModel
 
 class ItinSignInPresenter(context: Context, attr: AttributeSet?) : Presenter(context, attr) {
-    val signInContainer: CardView by bindView(R.id.account_sign_in_container)
-    val signInText: TextView by bindView(R.id.login_text_view)
+    val signInWidget: ItinSignInWidget by bindView(R.id.sign_in_widget)
 
-    val itinSignInViewModel= ItinSignInViewModel(context)
+    private val defaultTransition = object : Presenter.DefaultTransition(ItinSignInWidget::class.java.name) {
+        override fun endTransition(forward: Boolean) {
+            signInWidget.visibility = View.VISIBLE
+        }
+    }
 
     init {
         View.inflate(context, R.layout.itin_sign_in_presenter, this)
-
-        signInText.text = itinSignInViewModel.getSignInText()
-        signInText.contentDescription = itinSignInViewModel.getSignInContentDescription()
-        signInContainer.subscribeOnClick(itinSignInViewModel.signInClickSubject)
-
+        addDefaultTransition(defaultTransition)
+        show(signInWidget)
     }
 }
