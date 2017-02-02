f75c70fc08bb557159e063792e9c27e0500c1a65
Supreeth <supreeth@mobiata.com>
2017-02-02 15:21:07 -0800

    9709. Add Guest Itin Progress Bar
    Adding new progress bar instead of a dialog.

--- project/src/main/java/com/expedia/bookings/widget/itin/AddGuestItinWidget.kt
+++ project/src/main/java/com/expedia/bookings/widget/itin/AddGuestItinWidget.kt
@@ -1,14 +1,13 @@
 package com.expedia.bookings.presenter.trips
 
-import android.app.ProgressDialog
 import android.content.Context
+import android.support.design.widget.TextInputEditText
 import android.util.AttributeSet
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import com.expedia.bookings.R
-import com.expedia.bookings.utils.AccessibilityUtil
 import com.expedia.bookings.utils.bindView
 import com.expedia.util.notNullAndObservable
 import com.expedia.vm.itin.AddGuestItinViewModel
@@ -16,23 +15,20 @@ import com.expedia.vm.itin.AddGuestItinViewModel
 class AddGuestItinWidget(context: Context, attr: AttributeSet?) : LinearLayout(context, attr) {
 
     val findItinButton: Button by bindView(R.id.find_itinerary_button)
-    val itinNumberEditText: EditText by bindView(R.id.itin_number_edit_text)
+    val itinNumberEditText: TextInputEditText by bindView(R.id.itin_number_edit_text)
     val guestEmailEditText: EditText by bindView(R.id.email_edit_text)
-
-    private val guestItinSearchDialog = ProgressDialog(context)
+    val loadingContainer: LinearLayout by bindView(R.id.loading_container)
+    val addGuestFormFieldContainer: LinearLayout by bindView(R.id.outer_container)
 
     var viewModel: AddGuestItinViewModel by notNullAndObservable { vm ->
         vm.showSearchDialogObservable.subscribe { show ->
             if (show) {
-                guestItinSearchDialog.show()
-                guestItinSearchDialog.setContentView(R.layout.process_dialog_layout)
-                AccessibilityUtil.delayedFocusToView(guestItinSearchDialog.findViewById(R.id.progress_dialog_container), 0)
-                guestItinSearchDialog.findViewById(R.id.progress_dialog_container).contentDescription = context.getString(R.string.spinner_text_add_guest_trip)
-                announceForAccessibility(context.getString(R.string.spinner_text_add_guest_trip))
+                loadingContainer.visibility = View.VISIBLE
+                addGuestFormFieldContainer.visibility = View.GONE
             } else {
-                guestItinSearchDialog.dismiss()
+                loadingContainer.visibility = View.GONE
+                addGuestFormFieldContainer.visibility = View.VISIBLE
             }
-
         }
     }
 
@@ -42,11 +38,13 @@ class AddGuestItinWidget(context: Context, attr: AttributeSet?) : LinearLayout(c
         findItinButton.setOnClickListener {
             viewModel.performGuestTripSearch.onNext(Pair(guestEmailEditText.text.toString(), itinNumberEditText.text.toString()))
         }
-        setUpDialog()
-    }
 
-    private fun setUpDialog() {
-        guestItinSearchDialog.setCancelable(false)
-        guestItinSearchDialog.isIndeterminate = true
+        itinNumberEditText.setOnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                (view as EditText).hint = context.getString(R.string.itinerary_number_hint)
+            } else {
+                (view as EditText).hint = ""
+            }
+        }
     }
 }
