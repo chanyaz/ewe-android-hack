8225b5bfd402ba2477fd96d843d8aef243129b19
Win <srodjanamongko@expedia.com>
2017-02-09 10:05:12 -0600

    9800: fix hotel crash trying to show alert on activity that finish

--- lib/ExpediaBookings/src/main/java/com/expedia/bookings/services/HotelServices.kt
+++ lib/ExpediaBookings/src/main/java/com/expedia/bookings/services/HotelServices.kt
@@ -25,7 +25,7 @@ import rx.Scheduler
 import rx.Subscription
 import rx.subjects.PublishSubject
 
-class HotelServices(endpoint: String, okHttpClient: OkHttpClient, interceptor: Interceptor, val observeOn: Scheduler, val subscribeOn: Scheduler) {
+open class HotelServices(endpoint: String, okHttpClient: OkHttpClient, interceptor: Interceptor, val observeOn: Scheduler, val subscribeOn: Scheduler) {
 
 	val hotelApi: HotelApi by lazy {
 		val gson = GsonBuilder()
@@ -51,7 +51,7 @@ class HotelServices(endpoint: String, okHttpClient: OkHttpClient, interceptor: I
 			.subscribe(observer)
 	}
 
-	fun search(params: HotelSearchParams, numberOfResults: Int, resultsResponseReceivedObservable: PublishSubject<Unit>? = null): Observable<HotelSearchResponse> {
+	open fun search(params: HotelSearchParams, numberOfResults: Int, resultsResponseReceivedObservable: PublishSubject<Unit>? = null): Observable<HotelSearchResponse> {
 		// null out regionId and lat/lng if they're not set so we don't pass them in the request (Hotels API requirement #7218)
 		val lat = if (params.suggestion.coordinates.lat != 0.0) params.suggestion.coordinates.lat else null
 		val lng = if (params.suggestion.coordinates.lng != 0.0) params.suggestion.coordinates.lng else null
