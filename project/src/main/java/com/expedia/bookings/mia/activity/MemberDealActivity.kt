01cb6136396b944d0e76e2460e28d41e390908ab
bclifton-exp <bclifton@expedia.com>
2017-02-27 17:48:40 -0500

    Adding toolbar to MOD activity plus close button funcionality
    Adding the base toolbar to the MOD activity and the style + X button
    and closing logic.
    
    Toolbar style changes

--- project/src/main/java/com/expedia/bookings/mia/activity/MemberDealActivity.kt
+++ project/src/main/java/com/expedia/bookings/mia/activity/MemberDealActivity.kt
@@ -2,11 +2,17 @@ package com.expedia.bookings.mia.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import com.expedia.bookings.R
 
 class MemberDealActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.member_deal_activity)
+
+        val toolBar = findViewById(R.id.mod_search_toolbar) as Toolbar
+        toolBar.setNavigationOnClickListener { view ->
+            onBackPressed()
+        }
     }
 }
