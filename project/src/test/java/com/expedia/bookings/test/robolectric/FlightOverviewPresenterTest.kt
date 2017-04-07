a6d86ae2096b4307ba8a20e7ef027b7ca09fece2
Thomas Harman <tharman@expedia.com>
2017-04-06 22:12:40 -0700

    Fix BillingDetailsPaymentWidgetTests using immediate scheduler runner

--- project/src/test/java/com/expedia/bookings/test/robolectric/FlightOverviewPresenterTest.kt
+++ project/src/test/java/com/expedia/bookings/test/robolectric/FlightOverviewPresenterTest.kt
@@ -32,7 +32,6 @@ import com.mobiata.android.util.SettingUtils
 import org.joda.time.LocalDate
 import org.joda.time.format.DateTimeFormat
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
