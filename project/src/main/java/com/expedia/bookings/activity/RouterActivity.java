46c23117e5be18c137853c2d9eccea6d4575d8d6
Thomas Harman <tharman@expedia.com>
2017-04-12 08:34:21 -0700

    T1511 - Fetch launch screen itin card ab experiments on launch of app

--- project/src/main/java/com/expedia/bookings/activity/RouterActivity.java
+++ project/src/main/java/com/expedia/bookings/activity/RouterActivity.java
@@ -95,6 +95,8 @@ public class RouterActivity extends Activity implements UserAccountRefresher.IUs
 			query.addExperiment(AbacusUtils.EBAndroidAppTripsNewSignInPage);
 			query.addExperiment(AbacusUtils.EBAndroidAppShowAirAttachMessageOnLaunchScreen);
 			query.addExperiment(AbacusUtils.EBAndroidAppShowCarWebView);
+			query.addExperiment(AbacusUtils.EBAndroidAppLaunchShowGuestItinCard);
+			query.addExperiment(AbacusUtils.EBAndroidAppLaunchShowActiveItinCard);
 			if (FeatureToggleUtil.isFeatureEnabled(this, R.string.preference_itin_crystal_theme)) {
 				query.addExperiment(AbacusUtils.EBAndroidAppItinCrystalSkin);
 			}
