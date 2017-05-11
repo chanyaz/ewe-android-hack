fae48927bdc84525c53455171df30950d2200e62
Vaidehi Deshpande <v-vdeshpande@expedia.com>
2017-05-11 20:52:12 +0530

    Updated fastlane to support multibrands
    New changes :
    - Accepts multiple flavors and versions in a comma separated string
    - Fetches the current version for all the flavors
    - Forms branches based on the multiple flavors and their versions
    - Updates the new versions in Build.gradle file for all the flavors

--- tools/change_version.pl
+++ tools/change_version.pl
@@ -2,36 +2,45 @@
 #
 #
 
-if ($#ARGV < 2) {
-  print "Usage: change_version.pl <path/to/build.gradle> <flavor> <version>\n";
+if ($#ARGV < 1) {
+  print "Usage: change_version.pl <path/to/build.gradle> <flavor-version> <flavor-version>...<flavor-version> \n";
   exit;
 }
 
-my $fullContent = "";
-my $flavor = $ARGV[1];
-my ($major, $minor, $patch, $build) = split(/\./, $ARGV[2]);
+my @params = @ARGV[1..@ARGV-1];
+my %flavor_versions;
+
+foreach my $param(@params) {
+  my ($flavor,$version)= split(/-/, $param);
+  $flavor_versions{$flavor} = $version;
+}
 
 $patch //= 0;
 $build //= 0;
 
-open (BUILDFILE, $ARGV[0]);
-while (<BUILDFILE>) {
-  $fullContent = $fullContent . $_;
-}
-close BUILDFILE;
+for my $flavor(keys %flavor_versions) {
 
-if ($fullContent =~ /(\A.*?$flavor \{)([^\}]*)(\}.*?\z)/s) {
-  my $prefix = $1;
-  my $flavorContent = $2;
-  my $postfix = $3;
+  my $fullContent = "";
+  open(BUILDFILE, '+<', $ARGV[0]);
+  while (<BUILDFILE>) {
+    $fullContent = $fullContent . $_;
+  }
+  seek(BUILDFILE,0,0);
 
-  $flavorContent =~ s/(\A.*?def major = )\d+(.*?\z)/$1$major$2/s;
-  $flavorContent =~ s/(\A.*?def minor = )\d+(.*?\z)/$1$minor$2/s;
-  $flavorContent =~ s/(\A.*?def patch = )\d+(.*?\z)/$1$patch$2/s;
-  $flavorContent =~ s/(\A.*?def build = )\d+(.*?\z)/$1$build$2/s;
+  my ($major, $minor, $patch, $build) = split(/\./, $flavor_versions{$flavor});
+  for ($major, $minor, $patch, $build) { $_ = 0 if $_ eq ''; }
 
-  open(BUILDFILE, '>', $ARGV[0]);
-  print BUILDFILE $prefix . $flavorContent . $postfix;
+  if ($fullContent =~ /(\A.*?$flavor \{)([^\}]*)(\}.*?\z)/s) {
+    my $prefix = $1;
+    my $flavorContent = $2;
+    my $postfix = $3;
+
+    $flavorContent =~ s/(\A.*?def major = )\d+(.*?\z)/$1$major$2/s;
+    $flavorContent =~ s/(\A.*?def minor = )\d+(.*?\z)/$1$minor$2/s;
+    $flavorContent =~ s/(\A.*?def patch = )\d+(.*?\z)/$1$patch$2/s;
+    $flavorContent =~ s/(\A.*?def build = )\d+(.*?\z)/$1$build$2/s;
+
+    print BUILDFILE $prefix . $flavorContent . $postfix;
+  }
   close BUILDFILE;
 }
-
--- tools/extract_version.pl
+++ tools/extract_version.pl
@@ -3,12 +3,13 @@
 #
 
 if ($#ARGV < 1) {
-  print "Usage: extract_version.pl <path/to/build.gradle> <flavor>\n";
+  print "Usage: extract_version.pl <path/to/build.gradle> <flavor 1> <flavor 2>..<flavor n>\n";
   exit;
 }
 
 my $fullContent = "";
-my $flavor = $ARGV[1];
+my @flavors = @ARGV[1..@ARGV-1];
+my @version_numbers;
 
 $patch //= 0;
 $build //= 0;
@@ -19,24 +20,38 @@ while (<BUILDFILE>) {
 }
 close BUILDFILE;
 
-if ($fullContent =~ /(\A.*?$flavor \{)([^\}]*)(\}.*?\z)/s) {
-  my $flavorContent = $2;
+foreach my $flavor (@flavors) {
+  if ($fullContent =~ /(\A.*?$flavor \{)([^\}]*)(\}.*?\z)/s) {
+    my $flavorContent = $2;
 
-  if ($flavorContent =~ /.*?def major = (\d+).*?def minor = (\d+).*?def patch = (\d+).*?def build = (\d+)/s) {
-    my $major = $1;
-    my $minor = $2;
-    my $patch = $3;
-    my $build = $4;
+    if ($flavorContent =~ /.*?def major = (\d+).*?def minor = (\d+).*?def patch = (\d+).*?def build = (\d+)/s) {
+      my $major = $1;
+      my $minor = $2;
+      my $patch = $3;
+      my $build = $4;
 
-    $version = $major . "." . $minor;
-    if ($patch != 0 || $build != 0) {
-      $version .= "." . $patch;
-      if ($build != 0) {
-        $version .= "." . $build;
+      $version = $major . "." . $minor;
+      if ($patch != 0 || $build != 0) {
+        $version .= "." . $patch;
+        if ($build != 0) {
+          $version .= "." . $build;
+        }
       }
+      if($version eq "") { break; }
+      push @version_numbers, $version;
     }
-
-    print $version;
+    else {
+      break;
+    }
+  }
+  else {
+    break;
   }
 }
 
+if(scalar @version_numbers eq scalar @flavors) {
+  print join(',',@version_numbers);
+}
+else {
+  print "";
+}
