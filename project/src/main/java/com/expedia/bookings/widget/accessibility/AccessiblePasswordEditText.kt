59dc3b50ba5908e728bf07a5f476509c50d7faf4
Christian Gonzalez <christian.anchors@gmail.com>
2017-06-06 13:18:16 -0700

    d/a11y lack of null safety crashing both cko forms
    -seems to be affecting material forms specifically, added null safety for other
    a11y related edit text extensions

--- project/src/main/java/com/expedia/bookings/widget/accessibility/AccessiblePasswordEditText.kt
+++ project/src/main/java/com/expedia/bookings/widget/accessibility/AccessiblePasswordEditText.kt
@@ -7,7 +7,7 @@ import android.util.AttributeSet
 class AccessiblePasswordEditText(context: Context, attributeSet: AttributeSet) : AccessibleEditText(context, attributeSet) {
 
     override fun getAccessibilityNodeInfo(): String {
-        val hint = (this.parent as? TextInputLayout)?.hint ?: this.hint.toString() ?: ""
+        val hint = (this.parent as? TextInputLayout)?.hint ?: this.hint?.toString() ?: ""
         val error = (this.parent as? TextInputLayout)?.error ?:  errorMessage
         val sb: StringBuilder = StringBuilder(" $hint")
         if (!valid) {
