8bb09019552f2ba5ec20dd22d9ad36585dbeca24
Rajan Kochhar <rkochhar@expedia.com>
2017-06-19 23:55:48 +0530

    machine learning

--- project/src/main/java/com/expedia/bookings/activity/HackActivity.kt
+++ project/src/main/java/com/expedia/bookings/activity/HackActivity.kt
@@ -4,9 +4,25 @@ import android.os.Bundle
 import android.support.design.widget.AppBarLayout
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.expedia.bookings.R
+import com.expedia.bookings.data.rail.responses.TranslateResponse
+import com.expedia.bookings.services.TranslateServices
+import com.jakewharton.rxbinding.widget.RxTextView
+import rx.Observer
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class HackActivity : AppCompatActivity() {
+
+    var inputTranslate: EditText? = null
+    var outputTranslate: TextView? = null
+    val translateServices: TranslateServices = TranslateServices(AndroidSchedulers.mainThread(), Schedulers.io())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +30,8 @@ class HackActivity : AppCompatActivity() {
         val myToolbar = findViewById(R.id.main_toolbar) as Toolbar
 
         val appBarLayout = findViewById(R.id.main_appbar) as AppBarLayout
+        inputTranslate = findViewById(R.id.input_translate) as EditText
+        outputTranslate = findViewById(R.id.output_translate) as TextView
         appBarLayout.addOnOffsetChangedListener({ appBarLayout: AppBarLayout, offset: Int ->
             if (Math.abs(offset) >= appBarLayout.getTotalScrollRange()) {
                 myToolbar.setNavigationIcon(R.drawable.ic_action_bar_brand_logo)
@@ -21,6 +39,26 @@ class HackActivity : AppCompatActivity() {
                 myToolbar.navigationIcon = null
             }
         })
+
+        RxTextView.textChanges(inputTranslate as EditText).debounce(500, TimeUnit.MILLISECONDS).subscribe {
+            Log.d("Testingggg", it.toString())
+            translateServices.translate(it.toString(), object : Observer<TranslateResponse> {
+                override fun onError(e: Throwable?) {
+                    //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onCompleted() {
+                    //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onNext(t: TranslateResponse?) {
+                    Log.d("Testingggg", t!!.text[0])
+                    (outputTranslate as TextView).text = t!!.text[0]
+
+                }
+
+            })
+        }
     }
 
 }
