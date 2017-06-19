da66e6e647e9972791598ed6a94d8c612fafbf58
Pulkit Kothari <pkothari@expedia.com>
2017-06-20 00:12:50 +0530

    WIP

--- project/src/main/java/com/expedia/bookings/activity/HackActivity.kt
+++ project/src/main/java/com/expedia/bookings/activity/HackActivity.kt
@@ -3,9 +3,17 @@ package com.expedia.bookings.activity
 import android.os.Bundle
 import android.support.design.widget.AppBarLayout
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
 import com.expedia.bookings.R
+import com.expedia.bookings.data.lx.LXSearchResponse
+import com.expedia.bookings.data.lx.LxSearchParams
+import com.expedia.bookings.services.LxServices
+import com.expedia.bookings.utils.Ui
+import com.expedia.hackathon.LXCrossSellAdapter
 import com.tomerrosenfeld.customanalogclockview.CustomAnalogClock
+import org.joda.time.LocalDate
+import rx.Observer
 import java.util.*
 
 class HackActivity : AppCompatActivity() {
@@ -22,11 +30,36 @@ class HackActivity : AppCompatActivity() {
             myToolbar.navigationIcon = drawable
         })
 
+        setupMyViews()
+    }
+
+    private fun setupMyViews() {
         val departureClock = findViewById(R.id.departure_clock) as CustomAnalogClock
         departureClock.setTimezone(TimeZone.getTimeZone("Asia/Calcutta"))
 
         val arrivalClock = findViewById(R.id.arrival_clock) as CustomAnalogClock
         arrivalClock.setTimezone(TimeZone.getTimeZone("PST"))
+
+        val lxCrossSell = findViewById(R.id.lx_cross_sell) as RecyclerView
+        val params = LxSearchParams.Builder()
+                .location("Paris")
+                .startDate(LocalDate.now())
+                .endDate(LocalDate.now().plusDays(14))
+                .build() as LxSearchParams
+        Ui.getApplication(this).lxComponent().lxService.lxCategorySearch(params, object: Observer<LXSearchResponse> {
+            override fun onError(e: Throwable?) {
+
+            }
+
+            override fun onCompleted() {
+
+            }
+
+            override fun onNext(t: LXSearchResponse?) {
+                lxCrossSell.adapter = LXCrossSellAdapter(t!!.activities)
+            }
+
+        })
     }
 
 }
