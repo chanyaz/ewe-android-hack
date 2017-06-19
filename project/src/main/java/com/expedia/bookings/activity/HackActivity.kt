c8bf6a5c7b5d1f50df1ceb750670c398f830aa4a
Pulkit Kothari <pkothari@expedia.com>
2017-06-19 18:03:04 +0530

    Google home integration

--- project/src/main/java/com/expedia/bookings/activity/HackActivity.kt
+++ project/src/main/java/com/expedia/bookings/activity/HackActivity.kt
@@ -1,17 +1,26 @@
 package com.expedia.bookings.activity
 
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
 import android.support.v7.app.AppCompatActivity
-import android.view.ViewStub
+import android.support.v7.widget.Toolbar
 import com.expedia.bookings.R
-import com.expedia.bookings.hackathon1.FlightTripWidget
-import com.expedia.bookings.utils.bindView
 
 class HackActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.hack_activity)
+        val myToolbar = findViewById(R.id.main_toolbar) as Toolbar
+
+        val appBarLayout = findViewById(R.id.main_appbar) as AppBarLayout
+        appBarLayout.addOnOffsetChangedListener({ appBarLayout: AppBarLayout, offset: Int ->
+            if (Math.abs(offset) >= appBarLayout.getTotalScrollRange()) {
+                myToolbar.setNavigationIcon(R.drawable.ic_action_bar_brand_logo)
+            } else {
+                myToolbar.navigationIcon = null
+            }
+        })
     }
 
 }
