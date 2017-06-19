97bf6c2e94a523d8c425fdf5d889d05e86fab204
Neha Hemnani <nhemnani@DELC02T42QDG8WN.sea.corp.expecn.com>
2017-06-19 17:58:35 +0530

    Added tensor

--- project/src/main/java/com/expedia/bookings/activity/HackActivity.kt
+++ project/src/main/java/com/expedia/bookings/activity/HackActivity.kt
@@ -2,9 +2,13 @@ package com.expedia.bookings.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.ViewStub
 import com.expedia.bookings.R
+import com.expedia.bookings.hackathon1.FlightTripWidget
+import com.expedia.bookings.utils.bindView
 
 class HackActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.hack_activity)
