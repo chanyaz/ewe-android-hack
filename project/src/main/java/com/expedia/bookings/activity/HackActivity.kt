f3afbc84fbb1dd4eb35b0abd45b1feefe0a73ddf
Pulkit Kothari <pkothari@expedia.com>
2017-06-20 14:54:21 +0530

    Alert

--- project/src/main/java/com/expedia/bookings/activity/HackActivity.kt
+++ project/src/main/java/com/expedia/bookings/activity/HackActivity.kt
@@ -1,13 +1,18 @@
 package com.expedia.bookings.activity
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.AppBarLayout
 import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.view.menu.MenuBuilder
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -52,6 +57,30 @@ class HackActivity : AppCompatActivity() {
     var config: SessionConfiguration? = null
     var button: FloatingActionButton? = null
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        if (menu is MenuBuilder) {
+            menu.setOptionalIconsVisible(true)
+        }
+
+        menuInflater.inflate(R.menu.emergency_menu_item, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item == null) {
+            return super.onOptionsItemSelected(item)
+        }
+        when(item.itemId) {
+            R.id.emergency_contact -> {
+                val builder = AlertDialog.Builder(this)
+                builder.setView(View.inflate(this, R.layout.emergency_alert, null))
+                builder.setPositiveButton(getString(R.string.DONE), { dialog, which -> dialog.dismiss() })
+                builder.create()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.hack_activity)
