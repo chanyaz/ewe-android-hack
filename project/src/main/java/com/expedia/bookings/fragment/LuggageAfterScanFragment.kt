7efa459e4e27c862766854068ab61ac85f0dcf5c
Sriniketh Ramachandran <srramachandra@SFOC02R66P0G8WN.sea.corp.expecn.com>
2017-06-20 16:48:42 -0700

    added code to add/update firebase

--- project/src/main/java/com/expedia/bookings/fragment/LuggageAfterScanFragment.kt
+++ project/src/main/java/com/expedia/bookings/fragment/LuggageAfterScanFragment.kt
@@ -13,16 +13,17 @@ class LuggageAfterScanFragment : Fragment() {
     private var tagFoundPublicContainer: ScrollView? = null
     private var tagFoundPrivateContainer: ScrollView? = null
     private var tagNotFoundContainer: ScrollView? = null
+    private var tagId: String? = null
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = LayoutInflater.from(context).inflate(R.layout.fragment_luggage_after_scan, container, false)
+        tagId = arguments.getString("TAG_ID")
 
         tagFoundPublicContainer = Ui.findView(view, R.id.tag_found_public_container)
         tagFoundPrivateContainer = Ui.findView(view, R.id.tag_found_private_container)
         tagNotFoundContainer = Ui.findView(view, R.id.tag_not_found_container)
 
-        //TODO FETCH FROM FIREBASE AND HIDE/SHOW VIEWS
-        tagFoundPublicContainer?.visibility = View.VISIBLE
+
 
         return view
     }
--- project/src/main/java/com/expedia/bookings/luggagetags/ExpediaLuggageTags.kt
+++ project/src/main/java/com/expedia/bookings/luggagetags/ExpediaLuggageTags.kt
@@ -1,8 +1,8 @@
 package com.expedia.bookings.luggagetags
 
-data class ExpediaLuggageTags(val tagID: String,
-                              val expediaUserID: String,
-                              val isPublic: Boolean,
-                              val name: String,
-                              val address: String,
-                              val phoneNumber: String)
+data class ExpediaLuggageTags(val tagID: String = "",
+                              val expediaUserID: String = "",
+                              val isPublic: Boolean = false,
+                              val name: String = "",
+                              val address: String = "",
+                              val phoneNumber: String = "")
