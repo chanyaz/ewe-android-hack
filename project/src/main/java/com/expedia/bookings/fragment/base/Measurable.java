90015cf2f9460cc3b3215d7321ac6957e66f96f4
Sarthak Deshwal <sdeshwal@DELC02PQ65WG8WN.sea.corp.expecn.com>
2017-07-11 11:08:19 +0530

    Deleted files with *fragment* not required

--- project/src/main/java/com/expedia/bookings/fragment/base/Measurable.java
+++
@@ -1,9 +0,0 @@
-package com.expedia.bookings.fragment.base;
-
-/**
- * Marks a Fragment (or View or whatever) that can know when
- * it can be measured.
- */
-public interface Measurable {
-	boolean isMeasurable();
-}
