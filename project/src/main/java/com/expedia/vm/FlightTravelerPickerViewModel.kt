69fa8b56e931df6dd5b571acee5f07309233468d
Nidhi Bhardwaj <t-nidbhardwaj@expedia.com>
2017-07-14 19:22:43 +0530

    fixing bugs on new traveler form

--- project/src/main/java/com/expedia/vm/FlightTravelerPickerViewModel.kt
+++ project/src/main/java/com/expedia/vm/FlightTravelerPickerViewModel.kt
@@ -12,22 +12,18 @@ import rx.Observable
 
 class FlightTravelerPickerViewModel(context: Context) : BaseTravelerPickerViewModel(context) {
 
-    private val MAX_GUESTS = 6
     private val MIN_ADULTS = 1
-    private val MIN_YOUTHS = 0
-    private val MIN_CHILDREN = 0
-    private val MIN_INFANTS = 0
+    private val MAX_GUESTS = 6
 
-    private val MAX_YOUTHS = 4
+    private val MIN_CHILDREN = 0
     private val MAX_CHILDREN = 4
-    private val MAX_INFANT = 4
 
     private val DEFAULT_YOUTH_AGE = 16
     private val DEFAULT_CHILD_AGE = 10
     private val DEFAULT_INFANT_AGE = 1
 
-    private val infantAgeUpperLimit = 2
-    private val childAgeUpperLimit = 12
+    private val infantAgeUpperLimit = 1
+    private val childAgeUpperLimit = 11
     private val YouthAgeUpperLimit = 17
 
 
@@ -62,9 +58,10 @@ class FlightTravelerPickerViewModel(context: Context) : BaseTravelerPickerViewMo
             val total = travelers.numberOfAdults + travelers.childrenAges.size
             makeTravelerText(travelers)
 
-            val infant = travelers.childrenAges.count { infantAge -> infantAge < infantAgeUpperLimit }
-            val child = travelers.childrenAges.count { childAge -> childAge in infantAgeUpperLimit..childAgeUpperLimit }
-            val youth = travelers.childrenAges.count { youthAge -> youthAge in (childAgeUpperLimit+1)..YouthAgeUpperLimit }
+            val infant = travelers.childrenAges.count { infantAge -> infantAge <= infantAgeUpperLimit }
+            val child = travelers.childrenAges.count { childAge -> childAge in (infantAgeUpperLimit + 1)..childAgeUpperLimit }
+            val youth = travelers.childrenAges.count { youthAge -> youthAge in (childAgeUpperLimit + 1)..YouthAgeUpperLimit }
+            val totalChildren = youth + child + infant
 
             adultTextObservable.onNext(
                     context.resources.getQuantityString(R.plurals.number_of_adults, travelers.numberOfAdults, travelers.numberOfAdults)
@@ -81,16 +78,18 @@ class FlightTravelerPickerViewModel(context: Context) : BaseTravelerPickerViewMo
             infantTextObservable.onNext(
                     context.resources.getQuantityString(R.plurals.number_of_infant, infant, infant)
             )
+
             adultPlusObservable.onNext(total < MAX_GUESTS)
-            youthPlusObservable.onNext(total < MAX_GUESTS && youth < MAX_YOUTHS)
-            childPlusObservable.onNext(total < MAX_GUESTS && child < MAX_CHILDREN)
-            infantPlusObservable.onNext(total < MAX_GUESTS && infant < MAX_INFANT)
+            youthPlusObservable.onNext(total < MAX_GUESTS && totalChildren < MAX_CHILDREN)
+            childPlusObservable.onNext(total < MAX_GUESTS && totalChildren < MAX_CHILDREN)
+            infantPlusObservable.onNext(total < MAX_GUESTS && totalChildren < MAX_CHILDREN)
+
             validateInfants()
 
             adultMinusObservable.onNext(travelers.numberOfAdults > MIN_ADULTS)
-            youthMinusObservable.onNext(youth > MIN_YOUTHS)
+            youthMinusObservable.onNext(youth > MIN_CHILDREN)
             childMinusObservable.onNext(child > MIN_CHILDREN)
-            infantMinusObservable.onNext(infant > MIN_INFANTS)
+            infantMinusObservable.onNext(infant > MIN_CHILDREN)
 
         }
         isInfantInLapObservable.subscribe { inLap ->
@@ -100,99 +99,76 @@ class FlightTravelerPickerViewModel(context: Context) : BaseTravelerPickerViewMo
 
     // Inputs
     val incrementAdultsObserver: Observer<Unit> = endlessObserver {
-
-            val flightTravelerParams = travelerParamsObservable.value
-            travelerParamsObservable.onNext(TravelerParams(flightTravelerParams.numberOfAdults + 1, flightTravelerParams.childrenAges, emptyList(), emptyList()))
-            trackTravelerPickerClick("Add.Adult")
+        val flightTravelerParams = travelerParamsObservable.value
+        travelerParamsObservable.onNext(TravelerParams(flightTravelerParams.numberOfAdults + 1, flightTravelerParams.childrenAges, emptyList(), emptyList()))
+        trackTravelerPickerClick("Add.Adult")
         adultTravelerCountChangeObservable.onNext(Unit)
-
     }
 
     val decrementAdultsObserver: Observer<Unit> = endlessObserver {
-
-            val flightTravelerParams = travelerParamsObservable.value
-            travelerParamsObservable.onNext(TravelerParams(flightTravelerParams.numberOfAdults - 1, flightTravelerParams.childrenAges, emptyList(), emptyList()))
-            trackTravelerPickerClick("Remove.Adult")
-
+        val flightTravelerParams = travelerParamsObservable.value
+        travelerParamsObservable.onNext(TravelerParams(flightTravelerParams.numberOfAdults - 1, flightTravelerParams.childrenAges, emptyList(), emptyList()))
+        trackTravelerPickerClick("Remove.Adult")
         adultTravelerCountChangeObservable.onNext(Unit)
-
     }
 
     val incrementYouthObserver: Observer<Unit> = endlessObserver {
-
-            val flightTravelerParams = travelerParamsObservable.value
-            travelerParamsObservable.onNext(TravelerParams(flightTravelerParams.numberOfAdults, flightTravelerParams.childrenAges.plusElement(DEFAULT_YOUTH_AGE), emptyList(), emptyList()))
-            trackTravelerPickerClick("Add.Youth")
+        val flightTravelerParams = travelerParamsObservable.value
+        travelerParamsObservable.onNext(TravelerParams(flightTravelerParams.numberOfAdults, flightTravelerParams.childrenAges.plusElement(DEFAULT_YOUTH_AGE), emptyList(), emptyList()))
+        trackTravelerPickerClick("Add.Youth")
         youthTravelerCountChangeObservable.onNext(Unit)
-
     }
 
     val decrementYouthObserver: Observer<Unit> = endlessObserver {
-
-            val flightTravelerParams = travelerParamsObservable.value
-            travelerParamsObservable.onNext(TravelerParams(flightTravelerParams.numberOfAdults, flightTravelerParams.childrenAges.minusElement(DEFAULT_YOUTH_AGE), emptyList(), emptyList()))
-            trackTravelerPickerClick("Remove.Youth")
-
+        val flightTravelerParams = travelerParamsObservable.value
+        travelerParamsObservable.onNext(TravelerParams(flightTravelerParams.numberOfAdults, flightTravelerParams.childrenAges.minusElement(DEFAULT_YOUTH_AGE), emptyList(), emptyList()))
+        trackTravelerPickerClick("Remove.Youth")
         youthTravelerCountChangeObservable.onNext(Unit)
-
     }
 
     val incrementChildrenObserver: Observer<Unit> = endlessObserver {
-
-            val flightTravelerParams = travelerParamsObservable.value
-            travelerParamsObservable.onNext(TravelerParams(flightTravelerParams.numberOfAdults, flightTravelerParams.childrenAges.plusElement(DEFAULT_CHILD_AGE), emptyList(), emptyList()))
-            trackTravelerPickerClick("Add.Child")
-
+        val flightTravelerParams = travelerParamsObservable.value
+        travelerParamsObservable.onNext(TravelerParams(flightTravelerParams.numberOfAdults, flightTravelerParams.childrenAges.plusElement(DEFAULT_CHILD_AGE), emptyList(), emptyList()))
+        trackTravelerPickerClick("Add.Child")
         childTravelerCountChangeObservable.onNext(Unit)
-
     }
 
     val decrementChildrenObserver: Observer<Unit> = endlessObserver {
-
-            val flightTravelerParams = travelerParamsObservable.value
-            travelerParamsObservable.onNext(TravelerParams(flightTravelerParams.numberOfAdults, flightTravelerParams.childrenAges.minusElement(DEFAULT_CHILD_AGE), emptyList(), emptyList()))
-            trackTravelerPickerClick("Remove.Child")
-
+        val flightTravelerParams = travelerParamsObservable.value
+        travelerParamsObservable.onNext(TravelerParams(flightTravelerParams.numberOfAdults, flightTravelerParams.childrenAges.minusElement(DEFAULT_CHILD_AGE), emptyList(), emptyList()))
+        trackTravelerPickerClick("Remove.Child")
         childTravelerCountChangeObservable.onNext(Unit)
-
     }
 
     val incrementInfantObserver: Observer<Unit> = endlessObserver {
-
-            val flightTravelerParams = travelerParamsObservable.value
-            val infant = flightTravelerParams.childrenAges.count { infantAge -> infantAge < infantAgeUpperLimit }
-            if (showSeatingPreference && infant == 0) {
-                infantInSeatObservable.onNext(false)
-            }
-            travelerParamsObservable.onNext(TravelerParams(flightTravelerParams.numberOfAdults, flightTravelerParams.childrenAges.plusElement(DEFAULT_INFANT_AGE), emptyList(), emptyList()))
-            trackTravelerPickerClick("Add.Infant")
-
+        val flightTravelerParams = travelerParamsObservable.value
+        val infant = flightTravelerParams.childrenAges.count { infantAge -> infantAge <= infantAgeUpperLimit }
+        if (showSeatingPreference && infant == 0) {
+            infantInSeatObservable.onNext(false)
+        }
+        travelerParamsObservable.onNext(TravelerParams(flightTravelerParams.numberOfAdults, flightTravelerParams.childrenAges.plusElement(DEFAULT_INFANT_AGE), emptyList(), emptyList()))
+        trackTravelerPickerClick("Add.Infant")
         infantTravelerCountChangeObservable.onNext(Unit)
-
     }
 
     val decrementInfantObserver: Observer<Unit> = endlessObserver {
-
-            val flightTravelerParams = travelerParamsObservable.value
-            val infant = flightTravelerParams.childrenAges.count { infantAge -> infantAge < infantAgeUpperLimit }
-            if (showSeatingPreference && infant == 1) {
-                infantInSeatObservable.onNext(true)
-            }
-            travelerParamsObservable.onNext(TravelerParams(flightTravelerParams.numberOfAdults, flightTravelerParams.childrenAges.minusElement(DEFAULT_INFANT_AGE), emptyList(), emptyList()))
-            trackTravelerPickerClick("Remove.Infant")
-
+        val flightTravelerParams = travelerParamsObservable.value
+        val infant = flightTravelerParams.childrenAges.count { infantAge -> infantAge <= infantAgeUpperLimit }
+        if (showSeatingPreference && infant == 1) {
+            infantInSeatObservable.onNext(true)
+        }
+        travelerParamsObservable.onNext(TravelerParams(flightTravelerParams.numberOfAdults, flightTravelerParams.childrenAges.minusElement(DEFAULT_INFANT_AGE), emptyList(), emptyList()))
+        trackTravelerPickerClick("Remove.Infant")
         infantTravelerCountChangeObservable.onNext(Unit)
-
     }
 
     private fun validateInfants() {
         val travelerParams = travelerParamsObservable.value
-        val numberOfInfants = travelerParams.childrenAges.count { childAge -> childAge < infantAgeUpperLimit }
-        val numberOfChild = travelerParams.childrenAges.count { childAge -> childAge in infantAgeUpperLimit..childAgeUpperLimit }
-        val numberOfYouth = travelerParams.childrenAges.count { youthAge -> youthAge in (childAgeUpperLimit+1)..youthAge }
+        val numberOfInfants = travelerParams.childrenAges.count { infantAge -> infantAge <= infantAgeUpperLimit }
+        val numberOfYouth = travelerParams.childrenAges.count { youthAge -> youthAge > childAgeUpperLimit }
         infantPreferenceSeatingObservable.onNext(numberOfInfants > 0)
-        tooManyInfantsInLap.onNext(showSeatingPreference && isInfantInLapObservable.value && (numberOfInfants > (travelerParams.numberOfAdults + numberOfChild + numberOfYouth)))
-        tooManyInfantsInSeat.onNext(showSeatingPreference && !isInfantInLapObservable.value && (numberOfInfants > 2 && (travelerParams.numberOfAdults + numberOfChild + numberOfYouth) == 1))
+        tooManyInfantsInLap.onNext(showSeatingPreference && isInfantInLapObservable.value && (numberOfInfants > (travelerParams.numberOfAdults + numberOfYouth)))
+        tooManyInfantsInSeat.onNext(showSeatingPreference && !isInfantInLapObservable.value && (numberOfInfants > 2 && (travelerParams.numberOfAdults + numberOfYouth) == 1))
     }
 
 }
