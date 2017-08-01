566cb6d60e8b7ae05af36149b7a6baece5cefc3e
Felicia Lin <fjlin9@gmail.com>
2017-07-31 17:08:44 -0700

    s/4609 added frequent flyer program button to material forms

--- project/src/main/java/com/expedia/bookings/utils/FeatureUtil.kt
+++ project/src/main/java/com/expedia/bookings/utils/FeatureUtil.kt
@@ -27,6 +27,7 @@ fun isSecureIconEnabled(context: Context): Boolean {
 }
 
 fun isFrequentFlyerNumberForFlightsEnabled(context: Context): Boolean {
-    return Db.getAbacusResponse().isUserBucketedForTest(AbacusUtils.EBAndroidAppFlightFrequentFlyerNumber)
-            && FeatureToggleUtil.isFeatureEnabled(context, R.string.preference_enable_flights_frequent_flyer_number)
+    return FeatureToggleUtil.isUserBucketedAndFeatureEnabled(context,
+            AbacusUtils.EBAndroidAppFlightFrequentFlyerNumber,
+            R.string.preference_enable_flights_frequent_flyer_number)
 }
