60efdf02ca0cf279d99cd9f9d4e3c9291f80caa4
spalguyev <a-spalguyev@expedia.com>
2017-08-07 10:49:38 -0700

    Add alternative way of creating shortcuts for Android O and newer. This is because INSTALL_SHORTCUT broadcast is now private starting with Android O

--- project/src/debug/java/com/expedia/bookings/utils/DebugMenuImpl.java
+++ project/src/debug/java/com/expedia/bookings/utils/DebugMenuImpl.java
@@ -4,10 +4,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
+import android.graphics.drawable.Icon;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.Menu;
@@ -111,19 +117,41 @@ class DebugMenuImpl implements DebugMenu {
 				shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 				shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-				Intent addIntent = new Intent();
-				addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
-				addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, getBuildSpecificActivityName(activityInfo.displayName));
-				addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
-					Intent.ShortcutIconResource.fromContext(hostActivity, R.drawable.ic_launcher));
-				addIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
-				hostActivity.sendBroadcast(addIntent);
+				if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N_MR1) {
+					installShortcutToHomeScreen(activityInfo, shortcutIntent);
+				}
+				else {
+					createLongPressShortcut(activityInfo, shortcutIntent);
+				}
 			}
 			catch (ClassNotFoundException e) {
 				Log.d("DebugMenu", "Unable to install shortcuts", e);
 				Toast.makeText(hostActivity, "Unable to install shortcuts; see logs", Toast.LENGTH_LONG).show();
 			}
 		}
+	}
+
+	private void installShortcutToHomeScreen(DebugActivityInfo activityInfo, Intent shortcutIntent) {
+		Intent addIntent = new Intent();
+		addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+		addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, getBuildSpecificActivityName(activityInfo.displayName));
+		addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
+				Intent.ShortcutIconResource.fromContext(hostActivity, R.drawable.ic_launcher));
+		addIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+		hostActivity.sendBroadcast(addIntent);
+	}
+
+	@TargetApi(25)
+	private void createLongPressShortcut(DebugActivityInfo activityInfo, Intent shortcutIntent) {
+		Context context = hostActivity.getApplicationContext();
+		ShortcutManager shortcutManager = context.getSystemService(ShortcutManager.class);
+		ShortcutInfo shortcutInfo = new ShortcutInfo.Builder(context, activityInfo.displayName.replace(" ", ""))
+				.setShortLabel(activityInfo.displayName)
+				.setIcon(Icon.createWithResource(context, R.drawable.ic_launcher))
+				.setIntent(shortcutIntent.setAction(Intent.ACTION_MAIN))
+				.build();
+
+		shortcutManager.addDynamicShortcuts(Collections.singletonList(shortcutInfo));
 	}
 
 	@Override
