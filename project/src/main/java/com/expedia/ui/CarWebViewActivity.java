8d264933946613f08e53fe65795ecfb759486d14
raasinghal <a-raasinghal@expedia.com>
2017-08-17 13:39:07 -0700

    Reverting changes for removing interstitial - this is not a good user experience

--- project/src/main/java/com/expedia/ui/CarWebViewActivity.java
+++ project/src/main/java/com/expedia/ui/CarWebViewActivity.java
@@ -3,9 +3,6 @@ package com.expedia.ui;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ProgressBar;
-import com.expedia.bookings.R;
 import com.expedia.bookings.activity.WebViewActivity;
 import com.expedia.bookings.data.Db;
 import com.expedia.bookings.data.LineOfBusiness;
@@ -16,7 +13,6 @@ import com.expedia.bookings.utils.UserAccountRefresher;
 public class CarWebViewActivity extends WebViewActivity implements UserAccountRefresher.IUserAccountRefreshListener {
 
 	private UserAccountRefresher userAccountRefresher;
-	private ProgressBar mProgressBar;
 
 	public static class IntentBuilder extends WebViewActivity.IntentBuilder {
 		public IntentBuilder(Context context) {
@@ -29,7 +25,6 @@ public class CarWebViewActivity extends WebViewActivity implements UserAccountRe
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		userAccountRefresher = new UserAccountRefresher(this, LineOfBusiness.CARS, this);
-		mProgressBar = (ProgressBar) findViewById(R.id.webview_progress_view);
 	}
 
 	@Override
@@ -61,8 +56,4 @@ public class CarWebViewActivity extends WebViewActivity implements UserAccountRe
 		User.addUserToAccountManager(this, Db.getUser());
 	}
 
-	@Override
-	public void setLoading(boolean loading) {
-		mProgressBar.setVisibility(View.GONE);
-	}
 }
