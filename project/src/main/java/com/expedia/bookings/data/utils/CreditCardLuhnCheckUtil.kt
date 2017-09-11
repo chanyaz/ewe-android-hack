5672b25a55bd8e728ef2c013983f715888c600a3
Kevin Seo <seokev@gmail.com>
2017-09-11 09:37:21 -0700

    s/6002-luhn-algorithm-for-card-numbers

---
+++ project/src/main/java/com/expedia/bookings/data/utils/CreditCardLuhnCheckUtil.kt
@@ -0,0 +1,33 @@
+package com.expedia.bookings.data.utils
+
+object CreditCardLuhnCheckUtil {
+
+    fun cardNumberIsValid(cardNumber: String) : Boolean {
+        try {
+            return sumOfProductsIsValid(sumOfProducts(cardNumber.toLong()))
+        }
+        catch (e: Exception) {
+            return false
+        }
+    }
+
+    private fun sumOfProducts(cardNumber: Long) : Int {
+        var currNumbers = cardNumber
+        var currDigit : Int
+        var currSum = 0
+        while (currNumbers.compareTo(0) != 0) {
+            currSum += (currNumbers % 10).toInt()
+            currDigit = (((currNumbers / 10) % 10) * 2).toInt()
+            if (currDigit > 9) {
+                currDigit -= 9
+            }
+            currSum += currDigit
+            currNumbers /= 100
+        }
+        return currSum
+    }
+
+    private fun sumOfProductsIsValid(totalSum: Int) : Boolean {
+        return totalSum % 10 == 0
+    }
+}
--- project/src/test/java/com/expedia/bookings/test/robolectric/ValidFormOfUtilsTest.kt
+++ project/src/test/java/com/expedia/bookings/test/robolectric/ValidFormOfUtilsTest.kt
@@ -2,8 +2,10 @@ package com.expedia.bookings.test.robolectric
 
 import com.expedia.bookings.data.ValidPayment
 import com.expedia.bookings.data.flights.ValidFormOfPayment
+import com.expedia.bookings.data.utils.CreditCardLuhnCheckUtil
 import com.expedia.bookings.data.utils.ValidFormOfPaymentUtils
 import org.junit.Test
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class ValidFormOfUtilsTest {
@@ -16,7 +18,79 @@ class ValidFormOfUtilsTest {
         assertTrue(oldVfop is ValidPayment)
     }
 
-    fun getValidFormOfPayment(fee: String? = "3.50") : ValidFormOfPayment{
+    @Test
+    fun testInvalidTwoDigitLuhnCheck() {
+        val invalidTwoDigit = "10"
+        assertFalse(CreditCardLuhnCheckUtil.cardNumberIsValid(invalidTwoDigit))
+    }
+
+    @Test
+    fun testValidTwoDigitLuhnCheck() {
+        val validTwoDigit = "00"
+        assertTrue(CreditCardLuhnCheckUtil.cardNumberIsValid(validTwoDigit))
+    }
+
+    @Test
+    fun testInvalidThreeDigitLuhnCheck() {
+        val invalidThreeDigit = "110"
+        assertFalse(CreditCardLuhnCheckUtil.cardNumberIsValid(invalidThreeDigit))
+    }
+
+    @Test
+    fun testValidThreeDigitLuhnCheck() {
+        val validThreeDigit = "240"
+        assertTrue(CreditCardLuhnCheckUtil.cardNumberIsValid(validThreeDigit))
+    }
+
+    @Test
+    fun testInvalidFiveDigitLuhnCheck() {
+        val invalidFiveDigit = "09999"
+        assertFalse(CreditCardLuhnCheckUtil.cardNumberIsValid(invalidFiveDigit))
+    }
+
+    @Test
+    fun testValidFiveDigitLuhnCheck() {
+        val validFiveDigit = "73890"
+        assertTrue(CreditCardLuhnCheckUtil.cardNumberIsValid(validFiveDigit))
+    }
+
+    @Test
+    fun testInvalidInputLuhnCheck() {
+        val invalidInput = "1s2d"
+        assertFalse(CreditCardLuhnCheckUtil.cardNumberIsValid(invalidInput))
+    }
+
+    @Test
+    fun testInvalidEmptryStringLuhnCheck() {
+        val invalidInput = ""
+        assertFalse(CreditCardLuhnCheckUtil.cardNumberIsValid(invalidInput))
+    }
+
+    @Test
+    fun testInvalidLongWithDecimalsLuhnCheck() {
+        val invalidInput = "73890.00"
+        assertFalse(CreditCardLuhnCheckUtil.cardNumberIsValid(invalidInput))
+    }
+
+    @Test
+    fun testInvalidLongWithDecimalPointLuhnCheck() {
+        val invalidInput = "73890."
+        assertFalse(CreditCardLuhnCheckUtil.cardNumberIsValid(invalidInput))
+    }
+
+    @Test
+    fun testInvalidCardNumber() {
+        val invalidCardNumber = "1111111111111111"
+        assertFalse(CreditCardLuhnCheckUtil.cardNumberIsValid(invalidCardNumber))
+    }
+
+    @Test
+    fun testValidCardNumber() {
+        val invalidCardNumber = "378282246310005"
+        assertTrue(CreditCardLuhnCheckUtil.cardNumberIsValid(invalidCardNumber))
+    }
+
+    fun getValidFormOfPayment(fee: String? = "3.50"): ValidFormOfPayment {
         val vfop = ValidFormOfPayment()
         vfop.name = "Visa"
         return vfop
