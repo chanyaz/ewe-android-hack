8eba35b920a051236e6cce5603ed0146b0fe0824
DeepeshPanjabi <dkumarpanjabi@expedia.com>
2017-10-08 20:54:52 +0530

    Able to display using Dummy Json

--- project/src/main/java/com/expedia/bookings/fragment/WhatsNewFragment.kt
+++ project/src/main/java/com/expedia/bookings/fragment/WhatsNewFragment.kt
@@ -11,16 +11,12 @@ import com.expedia.bookings.R
 import com.expedia.bookings.utils.bindView
 import com.expedia.bookings.widget.CustomListAdapter
 import com.expedia.model.CustomPojo
+import com.google.gson.Gson
 
 class WhatsNewFragment : Fragment() {
 
     val latest_features: TextView by bindView(R.id.latest_features_of_app)
     val listView: ListView by bindView(R.id.list_to_be)
-    val values = arrayOf("Android", "iPhone", "WindowsMobile",
-        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-        "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
-        "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
-        "Android", "iPhone", "WindowsMobile")
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -30,8 +26,29 @@ class WhatsNewFragment : Fragment() {
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         latest_features.text = "Whats new , Huh!"
-        val listData = Array<CustomPojo>(values.size, { i ->  CustomPojo(name = values.get(i))})
+        val data = dummyData()
+        val listData = Array<CustomPojo>(data!!.size, { i ->  data.get(i)})
         val adapter = CustomListAdapter(data = listData, context = this.context)
         listView.adapter = adapter
+    }
+
+    private fun dummyData(): Array<CustomPojo>? {
+        val data = """
+        [
+            {
+                "monthAndYear": "July, 2017",
+                "featureList": [
+                    "Upsell Options New"
+                ]
+            },
+            {
+                "monthAndYear": "April, 2017",
+                "featureList": [
+                    "Now, get even better fares with SubPub"
+                ]
+            }
+        ]
+        """
+        return Gson().fromJson(data, Array<CustomPojo>::class.java)
     }
 }
