6f9380a22a081b4aae8bf86f7bcd42bba1814af2
spalguyev <a-spalguyev@expedia.com>
2017-10-09 14:51:08 -0700

    Mingle 6863 Add an emulator startup/shutdown script

---
+++ jenkins/emulator_startup_shutdown.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+getRunningDevices() {
+    devices=`adb devices | grep device$ | awk '{ print $1 }'`
+    echo -e "All Connected Devices: \n ${devices}"
+}
+
+getAvailableEmulators() {
+    emulators=`emulator -list-avds`
+    echo -e "All Available Emulators: \n ${emulators}"
+}
+
+shutdownRunningEmulators() {
+    for device in ${devices}; do
+        if [[ $device == *"emulator-"* ]]; then
+            echo "Shutting Down: $device"
+            adb -s $device -e emu kill
+        fi
+    done
+}
+
+startAllAvailableBuilderEmulators() {
+    shopt -s nocasematch
+    for emu in ${emulators}; do
+        if [[ $emu == *"builder"* ]]; then
+            emulator -avd $emu & disown
+            sleep 1
+        fi
+    done
+}
+
+waitForAllDevicesBootCompleted() {
+    for device in ${devices}; do
+        adb -s $device wait-for-device
+        adb -s $device shell getprop sys.boot_completed
+    done
+}
+
+getRunningDevices
+shutdownRunningEmulators
+sleep 5
+
+currDir=`pwd`
+cd `dirname $(which emulator)`
+getAvailableEmulators
+startAllAvailableBuilderEmulators
+cd $currDir
+sleep 10
+
+getRunningDevices
+waitForAllDevicesBootCompleted
