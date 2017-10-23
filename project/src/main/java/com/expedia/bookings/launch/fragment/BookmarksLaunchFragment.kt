e35a85c7b50975aca8605a8b5ecc7d32677e4d29
Kevin Seo <seokev@gmail.com>
2017-10-23 16:16:48 -0700

    made recycler view and adapter

--- project/src/main/java/com/expedia/bookings/launch/fragment/BookmarksLaunchFragment.kt
+++ project/src/main/java/com/expedia/bookings/launch/fragment/BookmarksLaunchFragment.kt
@@ -2,18 +2,42 @@ package com.expedia.bookings.launch.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.expedia.bookings.R
+import com.expedia.bookings.data.Bookmark
+import com.expedia.bookings.utils.BookmarkUtils
+import com.expedia.bookings.utils.bindView
+import com.expedia.bookings.widget.BookmarksListAdapter
+import com.expedia.bookings.widget.BookmarksListRecyclerView
+import com.expedia.bookings.widget.ScrollView
+import java.util.*
 
 class BookmarksLaunchFragment: Fragment() {
+
+    val scrollContainer: ScrollView by bindView(R.id.scroll_container)
+    val recyclerView: BookmarksListRecyclerView by bindView(R.id.list_view)
+    private lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_bookmarks, null)
     }
 
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bookmarks = BookmarkUtils.getAllBookmarks(context)
+        val adapter = BookmarksListAdapter(bookmarks)
+
+        linearLayoutManager = LinearLayoutManager(context)
+        recyclerView.layoutManager = linearLayoutManager
+
+        recyclerView.adapter = adapter
+    }
+
     fun smoothScrollToTop() {
-        //TODO
+        scrollContainer.smoothScrollTo(0, 0)
     }
 }
---
+++ project/src/main/java/com/expedia/bookings/widget/BookmarksListRecyclerView.kt
@@ -0,0 +1,9 @@
+package com.expedia.bookings.widget
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.util.AttributeSet
+
+class BookmarksListRecyclerView(context: Context, attrs: AttributeSet): RecyclerView(context, attrs) {
+
+}
