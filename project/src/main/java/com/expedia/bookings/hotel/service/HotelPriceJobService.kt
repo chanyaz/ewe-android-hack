16487fd5ea66492b6db0088fe3ff125c78bb6715
Monika Szymanski <mszymanski@expedia.com>
2017-10-23 17:04:41 -0500

    Fetch offers with dates.

--- project/src/main/java/com/expedia/bookings/hotel/service/HotelPriceJobService.kt
+++ project/src/main/java/com/expedia/bookings/hotel/service/HotelPriceJobService.kt
@@ -2,18 +2,18 @@ package com.expedia.bookings.hotel.service
 
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
+import android.content.Intent
+import android.util.Log
+import android.widget.RemoteViews
+import com.expedia.bookings.R
+import com.expedia.bookings.data.hotels.HotelOffersResponse
+import com.expedia.bookings.hotel.provider.HotelPriceAppWidgetProvider
 import com.expedia.bookings.hotel.util.HotelFavoriteCache
 import com.expedia.bookings.hotel.util.HotelInfoManager
 import com.expedia.bookings.utils.Ui
 import javax.inject.Inject
-import android.widget.RemoteViews
-import com.expedia.bookings.R
-import com.expedia.bookings.data.hotels.HotelOffersResponse
-import android.appwidget.AppWidgetManager
-import android.content.ComponentName
-import com.expedia.bookings.hotel.provider.HotelPriceAppWidgetProvider
-import android.content.Intent
-import android.util.Log
 
 
 class HotelPriceJobService : JobService() {
@@ -24,6 +24,7 @@ class HotelPriceJobService : JobService() {
 
 
     override fun onStopJob(params: JobParameters?): Boolean {
+        Log.v("HotelPriceJobService", ": onStopJob")
         hotelInfoManager.clearSubscriptions()
         return false
     }
@@ -37,26 +38,39 @@ class HotelPriceJobService : JobService() {
 
         val favorites = HotelFavoriteCache.getFavorites(applicationContext)
 
-        if (favorites != null) {
-            hotelInfoManager.infoSuccessSubject.subscribe { response ->
-                sendViews(response)
-                hotelsRefreshed++
-                if (hotelsRefreshed == favorites.size) {
-                    jobFinished(params, false)
-                }
-                Log.v("HotelPriceJobService", ": ${response.hotelId}")
+        hotelInfoManager.offerSuccessSubject.subscribe { response ->
+            cacheHotelData(response)
+            hotelsRefreshed++
+            Log.v("HotelPriceJobService", " favorites ${favorites.size} AND refreshed: ${hotelsRefreshed}")
+            if (hotelsRefreshed == favorites.size) {
+                sendViews()
+                jobFinished(params, false)
             }
+            Log.v("HotelPriceJobService", ": ${response.hotelId}")
+        }
 
-            for (id in favorites) {
-                hotelInfoManager.fetchDatelessInfo(id)
-            }
-            return true
-        } else {
-            return false
+        for (id in favorites) {
+            fetchOffers(id)
+        }
+        return true
+    }
+
+    private fun cacheHotelData(response: HotelOffersResponse) {
+        //ideally need a better way to get checkin checkout dates
+        val hotel = HotelFavoriteCache.getFavoriteHotelData(applicationContext, response.hotelId)
+        if (hotel != null) {
+            HotelFavoriteCache.saveHotelData(applicationContext, response, hotel.checkInDate, hotel.checkOutDate)
         }
     }
 
-    private fun sendViews(response: HotelOffersResponse) {
+    private fun fetchOffers(id: String) {
+        val hotel = HotelFavoriteCache.getFavoriteHotelData(applicationContext, id)
+        if (hotel != null) {
+            hotelInfoManager.fetchOffers(hotel.checkInDate, hotel.checkOutDate, id)
+        }
+    }
+
+    private fun sendViews() {
         Log.v("HotelPriceJobService", ": sendViews")
         val remoteViews = RemoteViews(applicationContext.packageName, R.layout.hotel_price_appwidget_layout)
         val intent = Intent(applicationContext, HotelRemoteViewService::class.java)
