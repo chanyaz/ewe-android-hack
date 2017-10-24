99ba4ceb852782b57c36cc406fbeab139b37892d
Jiarui Huo <jhuo@CHIC02QX1MEG8WN.sea.corp.expecn.com>
2017-10-24 17:36:22 -0500

    chart style changes

--- project/src/main/java/com/expedia/bookings/launch/widget/PriceGraphView.kt
+++ project/src/main/java/com/expedia/bookings/launch/widget/PriceGraphView.kt
@@ -1,6 +1,7 @@
 package com.expedia.bookings.launch.widget
 
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
 import android.view.View
 import android.widget.LinearLayout
@@ -9,6 +10,8 @@ import com.expedia.bookings.utils.bindView
 import com.expedia.bookings.widget.FrameLayout
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.LimitLine
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
@@ -26,7 +29,7 @@ class PriceGraphView(context: Context, val attrs: AttributeSet) : FrameLayout(co
     private var startDate = LocalDate()
 
     private val lineColors = ColorTemplate.MATERIAL_COLORS
-    
+
     init {
         View.inflate(context, R.layout.price_graph_view, this)
 //        val priceGraph = LineChart(context, attrs)
@@ -58,6 +61,7 @@ class PriceGraphView(context: Context, val attrs: AttributeSet) : FrameLayout(co
             }
 
             val lineDataSet = LineDataSet(prices, name)
+            lineDataSet.valueTextSize = 11F
 
             val color = lineColors[dataCount % lineColors.size]
             lineDataSet.setColor(color)
@@ -74,14 +78,14 @@ class PriceGraphView(context: Context, val attrs: AttributeSet) : FrameLayout(co
             params.height = resources.getDimensionPixelSize(R.dimen.graph_height)
             priceGraph.layoutParams = params
 
-            setUpGraph(priceGraph)
+            setUpGraph(priceGraph, priceData[0])
 
             priceGraph.data = data
             priceGraph.invalidate()
         }
     }
 
-    private fun setUpGraph(graph: LineChart) {
+    private fun setUpGraph(graph: LineChart, priceOnFirstDay: Float) {
 //        graph.setOnChartValueSelectedListener(this)
 
         graph.setDrawGridBackground(false)
@@ -90,37 +94,56 @@ class PriceGraphView(context: Context, val attrs: AttributeSet) : FrameLayout(co
         graph.description.isEnabled = false
         graph.setDrawBorders(false)
 
-        graph.axisLeft.isEnabled = false
+        graph.axisLeft.isEnabled = true
+        graph.axisRight.isEnabled = false
 
-        graph.axisRight.setDrawAxisLine(true)
-        graph.axisRight.setDrawGridLines(true)
+        graph.axisLeft.setDrawAxisLine(true)
+        graph.axisLeft.setDrawGridLines(false)
         // formatRate(BigDecimal amount, String currencyCode, int flags) in case want to format, should use flag no decimal, and take param currencyCode
 
         graph.xAxis.setDrawAxisLine(true)
         graph.xAxis.setDrawGridLines(false)
+        graph.xAxis.setTextSize(10f)
+        graph.xAxis.position = XAxis.XAxisPosition.BOTTOM
 
         graph.xAxis.setValueFormatter( { value, _ ->
             val date = startDate.plusDays(value.toInt())
 //            dateFormatter.format(date)
-            date.toString("dd/MM")
+            date.toString("MM/dd")
         })
+
+        graph.axisLeft.setValueFormatter( { value, _ ->
+            "$" + value
+        })
+
 
         // enable touch gestures
 //        graph.setTouchEnabled(true)
 
         // enable scaling and dragging
-        graph.isDragEnabled = true
+        graph.isDragEnabled = false
 //        graph.setScaleEnabled(true)
-        graph.isScaleYEnabled = true
+        graph.isScaleYEnabled = false
         graph.isScaleXEnabled = false
+
+        val leftAxis = graph.getAxisLeft()
+        leftAxis.setDrawLabels(false)
+        var ll = LimitLine(priceOnFirstDay, "")
+        ll.setLineColor(Color.GRAY)
+        ll.setLineWidth(1f)
+        leftAxis.setGranularity(priceOnFirstDay)
+        leftAxis.setLabelCount(0, true)
+        leftAxis.addLimitLine(ll)
+
+
 
         // if disabled, scaling can be done on x- and y-axis separately
         graph.setPinchZoom(false)
 
         val l = graph.legend
-        l.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
-        l.horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
+        l.verticalAlignment = Legend.LegendVerticalAlignment.TOP
+        l.horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
         l.orientation = Legend.LegendOrientation.HORIZONTAL
-        l.setDrawInside(true)
+        l.setDrawInside(false)
     }
 }
