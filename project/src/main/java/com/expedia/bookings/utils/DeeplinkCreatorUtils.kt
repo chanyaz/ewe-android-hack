b09d316490ab96b0894ad128bed6e73077461133
tripa013 <abtripathi@expedia.com>
2017-10-23 17:43:15 -0700

    Add deeplink hotel search params

---
+++ project/src/main/java/com/expedia/bookings/utils/DeeplinkCreatorUtils.kt
@@ -0,0 +1,32 @@
+package com.expedia.bookings.utils
+
+import org.joda.time.LocalDate
+
+
+class DeeplinkCreatorUtils() {
+
+    companion object {
+        var hotelSearchParams: HotelSearchParams? = null
+        var hotelSelectionParams: HotelSelectionParams? = null
+        var hotelRoomSelectionParams: HotelRoomSelectionParams? = null
+    }
+
+
+}
+
+class HotelRoomSelectionParams {
+    lateinit var selectedRoomID: String
+}
+
+class HotelSearchParams {
+    lateinit var origin: String
+    lateinit var destination: String
+    lateinit var startDate: LocalDate
+    lateinit var endDate: LocalDate
+    lateinit var originID: String
+    lateinit var destinationID: String
+}
+
+class HotelSelectionParams {
+    lateinit var selectedHotelID: String
+}
