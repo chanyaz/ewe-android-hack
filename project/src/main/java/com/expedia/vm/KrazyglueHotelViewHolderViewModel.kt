3a6a3431ad6a8e84ebf534592e95e18d00525694
tripa013 <abtripathi@expedia.com>
2017-10-26 10:58:34 -0700

    s/6262 Krazyglue UI with real data

---
+++ project/src/main/java/com/expedia/vm/KrazyglueHotelViewHolderViewModel.kt
@@ -0,0 +1,28 @@
+package com.expedia.vm
+
+import com.expedia.bookings.data.flights.KrazyglueResponse
+import rx.subjects.PublishSubject
+
+
+class KrazyglueHotelViewHolderViewModel() {
+    val hotelNameObservable = PublishSubject.create<String>()
+    val hotelStarRatingObservable = PublishSubject.create<Float>()
+    val hotelStarRatingVisibilityObservable = hotelStarRatingObservable.map { it > 0 }
+    val hotelGuestRatingObservable = PublishSubject.create<String>()
+    val hotelGuestRatingVisibilityObservable = hotelGuestRatingObservable.map { it.toFloat() > 0 }
+    val hotelStrikeThroughPriceObservable = PublishSubject.create<String>()
+    val hotelPricePerNightObservable = PublishSubject.create<String>()
+    val hotelImageURLObservable = PublishSubject.create<String>()
+    val hotelObservable = PublishSubject.create<KrazyglueResponse.KrazyglueHotel>()
+
+    init {
+        hotelObservable.subscribe { hotel ->
+            hotelNameObservable.onNext(hotel.hotelName)
+            hotelImageURLObservable.onNext(hotel.hotelImage)
+            hotelStarRatingObservable.onNext(hotel.starRating.toFloat())
+            hotelGuestRatingObservable.onNext(hotel.guestRating)
+            hotelPricePerNightObservable.onNext(hotel.airAttachedPrice)
+            hotelStrikeThroughPriceObservable.onNext(hotel.standAlonePrice)
+        }
+    }
+}
