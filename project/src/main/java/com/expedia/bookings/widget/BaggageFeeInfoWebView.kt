71f8317c2ba5d8e18ef75eb45286ac694953833e
Sarthak30 <>
2017-11-29 11:30:35 +0530

    Removed the loader from baggage webview

--- project/src/main/java/com/expedia/bookings/widget/BaggageFeeInfoWebView.kt
+++ project/src/main/java/com/expedia/bookings/widget/BaggageFeeInfoWebView.kt
@@ -12,36 +12,18 @@ import com.expedia.bookings.widget.shared.BaseWebViewWidget
 
 class BaggageFeeInfoWebView(context: Context, attrs: AttributeSet) : BaseWebViewWidget(context, attrs) {
 
-    val APP_DOWNLOAD_BANNER_ID = "mobile-app-banner-wrapper"
-    val isWebViewImprovementABTestEnabled = AbacusFeatureConfigManager.isUserBucketedForTest(context, AbacusUtils.EBAndroidAppFlightsBaggageWebViewHideAd)
-
-    private val progressDialog = ProgressDialog(context)
+    private val APP_DOWNLOAD_BANNER_ID = "mobile-app-banner-wrapper"
+    private val isWebViewImprovementABTestEnabled = AbacusFeatureConfigManager.isUserBucketedForTest(context, AbacusUtils.EBAndroidAppFlightsBaggageWebViewHideAd)
 
     override fun onFinishInflate() {
         super.onFinishInflate()
         toolbar.setTitle(R.string.package_flight_overview_baggage_fees)
     }
 
-    override fun onWebPageStarted(view: WebView, url: String, favicon: Bitmap?) {
-        super.onWebPageStarted(view, url, favicon)
-        if (isWebViewImprovementABTestEnabled && !progressDialog.isShowing) {
-            progressDialog.show()
-            progressDialog.setCancelable(false)
-            progressDialog.setContentView(R.layout.process_dialog_layout)
-        }
-    }
-
     override fun onWebViewLoadResource(url: String) {
         super.onWebViewLoadResource(url)
         if (isWebViewImprovementABTestEnabled) {
             preventLoadingOfDivId(APP_DOWNLOAD_BANNER_ID)
-        }
-    }
-
-    override fun onPageFinished(url: String) {
-        super.onPageFinished(url)
-        if ( progressDialog.isShowing) {
-            progressDialog.dismiss()
         }
     }
 }
