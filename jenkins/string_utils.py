eb439af05b951106f8ffdcc889056115f9c096d1
Brennan Taylor <taylor.brennan+github@gmail.com>
2018-01-04 16:11:22 -0800

    Split lines in python correctly (#10073)

--- jenkins/string_utils.py
+++ jenkins/string_utils.py
@@ -1,5 +1,5 @@
 def truncate_message(message, max_lines):
-    error_message_lines = message.lines()
+    error_message_lines = message.splitlines()
     if len(error_message_lines) > max_lines:
         message = "\n".join(error_message_lines[:max_lines]) + "\n... truncated"
     return message
