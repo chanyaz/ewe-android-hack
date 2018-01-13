76479cccde9a7e121da4af315e78ca4298acccfd
Mia <mclapham@expedia.com>
2018-01-13 13:16:05 -0800

    T7895 put new directions button behind FF (#10176)

--- project/src/main/java/com/expedia/bookings/itin/widget/HotelItinLocationDetails.kt
+++ project/src/main/java/com/expedia/bookings/itin/widget/HotelItinLocationDetails.kt
@@ -1,21 +1,28 @@
 package com.expedia.bookings.itin.widget
 
 import android.content.Context
+import android.content.pm.PackageManager
 import android.support.v4.app.ActivityOptionsCompat
+import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import com.expedia.bookings.R
+import com.expedia.bookings.data.abacus.AbacusUtils
 import com.expedia.bookings.itin.activity.HotelItinExpandedMapActivity
 import com.expedia.bookings.itin.data.ItinCardDataHotel
 import com.expedia.bookings.itin.vm.GoogleMapsLiteViewModel
 import com.expedia.bookings.tracking.OmnitureTracking
 import com.expedia.bookings.utils.ClipboardUtils
+import com.expedia.bookings.utils.FeatureToggleUtil
 import com.expedia.bookings.utils.bindView
+import com.expedia.bookings.widget.ItinActionsSection
 import com.expedia.bookings.widget.TextView
+import com.expedia.bookings.widget.itin.SummaryButton
 import com.google.android.gms.maps.model.LatLng
+import com.mobiata.android.SocialUtils
 import com.squareup.phrase.Phrase
 
 class HotelItinLocationDetails(context: Context, attr: AttributeSet?) : LinearLayout(context, attr) {
@@ -23,6 +30,7 @@ class HotelItinLocationDetails(context: Context, attr: AttributeSet?) : LinearLa
     val address: LinearLayout by bindView(R.id.widget_hotel_itin_address)
     val addressLine1: TextView by bindView(R.id.widget_hotel_itin_address_line_1)
     val addressLine2: TextView by bindView(R.id.widget_hotel_itin_address_line_2)
+    val actionButtons: ItinActionsSection by bindView(R.id.action_button_layout)
     val directionsButton: ImageView by bindView(R.id.hotel_directions_button)
 
     init {
@@ -30,6 +38,8 @@ class HotelItinLocationDetails(context: Context, attr: AttributeSet?) : LinearLa
     }
 
     fun setupWidget(itinCardDataHotel: ItinCardDataHotel) {
+        val phoneNumber: String
+        val callActionButton: SummaryButton
         if (itinCardDataHotel.propertyLocation != null) {
             val mapVm = GoogleMapsLiteViewModel(
                     listOf(LatLng(itinCardDataHotel.propertyLocation.latitude, itinCardDataHotel.propertyLocation.longitude))
@@ -39,12 +49,13 @@ class HotelItinLocationDetails(context: Context, attr: AttributeSet?) : LinearLa
                 context.startActivity(HotelItinExpandedMapActivity.createIntent(context, itinCardDataHotel.id), ActivityOptionsCompat.makeCustomAnimation(context, R.anim.slide_in_right, R.anim.slide_out_left_complete).toBundle())
                 OmnitureTracking.trackItinHotelExpandMap()
             }
-            directionsButton.visibility = View.VISIBLE
-            directionsButton.setOnClickListener {
-                context.startActivity(HotelItinExpandedMapActivity.createIntent(context, itinCardDataHotel.id), ActivityOptionsCompat.makeCustomAnimation(getContext(), R.anim.slide_in_right, R.anim.slide_out_left_complete).toBundle())
-                OmnitureTracking.trackItinHotelDirections()
+            if (FeatureToggleUtil.isUserBucketedAndFeatureEnabled(context, AbacusUtils.EBAndroidAppTripsMessageHotel, R.string.preference_enable_trips_hotel_messaging)) {
+                directionsButton.visibility = View.VISIBLE
+                directionsButton.setOnClickListener {
+                    context.startActivity(HotelItinExpandedMapActivity.createIntent(context, itinCardDataHotel.id), ActivityOptionsCompat.makeCustomAnimation(getContext(), R.anim.slide_in_right, R.anim.slide_out_left_complete).toBundle())
+                    OmnitureTracking.trackItinHotelDirections()
+                }
             }
-
         }
         addressLine1.text = itinCardDataHotel.propertyLocation.streetAddressString
         addressLine2.text = itinCardDataHotel.propertyLocation.toTwoLineAddressFormattedString()
@@ -57,5 +68,28 @@ class HotelItinLocationDetails(context: Context, attr: AttributeSet?) : LinearLa
         }
         address.contentDescription = Phrase.from(context, R.string.itin_hotel_details_address_copy_content_description_TEMPLATE)
                 .put("address", textToCopy).format().toString()
+
+        if (!FeatureToggleUtil.isUserBucketedAndFeatureEnabled(context, AbacusUtils.EBAndroidAppTripsMessageHotel, R.string.preference_enable_trips_hotel_messaging)) {
+            actionButtons.visibility = View.VISIBLE
+            phoneNumber = itinCardDataHotel.localPhone
+            callActionButton = SummaryButton(R.drawable.itin_call_hotel, phoneNumber, Phrase.from(context, R.string.itin_hotel_details_call_button_content_description_TEMPLATE).put("phonenumber", phoneNumber).format().toString(), OnClickListener {
+                if (phoneNumber.isNotEmpty()) {
+                    val pm = context.packageManager
+                    if (pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
+                        SocialUtils.call(context, phoneNumber)
+                    } else {
+                        ClipboardUtils.setText(context, phoneNumber)
+                        Toast.makeText(context, R.string.toast_copied_to_clipboard, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
+
+            val directionsActionButton = SummaryButton(R.drawable.ic_directions_icon_cta_button, context.getString(R.string.itin_action_directions), OnClickListener {
+                context.startActivity(HotelItinExpandedMapActivity.createIntent(context, itinCardDataHotel.id), ActivityOptionsCompat.makeCustomAnimation(getContext(), R.anim.slide_in_right, R.anim.slide_out_left_complete).toBundle())
+                OmnitureTracking.trackItinHotelDirections()
+            })
+            if (phoneNumber.isEmpty()) actionButtons.bind(null, directionsActionButton) else actionButtons.bind(callActionButton, directionsActionButton)
+            actionButtons.getmRightButton().setCompoundDrawablesTint(ContextCompat.getColor(context, R.color.app_primary))
+        }
     }
 }
--- project/src/test/java/com/expedia/bookings/itin/widget/HotelItinLocationDetailsTest.kt
+++ project/src/test/java/com/expedia/bookings/itin/widget/HotelItinLocationDetailsTest.kt
@@ -4,10 +4,13 @@ import android.view.LayoutInflater
 import android.view.View
 import com.expedia.bookings.OmnitureTestUtils
 import com.expedia.bookings.R
+import com.expedia.bookings.data.abacus.AbacusUtils
 import com.expedia.bookings.itin.activity.HotelItinDetailsActivity
 import com.expedia.bookings.test.robolectric.RobolectricRunner
+import com.expedia.bookings.utils.AbacusTestUtils
 import com.expedia.bookings.utils.ClipboardUtils
 import com.expedia.bookings.widget.itin.support.ItinCardDataHotelBuilder
+import com.mobiata.android.util.SettingUtils
 import com.squareup.phrase.Phrase
 import org.junit.Before
 import org.junit.Test
@@ -30,12 +33,32 @@ class HotelItinLocationDetailsTest {
     }
 
     @Test
-    fun testMapWidget() {
+    fun testMapWidgetMessagingFeatureOff() {
+        SettingUtils.save(activity, R.string.preference_enable_trips_hotel_messaging, false)
+        AbacusTestUtils.bucketTestAndEnableRemoteFeature(activity, AbacusUtils.EBAndroidAppTripsMessageHotel, 0)
+        locationDetailsWidget.setupWidget(itinCardDataHotel)
+        assertEquals(View.VISIBLE, locationDetailsWidget.mapView.visibility)
+        assertEquals(View.GONE, locationDetailsWidget.directionsButton.visibility)
+        assertEquals(locationDetailsWidget.addressLine1.text, itinCardDataHotel.propertyLocation.streetAddressString)
+        assertEquals(locationDetailsWidget.addressLine2.text, itinCardDataHotel.propertyLocation.toTwoLineAddressFormattedString())
+        assertEquals(locationDetailsWidget.actionButtons.getmLeftButton().text, itinCardDataHotel.localPhone)
+        locationDetailsWidget.address.performClick()
+        val address: String = Phrase.from(activity, R.string.itin_hotel_details_address_clipboard_TEMPLATE)
+                .put("addresslineone", locationDetailsWidget.addressLine1.text.toString())
+                .put("addresslinetwo", locationDetailsWidget.addressLine2.text.toString()).format().toString()
+        assertEquals(address, ClipboardUtils.getText(activity))
+    }
+
+    @Test
+    fun testMapWidgetMessagingFeatureOn() {
+        SettingUtils.save(activity, R.string.preference_enable_trips_hotel_messaging, true)
+        AbacusTestUtils.bucketTestAndEnableRemoteFeature(activity, AbacusUtils.EBAndroidAppTripsMessageHotel, 1)
         locationDetailsWidget.setupWidget(itinCardDataHotel)
         assertEquals(View.VISIBLE, locationDetailsWidget.mapView.visibility)
         assertEquals(View.VISIBLE, locationDetailsWidget.directionsButton.visibility)
         assertEquals(locationDetailsWidget.addressLine1.text, itinCardDataHotel.propertyLocation.streetAddressString)
         assertEquals(locationDetailsWidget.addressLine2.text, itinCardDataHotel.propertyLocation.toTwoLineAddressFormattedString())
+        assertEquals(View.GONE, locationDetailsWidget.actionButtons.visibility)
         locationDetailsWidget.address.performClick()
         val address: String = Phrase.from(activity, R.string.itin_hotel_details_address_clipboard_TEMPLATE)
                 .put("addresslineone", locationDetailsWidget.addressLine1.text.toString())
@@ -53,7 +76,21 @@ class HotelItinLocationDetailsTest {
     }
 
     @Test
-    fun testDirectionsOmnitureClick() {
+    fun testDirectionsOmnitureClickMessagingFeatureOff() {
+        SettingUtils.save(activity, R.string.preference_enable_trips_hotel_messaging, false)
+        AbacusTestUtils.bucketTestAndEnableRemoteFeature(activity, AbacusUtils.EBAndroidAppTripsMessageHotel, 0)
+        val mockAnalyticsProvider = OmnitureTestUtils.setMockAnalyticsProvider()
+        locationDetailsWidget.setupWidget(itinCardDataHotel)
+        val directionsButton = locationDetailsWidget.actionButtons.getmRightButton()
+        directionsButton.performClick()
+
+        OmnitureTestUtils.assertLinkTracked("Itinerary Action", "App.Itinerary.Hotel.Directions", mockAnalyticsProvider)
+    }
+
+    @Test
+    fun testDirectionsOmnitureClickMessagingFeatureOn() {
+        SettingUtils.save(activity, R.string.preference_enable_trips_hotel_messaging, true)
+        AbacusTestUtils.bucketTestAndEnableRemoteFeature(activity, AbacusUtils.EBAndroidAppTripsMessageHotel, 1)
         val mockAnalyticsProvider = OmnitureTestUtils.setMockAnalyticsProvider()
         locationDetailsWidget.setupWidget(itinCardDataHotel)
         val directionsButton = locationDetailsWidget.directionsButton
