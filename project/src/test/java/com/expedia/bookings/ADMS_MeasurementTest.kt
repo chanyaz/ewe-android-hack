dd9c99b6344999043351a59a6b1a3943bcfef624
Scott Weber <sweber@expedia.com>
2018-02-06 11:35:31 -0500

    refactor omniture unit test

--- project/src/test/java/com/expedia/bookings/ADMS_MeasurementTest.kt
+++ project/src/test/java/com/expedia/bookings/ADMS_MeasurementTest.kt
@@ -5,6 +5,7 @@ import com.expedia.bookings.test.CustomMatchers.Companion.matchesPattern
 import com.expedia.bookings.test.NullSafeMockitoHamcrest.mapThat
 import com.expedia.bookings.test.robolectric.RobolectricRunner
 import org.junit.Assert.assertThat
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito
@@ -14,44 +15,45 @@ import kotlin.test.assertNull
 
 @RunWith(RobolectricRunner::class)
 class ADMS_MeasurementTest {
+    lateinit var adms: ADMS_Measurement
+
+    @Before
+    fun setup() {
+        adms = ADMS_Measurement()
+    }
 
     @Test
     fun testPurchaseId() {
-        val sharedInstance = ADMS_Measurement()
         val samplePurchaseID = "sample purchase id"
-        sharedInstance.setPurchaseID(samplePurchaseID)
-        assertEquals(samplePurchaseID, sharedInstance.getOmnitureDataValue("&&purchaseID"))
+        adms.setPurchaseID(samplePurchaseID)
+        assertEquals(samplePurchaseID, adms.getOmnitureDataValue("&&purchaseID"))
     }
 
     @Test
     fun testCurrencyCode() {
-        val sharedInstance = ADMS_Measurement()
         val sampleCurrencyCode = "sample currency"
-        sharedInstance.setCurrencyCode(sampleCurrencyCode)
-        assertEquals(sampleCurrencyCode, sharedInstance.getOmnitureDataValue("&&cc"))
+        adms.setCurrencyCode(sampleCurrencyCode)
+        assertEquals(sampleCurrencyCode, adms.getOmnitureDataValue("&&cc"))
     }
 
     @Test
     fun testProducts() {
-        val sharedInstance = ADMS_Measurement()
         val sampleProducts = "sample products string"
-        sharedInstance.setProducts(sampleProducts)
-        assertEquals(sampleProducts, sharedInstance.getProducts())
-        assertEquals(sampleProducts, sharedInstance.getOmnitureDataValue("&&products"))
+        adms.setProducts(sampleProducts)
+        assertEquals(sampleProducts, adms.getProducts())
+        assertEquals(sampleProducts, adms.getOmnitureDataValue("&&products"))
     }
 
     @Test
     fun testEvents() {
-        val sharedInstance = ADMS_Measurement()
         val sampleEvents = "sample events"
-        sharedInstance.setEvents(sampleEvents)
-        assertEquals(sampleEvents, sharedInstance.getEvents())
-        assertEquals(sampleEvents, sharedInstance.getOmnitureDataValue("&&events"))
+        adms.setEvents(sampleEvents)
+        assertEquals(sampleEvents, adms.getEvents())
+        assertEquals(sampleEvents, adms.getOmnitureDataValue("&&events"))
     }
 
     @Test
     fun testAppendEventsEmpty() {
-        val adms = ADMS_Measurement()
         adms.appendEvents("")
         assertNull(adms.getEvents())
         assertNull(adms.getOmnitureDataValue("&&events"))
@@ -60,7 +62,6 @@ class ADMS_MeasurementTest {
     @Test
     fun testAppendEventsSimple() {
         val event = "event112"
-        val adms = ADMS_Measurement()
         adms.appendEvents(event)
         assertEquals(event, adms.getEvents())
         assertEquals(event, adms.getOmnitureDataValue("&&events"))
@@ -70,7 +71,6 @@ class ADMS_MeasurementTest {
     fun testAppendEventsAppending() {
         val event1 = "event1"
         val event2 = "event2"
-        val adms = ADMS_Measurement()
         adms.appendEvents(event1)
         assertEquals(event1, adms.getEvents())
         assertEquals(event1, adms.getOmnitureDataValue("&&events"))
@@ -84,7 +84,6 @@ class ADMS_MeasurementTest {
     fun testAppendEventsAppendingEmpty() {
         val event1 = "event1"
         val event2 = ""
-        val adms = ADMS_Measurement()
         adms.appendEvents(event1)
         assertEquals(event1, adms.getEvents())
         assertEquals(event1, adms.getOmnitureDataValue("&&events"))
@@ -96,49 +95,44 @@ class ADMS_MeasurementTest {
 
     @Test
     fun testProp() {
-        val sharedInstance = ADMS_Measurement()
         val sampleProp = "sample prop"
-        sharedInstance.setProp(10, sampleProp)
-        assertEquals(sampleProp, sharedInstance.getProp(10))
-        assertEquals(sampleProp, sharedInstance.getOmnitureDataValue("&&c10"))
+        adms.setProp(10, sampleProp)
+        assertEquals(sampleProp, adms.getProp(10))
+        assertEquals(sampleProp, adms.getOmnitureDataValue("&&c10"))
     }
 
     @Test
     fun testPropNull() {
-        val sharedInstance = ADMS_Measurement()
-        sharedInstance.setProp(10, null)
-        assertEquals("", sharedInstance.getOmnitureDataValue("&&c10"))
+        adms.setProp(10, null)
+        assertEquals("", adms.getOmnitureDataValue("&&c10"))
     }
 
     @Test
     fun testEvar() {
-        val sharedInstance = ADMS_Measurement()
         val sampleEvar10Value = "sample evar set"
-        sharedInstance.setEvar(10, sampleEvar10Value)
-        assertEquals(sampleEvar10Value, sharedInstance.getEvar(10))
-        assertEquals(sampleEvar10Value, sharedInstance.getOmnitureDataValue("&&v10"))
+        adms.setEvar(10, sampleEvar10Value)
+        assertEquals(sampleEvar10Value, adms.getEvar(10))
+        assertEquals(sampleEvar10Value, adms.getOmnitureDataValue("&&v10"))
     }
 
     @Test
     fun testEvarValueNull() {
-        val sharedInstance = ADMS_Measurement()
-        sharedInstance.setEvar(10, null)
-        assertEquals("", sharedInstance.getOmnitureDataValue("&&v10"))
+        adms.setEvar(10, null)
+        assertEquals("", adms.getOmnitureDataValue("&&v10"))
     }
 
     @Test
     fun visitorIdIsNonNullAndOnlyNumeric() {
-        val visitorId = ADMS_Measurement().visitorID
+        val visitorId = adms.visitorID
         assertNotNull(visitorId)
         assertThat(visitorId, matchesPattern("^[0-9]+$"))
     }
 
     @Test
     fun trackLinkIncludesCustomLinkType() {
-        val sharedInstance = ADMS_Measurement()
         val mockProvider = OmnitureTestUtils.setMockAnalyticsProvider()
 
-        sharedInstance.trackLink("Link.Name")
+        adms.trackLink("Link.Name")
 
         val expectedData = mapOf(
                 "&&linkType" to "o",
