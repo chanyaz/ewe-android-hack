4eba26be0a359d8f2f0bf7477a722372d5dbd1ff
Abhinav Tripathi <abtripathi@expedia.com>
2018-02-06 15:19:11 -0800

    temp

---
+++ project/src/main/java/com/expedia/bookings/activity/FlightConfirmationActivity.kt
@@ -0,0 +1,14 @@
+package com.expedia.bookings.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.expedia.bookings.R
+
+class FlightConfirmationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flight_confirmation)
+    }
+}
