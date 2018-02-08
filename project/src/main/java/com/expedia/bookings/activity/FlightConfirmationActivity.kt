a63a04e340ed3a68323bbb990c9e2a225298d99a
Abhinav Tripathi <abtripathi@expedia.com>
2018-02-08 11:18:49 -0800

    temp

--- project/src/main/java/com/expedia/bookings/activity/FlightConfirmationActivity.kt
+++ project/src/main/java/com/expedia/bookings/activity/FlightConfirmationActivity.kt
@@ -1,11 +1,21 @@
 package com.expedia.bookings.activity
 
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 import com.expedia.bookings.R
+import com.expedia.bookings.viewmodel.FlightConfirmationViewModel
+import javax.inject.Inject
 
 class FlightConfirmationActivity : AppCompatActivity() {
+
+    @Inject lateinit var viewModelFactory: ViewModelFactory
+
+
+    private val viewModel: FlightConfirmationViewModel by lazy {
+        ViewModelProviders.of(this, viewModelFactory).get(FlightConfirmationViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ project/src/main/java/com/expedia/bookings/viewmodel/FlightConfirmationViewModel.kt
@@ -0,0 +1,11 @@
+package com.expedia.bookings.viewmodel
+
+import android.arch.lifecycle.ViewModel
+
+class FlightConfirmationViewModel: ViewModel() {
+    override fun onCleared() {
+        super.onCleared()
+    }
+
+
+}
