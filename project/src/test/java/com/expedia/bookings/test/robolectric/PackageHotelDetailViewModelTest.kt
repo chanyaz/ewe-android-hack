a1b8b3ee79467fcd75a394f938692cca703c649f
vksinghh25 <vipkumar@expedia.com>
2018-02-19 15:27:16 +0530

    Adding unit tests for PackageHotelDetailViewModel

--- project/src/test/java/com/expedia/bookings/test/robolectric/PackageHotelDetailViewModelTest.kt
+++ project/src/test/java/com/expedia/bookings/test/robolectric/PackageHotelDetailViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.expedia.bookings.test.robolectric
 
 import android.content.Context
+import com.expedia.bookings.R
 import com.expedia.bookings.data.Db
 import com.expedia.bookings.data.Money
 import com.expedia.bookings.data.SuggestionV4
@@ -10,6 +11,8 @@ import com.expedia.bookings.data.hotels.HotelSearchParams
 import com.expedia.bookings.data.packages.PackageOffersResponse
 import com.expedia.bookings.data.packages.PackageSearchParams
 import com.expedia.bookings.data.packages.PackageSearchResponse
+import com.expedia.bookings.data.pos.PointOfSale
+import com.expedia.bookings.data.pos.PointOfSaleId
 import com.expedia.bookings.test.MultiBrand
 import com.expedia.bookings.test.RunForBrands
 import com.expedia.bookings.test.robolectric.shadows.ShadowAccountManagerEB
@@ -27,13 +30,14 @@ import org.junit.runner.RunWith
 import org.robolectric.RuntimeEnvironment
 import org.robolectric.annotation.Config
 import com.expedia.bookings.services.TestObserver
+import com.mobiata.android.util.SettingUtils
 import java.math.BigDecimal
 import java.util.ArrayList
 import kotlin.properties.Delegates
 import kotlin.test.assertEquals
 
 @RunWith(RobolectricRunner::class)
-@Config(shadows = arrayOf(ShadowGCM::class, ShadowUserManager::class, ShadowAccountManagerEB::class))
+@Config(shadows = [ShadowGCM::class, ShadowUserManager::class, ShadowAccountManagerEB::class])
 class PackageHotelDetailViewModelTest {
     private var testViewModel: PackageHotelDetailViewModel by Delegates.notNull()
 
@@ -41,7 +45,8 @@ class PackageHotelDetailViewModelTest {
     private var offer1: HotelOffersResponse by Delegates.notNull()
     private val expectedTotalPriceWithMandatoryFees = 42f
 
-    @Before fun before() {
+    @Before
+    fun before() {
         context = RuntimeEnvironment.application
         testViewModel = PackageHotelDetailViewModel(context)
 
@@ -56,7 +61,8 @@ class PackageHotelDetailViewModelTest {
         offer1.hotelRoomResponse = makeHotel()
     }
 
-    @Test fun packageSearchInfoShouldShow() {
+    @Test
+    fun packageSearchInfoShouldShow() {
         val searchParams = createSearchParams()
         searchParams.forPackage = true
         val response = PackageSearchResponse()
@@ -76,7 +82,82 @@ class PackageHotelDetailViewModelTest {
         assertEquals("${searchParams.guests} guests", testViewModel.searchInfoGuestsObservable.value)
     }
 
-    @Test @RunForBrands(brands = arrayOf(MultiBrand.EXPEDIA))
+    @Test
+    fun testOfferReturnedNonNullHotelRoomResponse() {
+        testViewModel.hotelOffersSubject.onNext(offer1)
+
+        assertEquals(Money(250, "USD"), testViewModel.bundlePricePerPersonObservable.value)
+        assertEquals(Money(500, "USD"), testViewModel.bundleTotalPriceObservable.value)
+        assertEquals(Money(100, "USD"), testViewModel.bundleSavingsObservable.value)
+    }
+
+    @Test
+    fun testOfferReturnedNullHotelRoomResponse() {
+        offer1.hotelRoomResponse = null
+        testViewModel.hotelOffersSubject.onNext(offer1)
+
+        assertEquals(null, testViewModel.bundlePricePerPersonObservable.value)
+        assertEquals(null, testViewModel.bundleTotalPriceObservable.value)
+        assertEquals(null, testViewModel.bundleSavingsObservable.value)
+    }
+
+    @Test
+    @RunForBrands(brands = [MultiBrand.EXPEDIA])
+    fun testGetFeeTypeTextUSPOS() {
+        val currentPOS = PointOfSale.getPointOfSale().pointOfSaleId
+        setPOS(PointOfSaleId.UNITED_STATES)
+        val feeTypeText = testViewModel.getFeeTypeText()
+        assertEquals(R.string.rate_per_night, feeTypeText)
+        setPOS(currentPOS)
+    }
+
+    @Test
+    @RunForBrands(brands = [MultiBrand.EXPEDIA])
+    fun testGetFeeTypeTextNonUSPOS() {
+        val currentPOS = PointOfSale.getPointOfSale().pointOfSaleId
+        setPOS(PointOfSaleId.UNITED_KINGDOM)
+        val feeTypeText = testViewModel.getFeeTypeText()
+        assertEquals(R.string.total_fee, feeTypeText)
+        setPOS(currentPOS)
+    }
+
+    @Test
+    @RunForBrands(brands = [MultiBrand.EXPEDIA])
+    fun testShouldShowBookByPhoneNonUSPOS() {
+        val currentPOS = PointOfSale.getPointOfSale().pointOfSaleId
+        setPOS(PointOfSaleId.UNITED_KINGDOM)
+        val showBookByPhone = testViewModel.shouldShowBookByPhone()
+        assertEquals(false, showBookByPhone)
+        setPOS(currentPOS)
+    }
+
+    @Test
+    @RunForBrands(brands = [MultiBrand.EXPEDIA])
+    fun testShouldShowBookByPhoneUSPOSWithNoTelesalesNumber() {
+        val currentPOS = PointOfSale.getPointOfSale().pointOfSaleId
+        testViewModel.hotelOffersSubject.onNext(offer1)
+        setPOS(PointOfSaleId.UNITED_STATES)
+
+        val showBookByPhone = testViewModel.shouldShowBookByPhone()
+        assertEquals(false, showBookByPhone)
+        setPOS(currentPOS)
+    }
+
+    @Test
+    @RunForBrands(brands = [MultiBrand.EXPEDIA])
+    fun testShouldShowBookByPhoneUSPOSWithTelesalesNumber() {
+        val currentPOS = PointOfSale.getPointOfSale().pointOfSaleId
+        offer1.packageTelesalesNumber = "1111111111"
+        testViewModel.hotelOffersSubject.onNext(offer1)
+        setPOS(PointOfSaleId.UNITED_STATES)
+
+        val showBookByPhone = testViewModel.shouldShowBookByPhone()
+        assertEquals(true, showBookByPhone)
+        setPOS(currentPOS)
+    }
+
+    @Test
+    @RunForBrands(brands = [MultiBrand.EXPEDIA])
     fun resortFeeShowsForPackages() {
         CurrencyUtils.initMap(RuntimeEnvironment.application)
         val vm = PackageHotelDetailViewModel(RuntimeEnvironment.application)
@@ -88,6 +169,11 @@ class PackageHotelDetailViewModelTest {
 
         assertEquals("$20", testSubscriber.values()[1])
         assertEquals("per night", context.getString(vm.getFeeTypeText()))
+    }
+
+    private fun setPOS(pos: PointOfSaleId) {
+        SettingUtils.save(RuntimeEnvironment.application, R.string.PointOfSaleKey, pos.id.toString())
+        PointOfSale.onPointOfSaleChanged(RuntimeEnvironment.application)
     }
 
     private fun makeResortFeeResponse(vm: BaseHotelDetailViewModel) {
@@ -143,6 +229,9 @@ class PackageHotelDetailViewModelTest {
         val lowRateInfo = HotelRate()
         lowRateInfo.discountPercent = -20f
         lowRateInfo.currencyCode = "USD"
+        lowRateInfo.packagePricePerPerson = Money(250, "USD")
+        lowRateInfo.packageTotalPrice = Money(500, "USD")
+        lowRateInfo.packageSavings = Money(100, "USD")
 
         val rateInfo = HotelOffersResponse.RateInfo()
         rateInfo.chargeableRateInfo = lowRateInfo
