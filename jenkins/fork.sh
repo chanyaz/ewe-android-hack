52bef4d8fe3834dc4df43fa8a68fb8fa61c56488
Brennan Taylor <taylor.brennan+github@gmail.com>
2018-02-20 14:28:17 -0800

    Cleanup unused parts of the fork script (#10599)

--- jenkins/fork.sh
+++ jenkins/fork.sh
@@ -9,38 +9,10 @@ adb devices | tail -n +2 | cut -sf 1 | xargs -I X adb -s X install -r tools/test
 adb devices | tail -n +2 | cut -sf 1 | xargs -I X adb -s X shell svc data disable
 adb devices | tail -n +2 | cut -sf 1 | xargs -I X adb -s X shell svc wifi disable
 
-if [ -z "${ghprbPullId}" ] ; then
-	hasPullId=false
-	jobName="hourly"
-else
-	hasPullId=true
-	jobName="uitests"
-fi
-internal_artifact() {
+prepare_frequencies_of_flaky_ui_tests() {
 	# Cook up the frequencies of flaky UI tests so they are readily available for consumption by the Topmost-Flaky-UI-Tests-Daily-Report Job
 	python ./jenkins/prepare_frequencies_of_flaky_ui_tests.py project/build/fork ~/artifacts/$jobName-$BUILD_NUMBER-$1.flaky.tests.frequency.txt
-	pushd project/build/fork
-	tar -czvf ~/artifacts/$jobName-$BUILD_NUMBER-$1.tar.gz expedia
-	popd
 }
-
-if [ "$hasPullId" = "true" ]; then
-	# exit if finds 'needs-human' label
-	python ./jenkins/prLabeledAsNeedsHuman.py $GITHUB_ACCESS_TOKEN $ghprbPullId
-	prLabeledAsNeedsHumanStatus=$?
-	if [ $prLabeledAsNeedsHumanStatus -ne 0 ]; then
-		echo "PR is labeled needs-human, so exiting..."
-		exit 1
-	fi
-
-	# exit if UI tests not required
-	python ./jenkins/changes_require_ui_test.py $GITHUB_ACCESS_TOKEN $ghprbPullId
-	requiresUITestRun=$?
-	if [ $requiresUITestRun -ne 0 ]; then
-		echo "PR does not have any changes which require UI tests to run"
-		exit 0
-	fi
-fi
 
 ./gradlew --no-daemon clean
 
@@ -50,24 +22,7 @@ fi
 # run test
 ./gradlew --no-daemon -PrunProguard=false forkExpediaDebug
 
-cat project/build/fork/expedia/debug/summary/fork-*.json |
-tr '}' '\n' |
-grep failureTrace |
-sed 's/.*"testClass":"\([^"]*\)","testMethod":"\([^"]*\)","failureTrace".*/\1/' > project/build/fork/expedia/debug/summary/failed_test_classes.txt
-
-failed_test_classes=$(cat project/build/fork/expedia/debug/summary/failed_test_classes.txt | tr '\n' ',')
-
-# exit if all test passed
-if [ "$failed_test_classes" == "" ]; then
-    echo "All tests passed quit build."
-    internal_artifact "success"
-    break
-else
-    internal_artifact "failure"
-fi
-if [ "$hasPullId" = "true" ]; then
-	python ./jenkins/pr_ui_feedback.py $GITHUB_ACCESS_TOKEN $ghprbGhRepository $ghprbPullId $SLACK_ACCESS_TOKEN
-fi
+prepare_frequencies_of_flaky_ui_tests
 
 adb devices | tail -n +2 | cut -sf 1 | xargs -I X adb -s X shell svc data enable
 adb devices | tail -n +2 | cut -sf 1 | xargs -I X adb -s X shell svc wifi enable
