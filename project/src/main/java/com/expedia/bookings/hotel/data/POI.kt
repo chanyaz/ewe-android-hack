944facaf7dccc548439887662af10c92a7e911dd
Danny Briggs <dbriggs@CHIC02TL1WTG8WN.sea.corp.expecn.com>
2018-02-20 16:00:28 -0600

    assets

---
+++ project/src/main/java/com/expedia/bookings/hotel/data/POI.kt
@@ -0,0 +1,10 @@
+package com.expedia.bookings.hotel.data
+
+import com.expedia.bookings.R
+
+enum class POI(val icon: Int) {
+    NIGHTLIFE(R.drawable.ic_poi_nightlife),
+    TRANSIT(R.drawable.ic_poi_train),
+    LANDMARK(R.drawable.ic_poi_camera),
+    RESTAURANT(R.drawable.ic_poi_restaurant)
+}
