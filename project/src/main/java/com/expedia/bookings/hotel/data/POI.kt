79becefd0c5cac3ed75b3cf8855deabcb72b6ebc
Danny Briggs <dbriggs@CHIC02TL1WTG8WN.sea.corp.expecn.com>
2018-02-20 16:03:18 -0600

    add shopping asset

--- project/src/main/java/com/expedia/bookings/hotel/data/POI.kt
+++ project/src/main/java/com/expedia/bookings/hotel/data/POI.kt
@@ -6,5 +6,6 @@ enum class POI(val icon: Int) {
     NIGHTLIFE(R.drawable.ic_poi_nightlife),
     TRANSIT(R.drawable.ic_poi_train),
     LANDMARK(R.drawable.ic_poi_camera),
-    RESTAURANT(R.drawable.ic_poi_restaurant)
+    RESTAURANT(R.drawable.ic_poi_restaurant),
+    SHOPPING(R.drawable.ic_poi_shopping)
 }
