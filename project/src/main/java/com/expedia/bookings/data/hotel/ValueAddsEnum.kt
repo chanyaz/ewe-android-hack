c1dcad6c0a329f467a58efbb125e3e090b440945
Jiarui Huo <jhuo@CHIC02QX1MEG8WN.sea.corp.expecn.com>
2018-02-22 01:06:21 -0600

    s/4384: Update value add amenity mappings

--- project/src/main/java/com/expedia/bookings/data/hotel/ValueAddsEnum.kt
+++ project/src/main/java/com/expedia/bookings/data/hotel/ValueAddsEnum.kt
@@ -2,9 +2,9 @@ package com.expedia.bookings.data.hotel
 
 import com.expedia.bookings.R
 
-enum class ValueAddsEnum(val iconId: Int, val descriptionId: Int) {
-    INTERNET(R.drawable.ic_amenity_internet, R.string.amenity_free_internet),
-    BREAKFAST(R.drawable.ic_amenity_breakfast, R.string.amenity_breakfast),
-    PARKING(R.drawable.ic_amenity_local_parking, R.string.amenity_parking),
-    FREE_AIRPORT_SHUTTLE(R.drawable.ic_amenity_airport_shuttle, R.string.amenity_free_airport_shuttle);
+enum class ValueAddsEnum(val iconId: Int, val descriptionId: Int, val jsonKey: String) {
+    INTERNET(R.drawable.ic_amenity_internet, R.string.amenity_free_internet, "internet"),
+    BREAKFAST(R.drawable.ic_amenity_breakfast, R.string.amenity_breakfast, "breakfast"),
+    PARKING(R.drawable.ic_amenity_local_parking, R.string.amenity_parking, "parking"),
+    FREE_AIRPORT_SHUTTLE(R.drawable.ic_amenity_airport_shuttle, R.string.amenity_free_airport_shuttle, "airportShuttle");
 }
