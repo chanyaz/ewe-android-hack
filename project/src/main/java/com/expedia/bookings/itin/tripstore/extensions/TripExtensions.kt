b545ad49839fedc3f580bafdfc6402cd774724f7
Sriniketh Ramachandran <srramachandra@expedia.com>
2018-03-09 13:49:28 -0800

    modified firstHotel() to be more succint

--- project/src/main/java/com/expedia/bookings/itin/tripstore/extensions/TripExtensions.kt
+++ project/src/main/java/com/expedia/bookings/itin/tripstore/extensions/TripExtensions.kt
@@ -4,11 +4,7 @@ import com.expedia.bookings.itin.tripstore.data.ItinHotel
 import com.expedia.bookings.itin.tripstore.data.Itin
 
 fun Itin.firstHotel(): ItinHotel? {
-    val packageHotels = packages?.first()?.hotels
-    if (packageHotels != null && packageHotels.isNotEmpty()) {
-        return packageHotels.first()
-    } else if (hotels != null && hotels.isNotEmpty()) {
-        return hotels.first()
-    }
-    return null
+    val packageHotels = packages?.first()?.hotels ?: emptyList()
+    val standAloneHotels = hotels.orEmpty()
+    return packageHotels.plus(standAloneHotels).firstOrNull()
 }
