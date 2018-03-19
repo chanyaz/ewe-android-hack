79e9a97cb84e84ee55d8d840b2db9ac1d2756069
Paul Kite <paulkite@mac.com>
2018-03-19 11:49:41 -0700

    12207 [a] Add missing config to Coveralls script.
    - Added CI_BRANCH and CI_BUILD_URL to script.

--- jenkins/pr_coveralls.sh
+++ jenkins/pr_coveralls.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 export CI_NAME="jenkins"
+export CI_BRANCH=$GIT_BRANCH
 export CI_BUILD_NUMBER=$BUILD_NUMBER
+export CI_BUILD_URL=$BUILD_URL
 export CI_PULL_REQUEST=$ghprbPullId
 
 ./gradlew jacocoTestReport coveralls
