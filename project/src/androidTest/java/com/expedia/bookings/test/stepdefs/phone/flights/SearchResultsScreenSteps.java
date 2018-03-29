e9480c5faa4c1658478dfc995341a03373da90fc
Lalit Sagar <lalitsagar0411@gmail.com>
2018-03-29 09:24:30 +0530

    Fixing cucumber tests

--- project/src/androidTest/java/com/expedia/bookings/test/stepdefs/phone/flights/SearchResultsScreenSteps.java
+++ project/src/androidTest/java/com/expedia/bookings/test/stepdefs/phone/flights/SearchResultsScreenSteps.java
@@ -66,7 +66,7 @@ public class SearchResultsScreenSteps {
 
 	@And("^I wait for results to load$")
 	public void waitForResultsToLoad() throws Throwable {
-		onView(withId(R.id.sort_filter_button)).perform(waitFor(isDisplayed(), 40, TimeUnit.SECONDS));
+		onView(allOf(withId(R.id.sort_filter_button), isDescendantOfA(withId(R.id.widget_flight_outbound)))).perform(waitFor(isDisplayed(), 40, TimeUnit.SECONDS));
 	}
 
 	@And("^I wait for inbound flights results to load$")
@@ -85,7 +85,7 @@ public class SearchResultsScreenSteps {
 
 	@And("^Validate that flight search results are displayed$")
 	public void validateThatFlightSearchResultsDisplayed() throws Throwable {
-		onView(withId(R.id.list_view)).check(matches(isDisplayed()));
+		onView(allOf(withId(R.id.list_view), isDescendantOfA(withId(R.id.widget_flight_outbound)))).check(matches(isDisplayed()));
 	}
 
 	@And("^Validate that flight search results are displayed for inbound flights$")
