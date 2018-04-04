7cacd21aadfdeea1c57c8b32cf6ce2a4074a9cf2
Mohammed Jaludi <mjaludi@expedia.com>
2018-04-04 10:13:15 -0700

    fix flightsearchpresentertest failure

--- project/src/main/java/com/expedia/bookings/itin/hotel/details/HotelItinPriceSummaryButtonViewModel.kt
+++ project/src/main/java/com/expedia/bookings/itin/hotel/details/HotelItinPriceSummaryButtonViewModel.kt
@@ -46,9 +46,10 @@ class HotelItinPriceSummaryButtonViewModel<S>(scope: S) : ItinBookingInfoCardVie
 
         val detailsUrl = scope.itin.webDetailsURL
         val tripNumber = scope.itin.tripNumber
+        val tripId = scope.itin.tripId
         cardClickListener = {
-            if (scope.abacus.isBucketedForTest(AbacusUtils.EBAndroidAppTripsHotelPricing)) {
-                scope.activityLauncher.launchActivity(HotelItinPricingRewardsActivity, scope.itin.tripId!!)
+            if (scope.abacus.isBucketedForTest(AbacusUtils.EBAndroidAppTripsHotelPricing) && tripId != null) {
+                scope.activityLauncher.launchActivity(HotelItinPricingRewardsActivity, tripId)
             } else if (detailsUrl != null && tripNumber != null) {
                 scope.webViewLauncher.launchWebViewActivity(R.string.itin_hotel_details_price_summary_heading, detailsUrl, "price-header", tripNumber)
             }
