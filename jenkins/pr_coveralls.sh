6970cab9aca3ac69a216e6850822d4cef3aee6bf
Paul Kite <paulkite@mac.com>
2018-04-03 19:58:32 -0700

    12681 Reset Coveralls.
    - Removed parameters from the bash script that are handled by the Gradle plugin.

--- jenkins/pr_coveralls.sh
+++ jenkins/pr_coveralls.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-export CI_NAME="jenkins"
-export CI_BRANCH=$GIT_BRANCH
-export CI_BUILD_NUMBER=$BUILD_NUMBER
-export CI_BUILD_URL=$BUILD_URL
-export CI_PULL_REQUEST=$ghprbPullId
-
 ./gradlew jacocoTestReport coveralls
