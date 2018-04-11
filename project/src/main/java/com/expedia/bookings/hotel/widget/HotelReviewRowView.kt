b1e4d98a1a654cab51001091c6d55fd6d474e066
Jiarui Huo <jhuo@CHIC02QX1MEG8WN.sea.corp.expecn.com>
2018-04-11 17:14:32 -0500

    s/12552: UGC design: Replace user rating bar with number

--- project/src/main/java/com/expedia/bookings/hotel/widget/HotelReviewRowView.kt
+++ project/src/main/java/com/expedia/bookings/hotel/widget/HotelReviewRowView.kt
@@ -5,8 +5,10 @@ import android.view.View
 import android.widget.LinearLayout
 import android.widget.RatingBar
 import com.expedia.bookings.R
+import com.expedia.bookings.data.abacus.AbacusUtils
 import com.expedia.bookings.extensions.subscribeOnClick
 import com.expedia.bookings.extensions.subscribeTextAndVisibility
+import com.expedia.bookings.featureconfig.AbacusFeatureConfigManager
 import com.expedia.bookings.utils.bindView
 import com.expedia.bookings.widget.TextView
 import com.expedia.vm.HotelReviewRowViewModel
@@ -16,6 +18,8 @@ class HotelReviewRowView(context: Context) : LinearLayout(context) {
 
     val title: TextView by bindView(R.id.review_title)
     val ratingBar: RatingBar by bindView(R.id.user_rating_bar)
+    val userReviewRating: TextView by bindView(R.id.user_review_rating_text)
+    val userReviewRatingOutOfFive: TextView by bindView(R.id.user_review_rating_out_of_five)
     val content: TextView by bindView(R.id.content)
     val reviewer: TextView by bindView(R.id.reviewer)
     val date: TextView by bindView(R.id.date)
@@ -23,12 +27,22 @@ class HotelReviewRowView(context: Context) : LinearLayout(context) {
 
     init {
         View.inflate(getContext(), R.layout.hotel_review_row, this)
+        if (isHotelUGCReviewsBoxRatingDesignEnabled()) {
+            userReviewRating.visibility = View.VISIBLE
+            userReviewRatingOutOfFive.visibility = View.VISIBLE
+        } else {
+            ratingBar.visibility = View.VISIBLE
+        }
     }
 
     fun bindData(vm: HotelReviewRowViewModel) {
         vm.titleTextObservable.subscribeTextAndVisibility(title)
         vm.ratingObservable.subscribe {
             ratingBar.rating = it
+            setRatingBarContentDescription(it.toInt())
+        }
+        vm.userReviewRatingTextObservable.subscribe {
+            userReviewRating.text = it.toString()
             setRatingBarContentDescription(it.toInt())
         }
         vm.reviewerTextObservable.subscribeTextAndVisibility(reviewer)
@@ -39,8 +53,18 @@ class HotelReviewRowView(context: Context) : LinearLayout(context) {
     }
 
     private fun setRatingBarContentDescription(rating: Int) {
-        ratingBar.contentDescription = Phrase.from(context, R.string.hotel_rating_bar_cont_desc_TEMPLATE)
+        val reviewContentDescription = Phrase.from(context, R.string.hotel_rating_bar_cont_desc_TEMPLATE)
                 .put("rating", rating)
                 .format().toString()
+
+        if (isHotelUGCReviewsBoxRatingDesignEnabled()) {
+            ratingBar.contentDescription = reviewContentDescription
+        } else {
+            userReviewRating.contentDescription = reviewContentDescription
+        }
+    }
+
+    private fun isHotelUGCReviewsBoxRatingDesignEnabled(): Boolean {
+        return AbacusFeatureConfigManager.isBucketedForTest(context, AbacusUtils.HotelUGCReviewsBoxRatingDesign)
     }
 }
