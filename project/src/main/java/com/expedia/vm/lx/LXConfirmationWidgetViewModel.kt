4db4a9578fc24251076d303eb47419c8b50d4060
Ashwin Badri Srimannarayanan <abadrisrimanna@expedia.com>
2018-05-14 15:26:24 -0700

    s/12871 add guest booking to trips

--- project/src/main/java/com/expedia/vm/lx/LXConfirmationWidgetViewModel.kt
+++ project/src/main/java/com/expedia/vm/lx/LXConfirmationWidgetViewModel.kt
@@ -1,10 +1,11 @@
 package com.expedia.vm.lx
 
 import android.content.Context
-import android.net.Uri
 import com.expedia.bookings.R
 import com.expedia.bookings.data.AbstractItinDetailsResponse
 import com.expedia.bookings.data.LXState
+import com.expedia.bookings.data.trips.ItineraryManager
+import com.expedia.bookings.utils.Ui
 import io.reactivex.subjects.PublishSubject
 
 class LXConfirmationWidgetViewModel(val context: Context) {
@@ -21,17 +22,21 @@ class LXConfirmationWidgetViewModel(val context: Context) {
     val itinDetailsResponseObservable = PublishSubject.create<AbstractItinDetailsResponse>()
     val lxStateObservable = PublishSubject.create<LXState>()
     val confirmationScreenUiObservable = PublishSubject.create<Unit>()
+    private val userStateManager = Ui.getApplication(context).appComponent().userStateManager()
 
     init {
         itinDetailsResponseObservable.subscribe { response ->
             val itinDetails = response.getResponseDataForItin()!!
             dateTextObservable.onNext(itinDetails.startTime.localizedFullDate)
-            val email = getEmailFromURL(itinDetails.webDetailsURL)
+            val email = itinDetails.email?.address ?: context.getString(R.string.lx_default_when_no_email)
             emailTextObservable.onNext(email)
             if (email == context.getString(R.string.lx_default_when_no_email)) {
                 confirmationTextObservable.onNext(context.getString(R.string.lx_successful_checkout_no_email_label))
             } else {
                 confirmationTextObservable.onNext(context.getString(R.string.lx_successful_checkout_email_label))
+            }
+            if (!userStateManager.isUserAuthenticated()) {
+                ItineraryManager.getInstance().addGuestTrip(email, itinDetails.tripNumber.toString())
             }
             reservationConfirmationTextObservable.onNext(context.getString(R.string.lx_successful_checkout_reservation_label))
             itineraryNumberTextObservable.onNext(context.getString(R.string.successful_checkout_TEMPLATE, itinDetails.tripNumber))
@@ -41,14 +46,5 @@ class LXConfirmationWidgetViewModel(val context: Context) {
             ticketsTextObservable.onNext(lxState.selectedTicketsCountSummary(context))
             locationTextObservable.onNext(lxState.activity.location)
         }
-    }
-
-    private fun getEmailFromURL(url: String?): String {
-        val uri = Uri.parse(url)
-        val email = uri.getQueryParameter("email")
-        if (email.isNullOrEmpty()) {
-            return context.getString(R.string.lx_default_when_no_email)
-        }
-        return email
     }
 }
