3ee6e4bbe831a5491f9bf59491b8e106d31d0da8
Kevin Seo <seokev@gmail.com>
2018-05-22 10:58:04 -0700

    wip

--- project/src/test/java/com/expedia/bookings/itin/lx/moreHelp/LxItinMoreHelpWidgetTest.kt
+++ project/src/test/java/com/expedia/bookings/itin/lx/moreHelp/LxItinMoreHelpWidgetTest.kt
@@ -3,7 +3,6 @@ package com.expedia.bookings.itin.lx.moreHelp
 import android.app.Activity
 import android.content.pm.PackageManager
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.widget.Toolbar
 import com.expedia.account.BuildConfig
--- project/src/test/java/com/expedia/bookings/itin/widget/HotelItinManageBookingHelpTest.kt
+++ project/src/test/java/com/expedia/bookings/itin/widget/HotelItinManageBookingHelpTest.kt
@@ -1,7 +1,6 @@
 package com.expedia.bookings.itin.widget
 
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import com.expedia.account.BuildConfig
 import com.expedia.bookings.R
