4b35e05505cd3e8e67dece53b1357b4d941dc669
Prasanna Hajare <v-phajare@expedia.com>
2018-05-24 20:13:23 +0530

    Adding disable feature toggle code in FeatureTestUtils

--- project/src/test/java/com/expedia/bookings/utils/FeatureTestUtils.kt
+++ project/src/test/java/com/expedia/bookings/utils/FeatureTestUtils.kt
@@ -12,8 +12,20 @@ object FeatureTestUtils {
         val prefs = getFeatureConfigPreferences(context)
         val supportedFeaturesSet = prefs.getStringSet(SatelliteFeatureConfigManager.PREFS_SUPPORTED_FEATURE_SET, mutableSetOf())
         supportedFeaturesSet.add(feature.name)
+        updatePrefFile(prefs, supportedFeaturesSet)
+    }
+
+    @JvmStatic
+    fun disableFeature(context: Context, feature: Feature) {
+        val prefs = getFeatureConfigPreferences(context)
+        val supportedFeaturesSet = prefs.getStringSet(SatelliteFeatureConfigManager.PREFS_SUPPORTED_FEATURE_SET, mutableSetOf())
+        supportedFeaturesSet.remove(feature.name)
+        updatePrefFile(prefs, supportedFeaturesSet)
+    }
+
+    private fun updatePrefFile(prefs: SharedPreferences, featureSet: Set<String>) {
         val editor = prefs.edit()
-        editor.putStringSet(SatelliteFeatureConfigManager.PREFS_SUPPORTED_FEATURE_SET, supportedFeaturesSet)
+        editor.putStringSet(SatelliteFeatureConfigManager.PREFS_SUPPORTED_FEATURE_SET, featureSet)
         editor.apply()
     }
 
