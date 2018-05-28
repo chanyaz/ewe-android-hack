d1a79dfd9df46f46a7f6aa2e001e731bd5d293c9
DeepeshPanjabi <dkumarpanjabi@expedia.com>
2018-05-28 12:52:19 +0530

    Moved BundlePriceViewModel Tests into one test class

--- project/src/test/java/com/expedia/bookings/test/robolectric/BundlePriceViewModelTest.kt
+++ project/src/test/java/com/expedia/bookings/test/robolectric/BundlePriceViewModelTest.kt
@@ -3,6 +3,7 @@ package com.expedia.bookings.test.robolectric
 import android.content.Context
 import com.expedia.bookings.R
 import com.expedia.bookings.data.Money
+import com.expedia.bookings.data.pos.PointOfSaleId
 import com.expedia.bookings.packages.vm.PackageTotalPriceViewModel
 import com.squareup.phrase.Phrase
 import org.junit.Test
@@ -44,5 +45,64 @@ class BundlePriceViewModelTest {
         val someMoney = Money("-50.0", "USD")
         testViewModelUniversalCKO.savings.onNext(someMoney)
         assertEquals("", testSubscriber.values()[1])
+    }
+
+    @Test
+    fun testBundleSaveLabelForJP() {
+        RoboTestHelper.setPOS(PointOfSaleId.JAPAN)
+        val testSubscriber = TestObserver<String>()
+        val testViewModelUniversalCKO = PackageTotalPriceViewModel(context)
+        testViewModelUniversalCKO.savingsPriceObservable.subscribe(testSubscriber)
+
+        val someMoney = Money("50.00", "JPY")
+        val expectedSavingLabel = Phrase.from(context, R.string.bundle_total_savings_TEMPLATE)
+                .put("savings", someMoney.getFormattedMoney(Money.F_NO_DECIMAL_IF_INTEGER_ELSE_TWO_PLACES_AFTER_DECIMAL))
+                .format().toString()
+
+        testViewModelUniversalCKO.savings.onNext(someMoney)
+
+        assertEquals(expectedSavingLabel, testSubscriber.values()[1])
+    }
+
+    @Test
+    fun testBundleSaveLabelNonIntegerForJP() {
+        RoboTestHelper.setPOS(PointOfSaleId.JAPAN)
+        val testSubscriber = TestObserver<String>()
+        val testViewModelUniversalCKO = PackageTotalPriceViewModel(context)
+        testViewModelUniversalCKO.savingsPriceObservable.subscribe(testSubscriber)
+
+        val someMoney = Money("50.34", "JPY")
+        val expectedSavingLabel = Phrase.from(context, R.string.bundle_total_savings_TEMPLATE)
+                .put("savings", someMoney.getFormattedMoney(Money.F_NO_DECIMAL_IF_INTEGER_ELSE_TWO_PLACES_AFTER_DECIMAL))
+                .format().toString()
+
+        testViewModelUniversalCKO.savings.onNext(someMoney)
+        assertEquals(expectedSavingLabel, testSubscriber.values()[1])
+    }
+
+    @Test
+    fun testBundleTotalIntegerForJP() {
+        RoboTestHelper.setPOS(PointOfSaleId.JAPAN)
+        val testSubscriber = TestObserver<String>()
+        val testViewModelUniversalCKO = PackageTotalPriceViewModel(context)
+        testViewModelUniversalCKO.totalPriceObservable.subscribe(testSubscriber)
+
+        val someMoney = Money("1120.00", "JPY")
+
+        testViewModelUniversalCKO.total.onNext(someMoney)
+        assertEquals("JPY1,120", testSubscriber.values()[0])
+    }
+
+    @Test
+    fun testBundleTotalIntegerNonIntegerForJP() {
+        RoboTestHelper.setPOS(PointOfSaleId.JAPAN)
+        val testSubscriber = TestObserver<String>()
+        val testViewModelUniversalCKO = PackageTotalPriceViewModel(context)
+        testViewModelUniversalCKO.totalPriceObservable.subscribe(testSubscriber)
+
+        val someMoney = Money("1120.86", "JPY")
+
+        testViewModelUniversalCKO.total.onNext(someMoney)
+        assertEquals("JPY1,120.86", testSubscriber.values()[0])
     }
 }
