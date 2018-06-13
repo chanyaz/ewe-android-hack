03509adb6dfecfef242d9bd7968c3ff6ff8e9a70
Sriniketh Ramachandran <srramachandra@expedia.com>
2018-06-12 17:09:04 -0700

    fix-feature-provider-to-check-feature-enabled

--- project/src/test/java/com/expedia/bookings/itin/utils/FeatureProviderTest.kt
+++ project/src/test/java/com/expedia/bookings/itin/utils/FeatureProviderTest.kt
@@ -1,46 +1,29 @@
 package com.expedia.bookings.itin.utils
 
-import android.content.Context
 import com.expedia.bookings.features.Feature
-import com.expedia.bookings.features.Features
-import com.expedia.bookings.preference.extensions.features
-import com.expedia.bookings.test.robolectric.RobolectricRunner
-import com.expedia.bookings.utils.FeatureTestUtils
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RuntimeEnvironment
 import kotlin.test.assertFalse
-import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
-@RunWith(RobolectricRunner::class)
 class FeatureProviderTest {
     private val featureProvider = FeatureProvider
-    private val context: Context = RuntimeEnvironment.application
 
     @Test
     fun testIsFeatureEnabledTrue() {
-        val feature = Features.all.activityMap
-        FeatureTestUtils.enableFeature(context, feature)
+        val feature = MockFeature(true)
         assertTrue(featureProvider.isFeatureEnabled(feature))
     }
 
     @Test
     fun testIsFeatureEnabledFalse() {
-        val feature = Features.all.activityMap
-        FeatureTestUtils.disableFeature(context, feature)
+        val feature = MockFeature(false)
         assertFalse(featureProvider.isFeatureEnabled(feature))
     }
 
-    @Test
-    fun testFeatureNotFound() {
-        val feature = MockFeature()
-        assertNull(Features.all.features().find { it.name == feature.name })
-        assertFalse(featureProvider.isFeatureEnabled(feature))
-    }
-
-    class MockFeature : Feature {
+    class MockFeature(val isEnabled: Boolean) : Feature {
         override val name: String = "MockFeature"
-        override fun enabled(): Boolean = true
+        override fun enabled(): Boolean {
+            return isEnabled
+        }
     }
 }
