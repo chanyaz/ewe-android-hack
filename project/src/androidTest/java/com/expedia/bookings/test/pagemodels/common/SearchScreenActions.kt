2dc6f3e1e9600a3280d99d744947de593e4cae7a
Vikas Suriyal <v-vsuriyal@expedia.com>
2018-06-25 13:05:58 +0530

    15421 : fixed session time out scenerio for orbitz

--- project/src/androidTest/java/com/expedia/bookings/test/pagemodels/common/SearchScreenActions.kt
+++ project/src/androidTest/java/com/expedia/bookings/test/pagemodels/common/SearchScreenActions.kt
@@ -69,7 +69,7 @@ object SearchScreenActions {
     @JvmStatic fun selectLocation(location: String) {
         val viewMatcher = Matchers.allOf(
                 hasDescendant(withId(R.id.suggestion_text_container)),
-                hasDescendant(withText(location)),
+                hasDescendant(withText(Matchers.containsString(location))),
                 withEffectiveVisibility(VISIBLE))
         selectSuggestion(viewMatcher)
     }
