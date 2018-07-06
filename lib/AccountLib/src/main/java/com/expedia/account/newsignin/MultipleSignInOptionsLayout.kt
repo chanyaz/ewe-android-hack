8c5116e22b4e8d483502f38537ccbd5ed5586370
Abhinav Tripathi <abtripathi@expedia.com>
2018-07-05 22:11:18 -0700

    Rename Mulitiple to Multiple

--- lib/AccountLib/src/main/java/com/expedia/account/newsignin/MultipleSignInOptionsLayout.kt
+++ lib/AccountLib/src/main/java/com/expedia/account/newsignin/MultipleSignInOptionsLayout.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.widget.FrameLayout
 import com.expedia.account.R
 
-class MulitipleSignInOptionsLayout(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
+class MultipleSignInOptionsLayout(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
 
     init{
         View.inflate(context, R.layout.acct__widget_multiple_signin_options, this)
